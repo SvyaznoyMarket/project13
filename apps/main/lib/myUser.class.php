722e9164c71e736ac0e319efd989c132ea70ec1d
gray <ssapego@maxus.ru>
2011-06-02 17:19:42 +0400

    Добавлена корзина

--- apps/main/lib/myUser.class.php
+++ apps/main/lib/myUser.class.php
@@ -2,4 +2,28 @@
 
 class myUser extends sfBasicSecurityUser
 {
+
+  protected $cart;
+
+  public function initialize(sfEventDispatcher $dispatcher, sfStorage $storage, $options = array())
+  {
+    parent::initialize($dispatcher, $storage, $options);
+
+    $cart = $this->getAttribute('cart', array());
+    $this->cart = new Cart($cart);
+  }
+
+  public function shutdown()
+  {
+    $this->setAttribute('cart', $this->cart->dump());
+
+    parent::shutdown();
+  }
+
+  public function getCart()
+  {
+    return $this->cart;
+  }
+
+
 }
---
+++ apps/main/modules/cart/actions/actions.class.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * cart actions.
+ *
+ * @package    enter
+ * @subpackage cart
+ * @author     Связной Маркет
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class cartActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+
+    //$productTable = ProductTable::getInstance();
+    //$product = Doctrine_Manager::connection()->queryOne("SELECT * FROM product WHERE id = ?", array(10, ));
+    //myDebug::dump($product);
+
+    $cart = $this->getUser()->getCart();
+    //$product = ProductTable::getInstance()->getById(1);
+    //myDebug::dump($product);
+    //$cart->addProduct($product);
+    //myDebug::dump($cart->dump());
+    //$cart->addProduct($product, 10);
+    //$product = ProductTable::getInstance()->getById(5);
+    //$cart->addProduct($product, 2);
+    //$product = $cart->getProduct(1);
+    //myDebug::dump($cart->hasProduct($product));
+    //$cart->clear();
+    //$cart->deleteProduct(1);
+    $products = $cart->getProducts()->toArray();
+    myDebug::dump($products);
+    //$cart->getProducts()->getData();
+    //echo "<pre>"; print_r($cart->getProducts()->toArray()); echo "</pre>";
+    //myDebug::dump($cart->getProducts()->toArray());
+    //$cart->deleteProduct(1);
+    //myDebug::dump($cart->dump());
+
+  }
+}
