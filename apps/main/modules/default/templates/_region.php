bb39d082c92210d24e7ee71060555ade97ba5a63
gray <ssapego@maxus.ru>
2011-06-05 21:33:34 +0400

    рефакторинг default/region

--- apps/main/modules/default/templates/_region.php
+++ apps/main/modules/default/templates/_region.php
@@ -1,9 +1,7 @@
 <div class="block region left">
-<?php
-$geoip = $sf_request->getParameter('geoip');
-echo "Код страны: " . $geoip['country_code'] . "\n";
-echo "Регион:     " . $geoip['region'] . "\n";
-echo "Имя страны: " . $geoip['country_name'] . "\n";
-echo "Город:      " . $geoip['city_name'] . "\n";
-?>
+<?php $geoip = $sf_request->getParameter('geoip') ?>
+<?php echo "Код страны: " . $geoip['country_code'] ?>
+<?php echo "Регион:     " . $geoip['region'] ?>
+<?php echo "Имя страны: " . $geoip['country_name'] ?>
+<?php echo "Город:      " . $geoip['city_name'] ?>
 </div>
