54b7620398f98781c5eca9dedfa9ae1069fac4c7
green <green@green-vs.(none)>
2011-06-06 11:57:43 +0400

    Подключен плагин sfDoctrineGuardPlugin

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -12,6 +12,7 @@ class ProjectConfiguration extends sfProjectConfiguration
     $this->enablePlugins(array(
       'sfDoctrinePlugin',
       'sfTaskExtraPlugin',
+      'sfDoctrineGuardPlugin',
     ));
   }
 }
