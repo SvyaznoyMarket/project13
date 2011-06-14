f4bfd407c3f43c51d2f36ffef9ead01b5487f039
green <green@mail.ru>
2011-06-14 18:05:18 +0400

    Добавлена тестовая информационная страница

--- apps/main/modules/default/templates/indexSuccess.php
+++ apps/main/modules/default/templates/indexSuccess.php
@@ -3,7 +3,11 @@
 <div class="block">
   <p><?php echo link_to('Личный кабинет', 'user') ?></p>
 </div>
+
 <div class="block">
   <p><?php echo link_to('Каталог товаров', 'productCatalog') ?></p>
 </div>
 
+<div class="block">
+  <p><?php echo link_to('О компании', 'default_show', array('page' => 'page-1')) ?></p>
+</div>
