751a10eb430a37957cb8327dc3c6e9337c923f46
green <green@mail.ru>
2011-06-17 09:32:38 +0400

    Изменена конфигурация Doctrine

--- apps/main/config/mainConfiguration.class.php
+++ apps/main/config/mainConfiguration.class.php
@@ -9,6 +9,16 @@ class mainConfiguration extends sfApplicationConfiguration
 
   public function configureDoctrine(Doctrine_Manager $manager)
   {
+    $manager->setAttribute(Doctrine_Core::ATTR_QUOTE_IDENTIFIER, true);
+    $manager->setAttribute(Doctrine_Core::ATTR_QUERY_CLASS, 'myDoctrineQuery');
+    $manager->setAttribute(Doctrine_Core::ATTR_COLLECTION_CLASS, 'myDoctrineCollection');
+    //$manager->setAttribute(Doctrine_Core::ATTR_AUTO_FREE_QUERY_OBJECTS, true );
+
+    sfConfig::set('doctrine_model_builder_options', array(
+      'baseTableClassName' => 'myDoctrineTable',
+      'baseClassName'      => 'myDoctrineRecord',
+    ));
+
     // настройка кеширования
     //$manager->setAttribute(Doctrine_Core::ATTR_QUERY_CACHE, new Doctrine_Cache_Apc());
     //$manager->setAttribute(Doctrine_Core::ATTR_QUERY_CACHE_LIFESPAN, 60);
--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -13,17 +13,4 @@ class ProjectConfiguration extends sfProjectConfiguration
       'sfDoctrinePlugin',
     ));
   }
-
-  public function configureDoctrine(Doctrine_Manager $manager)
-  {
-    $manager->setAttribute(Doctrine_Core::ATTR_QUOTE_IDENTIFIER, true);
-    $manager->setAttribute(Doctrine_Core::ATTR_QUERY_CLASS, 'myDoctrineQuery');
-    $manager->setAttribute(Doctrine_Core::ATTR_COLLECTION_CLASS, 'myDoctrineCollection');
-    //$manager->setAttribute(Doctrine_Core::ATTR_AUTO_FREE_QUERY_OBJECTS, true );
-
-    sfConfig::set('doctrine_model_builder_options', array(
-      'baseTableClassName' => 'myDoctrineTable',
-      'baseClassName'      => 'myDoctrineRecord',
-    ));
-  }
 }
