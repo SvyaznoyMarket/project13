a7f67031b0757da5060cc8d280771745041a0261
gray <ssapego@maxus.ru>
2011-07-14 15:55:27 +0400

    Модуль редактирования адреса доставки пользователя

---
+++ apps/main/modules/userAddress/templates/editSuccess.php
@@ -0,0 +1,19 @@
+<div class="block">
+  <?php include_component('user', 'menu') ?>
+</div>
+
+<h1>Адреса доставки</h1>
+
+<div class="block">
+  <?php if (count($userAddressList) > 0): ?>
+    <?php include_component('userAddress', 'list', array('userAddressList' => $userAddressList, )) ?>
+
+  <?php else: ?>
+    <p>нет адресов</p>
+
+  <?php endif ?>
+
+  <div class="block-inline">
+    <?php include_component('userAddress', 'form', array('userAddress' => $userAddress, )) ?>
+  </div>
+</div>
