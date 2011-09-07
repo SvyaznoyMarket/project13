ff757f9a4f897e4a8bea26746182d760241692ae
Gray <ssapego@maxus.ru>
2011-09-07 15:24:58 +0400

    Add test module for core

---
+++ apps/main/modules/core/actions/actions.class.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * core actions.
+ *
+ * @package    enter
+ * @subpackage core
+ * @author     Связной Маркет
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class coreActions extends myActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+	$core = Core::getInstance();
+	
+	$order = new Order();
+	$order->fromArray(array(
+	  'recipient_first_name' => 'Я',
+	  'recipient_last_name' => 'ТЫ',
+	  'recipient_middle_name' => 'ВЫ',
+	));
+
+	if (!$response = $core->createOrder($order))
+	{
+      myDebug::dump($core->getError());
+	}
+	else
+	{
+	  myDebug::dump($response);
+	};
+	
+  }
+}
