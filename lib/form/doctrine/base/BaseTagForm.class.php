5cb45460ebe4af95f1bafe8d83fd946645ec4654
green <green@mail.ru>
2011-09-15 12:48:07 +0400

    TagGroup model added

--- lib/form/doctrine/base/BaseTagForm.class.php
+++ lib/form/doctrine/base/BaseTagForm.class.php
@@ -15,15 +15,17 @@ abstract class BaseTagForm extends BaseFormDoctrine
   public function setup()
   {
     $this->setWidgets(array(
-      'id'    => new sfWidgetFormInputHidden(),
-      'token' => new sfWidgetFormInputText(),
-      'name'  => new sfWidgetFormInputText(),
+      'id'       => new sfWidgetFormInputHidden(),
+      'group_id' => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('Group'), 'add_empty' => true)),
+      'token'    => new sfWidgetFormInputText(),
+      'name'     => new sfWidgetFormInputText(),
     ));
 
     $this->setValidators(array(
-      'id'    => new sfValidatorChoice(array('choices' => array($this->getObject()->get('id')), 'empty_value' => $this->getObject()->get('id'), 'required' => false)),
-      'token' => new sfValidatorString(array('max_length' => 255)),
-      'name'  => new sfValidatorString(array('max_length' => 255)),
+      'id'       => new sfValidatorChoice(array('choices' => array($this->getObject()->get('id')), 'empty_value' => $this->getObject()->get('id'), 'required' => false)),
+      'group_id' => new sfValidatorDoctrineChoice(array('model' => $this->getRelatedModelName('Group'), 'required' => false)),
+      'token'    => new sfValidatorString(array('max_length' => 255)),
+      'name'     => new sfValidatorString(array('max_length' => 255)),
     ));
 
     $this->validatorSchema->setPostValidator(
