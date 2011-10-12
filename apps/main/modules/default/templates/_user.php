4f2107292b049f8f9e57dc896e66a04c9fd26890
green <green@mail.ru>
2011-10-12 12:49:20 +0400

    Auth: signin, register, open auth...

--- apps/main/modules/default/templates/_user.php
+++ apps/main/modules/default/templates/_user.php
@@ -1,21 +1,5 @@
-<?php if (false): ?>
-<div class="right">
-  <ul class="inline">
-  <?php if ($sf_user->isAuthenticated()): ?>
-    <li>
-      <a href="<?php echo url_for('user') ?>"><?php echo $sf_user->getGuardUser() ?></a>
-    </li>
-    <li>
-      <a href="<?php echo url_for('user_signout') ?>">Выйти</a>
-    </li>
-    <?php else: ?>
-      <a href="<?php echo url_for('user_signin') ?>">Войти</a>
-    <?php endif ?>
-  </ul>
-</div>
-<?php endif ?>
 <?php if ($sf_user->isAuthenticated()): ?>
   <a href="<?php echo url_for('user') ?>"><?php echo $sf_user->getGuardUser() ?></a>&nbsp;<a href="<?php echo url_for('user_signout') ?>">(выйти)</a>
 <?php else: ?>
-  <a href="<?php echo url_for('user_signin') ?>" class="entry">Войти на сайт</a>
+  <a id="auth-link" href="<?php echo url_for('user_signin') ?>" class="entry">Войти на сайт</a>
 <?php endif ?>
