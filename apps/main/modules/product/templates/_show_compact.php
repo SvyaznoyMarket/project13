6f3c49683071dff7b7be29d3e168f513a6ccbf00
Ivn Local <ikotov@maxus.ru>
2011-10-14 11:51:53 +0400

    URL fix

--- apps/main/modules/product/templates/_show_compact.php
+++ apps/main/modules/product/templates/_show_compact.php
@@ -9,13 +9,12 @@
   <div class="boxhover" ref="<?php echo $item['product']->token ?>">
     <b class="rt"></b><b class="lb"></b>
     <div class="rb">
-      <!--div class="lt" onclick="window.location.href='<?php echo $item['url'] ?>'"-->
-      <div class="lt" onclick="window.location.href='<?php echo $item['url'] ?>'">
+      <div class="lt"  onclick="window.location.href='<?php echo $item['url'] ?>'">
         <!--a href="" class="fastview">Быстрый просмотр</a-->
 
         <div class="photo"><!--<i class="new" title="Новинка"></i>--><img src="<?php echo $item['photo'] ?>" alt="" title="" width="160" height="160" /></div>
         <span class="ratingview"></span>
-        <h3><a href=""><?php echo $item['name'] ?></a></h3>
+        <h3><a href="<?php echo $item['url'] ?>"><?php echo $item['name'] ?></a></h3>
         <div class="font18 pb10"><span class="price"><?php echo $item['price'] ?></span> <span class="rubl">p</span></div>
         <div class="goodsbar">
           <?php include_component('cart', 'buy_button', array('product' => $item['product'], 'quantity' => 1)) ?>
