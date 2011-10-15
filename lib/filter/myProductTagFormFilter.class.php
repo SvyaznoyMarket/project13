ce3cc06c57693fcd56bc7e28e1b7bee73ba391e7
Gray <ssapego@maxus.ru>
2011-10-15 14:10:29 +0400

    Добавлен класс фильтров для тегов

---
+++ lib/filter/myProductTagFormFilter.class.php
@@ -0,0 +1,127 @@
+<?php
+
+class myProductTagFormFilter extends sfFormFilter
+{
+  public function configure()
+  {
+    parent::configure();
+
+    $this->setOption('mark_required', false);
+
+    $this->disableCSRFProtection();
+
+    if (!$productCategory = $this->getOption('productCategory'))
+    {
+      throw new InvalidArgumentException('You must provide a productCategory object.');
+    }
+    $creator = $this->getOption('creator', null);
+
+    $productTable = ProductTable::getInstance();
+
+    // виджет цены
+    $this->widgetSchema['price'] = new myWidgetFormRange(array(
+      'value_from' => 100,
+      'value_to'   => 100000,
+    ));
+    $this->widgetSchema['price']->setLabel('Цена');
+    $this->setDefault('price', array(
+      'from' => (int)$productTable->getMinPriceByCategory($productCategory),
+      'to'   => (int)$productTable->getMaxPriceByCategory($productCategory)
+    ));
+    $this->validatorSchema['price'] = new sfValidatorPass();
+/*
+    // виджет производителя
+    $choices = CreatorTable::getInstance()
+      ->getListByProductCategory($productCategory, array('select' => 'creator.id, creator.name'))
+      ->toKeyValueArray('id', 'name')
+    ;
+    $this->widgetSchema['creator'] = new myWidgetFormChoice(array(
+      'choices'          => $choices,
+      'multiple'         => true,
+      'expanded'         => true,
+      'renderer_class'   => 'myWidgetFormSelectCheckbox',
+      'renderer_options' => array(
+        'label_separator' => '',
+      ),
+    ));
+    $this->widgetSchema['creator']->setLabel('Производитель');
+    $this->widgetSchema['creator']->setDefault($creator ? $creator->id : null);
+    $this->validatorSchema['creator'] = new sfValidatorPass();
+*/
+    // виджеты параметров
+    foreach ($productCategory->getTagGroupRelation() as $productCategoryTagGroupRelation)
+    {
+      $tagGroup = $productCategoryTagGroupRelation->getTagGroup();
+      //myDebug::dump($tagGroup); continue;
+      if (!count($tagGroup->Tag)) continue;
+
+      if (!$widget = call_user_func(array($this, 'getWidgetChoice'), $tagGroup)) continue;
+
+      $index = "tag-{$tagGroup->id}";
+      $this->setWidget($index, $widget);
+      $this->setValidator($index, new sfValidatorPass());
+      $this->widgetSchema[$index]->setLabel($tagGroup->name);
+    }
+
+    $this->widgetSchema->setNameFormat('f[%s]');
+  }
+
+  public function buildQuery(myDoctrineQuery $q)
+  {
+    $productCategory = $this->getOption('productCategory');
+
+    $filter = array(
+      'category'   => $productCategory,
+      'creator'    => $this->values['creator'],
+      'price'      => array(
+        'from' => $this->values['price']['from'],
+        'to'   => $this->values['price']['to'],
+      ),
+      'parameters' => array(),
+    );
+
+    $productFilterList = $productCategory->ProductType->TagGroup;
+    $productFilterList->indexBy('id');
+    foreach ($this->values as $id => $param)
+    {
+      if (0 !== strpos($id, 'param-')) continue;
+
+      $productFilter = $productFilterList->getByIndex('id', $id);
+      if (!$productFilter) continue;
+
+      $filter['parameters'][] = array(
+        'filter' => $productFilter,
+        'values' => $param,
+      );
+    }
+
+    ProductTable::getInstance()->setQueryForTagFilter($q, $filter);
+  }
+
+  protected function getWidgetChoice(TagGroup $tagGroup)
+  {
+    $choices = array();
+    foreach ($tagGroup->Tag as $tag)
+    {
+      $choices[$tag->id] = $tag->name;
+    }
+
+    return new myWidgetFormChoice(array(
+      'choices'          => $choices,
+      'multiple'         => false,
+      'expanded'         => true,
+      'renderer_class'   => 'myWidgetFormSelectCheckbox',
+      'renderer_options' => array(
+        'label_separator' => '',
+      ),
+    ));
+  }
+
+  protected function getWidgetRange(ProductFilter $productFilter)
+  {
+    return new myWidgetFormRange(array(
+      'value_from' => 0,
+      'value_to'   => 100,
+    ));
+  }
+}
