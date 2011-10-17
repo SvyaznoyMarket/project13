67974a48ad890d54ff3318b69e108f58699fda06
Ivn Local <ikotov@maxus.ru>
2011-10-17 15:55:09 +0400

    filters

--- web/js/main.js
+++ web/js/main.js
@@ -1,212 +1,3 @@
-//var debug = true
-//
-//
-//window.isInFrame = function() {
-//  return window.location != window.parent.location
-//}
-//
-//
-//// jQuery.extend
-//jQuery.extend({
-//  log: function(msg) {
-//    this.counter = undefined == this.counter ? 1 : (this.counter + 1)
-//
-//    if (false == debug) return false
-//
-//    if (window.console) {
-//      // Firefox & Google Chrome
-//      console.log(msg);
-//    }
-//    else {
-//    // Other browsers
-//      var el = $('<div style="position: absolute; top: ' + (this.counter * 18) + 'px; padding: 2px; color: #00ff00; font: normal 12px Courier New; background: #000; opacity: 0.8; filter:progid:DXImageTransform.Microsoft.Alpha(opacity=80); -khtml-opacity: 0.8">#' + this.counter + ': ' + msg + ' <a href="#" style="color: #ff0000; font: bold 14px Arial; text-decoration: none;">&times;</a></div>')
-//      el.oneTime(10000, function() {$(this).remove()})
-//      el.find('a').bind('click', function() {$(this).parent().remove()})
-//
-//      $('body').append(el);
-//    }
-//
-//    return true;
-//  }
-//})
-//
-//
-//// Обработчик событий
-//EventHandler = {
-//  'trigger': function(e, param) {
-//    if ('string' == typeof(param)) {
-//      var name = param
-//    }
-//    else {
-//      var el = $(e.target)
-//      var name = el.data('event')
-//
-//      el.trigger(name+'.prepare', param)
-//    }
-//
-//    if ('function' != typeof(callback)) {
-//      callback = $.noop
-//    }
-//
-//    $.log('Event ' + name + ' fired')
-//    if (typeof this[name] == 'function')
-//    {
-//      this[name](e, param)
-//    }
-//  },
-//
-//  // Ошибка 401
-//  'secure': function(e) {
-//     e.preventDefault()
-//
-//    $('#auth-block').lightbox_me({
-//        centered: true,
-//        onLoad: function() {
-//            $('#auth-block').find('input:first').focus()
-//        }
-//    })
-//  },
-//
-//  // Обновление DOM-элемента
-//  'content.update': function(e, param) {
-//    e.preventDefault()
-//
-//    var el = $(e.target)
-//
-//    el.trigger('content.update.prepare')
-//
-//    var url = el.is('a') ? el.attr('href') : false
-//    if (url) {
-//      $.get(url, function(result, status, x) {
-//        var target = null == el.data('target') ? el : $(el.data('target'))
-//        if (target) {
-//          if ('append' == el.data('update')) {
-//            target.append(result.data.content)
-//          }
-//          else {
-//            target.replaceWith(result.data.content)
-//          }
-//
-//          el.trigger('content.update.success', [result])
-//        }
-//      }, 'json')
-//    }
-//  },
-//
-//  // Открытие модального окна
-//  'window.open': function(e, param) {
-//    e.preventDefault()
-//
-//    var el = $(e.target)
-//
-//    var href = el.attr('href') + (-1 != el.attr('href').indexOf('?') ? '&' : '?') + 'frame=true'
-//
-//    /*
-//    $.colorbox({
-//      href: href,
-//      iframe: true,
-//      scrolling: false,
-//      transition: 'elastic',
-//      speed: 250,
-//      initialWidth: 1,
-//      initialHeight: 1,
-//      onClosed: function() {
-//        if (el.data('reload')) {
-//          window.location.reload()
-//        }
-//      }
-//    })
-//    */
-//  },
-//
-//  // Отправка формы
-//  'form.submit': function(e, param) {
-//    var el = $(e.target)
-//
-//    if (window.isInFrame()) {
-//      el.attr('action', el.attr('action') + (-1 != el.attr('action').indexOf('?') ? '&' : '?') + 'frame=true&reload-parent='+el.data('reload'))
-//    }
-//  },
-//
-//  // Ajax-отправка формы
-//  'form.ajax-submit': function(e, param) {
-//    e.preventDefault()
-//
-//    var el = $(e.target)
-//
-//    el.ajaxSubmit({
-//      success: function(result) {
-//        var target = el.data('target') ? $(el.data('target')) : el
-//
-//        target.replaceWith(result.data.content);
-//
-//        el.trigger('form.ajax-submit.success', [result])
-//      }
-//    })
-//  }
-//}
-//
-//
-//
-//$(document).ready(function() {
-//
-//  // Настройки colorbox
-//  /*
-//  $.extend($.colorbox.settings, {
-//    opacity: 0.7,
-//    fixed: false,
-//    close: 'закрыть <span style="font: bold 16px Verdana">&times;</span>'
-//  })
-//  */
-//
-//  // Обработчики ajax-ошибок
-//  $(document).ajaxError(function(e, x, settings, exception) {
-//    /*
-//    $.extend($.colorbox.settings, {
-//      width: 400,
-//      height: 200
-//    })
-//    */
-//
-//    if (x.status == 0) {
-//      $.colorbox({html: '<h1>Ошибка</h1><br />Не удается подключиться к серверу', fixed: true})
-//    } else if (x.status == 401) {
-//      EventHandler.trigger(e, 'secure')
-//    } else if (x.status == 404) {
-//      $.colorbox({html: '<h1>Ошибка 404</h1><br />Запрашиваемая страница не найдена', fixed: true})
-//    } else if (x.status == 403) {
-//      $.colorbox({html: '<h1>Ошибка 403</h1><br />Время сессии пользователя истекло. Авторизуйтесь заново, пожалуйста', fixed: true})
-//    } else if (x.status == 500) {
-//      $.colorbox({html: '<h1>Ошибка 500</h1><br />Ошибка сервера', fixed: true})
-//    } else if (e == 'parsererror') {
-//      $.colorbox({html: '<h1>Ошибка</h1><br />Не удалось обработать ответ сервера', fixed: true})
-//    } else if (e == 'timeout') {
-//      $.colorbox({html: '<h1>Ошибка</h1><br />Время ожидания ответа истекло', fixed: true})
-//    } else {
-//      $.colorbox({html: '<h1>Ошибка</h1><br />Неизвестная ошибка.' + "\n" + x.responseText, fixed: true})
-//    }
-//  })
-//
-//  // Если окно находится во фрейме, то изменить размер модального окна
-//  if (window.isInFrame()) {
-//    /*
-//    window.parent.$.fn.colorbox.resize({innerHeight: $(document).height(), innerWidth: $(document).width()})
-//    */
-//  }
-//
-//  // Подключение стандартных обработчиков
-//  $.each({
-//    '.event-click': 'click',
-//    '.event-submit': 'submit'
-//  }, function(selector, eventName) {
-//    $(selector).live(eventName, function(e) {
-//      EventHandler.trigger(e)
-//    })
-//  })
-//
-//})
-// JavaScript Document
-
 $(document).ready(function(){
 
     $('.form input[type=checkbox],.form input[type=radio]').prettyCheckboxes();
@@ -234,13 +25,19 @@ $(document).ready(function(){
 
 /* —Î‡È‰Â ---------------------------------------------------------------------------------------*/
 $(document).ready(function(){
+		var mini = $('.sliderbox .fl').html() * 1
+		var maxi = $('.sliderbox .fr').html() * 1
+		console.info(mini, maxi)
         if ($( "#slider-range1" ).length) $( "#slider-range1" ).slider({
             range: true,
-            min: 2000,
-            max: 200000,
-            values: [ 20000, 100000 ],
+            min: mini,
+            max: maxi,
+            values: [ mini , maxi ],
             slide: function( event, ui ) {
-                $( "#amount1" ).val( ui.values[ 0 ] + " - " + ui.values[ 1 ] );
+            console.info( ui ) 
+                //$( "#amount1" ).val( ui.values[ 0 ] + " - " + ui.values[ 1 ] )
+                $('#f_price_from').val( ui.values[ 0 ] )
+				$('#f_price_to').val( ui.values[ 1 ] )
             }
         });
         $( "#amount1" ).val( $( "#slider-range" ).slider( "values", 0 ) +
@@ -355,13 +152,14 @@ $(document).ready(function(){
 			$( '#extramenu-root-'+currentMenu+'').data('run', false).hide()
 	})
 	/* ---- */	
+	
 	/* cards carousel  */
 	
 	$('.forvard').click( function(){
 		//сделать запрос, получить новые данные
 		//найти блок карусели
 		//скрыть старые, показать новое
-		console.info( $(this) )
+		//console.info( $(this) )
 		//обновить внутреннюю переменную
 		//обновить состояние кнопок
 		return false
