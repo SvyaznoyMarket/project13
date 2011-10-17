7913a66db0ed7bf9cee59d60bc4cdbf79de42959
green <green@mail.ru>
2011-10-17 19:01:54 +0400

    Added ajax support for auth

--- apps/main/modules/guard/templates/_form_auth.php
+++ apps/main/modules/guard/templates/_form_auth.php
@@ -1,46 +1,5 @@
 <div class="registerbox">
-  <form id="signin-form" action="<?php echo url_for('@user_signin') ?>" class="form" method="post">
-    <div class="fl width327 mr20">
-      <div class="font16 pb20">У меня есть логин и пароль</div>
-
-      <div class="pb5">E-mail или мобильный телефон:</div>
-      <div class="pb5">
-        <?php echo $formSignin['username']->render(array('class' => 'text width315 mb10')) ?>
-        <!--<input type="text" class="text width315 mb10" value="ivanov@domen.com" />-->
-      </div>
-
-      <div class="pb5"><a href="<?php echo url_for('user_forgotPassword') ?>" class="fr orange underline">Забыли пароль?</a>Пароль:</div>
-      <div class="pb5">
-        <?php echo $formSignin['password']->render(array('class' => 'text width315 mb10')) ?>
-        <!--<input type="password" class="text width315 mb10" value="Пароль" />-->
-      </div>
-
-      <input type="submit" class="fr button bigbutton" value="Войти" tabindex="4" />
-      <div class="ml20 pt10">
-        <label for="checkbox-8" class="prettyCheckbox checkbox list"><span class="holderWrap" style="width: 13px; height: 13px;"><span class="holder" style="width: 13px;"></span></span>Запомнить меня на этом компьютере</label>
-        <?php echo $formSignin['remember']->render(array('class' => 'hiddenCheckbox', 'id' => 'checkbox-8')) ?>
-        <!--<input type="checkbox" value="checkbox-1" name="checkbox-3" id="checkbox-8" class="hiddenCheckbox">-->
-      </div>
-    </div>
-  </form>
-
-  <form id="register-form" action="<?php echo url_for('@user_register') ?>" class="form" method="post">
-    <div class="fr width327 ml20">
-      <div class="font16 pb20">Я новый пользователь</div>
-      <div class="pb5">Как к вам обращаться?</div>
-      <div class="pb5">
-        <?php echo $formRegister['first_name']->render(array('class' => 'text width315 mb10')) ?>
-        <!--<input type="text" class="text width315 mb10" value="ivanov@domen.com" />-->
-      </div>
-      <div class="pb5">E-mail или мобильный телефон:</div>
-      <div class="pb5">
-        <?php echo $formRegister['username']->render(array('class' => 'text width315 mb10')) ?>
-        <!--<input type="password" class="text width315 mb10" value="Пароль" />-->
-      </div>
-      <input type="submit" class="fr button bigbutton" value="Регистрация" tabindex="10" />
-
-    </div>
-  </form>
-
+  <?php include_partial('guard/form_signin', array('form' => $formSignin)) ?>
+  <?php include_partial('guard/form_register', array('form' => $formRegister)) ?>
   <div class="clear"></div>
 </div>
