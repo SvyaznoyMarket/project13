4d5f1901e9edf3a0cb40dc6b02ae3b98d8a4d221
Ivn Local <ikotov@maxus.ru>
2011-10-20 11:55:11 +0400

    second step

--- apps/main/modules/order/templates/_field_recipient_first_name.php
+++ apps/main/modules/order/templates/_field_recipient_first_name.php
@@ -1,6 +1,6 @@
         </div>
         <div class="clear width500 pb25"></div>
-        <div class="fl width215 mr20"><strong class="font16">Кому и куда доставить:</strong></div>
+        <div class="fl width215 mr20"><strong class="font16 deliverytext">Кому и куда доставить:</strong></div>
         <div class="fl width430">
             <div class="pb15">
               <?php echo $form['recipient_first_name']->renderLabel() ?>
