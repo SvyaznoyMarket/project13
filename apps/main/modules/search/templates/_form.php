0475820872cb1e8898ccfa2d4c2d5caefdaf702d
green <green@mail.ru>
2011-10-20 11:15:52 +0400

    Search: added form to main page

--- apps/main/modules/search/templates/_form.php
+++ apps/main/modules/search/templates/_form.php
@@ -1,4 +1,1 @@
-<form class="search-form" action="<?php echo url_for('search', array('searchString' => $searchString)) ?>" method="get">
-  <input type="text" class="searchtext<?php if (isset($wide)) echo ' width483' ?>" name="q" value="Поиск товаров" onfocus="if (this.value == 'Поиск товаров') this.value = '';" onblur="if (this.value == '') this.value = 'Поиск товаров';"  />
-  <input type="submit" class="searchbutton" value="Найти" title="Найти" id="try-1" />
-</form>
+<?php include_partial("search/form_{$view}", $sf_data) ?>
