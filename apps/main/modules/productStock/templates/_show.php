ce8bf82728a56b6b9d15390f9511157576e91e9d
green <green@mail.ru>
2011-10-29 15:08:52 +0400

    Module: updated productStock

--- apps/main/modules/productStock/templates/_show.php
+++ apps/main/modules/productStock/templates/_show.php
@@ -1,8 +1,28 @@
-<table class="table">
+    
+<!-- Shop -->
+<h2 class="bold">Где купить <?php echo $product ?></h2>
+<div class="line pb15"></div>
+<div class="descriptionlist shoplist">
+  <div class="point">
+    <div class="title"><h3>Интернет магазин www.enter.ru (Доставка по всей России)</h3></div>
+    <div class="description"><b class="supply1"></b>Много</div>
+
+  </div>
+
+  <div class="pb15"><strong>Москва:</strong></div>
+  
   <?php foreach ($list as $item): ?>
-  <tr>
-    <td><a href="<?php echo $item['url'] ?>"><?php echo $item['name'] ?></a></td>
-    <td><?php echo $item['quantity'] ?></td>
-  </tr>
+  <div class="point">
+    <div class="title"><h3><?php echo $item['name'] ?></h3></div>
+    <div class="description">
+      <?php include_partial('productStock/quantity', array('quantity' => $item['quantity'])) ?>
+    </div>
+  </div>
   <?php endforeach ?>
-</table>
+
+</div>
+<!-- /Shop  -->
+
+<?php foreach ($list as $time): ?>
+
+<?php endforeach; ?>
--- apps/main/modules/productStock/templates/indexSuccess.php
+++ apps/main/modules/productStock/templates/indexSuccess.php
@@ -1,8 +1,1 @@
-<div class="block">
-  <?php include_component('productCard', 'navigation', array('product' => $product)) ?>
-</div>
-
-<div class="block">
-  <?php include_partial('product/name', array('product' => $product)) ?>
-  <?php include_component('productStock', 'show', array('product' => $product)) ?>
-</div>
+<?php include_component('productStock', 'show', array('product' => $product)) ?>
