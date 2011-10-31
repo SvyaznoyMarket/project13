5c2e8744bc4ef2e96d8840aa0fbb3404c6c15161
green <green@mail.ru>
2011-10-31 15:59:23 +0300

    productCard: added new view for "show" template

--- apps/main/modules/productComment/templates/_product.php
+++ apps/main/modules/productComment/templates/_product.php
@@ -1,26 +1,17 @@
 <!-- Goods info -->
 <div class="goodsphotosmall"><div class="photo"><img src="<?php echo $product->getMainPhotoUrl(2) ?>" alt="" width="163" height="163" title="" /></div></div>
 <div class="fr width219">
-	<div class="font11 gray pb10">Перейти в:</div>
-	<div class="articlemenu">
-		<ul>
-			<li><a href="<?php echo url_for('productCard', $sf_data->getRaw('product')) ?>">Карточка товара</a></li>
-			<li class="next"><a href="<?php echo url_for('productComment', $sf_data->getRaw('product')) ?>" class="current">Отзывы пользователей</a></li>
-<!--			<li class="next"><a href="<?php echo url_for('productStock', $sf_data->getRaw('product')) ?>">Где купить в магазинах</a></li>-->
-		</ul>
-	</div>
+  <div class="font11 gray pb10">Перейти в:</div>
+  <div class="articlemenu">
+    <ul>
+      <li><a href="<?php echo url_for('productCard', $sf_data->getRaw('product')) ?>">Карточка товара</a></li>
+      <li><a href="<?php echo url_for('productComment', $sf_data->getRaw('product')) ?>" class="current">Отзывы пользователей</a></li>
+      <li class="next"><a href="<?php echo url_for('productStock', $sf_data->getRaw('product')) ?>">Где купить в магазинах</a></li>
+    </ul>
+  </div>
 </div>
 
-<div class="goodsinfosmall">
-	<div class="font11 gray pb10">Артикул <?php echo $product->article ?></div>
-	<div class="font14 pb15"><?php echo $product->description ?></div>
-	<div class="font18 pb10"><?php echo $product->getFormattedPrice() ?> <span class="rubl">p</span></div>
-	<div class="goodsbar">
-    <?php include_component('cart', 'buy_button', array('product' => $product, 'quantity' => 1)) ?>
-    <?php include_component('userDelayedProduct', 'add_button', array('product' => $product)) ?>
-    <?php include_component('userProductCompare', 'button', array('product' => $product)) ?>
-	</div>
-</div>
+<?php include_component('product', 'show', array('view' => 'description', 'product' => $product)) ?>
 <!-- /Goods info -->
 
 <div class="clear pb20"></div>
