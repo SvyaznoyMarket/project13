f17ba37b5c24a82987b399313ff05f3667732367
green <green@mail.ru>
2011-11-01 09:22:53 +0300

    Model: added ProductPropertyRelation.real_value property

--- data/sql/diff-20111031.sql
+++ data/sql/diff-20111031.sql
@@ -3,3 +3,5 @@ ALTER TABLE `product_price`  ADD COLUMN `core_id` BIGINT NULL DEFAULT NULL COMME
 ALTER TABLE `product_category`  ADD COLUMN `core_lft` BIGINT(20) NULL DEFAULT NULL COMMENT 'lft записи в Core' AFTER `core_parent_id`,  ADD COLUMN `core_rgt` BIGINT(20) NULL DEFAULT NULL COMMENT 'rgt записи в Core' AFTER `core_lft`;
 
 ALTER TABLE `task`  ADD COLUMN `core_packet_id` BIGINT(20) NULL DEFAULT NULL COMMENT 'ид пакета в Core' AFTER `core_id`;
+
+ALTER TABLE `product_property_relation`  ADD COLUMN `value_boolean` TINYINT NULL DEFAULT NULL AFTER `value_text`;
