4779a814b09a27822367b0535e399f190e38cf13
Trushina <otrushina@maxus.ru>
2011-11-03 04:09:12 -0400

    remove of validatio in symfony

--- lib/vendor/symfony/lib/autoload/sfCoreAutoload.class.php
+++ lib/vendor/symfony/lib/autoload/sfCoreAutoload.class.php
@@ -457,7 +457,6 @@ class sfCoreAutoload
     'sfvalidatordatetime' => 'validator/sfValidatorDateTime.class.php',
     'sfvalidatordecorator' => 'validator/sfValidatorDecorator.class.php',
     'sfvalidatoremail' => 'validator/sfValidatorEmail.class.php',
-    'sfvalidatorphone' => 'validator/sfValidatorPhone.class.php',
     'sfvalidatorerror' => 'validator/sfValidatorError.class.php',
     'sfvalidatorerrorschema' => 'validator/sfValidatorErrorSchema.class.php',
     'sfvalidatorfile' => 'validator/sfValidatorFile.class.php',
