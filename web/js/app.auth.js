1968096b7e7e3a24732226c46b644f83444e0b56
Ivn Local <ikotov@maxus.ru>
2011-11-07 10:49:32 +0300

    psw warning

--- web/js/app.auth.js
+++ web/js/app.auth.js
@@ -17,24 +17,47 @@ $(document).ready(function() {
 
     return false
   })
-  
-  $('#signin_password').keypress(function(e) { 
-		var s = String.fromCharCode( e.which )		
-		var cln = $('<strong id="capslock" style="border: 1px solid red; color: red; border-radius: 3px 3px 3px 3px; background: none repeat scroll 0% 0% white; position: absolute; height: 16px; padding: 1px 3px; margin-top: 2px; margin-left: -78px;">CAPS LOCK</strong>')		
-		if ( s.toUpperCase() === s && s.toLowerCase() !== s && !e.shiftKey ) {
-			if( !$('#capslock').length ) $(this).after(cln)
-		} else {
-			if( $('#capslock').length ) $('#capslock').remove()
-		}			
-  })
-  $('#signin_password').keyup(function(e) { 
-  		var rwn = $('<strong id="ruschars" style="border: 1px solid red; color: red; border-radius: 3px 3px 3px 3px; background: none repeat scroll 0% 0% white; position: absolute; height: 16px; padding: 1px 3px; margin-top: 2px; margin-left: -36px;">RUS</strong>')
-  		if( /[а-яА-ЯёЁ]/.test( $(this).val() ) ) {
-			if( !$('#ruschars').length ) $(this).after(rwn)
-		} else { 
-			if( $('#ruschars').length ) $('#ruschars').remove()
-		}
-  })
+
+	;(function($) {
+		$.fn.warnings = function() {
+			var rwn = $('<strong id="ruschars" class="pswwarning">RUS</strong>')
+			rwn.css({
+				'border': '1px solid red',
+				'color': 'red',
+				'border-radius': '3px',
+				'background-color':'#fff',
+				'position': 'absolute',
+				'height': '16px',
+				'padding': '1px 3px',
+				'margin-top': '2px'
+			})
+			var cln = rwn.clone().attr('id','capslock').html('CAPS LOCK').css('marginLeft', '-78px')
+			
+			$(this).keypress(function(e) { 
+				var s = String.fromCharCode( e.which )								
+				if ( s.toUpperCase() === s && s.toLowerCase() !== s && !e.shiftKey ) {
+					if( !$('#capslock').length ) $(this).after(cln)
+				} else {
+					if( $('#capslock').length ) $('#capslock').remove()
+				}			
+		  })
+		  $(this).keyup(function(e) { 								
+				if( /[а-яА-ЯёЁ]/.test( $(this).val() ) ) {
+					if( !$('#ruschars').length ) {
+						if( $('#capslock').length ) 
+							rwn.css('marginLeft','-116px')
+						else	
+							rwn.css('marginLeft','-36px')
+						$(this).after(rwn)
+					}
+				} else { 
+					if( $('#ruschars').length ) $('#ruschars').remove()
+				}
+		  })
+		}	
+	})(jQuery); 
+	
+  $('#signin_password').warnings()
 
   $('#login-form, #register-form').bind('submit', function(e) {
     e.preventDefault()
