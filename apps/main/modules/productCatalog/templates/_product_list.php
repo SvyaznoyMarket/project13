4cd8c14a4dcfa10b89794965a27df1a9dc9ccc1f
Trushina <otrushina@maxus.ru>
2011-11-09 08:11:57 -0500

    fix of ajax catalog html

--- apps/main/modules/productCatalog/templates/_product_list.php
+++ apps/main/modules/productCatalog/templates/_product_list.php
@@ -17,7 +17,7 @@ $view =$request->getParameter('view');
   <div class="line"></div>
 <?php endif ?>
 
-<?php include_component('product', 'pager', array('pager' => $productPager)) ?>
+<?php include_component('product', 'pager', array('pager' => $productPager, 'ajax_flag' => false)) ?>
 
 <?php if (false): ?>
   <div class="line pb10"></div>
--- apps/main/modules/productCatalog/templates/_product_list_ajax.php
+++ apps/main/modules/productCatalog/templates/_product_list_ajax.php
@@ -1,14 +1,7 @@
 <?php $empty = 0 == $productPager->getNbResults() ?>
 
-<?php if (!$empty): ?>
-  <div class="line"></div>
-<?php endif ?>
+<?php include_component('product', 'pager', array('pager' => $productPager, 'ajax_flag' => true)) ?>
 
-<?php include_component('product', 'pager', array('pager' => $productPager)) ?>
-
-<?php if (false): ?>
-  <div class="line pb10"></div>
-<?php endif ?>
 
 
 
