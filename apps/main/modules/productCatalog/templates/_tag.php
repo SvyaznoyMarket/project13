371bc2b232d1059c04d4d215588d3f5c845ff399
Trushina <otrushina@maxus.ru>
2011-11-10 06:47:21 -0500

    layout of left filter

--- apps/main/modules/productCatalog/templates/_tag.php
+++ apps/main/modules/productCatalog/templates/_tag.php
@@ -7,12 +7,13 @@
 <!-- Filter -->
 <form class="product_filter-block" action="<?php echo $url ?>" method="get" data-action-count="<?php echo url_for('productCatalog_count', $sf_data->getRaw('productCategory')) ?>">
 
-  <dl class="bigfilter form">
+  <dl class="bigfilter form bSpec">
+    <h2>Enterесный выбор</h2>  
     <?php $i = 0; foreach ($form as $name => $field): if ($i > 7) break; ?>
       <?php if ((isset($form[$name]) && $form[$name]->isHidden()) || (!isset($form[$name]) && $field->isReal())) continue ?>
       <?php $i++ ?>
 
-      <dt<?php if (4 > $i) echo ' class="current'.((1 == $i) ? ' first' : '').'"' ?>><?php echo $form[$name]->renderLabelName() ?>
+      <dt<?php //if (4 > $i) echo ' class="'.((1 == $i) ? ' first' : '').'"' ?>><?php echo $form[$name]->renderLabelName() ?>
         <?php //include_partial('productCatalog/filter_hint')  ?>
       </dt>
 
@@ -20,9 +21,9 @@
         <?php echo $form[$name]->render() ?>
       </dd>
     <?php endforeach ?>
+    <div class="pb10"><input type="submit" class="button yellowbutton" value="Подобрать" /></div>
   </dl>
 
-  <div class="pb10"><input type="submit" class="button yellowbutton" value="Подобрать" /></div>
   <!--div class="pb15"><a href="" class="button whitelink">Расширенный поиск</a></div-->
 </form>
 
