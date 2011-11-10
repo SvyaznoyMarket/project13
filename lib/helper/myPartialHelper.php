b1b5b103633c2f7d69410530892b4cf0f51f97a8
green <green@mail.ru>
2011-11-10 13:24:34 +0300

    Added debug env

--- lib/helper/myPartialHelper.php
+++ lib/helper/myPartialHelper.php
@@ -136,8 +136,9 @@ function get_component($moduleName, $componentName, $vars = array())
   $context = sfContext::getInstance();
   $actionName = '_'.$componentName;
 
-  $class = sfConfig::get('mod_'.strtolower($moduleName).'_partial_view_class', 'sf').'PartialView';
-  $view = new $class($context, $moduleName, $actionName, '');
+  //$class = sfConfig::get('mod_'.strtolower($moduleName).'_partial_view_class', 'sf').'PartialView';
+  //$view = new $class($context, $moduleName, $actionName, '');
+  $view = new myPartialView($context, $moduleName, $actionName, '');
   $view->setPartialVars(true === sfConfig::get('sf_escaping_strategy') ? sfOutputEscaper::unescape($vars) : $vars);
 
   if ($retval = $view->getCache())
