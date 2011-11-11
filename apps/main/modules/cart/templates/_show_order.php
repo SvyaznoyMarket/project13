2fcb91962ed059f5e1c2a97bb12f86623231ab78
Gray <ssapego@maxus.ru>
2011-11-11 11:43:20 +0300

    Фикс отображения картинок товаров при подтверждении заказа

--- apps/main/modules/cart/templates/_show_order.php
+++ apps/main/modules/cart/templates/_show_order.php
@@ -1,6 +1,6 @@
   <?php $i = 0; foreach ($list as $item): $i++ ?>
         <div class="fl width685 pb20<?php if ($i > 1) echo ' pl235' ?>">
-            <div class="fl width140"><a href="<?php echo url_for('productCard', $item['product']) ?>"><img src="<?php echo $item['product']->getMainPhotoUrl(4) ?>" alt="" width="120" height="120" /></a></div>
+            <div class="fl width140"><a href="<?php echo url_for('productCard', $item['product']) ?>"><img src="<?php echo $item['product']->getMainPhotoUrl(1) ?>" alt="" width="120" height="120" /></a></div>
             <div class="fr width545 pb20">
                 <div class="fl width360"><a href="<?php echo url_for('productCard', $item['product']) ?>" class="font16"><?php echo $item['product']->name ?> (<?php echo $item['quantity'] ?> шт.)</a></div>
                 <div class="fr font16"><?php echo $item['priceFormatted'] ?> <span class="rubl">p</span></div>
@@ -21,7 +21,7 @@
                     </div>
                 </div>
                 <div class="line pb20"></div>
-                
+
             <?php endforeach; ?>
         <?php endif ?>
 
