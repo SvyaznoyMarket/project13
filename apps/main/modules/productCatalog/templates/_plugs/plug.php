f275a10fbffb54c8ca90af832363e15d9ebe14a7
Ivn Local <ikotov@maxus.ru>
2011-11-11 11:00:10 +0300

    Rotator banners

---
+++ apps/main/modules/productCatalog/templates/_plugs/plug.php
@@ -0,0 +1,38 @@
+           <!--div class="plug plug4">
+               <h2>Мебель</h2>
+               Что бы вы ни искали: матрасы для самых сладких снов, или кресла для самых уютных вечеров, или кухни для самых бодрых завтраков &mdash; с Enter ваш дом станет уютнее.
+           </div-->
+<!--  info block -->
+			<div class='bInfoBlock'>
+				<div id="rotator">
+				<!--div title="1">
+					<div class='bInfoBlock__eLeftImage'>
+						<img src='/images/circle.png'>
+					</div>
+					<div class='bInfoBlock__eRightContent'>
+					<p>первый слайд</p>
+						<span class='bInfoBlock__eSpan'>1. Доставим за 1 день* в любое место в удобное время**</span><br>
+						<span class='bInfoBlock__eSpan'>2. Заказывайте как вам удобно:</span>
+						<p class='bInfoBlock__eP'>
+							- В ближайшем магазине на терминале или у продавца<br>
+							- По каталогу, позвонив в Контакт cENTER ежедневно круглосуточно<br>
+							- На сайте <a href='http://www.enter.ru'>www.Enter.ru</a> ежедневно круглосуточно<br>
+							- Через мобильные приложения
+						</p>
+					</div>
+				</div-->
+				<div title="1">
+					<img class="banner" src="/images/promo_1.jpg" alt=""/>
+				</div>
+				<div title="2">
+					<img class="banner" src="/images/promo_2.jpg" alt=""/>
+				</div>
+				<div title="3">
+					<img class="banner" src="/images/promo_3.jpg" alt=""/>
+				</div>
+				</div>
+				<!--div class='bInfoBlock__eBottomControls'>
+					<a href><span>1</span></a><a href><span>2</span></a><a href><span>3</span></a>
+				</div-->
+			</div>
+			<!-- /info block -->           
