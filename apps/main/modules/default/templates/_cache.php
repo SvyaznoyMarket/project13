802e60a4b8c89f8dc1aa02020bbf399cc259b8c3
Gray <ssapego@maxus.ru>
2011-11-16 14:47:09 +0300

    Фикс тэга для кеша

--- apps/main/modules/default/templates/_cache.php
+++ apps/main/modules/default/templates/_cache.php
@@ -1,3 +1,3 @@
 <?php if (count($keys)): ?>
-<!-- <?php echo CacheEraser::getInstance()->getPrefix() ?> <?php foreach ($keys as $key) echo $key.' ' ?> -->
+<!--<?php echo CacheEraser::getInstance()->getPrefix() ?> <?php foreach ($keys as $key) echo $key.' ' ?> -->
 <?php endif ?>
