19387afcdb06299cc2a95888b3c71a2722ea1f63
green <green@mail.ru>
2011-11-22 17:23:23 +0300

    Added excluded_env option for WelcomeFilter

--- lib/filter/WelcomeFilter.class.php
+++ lib/filter/WelcomeFilter.class.php
@@ -4,8 +4,11 @@ class WelcomeFilter extends sfFilter
 {
   public function execute ($filterChain)
   {
-    if (('live' == sfConfig::get('sf_environment')) || ('prod' == sfConfig::get('sf_environment')))
-    {
+    if (false
+      || ('live' == sfConfig::get('sf_environment'))
+      || ('prod' == sfConfig::get('sf_environment'))
+      || in_array(sfConfig::get('sf_environment'), $this->getParameter('excluded_env'))
+    ) {
       // ignore
     }
     else {
