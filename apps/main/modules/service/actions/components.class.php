9665329c69ad60b2ed5ad06c5056b7e7e5896510
Gray <ssapego@maxus.ru>
2011-11-30 14:04:42 +0300

    Фикс service/listByProduct component

--- apps/main/modules/service/actions/components.class.php
+++ apps/main/modules/service/actions/components.class.php
@@ -15,7 +15,7 @@ class serviceComponents extends myComponents
   *
   * @param product $product Продукт
   */
-  public function iexecuteListByProduct()
+  public function executeListByProduct()
   {
     $list = $this->product->getServiceList();
 
@@ -33,14 +33,14 @@ class serviceComponents extends myComponents
       $serviceData['currentPrice'] = $this->service->getCurrentPrice();
       if (isset($service['currentPrice'])) {
           $service['currentPrice'] = number_format($serviceData['currentPrice'], 2, ',', ' ');
-      } 
+      }
       $serviceData['name'] = $this->service->name;
       $serviceData['description'] = $this->service->description;
       $serviceData['work'] = $this->service->work;
       $serviceData['main_photo'] = $this->service->getPhotoUrl();
       $this->setVar('service', $serviceData);
   }
-  
+
   public function executeAlike_service()
   {
     $nearParent = ServiceCategoryTable::getInstance()
@@ -55,56 +55,56 @@ class serviceComponents extends myComponents
             ->addWhere('s.id != ?', $this->service->id)
             ->execute();
     $this->setVar('list', $list);
-      
-  }  
-  
+
+  }
+
   public function executeRoot_page()
-  {        
+  {
   }
   public function executeLeft_menu()
-  {        
-  }  
-  
+  {
+  }
+
   public function executeCurrent_category_tree()
-  {  
-  }   
-  
-  
+  {
+  }
+
+
   public function executeNavigation()
-  {  
+  {
     $list = array();
     $list[] = array(
       'name' => 'F1 Сервис',
       'url'  => url_for('service_list'),
-    );  
-    
-    if (isset($this->serviceCategory) && $this->serviceCategory) {        
+    );
+
+    if (isset($this->serviceCategory) && $this->serviceCategory) {
         $parentCategory = $this->serviceCategory->getParentCategory();
         if (isset($parentCategory) && isset($parentCategory['name'])) {
             $list[] = array(
               'name' => $parentCategory['name'],
               'url'  => url_for('service_list', array('serviceCategory' => $parentCategory['token'])),
-            );     
+            );
         }
         $list[] = array(
           'name' => $this->serviceCategory['name'],
           'url'  => url_for('service_list'),
-        );  
+        );
     } elseif (isset($this->service)) {
-        $parentCategory = $this->service->getCatalogParent();      
+        $parentCategory = $this->service->getCatalogParent();
         if (isset($parentCategory) && isset($parentCategory['name'])) {
             $list[] = array(
               'name' => $parentCategory['name'],
               'url'  => url_for('service_list', array('serviceCategory' => $parentCategory['token'])),
-            );     
-        } 
+            );
+        }
         $list[] = array(
           'name' => $this->service['name'],
-        );  
+        );
     }
 
 
-    $this->setVar('list', $list);      
+    $this->setVar('list', $list);
   }
 }
 
