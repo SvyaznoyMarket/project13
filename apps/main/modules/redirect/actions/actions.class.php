327b7dcabc35a184c30c8936cb1dde37d57b82b6
green <green@mail.ru>
2011-12-06 16:00:53 +0300

    Added Redirect model, added support for old url

--- apps/main/modules/redirect/actions/actions.class.php
+++ apps/main/modules/redirect/actions/actions.class.php
@@ -18,33 +18,41 @@ class redirectActions extends myActions
   public function executeIndex(sfWebRequest $request)
   {
     $route = $request['route'];
-    $this->forward404Unless($route);
+    if ($route)
+    {
+      $routes = array(
+        'default' => function($action) use ($request) {
+          $return = array();
+          foreach ($request->getRequestParameters() as $k => $v)
+          {
+            if (in_array($k, array('action', 'module', 'route')) || (0 === strpos($k, '_sf_'))) continue;
+            $return[$k] = $v;
+          }
 
-    $routes = array(
-      'default' => function($action) use ($request) {
-        $return = array();
-        foreach ($request->getRequestParameters() as $k => $v)
-        {
-          if (in_array($k, array('action', 'module', 'route')) || (0 === strpos($k, '_sf_'))) continue;
-          $return[$k] = $v;
+          return $return;
+        },
+        'productCard' => function($action) use ($request) {
+          $product = ProductTable::getInstance()->findOneByBarcode($request['product']);
+          if (!$product)
+          {
+            $action->forward404();
+            return false;
+          }
+
+          return array('product' => $product->token);
         }
+      );
 
-        return $return;
-      },
-      'productCard' => function($action) use ($request) {
-        $product = ProductTable::getInstance()->findOneByBarcode($request['product']);
-        if (!$product)
-        {
-          $action->forward404();
-          return false;
-        }
+      $params = call_user_func_array(isset($routes[$route]) ? $routes[$route] : $routes['default'], array($this));
 
-        return array('product' => $product->token);
-      }
-    );
+      $this->redirect('@'.$route.'?'.http_build_query($params), 301);
+    }
+    else {
+      $url = urldecode($request->getPathInfo());
+      $redirect = RedirectTable::getInstance()->getByUrl($url);
+      $this->forward404Unless($redirect);
 
-    $params = call_user_func_array(isset($routes[$route]) ? $routes[$route] : $routes['default'], array($this));
-
-    $this->redirect('@'.$route.'?'.http_build_query($params), 301);
+      $this->redirect((sfConfig::get('sf_no_script_name') ? '' : $request->getScriptName()).$redirect['new_url'], $redirect['status_code']);
+    }
   }
 }
