e1b3a19cb490bb430b78cc98f142fa183e547b41
green <green@mail.ru>
2011-12-07 16:59:26 +0300

    Style fix for model select

--- apps/main/modules/product/templates/_product_model.php
+++ apps/main/modules/product/templates/_product_model.php
@@ -1,6 +1,6 @@
 <!-- Variation -->
 <div class="fr width400">
-  <h2>Другие вариации:</h2>
+  <h2>Этот товар с другими параметрами:</h2>
   <?php foreach ($properties as $property): ?>
     <?php include_partial('product/product_model_'.($property->ProductModelRelation[0]->is_image ? 'image' : 'select'), array('product' => $product, 'property' => $property, )) ?>
   <?php endforeach; ?>
