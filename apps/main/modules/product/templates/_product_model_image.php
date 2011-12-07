6176ea2c7b3aeda7201ad9183309f09b7d89dcf1
green <green@mail.ru>
2011-12-07 16:59:26 +0300

    Style fix for model select

--- apps/main/modules/product/templates/_product_model_image.php
+++ apps/main/modules/product/templates/_product_model_image.php
@@ -1,8 +1,11 @@
+<div class="bDropWrap">
+  <h5><?php echo $property->name ?>:</h5>
 
-<div class="font11 pb10"><?php echo $property->name ?> <span class="gray"><?php echo isset($property->current) ? $property->values[$property->current]['parameter']->getValue() : '' ?></span></div>
-<ul class="previewlist">
-<?php foreach ($property->values as $value): ?>
-    <li><b<?php echo ($value['is_selected']) ? ' class="current"' : '' ?>><a href="<?php echo $value['url'] ?>"></a></b><img src="<?php echo $value['photo'] ?>" alt="<?php echo $value['parameter']->getValue() ?>" width="48" height="48" /></li>
-<?php endforeach ?>
-</ul>
+  <ul class="previewlist">
+  <?php foreach ($property->values as $value): ?>
+      <li><b<?php echo ($value['is_selected']) ? ' class="current"' : '' ?>><a href="<?php echo $value['url'] ?>"></a></b><img src="<?php echo $value['photo'] ?>" alt="<?php echo $value['parameter']->getValue() ?>" width="48" height="48" /></li>
+  <?php endforeach ?>
+  </ul>
+</div>
+
 <div class="clear"></div>
