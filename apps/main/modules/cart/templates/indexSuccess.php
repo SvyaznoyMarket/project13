14d78d652490dc993385cc5d3129054bfc33f4df
Trushina <otrushina@maxus.ru>
2011-12-08 01:44:37 -0500

    seo scripts

--- apps/main/modules/cart/templates/indexSuccess.php
+++ apps/main/modules/cart/templates/indexSuccess.php
@@ -46,3 +46,9 @@
       <p>в корзине нет товаров</p>
 
     <?php endif ?>
+      
+      
+      
+<!--Трэкер "Корзина"--> 
+<script>document.write('<img src="http://mixmarket.biz/tr.plx?e=3779415&r='+escape(document.referrer)+'&t='+(new Date()).getTime()+'" width="1" height="1"/>');</script> 
+<!--Трэкер "Корзина"-->
