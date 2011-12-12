d37a43ec5754bfc428193584a050e51a5985cda0
Gray <ssapego@maxus.ru>
2011-12-12 20:03:07 +0300

    Добавлена подскаска для характеристик-картинок

--- apps/main/modules/product/templates/_product_model_image.php
+++ apps/main/modules/product/templates/_product_model_image.php
@@ -3,7 +3,7 @@
 
   <ul class="previewlist">
   <?php foreach ($property->values as $value): ?>
-      <li><b<?php echo ($value['is_selected']) ? ' class="current"' : '' ?>><a href="<?php echo $value['url'] ?>"></a></b><img src="<?php echo $value['photo'] ?>" alt="<?php echo $value['parameter']->getValue() ?>" width="48" height="48" /></li>
+      <li><b<?php echo ($value['is_selected']) ? ' class="current"' : '' ?> title="<?php echo $value['parameter']->getValue() ?>"><a href="<?php echo $value['url'] ?>"></a></b><img src="<?php echo $value['photo'] ?>" alt="<?php echo $value['parameter']->getValue() ?>" width="48" height="48" /></li>
   <?php endforeach ?>
   </ul>
 </div>
