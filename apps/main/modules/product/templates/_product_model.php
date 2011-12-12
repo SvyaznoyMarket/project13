56607f78be32229f5b5beeab55e3c6aa89818078
Gray <ssapego@maxus.ru>
2011-12-12 19:16:57 +0300

    Фикс для моделей

--- apps/main/modules/product/templates/_product_model.php
+++ apps/main/modules/product/templates/_product_model.php
@@ -1,10 +1,6 @@
 <!-- Variation -->
 <div class="fr width400">
-<<<<<<< HEAD
   <h2>Этот товар с другими параметрами:</h2>
-=======
-  <h2>Другие вариации:</h2>
->>>>>>> e3d3e88... Changed template of product's model
   <?php foreach ($properties as $property): ?>
     <?php include_partial('product/product_model_'.($property->ProductModelRelation[0]->is_image ? 'image' : 'select'), array('product' => $product, 'property' => $property, )) ?>
   <?php endforeach; ?>
