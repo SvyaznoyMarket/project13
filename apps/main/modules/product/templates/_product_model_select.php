47d585ab1be0494505835b5efe4a1ba9bf011bd3
Gray <ssapego@maxus.ru>
2011-12-12 19:07:51 +0300

    Фикс для моделей

--- apps/main/modules/product/templates/_product_model_select.php
+++ apps/main/modules/product/templates/_product_model_select.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <div class="bDropWrap">
   <h5><?php echo $property->name ?>:</h5>
   <div class="bDropMenu">
@@ -8,8 +9,54 @@
       <?php foreach ($property->values as $key => $value): if ($property->current == $key) continue; ?>
         <span><a href="<?php echo $value['url'] ?>"><?php echo $value['parameter']->getValue() ?></a></span>
       <?php endforeach ?>
+=======
+<?php if (false): ?>
+    <div class="font11 pb10 mr10 fl"><?php echo $property->name ?></div>
+    <div class="selectbox fl"><i></i>
+    <select class="styled">
+    <?php foreach ($property->values as $value): ?>
+        <option<?php echo ($value['is_selected']) ? ' selected' : '' ?> data-ref="<?php echo $value['url'] ?>"><?php echo $value['parameter']->getValue() ?></option>
+    <?php endforeach ?>
+    </select>
+    </div>
+<?php endif ?>
+
+<?php if (false): ?>
+<div class="filter">
+    <span class="fl" style="font-weight: bold; color: #000;"><?php echo $property->name ?>:</span>
+    <div class="filterchoice">
+        <a href="<?php echo $property->values[$property->current]['url'] ?>" class="filterlink orange"><?php echo $property->values[$property->current]['parameter']->getValue() ?></a>
+        <div class="filterlist">
+            <a href="<?php echo $property->values[$property->current]['url'] ?>" class="filterlink orange"><?php echo $property->values[$property->current]['parameter']->getValue() ?></a>
+            <ul>
+                <?php foreach ($property->values as $key => $value): if ($property->current == $key): continue; endif; ?>
+                <li><a href="<?php echo $value['url'] ?>"><?php echo $value['parameter']->getValue() ?></a></li>
+                <?php endforeach ?>
+            </ul>
+        </div>
+>>>>>>> e3d3e88... Changed template of product's model
     </div>
 
   </div>
 </div>
+<<<<<<< HEAD
+=======
+<div class="clear"></div>
+<?php endif ?>
+
+<div class="bDropWrap">
+  <h5><?php echo $property->name ?>:</h5>
+  <div class="bDropMenu">
+    <span><a href="<?php echo $property->values[$property->current]['url'] ?>"><?php echo $property->values[$property->current]['parameter']->getValue() ?></a></span>
+
+    <div>
+      <span><a href="<?php echo $property->values[$property->current]['url'] ?>"><?php echo $property->values[$property->current]['parameter']->getValue() ?></a></span>
+      <?php foreach ($property->values as $key => $value): if ($property->current == $key) continue; ?>
+        <span><a href="<?php echo $value['url'] ?>"><?php echo $value['parameter']->getValue() ?></a></span>
+      <?php endforeach ?>
+    </div>
+
+  </div>
+</div>
+>>>>>>> e3d3e88... Changed template of product's model
 
