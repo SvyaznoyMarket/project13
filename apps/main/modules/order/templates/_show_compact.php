a2fb78fec5fc8daf3fbf4c9d8a170f0f99c8cd4c
Trushina <otrushina@maxus.ru>
2011-12-20 07:54:38 -0500

    fix for private orders list

--- apps/main/modules/order/templates/_show_compact.php
+++ apps/main/modules/order/templates/_show_compact.php
@@ -62,7 +62,7 @@ use_helper('Date');
            <?php } ?>
        <?php endforeach; ?>
            
-       <?php if ($item['delivery_price'] && $item['delivery_type'] ) { ?>
+       <?php if (isset($item['delivery_price']) && isset($item['delivery_type']) && (int)$item['delivery_price']>0 ) { ?>
             <tr>
                <th>
                     <?php echo $item['delivery_type'] ?> 
@@ -77,7 +77,13 @@ use_helper('Date');
                
        <tr>
            <th>
-               <?php if (isset($item['delivered_at'])){ ?><div class="font12 pb5"> Дата доставки: <?php $date = explode(" ",$item['delivered_at']);  echo format_date($date[0],'dd MMMM yyyy','ru')?>г. <?php if (isset($item['delivered_period'])) echo '('.$item['delivered_period'].')'; ?></div><?php } ?>
+               <?php if (isset($item['delivered_at'])){ ?>
+                    <div class="font12 pb5">
+                        <?php echo $item['delivery_type'] ."."; ?>
+                        <?php $date = explode(" ",$item['delivered_at']);  echo format_date($date[0],'dd MMMM yyyy','ru')?>г. 
+                        <?php if (isset($item['delivered_period'])) echo '('.$item['delivered_period'].')'; ?>
+                    </div>
+               <?php } ?>
                <div class="font12">Способ оплаты: <?php echo $item['payment_method_name']?></div>
            </th>
            <td>Итого к оплате:<br><strong class="font18"><?php echo $item['sum'] ?>&nbsp;<span class="rubl">p</span></strong></td>
