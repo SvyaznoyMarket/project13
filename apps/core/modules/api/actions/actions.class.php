33642e589018b807d6b223d4b0860a01a702893d
Trushina <otrushina@maxus.ru>
2011-12-22 06:04:32 -0500

    cashe clean and packet status api

--- apps/core/modules/api/actions/actions.class.php
+++ apps/core/modules/api/actions/actions.class.php
@@ -10,13 +10,121 @@
  */
 class apiActions extends myActions
 {
+   
+    
+  /**
+   * Сбрасывает кеш сущности.
+   * 
+   * @param sfWebRequest $request
+   *  - type = [product, product_category]
+   *  - id - id сущности
+   */
+  public function executeCacheClean(sfWebRequest $request)
+  {
+      #echo 'clean cash';
+      #exit();
+      
+    try {
+      $response = trim(file_get_contents('php://input'));
+
+      $logger = new sfFileLogger(new sfEventDispatcher(), array('file' => $this->getLogFilename()));
+      $logger->log('Response: '.$response);
+
+      $data = json_decode($response, true);
+      if (!isset($data['type']) || !isset($data['id'])) {
+          $logger->err('Получен некорректный запрос на очистку кеша.');          
+          return $this->renderJson(array(
+            'confirmed' => false,
+          ));          
+      }
+      if ($data['type'] == 'product') {
+          $productTable = ProductTable::getInstance();
+          $product = $productTable->findOneBy('core_id', $data['id']);
+          #myDebug::dump($product);
+          CacheEraser::getInstance()->erase($product->getTable()->getCacheEraserKeys($product, 'show'));          
+      } else {
+          return $this->renderJson(array(
+            'confirmed' => false,
+          ));          
+      }
+      //очищаем кеш сущности
+      #print_r($data);
+      
+      return $this->renderJson(array(
+        'confirmed' => true,
+      ));
+      
+    }
+    catch (Exception $e) {
+      $logger->err($e->getCode().' - '.$e->getMessage());
+      return $this->renderJson(array(
+        'confirmed' => false,
+      ));          
+    }      
+  }    
+    
+  
+  /**
+   * Возвращает статус пакета синхранизации
+   * @param sfWebRequest $request
+   * @return int 
+   */
+  public function executePacketStatus(sfWebRequest $request)
+  {
+    try {
+      $response = trim(file_get_contents('php://input'));
+
+      $logger = new sfFileLogger(new sfEventDispatcher(), array('file' => $this->getLogFilename()));
+      $logger->log('Response: '.$response);
+
+      $data = json_decode($response, true);
+      #$data['id'] = 85318;
+      if (!isset($data['id'])) {
+          $logger->err('Ошибка. В запросе статуса пакета не передан id пакета.');          
+          return $this->renderJson(array(
+            'confirmed' => false,
+          ));          
+      }
+      #print_r($data);
+      
+      $table = TaskTable::getInstance();
+      $packetInfo = $table->getQueryObject()
+              ->addWhere('core_packet_id = ?', $data['id'])
+              ->fetchArray()
+            ;
+      if (isset($packetInfo[0]) && isset($packetInfo[0]['status'])) {
+          if ($packetInfo[0]['status'] == 'success') {
+              $status = 1;
+          } elseif ($packetInfo[0]['status'] == 'fail') {
+              $status = 2;
+          } else {
+              $status = 0;
+          }
+      } else {
+          $status = 0;          
+      }
+      #echo $status .'==$status';
+      return $this->renderJson(array(
+        'confirmed' => true,
+        'status' => $status  
+      ));
+      
+    }
+    catch (Exception $e) {
+      $logger->err($e->getCode().' - '.$e->getMessage());
+      return $this->renderJson(array(
+        'confirmed' => false,
+      ));          
+    }
+    
+  }
  /**
   * Executes index action
   *
   * @param sfRequest $request A request object
   */
   public function executeIndex(sfWebRequest $request)
-  {
+  {              
     try {
       $response = trim(file_get_contents('php://input'));
 
