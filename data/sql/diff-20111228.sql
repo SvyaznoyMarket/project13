f3eb8bf820650557efacd5a3d90946a84b707d65
green <green@mail.ru>
2011-12-28 16:12:35 +0300

    Added Product.view field

---
+++ data/sql/diff-20111228.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `product`  ADD COLUMN `view` VARCHAR(10) NOT NULL DEFAULT 'default' AFTER `preview`;
--- lib/model/doctrine/Product.class.php
+++ lib/model/doctrine/Product.class.php
@@ -53,6 +53,16 @@ class Product extends BaseProduct
     $this->token = array_pop($v);
     $this->token_prefix = count($v) ? array_shift($v) : null;
 
+    $this->view = 'default';
+    if (isset($data['view_id'])) foreach (array(1 => 'default', 2 => 'kit') as $k => $v)
+    {
+      if ($data['view_id'] == $k)
+      {
+        $this->view = $v;
+        break;
+      }
+    }
+
     // check if creator doesn't exist
     if (!empty($data['brand_id']) && empty($this->creator_id))
     {
