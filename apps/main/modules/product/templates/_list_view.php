db3db8be9f558aa2cdb7891d6abc857a6f724979
Gray <ssapego@maxus.ru>
2012-01-27 17:31:32 +0300

    Исправлена надпись <Вид> на <Вид страницы>

--- apps/main/modules/product/templates/_list_view.php
+++ apps/main/modules/product/templates/_list_view.php
@@ -1,6 +1,6 @@
 <!-- View -->
 <div class="view">
-  <span>Вид:</span>
+  <span>Вид страницы:</span>
   <?php foreach ($list as $item): ?>
     <a href="<?php echo $item['url'] ?>" class="<?php echo $item['class'] . ($item['current'] ? ' active' : '') ?>" title="<?php echo $item['title'] ?>"><?php echo $item['title'] ?></a>
   <?php endforeach ?>
