91c3b3f9bfe7aec375d6557c21ee95be0ce9f965
Gray <ssapego@maxus.ru>
2012-02-08 17:28:59 +0300

    Испралвена ошибка, убивающая банеры на главной при наличии одинарной кавычки в названии

--- apps/main/modules/banner/templates/_show_main.php
+++ apps/main/modules/banner/templates/_show_main.php
@@ -1,1 +1,1 @@
-<input id="main_banner-data" type="hidden" disabled="disabled" data-value='<?php echo json_encode($list) ?>' />
+<input id="main_banner-data" type="hidden" disabled="disabled" data-value='<?php echo str_replace("'", "&#39;", json_encode($list)) ?>' />
