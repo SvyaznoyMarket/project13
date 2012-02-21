e491ec92c101921e6ad00901a8289aee1f0e3052
green <green@mail.ru>
2012-02-21 11:56:29 +0300

    Форма быстрого заказа: изменен стиль

--- apps/main/modules/order/templates/_form_oneClick.php
+++ apps/main/modules/order/templates/_form_oneClick.php
@@ -3,25 +3,37 @@
 
 <div class="basketline">
   <?php include_component('product', 'show', array('view' => 'orderOneClick', 'product' => $product)) ?>
+
+  <div class="clear"></div>
+
+  <div class="b1Click__eTop mPR">
+    <div class="b1Click__ePrice">Сумма заказа:
+      <span class="b1Click__ePriceBig">
+        <span class="price"><?php echo number_format($order['sum'], 0, ',', ' ') ?></span> <span class="rubl">p</span>
+      </span>
+    </div>
+  </div>
 </div>
 
 <div class="clear"></div>
 
 <div class="b1Click">
+
   <div class="b1Click__eTop">
-    <div class="b1Click__ePrice">Сумма заказа:<br><span class="b1Click__ePriceBig"><span class="price"><?php echo number_format($order['sum'], 0, ',', ' ') ?></span> <span class="rubl">p</span></span></div>
-    Вы можете купить товар за 1 клик &mdash; оформление заказа займет всего 20 секунд, просто заполните 2 поля.
+    Это быстро! Просто заполните 2 поля.<br>
+    После оформления заказа с вами обязательно свяжется специалист нашего Контакт-cENTER
     <div class="clear"></div>
+
     <div class="b1Click__eInp">
-      <span>Имя получателя: </span>
-      <?php echo $form['recipient_first_name']->render($form['recipient_first_name']->hasError() ? array('class' => 'mRed') : array()) ?>
+      <span>Имя получателя:</span><?php echo $form['recipient_first_name']->render($form['recipient_first_name']->hasError() ? array('class' => 'mRed') : array()) ?>
     </div>
+
     <div class="b1Click__eInp">
-      <span>Телефон для связи:</span>
-      <?php echo $form['recipient_phonenumbers']->render($form['recipient_phonenumbers']->hasError() ? array('class' => 'mRed') : array()) ?>
+      <span>Телефон для связи:</span><?php echo $form['recipient_phonenumbers']->render($form['recipient_phonenumbers']->hasError() ? array('class' => 'mRed') : array()) ?>
     </div>
-    <div class="bFormB2"><span><input type="submit" value="Оформить заказ" id="bigbutton" class="button bigbutton"> </span></div>
+
+    <div class="bFormB2">
+      <input type="submit" value="Оформить заказ" id="bigbutton" class="bBigOrangeButton" style="margin-left: 20px; display: inline-block; width: 592px; cursor: pointer; height: 45px;" />
+    </div>
   </div>
 </div>
-
-</form>
