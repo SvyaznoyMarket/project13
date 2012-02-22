b30369698b1ef317b77b4bf8b29c93b90a8c5491
Gray <ssapego@maxus.ru>
2012-02-22 09:40:49 +0300

    Изменеие ссылки где купить на странице комментариев

--- apps/main/modules/product/templates/_show_stock.php
+++ apps/main/modules/product/templates/_show_stock.php
@@ -12,7 +12,7 @@
 
 <div class='bInShop__ePrice'><span class="price"><?php echo $item['price'] ?></span> <span class="rubl">p</span></div>
 <div class='bInShop__eButton' ref="<?php echo $item['token'] ?>" data-value='<?php echo json_encode( $json ) ?>'>
-  <?php include_component('cart', 'buy_button', array('product' => $product, 'quantity' => 1, 'view' => 'delivery')) ?>
+  <?php include_component('cart', 'buy_button', array('product' => $product, 'quantity' => 1, 'view' => 'add')) ?>
 </div>
 <p class='bInShop__eDescription'><?php echo $item['description'] ?></p>
 <div class='bInShop__eButton'><a href="<?php echo $item['url'] ?>" class='bWhiteButton'>Подробнее о товаре</a></div>
