dfc452a6de35273a18fbe40885ff81e048750509
Alexey Korchevsky <alexey.korchevsky@wikimart.ru>
2012-03-28 12:17:58 +0400

    Soa: Fix filter choice

--- apps/main/modules/productCatalog_/templates/_filter_choice.php
+++ apps/main/modules/productCatalog_/templates/_filter_choice.php
@@ -17,19 +17,19 @@ $values = $productFilterRaw->getValue($filter->getRawValue());
 <dd style="display: <?php echo $open ?>;">
   <ul class="checkbox_list">
     <?php  ?>
-    <?php foreach (array('да','нет') as $id => $name): $id = (int)$id; ?>
-      <li>
-        <input name="<?php echo $productFilter->getName() ?>[<?php echo $filter->getFilterId()?>][]"
-               type="checkbox"
-               value="<?php echo $id?>"
-               <?php if(in_array($id, $values)) echo 'checked'; ?>
-               id="<?php echo $productFilter->getName() ?>_<?php echo $filter->getFilterId()?>_<?php echo $id?>"
-               class="hiddenCheckbox">
-        <label for="<?php echo $productFilter->getName() ?>_<?php echo $filter->getFilterId()?>_<?php echo $id?>"
-               class="prettyCheckbox checkbox list">
-          <span class="holderWrap" style="width: 13px; height: 13px; "><span class="holder" style="width: 13px; "></span></span>
-          <?php echo $name?>
-        </label>
-      </li>
+    <?php foreach (array('нет', 'да') as $id => $name): $id = (int)$id; ?>
+    <li>
+      <input name="<?php echo $productFilter->getName() ?>[<?php echo $filter->getFilterId()?>][]"
+             type="checkbox"
+             value="<?php echo $id?>"
+        <?php if (in_array($id, $values)) echo 'checked'; ?>
+             id="<?php echo $productFilter->getName() ?>_<?php echo $filter->getFilterId()?>_<?php echo $id?>"
+             class="hiddenCheckbox">
+      <label for="<?php echo $productFilter->getName() ?>_<?php echo $filter->getFilterId()?>_<?php echo $id?>"
+             class="prettyCheckbox checkbox list">
+        <span class="holderWrap" style="width: 13px; height: 13px; "><span class="holder" style="width: 13px; "></span></span>
+        <?php echo $name?>
+      </label>
+    </li>
     <?php endforeach; ?>
 </dd>
