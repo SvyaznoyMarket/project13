ed54a9fa80d760ca41578e414cc77c6569ec8375
Trushina Olga <olga.trushina@enter.ru>
2012-03-30 17:19:27 +0400

    refs #1059. Отображение даты доставки по умолчанию, если из ядра доставки не получены

--- apps/main/modules/productSoa/templates/_delivery.php
+++ apps/main/modules/productSoa/templates/_delivery.php
@@ -3,25 +3,33 @@
     <ul>
         <li>
             <?php
-            foreach ($delivery as $info) {
-                if ($info['price'] > 0) {
-                    $price = $info['price'] . ' руб.';
-                } else {
-                    $price = 'бесплатно.';
+            if (!count($delivery)) {
+            ?>
+                <h5>Можно заказать сейчас с доставкой</h5>
+                <div>&mdash; Можем доставить <?php echo myToolkit::formatDeliveryDate(2); ?></div>
+            <?
+            } else {
+                foreach ($delivery as $info) {
+                    if ($info['price'] > 0) {
+                        $price = $info['price'] . ' руб.';
+                    } else {
+                        $price = 'бесплатно.';
+                    }
+                    $deliveryText = str_replace(array('сегодня', 'завтра'), array('<b>сегодня</b>', '<b>завтра</b>'), $info['deliveryText']);
+                    if ($info['mode'] == 1) {
+                        ?>
+                        <h5>Можно заказать сейчас с доставкой</h5>
+                        <div> &mdash; Можем доставить <?php echo $deliveryText ?>, <?php echo $price ?></div>
+                    <?php } elseif ($info['mode']==2) { ?>
+                        <h5>Можно заказать сейчас с доставкой</h5>
+                        <div> &mdash; Можем доставить <?php echo $deliveryText ?>, <?php echo $price ?></div>
+                    <?php } elseif ($info['mode']==3) { ?>
+                        <h5>Можно заказать сейчас и самостоятельно забрать в магазине <?php echo $deliveryText ?></h5>
+                        <div>&mdash; <a target="blank" href="<?php echo url_for('productStock', array('product' => $product->path)) ?>">В каких магазинах ENTER можно забрать?</a></div>
+                        <?php }
                 }
-                $deliveryText = str_replace(array('сегодня', 'завтра'), array('<b>сегодня</b>', '<b>завтра</b>'), $info['deliveryText']);
-                if ($info['mode'] == 1) {
-                    ?>
-                    <h5>Можно заказать сейчас с доставкой</h5>
-                    <div> &mdash; Можем доставить <?php echo $deliveryText ?>, <?php echo $price ?></div>
-                <?php } elseif ($info['mode']==2) { ?>
-                    <h5>Можно заказать сейчас с доставкой</h5>
-                    <div> &mdash; Можем доставить <?php echo $deliveryText ?>, <?php echo $price ?></div>
-                <?php } elseif ($info['mode']==3) { ?>
-                    <h5>Можно заказать сейчас и самостоятельно забрать в магазине <?php echo $deliveryText ?></h5>
-                    <div>&mdash; <a target="blank" href="<?php echo url_for('productStock', array('product' => $product->path)) ?>">В каких магазинах ENTER можно забрать?</a></div>
-                    <?php }
-                } ?>
+            }
+            ?>
         </li>
     </ul>
 </div>
