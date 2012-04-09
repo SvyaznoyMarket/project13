bbbcf3acabeb6d3157d511d57dd003863ba34353
Ivn Local <ikotov@maxus.ru>
2012-04-09 12:59:23 +0400

    refs #1783

--- web/js/combine.js
+++ web/js/combine.js
@@ -1,2 +1,2 @@
 window.filesWithVersion = 
-{"infopages.js":1333026941,"library.js":1333026941,"app.cart.js":1331471204,"bigjquery.min.js":1333026941,"wellcome.js":1333026941,"main.js":1333438808,"dash.js":1333026941,"app.order.js":1333438808,"app.product.comment.list.js":1331471204,"app.product.comment.new.js":1329210285,"app.product.js":1333438808,"app.shop.js":1324571820}
+{"infopages.js":1333961092,"library.js":1333961092,"app.cart.js":1331468836,"bigjquery.min.js":1333961092,"wellcome.js":1333961092,"main.js":1333961753,"dash.js":1333961092,"app.order.js":1333961092,"app.product.comment.list.js":1331468836,"app.product.comment.new.js":1330344886,"app.product.js":1333961092,"app.shop.js":1328172656}
