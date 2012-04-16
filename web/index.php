a29c4331d8d2ff743f2f7ce0ddc24fd5360977d0
Alexey Korchevsky <alexey.korchevsky@wikimart.ru>
2012-04-16 16:47:08 +0400

    Soa: fix index.php enviroment

--- web/index.php
+++ web/index.php
@@ -2,5 +2,5 @@
 
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
-$configuration = ProjectConfiguration::getApplicationConfiguration('main', 'dev', true);
+$configuration = ProjectConfiguration::getApplicationConfiguration('main', 'prod', false);
 sfContext::createInstance($configuration)->dispatch();
