6bd4ea7df0e58590e18b4ca29596e1cc8542bb49
Trushina Olga <olga.trushina@enter.ru>
2012-04-17 16:19:32 +0400

    refs #2122. фикс уехавшей вёрстки

--- apps/main/modules/productSoa/templates/_show_default.php
+++ apps/main/modules/productSoa/templates/_show_default.php
@@ -182,6 +182,7 @@ foreach ($p3d as $p3d_obj)
 
 <?php include_component('productSoa', 'product_model', array('product' => $product,)) ?>
 
+<div class="clear"></div>
 
 <?php if (!empty($product->description)): ?>
     <!-- Information -->
