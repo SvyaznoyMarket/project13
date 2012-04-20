e94301326539e58c0dffea1808def200a9955aa6
pavel.kuznetsov <pavel.kuznetsov@enter.ru>
2012-04-20 19:03:09 +0400

    ошибки убраны из контроллеров, теперь генерируются исключения и отлавливаются выше (перенаправляясь в контроллер ошибок)

--- light/controller/delivery.php
+++ light/controller/delivery.php
@@ -20,36 +20,28 @@ class delivery
   public function ProductDeliveryJson($params, Response $response){
     TimeDebug::start('controller:delivery:ProductDeliveryJson');
 
-    try{
-      $return = array();
-      $model = new DeliveryModel();
-      $result = $model->getProductDeliveries(intval($params['product_id']), intval($params['quantity']) ,$params['region']);
+    $return = array();
+    $model = new DeliveryModel();
+    $result = $model->getProductDeliveries(intval($params['product_id']), intval($params['quantity']) ,$params['region']);
 
-      $return = array('success' => true, 'data' => array());
-      foreach($result as $key => $deliveryObject){
-        $deliveryArray = $deliveryObject->toArray();
-        foreach($deliveryArray['dates'] as $key=>$date){
-          if($key > 7){
-            unset($deliveryArray['dates'][$key]);
-            continue;
-          }
-          if(!isset($date['shops'])){
-            continue;
-          }
-          $shops = $date['shops'];
-          unset($deliveryArray['dates'][$key]['shops']);
-          if(count($shops > 0)){
-            $ret['dates'][$key]['shopIds'] = array_keys($shops);
-          }
+    $return = array('success' => true, 'data' => array());
+    foreach($result as $key => $deliveryObject){
+      $deliveryArray = $deliveryObject->toArray();
+      foreach($deliveryArray['dates'] as $key=>$date){
+        if($key > 7){
+          unset($deliveryArray['dates'][$key]);
+          continue;
         }
-        $return['data'][$deliveryArray['token']] = $deliveryArray;
+        if(!isset($date['shops'])){
+          continue;
+        }
+        $shops = $date['shops'];
+        unset($deliveryArray['dates'][$key]['shops']);
+        if(count($shops > 0)){
+          $ret['dates'][$key]['shopIds'] = array_keys($shops);
+        }
       }
-    }
-    catch(Exception $e){
-      $return = array(
-        'success' => false,
-        'error' => $e->getMessage()
-      );
+      $return['data'][$deliveryArray['token']] = $deliveryArray;
     }
 
     $response->setContentType('application/json');
