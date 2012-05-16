13a9bf3612f69633ca281cad55e5369745f407ee
pavel.kuznetsov <pavel.kuznetsov@enter.ru>
2012-05-16 17:49:02 +0400

    bugfix

--- light/view/dataObject/pageObject/mainMenuData.php
+++ light/view/dataObject/pageObject/mainMenuData.php
@@ -22,27 +22,22 @@ class mainMenuData implements Iterator {
   }
 
   function rewind() {
-    var_dump(__METHOD__);
     $this->position = 0;
   }
 
   function current() {
-    var_dump(__METHOD__);
     return $this->elements[$this->position]['category'];
   }
 
   function key() {
-    var_dump(__METHOD__);
     return $this->position;
   }
 
   function next() {
-    var_dump(__METHOD__);
     ++$this->position;
   }
 
   function valid() {
-    var_dump(__METHOD__);
     return (isset($this->elements[$this->position]) && isset($this->elements[$this->position]['category']));
   }
 
