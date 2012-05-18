53a141fa00a6581f32bf8e056abaef7ec9904569
green <georgiy.lazukin@enter.ru>
2012-05-18 11:04:31 +0400

    refs #856 исправлены стили для карты магазинов

--- apps/main/modules/order_/templates/_map.php
+++ apps/main/modules/order_/templates/_map.php
@@ -4,14 +4,9 @@
     <div class='bMobDown mBR5 mW2'>
       <div class='bMobDown__eWrap'>
         <div class='bMobDown__eClose close'></div>
-        <div class='bMapShops__eMapWrap' id="mapPopup" >
+        <div class='bMapShops__eMapWrap' id="mapPopup" style="float: right;">
         </div>
         <div class='bMapShops__eList'>
-<!--          <div class='bMapShops__eListTitle'>-->
-<!--            <img data-bind="attr: { src: img }" alt=""/>-->
-<!--            <b data-bind="text: title"></b>-->
-<!--            <br/>(<span data-bind="text: price"></span> <span class="rubl">p</span>)-->
-<!--          </div>-->
           <h3>Выберите магазин Enter для самовывоза</h3>
           <ul id="mapPopup_shopInfo">
           </ul>
