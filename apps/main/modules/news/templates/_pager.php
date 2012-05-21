73fdc94e2531888438fd3ce7be6f300befe2a4fb
pavel.kuznetsov <pavel.kuznetsov@enter.ru>
2012-05-21 16:58:57 +0400

    remove News from code

--- apps/main/modules/news/templates/_pager.php
+++
@@ -1,1 +0,0 @@
-<?php include_component('news', 'list', array('newsList' => $newsList)) ?>
--- apps/main/modules/news/templates/_show.php
+++
@@ -1,3 +0,0 @@
-<?php use_helper('Date') ?>
-
-<span><?php echo format_date($news->published_at, 'D') ?></span>
