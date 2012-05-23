4e2d484c362be29be297540acf2202c8597321bc
green <georgiy.lazukin@enter.ru>
2012-05-23 17:04:48 +0400

    js-минификация

--- web/js/combine.js
+++ web/js/combine.js
@@ -1,2 +1,2 @@
 window.filesWithVersion = 
-{"app.oneclick.js":1337088067,"infopages.js":1336223668,"library.js":1336819110,"app.cart.js":1336223668,"bigjquery.min.js":1337088049,"wellcome.js":1337088049,"main.js":1337088049,"dash.js":1336223668,"app.order.js":1336223668,"app.product.comment.list.js":1336223668,"app.product.comment.new.js":1330432235,"app.product.js":1337088049,"app.shop.js":1329202458}
+{"app.oneclick.js":1337778079,"infopages.js":1335616783,"library.js":1337778079,"app.cart.js":1335616783,"bigjquery.min.js":1337778071,"wellcome.js":1337778079,"main.js":1337778079,"dash.js":1337693497,"app.order.js":1337601998,"app.product.comment.list.js":1335616783,"app.product.comment.new.js":1335616783,"app.product.js":1337778079,"app.shop.js":1337761084}
