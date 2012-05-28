70075cec6930ee61a08ee646097583de5e87a3d4
pavel.kuznetsov <pavel.kuznetsov@enter.ru>
2012-05-28 12:50:18 +0400

    refs #2554 copyrights

--- apps/main/modules/default/templates/_footer_default.php
+++ apps/main/modules/default/templates/_footer_default.php
@@ -71,10 +71,11 @@
   </div>
 
   <div class='bFooterBottom'>
-    &copy; &laquo;Enter&raquo; 2002&ndash;<?php echo date("Y") ?>. Все права защищены. Указанная стоимость товаров и условия их приобретения действительны по состоянию на текущую дату.<br>
+    Указанная стоимость товаров и условия их приобретения действительны по состоянию на текущую дату.<br>
     <a href="<?php echo url_for('default_show', array('page' => 'legal')) ?>">Правовая информация</a>
     <a href="<?php echo url_for('default_show', array('page' => 'terms')) ?>">Условия продажи</a>
-    <a href="<?php echo url_for('default_show', array('page' => 'media_info')) ?>">Информация о СМИ</a>
+    <a href="<?php echo url_for('default_show', array('page' => 'media_info')) ?>">Информация о СМИ</a><br/><br/>
+    <span class="copyright">&copy; ООО &laquo;Энтер&raquo; 2011&ndash;<?php echo date("Y") ?>. ENTER<sup>&reg;</sup> ЕНТЕР<sup>&reg;</sup> <span style="color: #FF0000;">Enter<sup>&reg;</sup></span>. Все права защищены.</span>
   </div>
 </div>
 <!-- /Footer -->
