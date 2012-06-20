f97cd6a7463eb7287c8ce4b6eb62ff57d0123304
green <georgiy.lazukin@enter.ru>
2012-06-20 12:57:24 +0400

    refs #2898 CEO-доработки

--- apps/main/modules/productComment/templates/indexSuccess.php
+++ apps/main/modules/productComment/templates/indexSuccess.php
@@ -2,8 +2,10 @@
   <?php include_component('productCard_', 'navigation', array('product' => $product)) ?>
 <?php end_slot() ?>
 
-<?php slot('title', $product->name) ?>
+<?php slot('title', $product->getName().': отзывы покупателей') ?>
 
 <?php include_partial('product', array('product' => $product)) ?>
+
+<p>Если вы затрудняетесь с выбором товара, мы рекомендуем вам узнать мнение и отзывы покупателей о товаре <strong><?php echo $product->getName() ?></strong>, представленные на этой странице. Вы также можете выразить свое мнение о товаре <strong><?php echo $product->getName() ?></strong>, оставив свой отзыв.</p>
 
 <?php include_component('productComment', 'list', array('product' => $product, 'page' => $page, 'sort' => $sort, 'showSort' => true, 'showPage' => true)) ?>
