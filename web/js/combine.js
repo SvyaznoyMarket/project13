cb3d33cf203a73bd8fa0ee51144a646a84b6fdbd
Trushina Olga <olga.trushina@enter.ru>
2012-06-28 11:09:09 +0400

    conflict resolve

--- web/js/combine.js
+++ web/js/combine.js
@@ -1,6 +1,2 @@
 window.filesWithVersion = 
-<<<<<<< HEAD
 {"app.order.v3.2.js":1340361261,"app.shop.map.js":1339066215,"app.oneclick.js":1339773234,"infopages.js":1339057619,"library.js":1340360157,"app.cart.js":1340360157,"bigjquery.min.js":1339066215,"welcome.js":1339659043,"main.js":1339659043,"dash.js":1339773234,"app.order.js":1340360157,"app.product.comment.list.js":1333721686,"app.product.comment.new.js":1333721686,"app.product.js":1340126198,"app.shop.js":1339066215}
-=======
-{"app.order.v3.2.js":1340371494,"app.shop.map.js":1340028639,"app.oneclick.js":1340287625,"infopages.js":1339765144,"library.js":1340363803,"app.cart.js":1340367503,"bigjquery.min.js":1340028639,"welcome.js":1340287530,"main.js":1340287530,"dash.js":1340365074,"app.order.js":1340616806,"app.product.comment.list.js":1339761826,"app.product.comment.new.js":1330344886,"app.product.js":1340364603,"app.shop.js":1339765144}
->>>>>>> ee0e9f6507bfab02d0f2c4f8b087535301398a1c
