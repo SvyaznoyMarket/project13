0c44575c0eac84f110a553504e184cf8bb2dc842
zeroshade <zeroshade@dev.enter.lan>
2012-06-29 06:38:51 -0400

    Revert "refs 2921 adfox temporary commented ant commited for next revert"
    This reverts commit 06896bbbc013ea9b238814a8b724fa1603693520.

--- apps/main/modules/productCatalog_/templates/_plugs/for_tag.php
+++ apps/main/modules/productCatalog_/templates/_plugs/for_tag.php
@@ -14,7 +14,7 @@ if (typeof(document.referrer) != 'undefined') {
     afReferrer = '';
   }
   var addate = new Date(); 
-//  document.write('<scr' + 'ipt type="text/javascript" src="http://ads.adfox.ru/171829/prepareCode?pp=g&amp;ps=bdto&amp;p2=emue&amp;pct=a&amp;plp=a&amp;pli=a&amp;pop=a&amp;pr=' + pr +'&amp;pt=b&amp;pd=' + addate.getDate() + '&amp;pw=' + addate.getDay() + '&amp;pv=' + addate.getHours() + '&amp;prr=' + afReferrer + '"><\/scr' + 'ipt>');
+  document.write('<scr' + 'ipt type="text/javascript" src="http://ads.adfox.ru/171829/prepareCode?pp=g&amp;ps=bdto&amp;p2=emue&amp;pct=a&amp;plp=a&amp;pli=a&amp;pop=a&amp;pr=' + pr +'&amp;pt=b&amp;pd=' + addate.getDate() + '&amp;pw=' + addate.getDay() + '&amp;pv=' + addate.getHours() + '&amp;prr=' + afReferrer + '"><\/scr' + 'ipt>');
 // -->
 </script>
 <!--AdFox END-->
