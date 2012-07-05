ed66c52ad5e493119c4d01ddd6bcc14295ab247f
pavel.kuznetsov <pavel.kuznetsov@enter.ru>
2012-07-05 11:56:23 +0400

    refs #2377 пофикшены ссылки

--- apps/main/modules/productCategory_/templates/_root_list.php
+++ apps/main/modules/productCategory_/templates/_root_list.php
@@ -1,7 +1,7 @@
 <?php foreach ($list as $item): /** @var $item ProductCategoryEntity */ ?>
 <a id="topmenu-root-<?php echo $item->getId() ?>" title="<?php echo $item->getName() ?>"
    alt="<?php echo $item->getName() ?>" class="bToplink"
-   href="<?php echo url_for('productCatalog_category', array('productCategory' => $item->getLink())) ?>">
+   href="<?php echo $item->getLink() ?>">
     <span class="category-<?php echo $item->getId() ?>">
     </span>
 </a>
