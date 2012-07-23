a60dc8604e0212b7eda4a52c0bbeb3bb846df7c7
green <green@mail.ru>
2012-07-23 18:36:18 +0400

    refs #3214 На странице авторизации, кнопки наезжают на нижнюю подложку

--- apps/main/modules/user/templates/editSuccess.php
+++ apps/main/modules/user/templates/editSuccess.php
@@ -2,12 +2,15 @@
 <?php slot('navigation') ?>
   <?php include_component('user', 'navigation') ?>
 <?php end_slot() ?>
-    <div class="float100">
-		<div class="column685">
-        <ul class="error_list"><li><?php if (isset($error)) echo $error ?></li></ul>
-            
-            <?php include_component('user', 'profile', array('form' => $form)) ?>
-        </div>
-    </div>
+
+<div class="float100">
+<div class="column685">
+  <ul class="error_list"><li><?php if (isset($error)) echo $error ?></li></ul>
+
+  <?php include_component('user', 'profile', array('form' => $form)) ?>
+</div>
+</div>
 
 <?php include_component('user', 'menu') ?>
+
+<br />
