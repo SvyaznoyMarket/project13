2b736398fd53eb3bfd60f369c8f6a671783c3005
IvnLocal2 <ivan.kotov@enter.ru>
2012-07-30 13:18:38 +0400

    crash fix, refs #3486

--- apps/main/templates/order_.php
+++ apps/main/templates/order_.php
@@ -27,6 +27,7 @@
 <?php include_partial('region/select') ?>
 
 <?php include_component('default', 'footer', array('view' => 'compact')) ?>
+<script src="/js/jquery-1.6.4.min.js" type="text/javascript"></script>
 <script src="/js/LAB.min.js" type="text/javascript"></script>
 <script src="/js/loadjs.js" type="text/javascript"></script>
 
