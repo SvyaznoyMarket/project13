30a828524143472dda97f1b5db5d10506b973bd7
green <green@mail.ru>
2012-08-21 13:36:58 +0400

    refs #3439 исправлен shortinfo

--- light/lib/CurrentUser.php
+++ light/lib/CurrentUser.php
@@ -58,46 +58,16 @@ class CurrentUser
    * @return UserData|null
    */
   public function getUser(){
-    if(is_object($this->user)){
-      return $this->user;
-    }
-    try{
-      $user_id = isset($_SESSION['symfony/user/sfUser/attributes']['guard']['user_id']) ? $_SESSION['symfony/user/sfUser/attributes']['guard']['user_id'] : null;
-      $user_id = (int)$user_id;
-      if ($user_id < 1) {
-        return null;
-      }
+    if (!$this->user) {
+      $token = $token = isset($_SESSION['symfony/user/sfUser/attributes']['guard']['token']) ? $_SESSION['symfony/user/sfUser/attributes']['guard']['token'] : null;
+      if ($token) {
+        $data = CoreClient::getInstance()->query('user/get', array('token' => $token));
 
-      if (!($conn = mysql_connect(DB_HOST, DB_USERNAME, DB_PASSWORD))) {
-        throw new \Exception('cant connect to db "' . DB_HOST . '"');
-      }
-      if (!mysql_select_db(DB_NAME, $conn)) {
-        throw new \Exception('cant select db "' . DB_NAME . '"');
-      }
-
-      $query = 'SELECT first_name,last_name, middle_name  FROM guard_user WHERE id="' . $user_id . '"';
-
-
-      if ($result = mysql_query($query, $conn)) {
-        $userData = mysql_fetch_array($result, MYSQL_ASSOC);
-        if (!$userData) {
-          throw new \Exception('mysql_fetch_array error');
-        }
-
-        $this->user = new UserData($userData);
-        mysql_close($conn);
-        return $this->user;
-      }
-      else{
-        throw new \Exception('mysql_query error');
+        $this->user = new UserData($data);
       }
     }
-    catch(\Exception $e){
-      if ($conn) {
-        mysql_close($conn);
-      }
-      return null;
-    }
+
+    return $this->user;
   }
 
   /**
