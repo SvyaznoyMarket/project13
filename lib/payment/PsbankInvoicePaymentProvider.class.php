f233febf3b4eb04acc787eede17925d90582ae54
green <green@mail.ru>
2012-08-22 18:18:55 +0400

    refs #3760 PSB. инвойсинг

---
+++ lib/payment/PsbankInvoicePaymentProvider.class.php
@@ -0,0 +1,51 @@
+<?php
+
+class PsbankInvoicePaymentProvider
+{
+  private $config = array();
+
+  public function __construct(array $config)
+  {
+    $this->config = myToolkit::arrayDeepMerge(sfYaml::load(sfConfig::get('sf_config_dir').'/psbank_invoice.yml'), $config);
+  }
+
+  public function getConfig($name = null)
+  {
+    return null == $name ? $this->config : $this->config[$name];
+  }
+
+  public function getFormUrl() {
+    return $this->config['url'];
+  }
+
+  /**
+   *
+   * @param $order
+   * @return PsbankInvoiceForm
+   */
+  public function getForm($order)
+  {
+    $data = array(
+      'ContractorID'   => $this->config['contractor_id'],
+      'InvoiceID'      => $order['id'],
+      'Sum'            => sprintf("%01.2f", $order['sum']),
+      'PayDescription' => sprintf('Оплата заказа №%s', $order['number']),
+      'AdditionalInfo' => '',
+      'redirect_url'   => sfContext::getInstance()->getRouting()->generate('order_complete', array('order' => $order['number']), true),
+    );
+
+    $signature = $this->config['contractor_id'].$data['InvoiceID'].$data['Sum'].$data['PayDescription'];
+    $data['Signature'] = base64_encode(
+      iconv('UTF-8', 'windows-1251', $signature)
+    );
+
+    $form = new PsbankInvoicePaymentForm($data);
+
+    return $form;
+  }
+
+  public function getOrderIdFromRequest($request)
+  {
+    return $request['order'];
+  }
+}
