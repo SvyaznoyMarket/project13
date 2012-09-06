c8d1833b459492252c98af6bebed5fba528630e5
green <green@mail.ru>
2012-09-06 15:33:39 +0400

    refs #3516 исправлен sql для таблицы queue

--- data/sql/3516-smartengine/update/3516.sql
+++ data/sql/3516-smartengine/update/3516.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS `queue` (
   `id` bigint(11) NOT NULL AUTO_INCREMENT,
-  `is_locked` tinyint(1) NOT NULL DEFAULT '0',
-  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `body` text COLLATE utf8_unicode_ci NOT NULL,
+  `is_locked` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'флаг блокировки',
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'название обработчика',
+  `body` text COLLATE utf8_unicode_ci NOT NULL COMMENT 'json-данные для обработчика',
   PRIMARY KEY (`id`),
   KEY `is_locked-name` (`is_locked`,`name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
