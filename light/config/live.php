919294c943b7bffd6daecce05ded9f3d2391ca4f
green <green@mail.ru>
2012-09-10 18:20:15 +0400

    refs #3516 исправлен путь для сертификата

--- light/config/live.php
+++ light/config/live.php
@@ -26,4 +26,5 @@ define('DB_PASSWORD', 'qazwsxedc');
 define('SMARTENGINE_API_URL', 'https://selightprod.smartengine.at/se-light/api/1.0/json/');
 define('SMARTENGINE_API_KEY', 'c41851b19511c20acc84f47b7816fb8e');
 define('SMARTENGINE_TENANTID', 'ENojUTRcD8');
-define('SMARTENGINE_CERT_PATH', ROOT_PATH.'data/cert/smartengine-server2.pem');
+define('SMARTENGINE_CERT_PATH', ROOT_PATH.'../data/cert/smartengine-server2.pem');
+die(SMARTENGINE_CERT_PATH);
