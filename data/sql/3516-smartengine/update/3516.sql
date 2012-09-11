f68a3e3f6b60cc7231a2bc77ef2bedd6b2192937
green <green@mail.ru>
2012-09-11 11:27:11 +0400

    refs #3516 is_locked --> locked_at

--- data/sql/3516-smartengine/update/3516.sql
+++ data/sql/3516-smartengine/update/3516.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS `queue` (
   `id` bigint(11) NOT NULL AUTO_INCREMENT,
-  `is_locked` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'флаг блокировки',
+  `locked_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'время блокировки',
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'название обработчика',
   `body` text COLLATE utf8_unicode_ci NOT NULL COMMENT 'json-данные для обработчика',
   PRIMARY KEY (`id`),
-  KEY `is_locked-name` (`is_locked`,`name`)
+  KEY `locked_at-name` (`locked_at`,`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
