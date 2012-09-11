d7fcca01abeed2319623d22f923c92d1f79dea7c
green <green@mail.ru>
2012-09-11 13:17:25 +0400

    refs #3516 sql fix

--- data/sql/3516-smartengine/update/3516.sql
+++ data/sql/3516-smartengine/update/3516.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS `queue` (
   `id` bigint(11) NOT NULL AUTO_INCREMENT,
-  `locked_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'время блокировки',
+  `locked_at` timestamp NULL DEFAULT NULL COMMENT 'время блокировки',
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'название обработчика',
   `body` text COLLATE utf8_unicode_ci NOT NULL COMMENT 'json-данные для обработчика',
   PRIMARY KEY (`id`),
