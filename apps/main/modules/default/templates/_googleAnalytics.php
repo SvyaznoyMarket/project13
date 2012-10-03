7798a77a40c2498073055ea0e86e87e63043feda
Ivn Local <ikotov@maxus.ru>
2012-10-03 18:51:57 +0400

    GA minor change, SITE-98

--- apps/main/modules/default/templates/_googleAnalytics.php
+++ apps/main/modules/default/templates/_googleAnalytics.php
@@ -23,6 +23,6 @@
       _gaq.push(['_addOrganic', 'm.yandex.ru','query']);
       _gaq.push(['_trackPageview']);
       _gaq.push(['_trackPageLoadTime']);
-      (function() { var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s); })();
+      (function() { var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s); })();
   </script>
 <?php endif ?>
