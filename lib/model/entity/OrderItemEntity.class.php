0a1fa6d9691f9fa0b5b5af794369b84939fc884a
green <green@mail.ru>
2012-10-04 16:54:49 +0400

    refs #4008 добавлен вывод гарантии в ЛК

--- lib/model/entity/OrderItemEntity.class.php
+++ lib/model/entity/OrderItemEntity.class.php
@@ -3,132 +3,137 @@
 /**
  * Элемент заказа
  */
-class OrderItemEntity
-{
-  const TYPE_PRODUCT = 1;
-  const TYPE_SERVICE = 2;
+class OrderItemEntity {
+    const TYPE_PRODUCT = 1;
+    const TYPE_SERVICE = 2;
 
-  /* @var integer */
-  private $orderId;
+    /* @var integer */
+    private $orderId;
 
-  /* @var integer */
-  private $serviceId;
+    /* @var integer */
+    private $serviceId;
 
-  /* @var integer */
-  private $productId;
+    /* @var integer */
+    private $productId;
 
-  /* @var float */
-  private $price;
+    /* @var float */
+    private $price;
 
-  /* @var integer */
-  private $quantity;
+    /** @var WarrantyEntity */
+    private $warranty;
 
-  /* @var string example: 2011-10-22 22:24:48 */
-  private $createdAt;
+    /* @var integer */
+    private $quantity;
 
-  public function __construct($data = array()){
-    if(array_key_exists('order_id', $data))      $this->orderId    = (int)$data['order_id'];
-    if(array_key_exists('product_id', $data))    $this->productId  = (int)$data['product_id'];
-    if(array_key_exists('service_id', $data))    $this->serviceId  = (int)$data['service_id'];
-    if(array_key_exists('price', $data))         $this->price      = (float)$data['price'];
-    if(array_key_exists('quantity', $data))      $this->quantity   = (int)$data['quantity'];
-    if(array_key_exists('added', $data))         $this->createdAt  = $data['added'];
+    /* @var string example: 2011-10-22 22:24:48 */
+    private $createdAt;
 
-  }
+    public function __construct($data = array()) {
+        if (array_key_exists('order_id', $data)) $this->orderId = (int)$data['order_id'];
+        if (array_key_exists('product_id', $data)) $this->productId = (int)$data['product_id'];
+        if (array_key_exists('service_id', $data)) $this->serviceId = (int)$data['service_id'];
+        if (array_key_exists('warranty', $data)) $this->warranty = new WarrantyEntity($data['warranty']);
+        if (array_key_exists('price', $data)) $this->price = (float)$data['price'];
+        if (array_key_exists('quantity', $data)) $this->quantity = (int)$data['quantity'];
+        if (array_key_exists('added', $data)) $this->createdAt = $data['added'];
+    }
 
-  /**
-   * @param string $createdAt
-   */
-  public function setCreatedAt($createdAt)
-  {
-    $this->createdAt = $createdAt;
-  }
+    /**
+     * @param string $createdAt
+     */
+    public function setCreatedAt($createdAt) {
+        $this->createdAt = $createdAt;
+    }
 
-  /**
-   * @return string
-   */
-  public function getCreatedAt()
-  {
-    return $this->createdAt;
-  }
+    /**
+     * @return string
+     */
+    public function getCreatedAt() {
+        return $this->createdAt;
+    }
 
-  /**
-   * @param int $orderId
-   */
-  public function setOrderId($orderId)
-  {
-    $this->orderId = $orderId;
-  }
+    /**
+     * @param int $orderId
+     */
+    public function setOrderId($orderId) {
+        $this->orderId = $orderId;
+    }
 
-  /**
-   * @return int
-   */
-  public function getOrderId()
-  {
-    return $this->orderId;
-  }
+    /**
+     * @return int
+     */
+    public function getOrderId() {
+        return $this->orderId;
+    }
 
-  /**
-   * @param float $price
-   */
-  public function setPrice($price)
-  {
-    $this->price = $price;
-  }
+    /**
+     * @param float $price
+     */
+    public function setPrice($price) {
+        $this->price = $price;
+    }
 
-  /**
-   * @return float
-   */
-  public function getPrice()
-  {
-    return $this->price;
-  }
+    /**
+     * @return float
+     */
+    public function getPrice() {
+        return $this->price;
+    }
 
-  /**
-   * @param int $productId
-   */
-  public function setProductId($productId)
-  {
-    $this->productId = $productId;
-  }
+    /**
+     * @param int $productId
+     */
+    public function setProductId($productId) {
+        $this->productId = $productId;
+    }
 
-  /**
-   * @return int
-   */
-  public function getProductId()
-  {
-    return $this->productId;
-  }
+    /**
+     * @return int
+     */
+    public function getProductId() {
+        return $this->productId;
+    }
 
-  /**
-   * @param int $quantity
-   */
-  public function setQuantity($quantity)
-  {
-    $this->quantity = $quantity;
-  }
+    /**
+     * @param int $quantity
+     */
+    public function setQuantity($quantity) {
+        $this->quantity = $quantity;
+    }
 
-  /**
-   * @return int
-   */
-  public function getQuantity()
-  {
-    return $this->quantity;
-  }
+    /**
+     * @return int
+     */
+    public function getQuantity() {
+        return $this->quantity;
+    }
 
-  /**
-   * @param int $serviceId
-   */
-  public function setServiceId($serviceId)
-  {
-    $this->serviceId = $serviceId;
-  }
+    /**
+     * @param int $serviceId
+     */
+    public function setServiceId($serviceId) {
+        $this->serviceId = $serviceId;
+    }
 
-  /**
-   * @return int
-   */
-  public function getServiceId()
-  {
-    return $this->serviceId;
-  }
+    /**
+     * @return int
+     */
+    public function getServiceId() {
+        return $this->serviceId;
+    }
+
+    /**
+     * @param $warranty
+     */
+    public function setWarranty(WarrantyEntity $warranty = null) {
+        $this->warranty = $warranty;
+    }
+
+    /**
+     * @return WarrantyEntity
+     */
+    public function getWarranty() {
+        return $this->warranty;
+    }
+
 }
