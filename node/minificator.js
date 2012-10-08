7130ad542d6fc5a9341c66d7b97a1d5bbad29882
Ivn Local <ikotov@maxus.ru>
2012-10-08 12:14:41 +0400

    file watching, SITE-45

--- node/minificator.js
+++ node/minificator.js
@@ -8,61 +8,86 @@ var POINTS = {
 	'jsdir': '../web/js/',
 	'cssdir': '../web/css/',
 	'js': '../web/js/combine.js',
-	'js': '../web/js/combine.js',
 	'less': '../web/css/global.less',
-	'css': '../web/css/global2.css'
+	'css': '../web/css/global.css'
 }	
-var red   = '\033[31m';
 
-var parser = new(less.Parser)({
-    paths: [ POINTS.cssdir ] // Specify search paths for @import directives
-    // filename: 'style.less' // Specify a filename, for better error messages
-})
-fs.readFile( POINTS.less , 'utf8', function(e, data ) { 
-	parser.parse( data, function (err, tree) {
-		console.log( '< CSS >')
-	    if (err) { 
-	    	console.log('</ CSS >')
-	    	return console.error( red + 'Error processing less file '+err)
-	    }
-    	
-	    // console.log( tree.toCSS().length )
-		fs.writeFile( POINTS.cssdir , tree.toCSS(), 'utf8', function(curr, prev) {})
-		console.log('OK')
-		console.log('</ CSS >')
-	})
-})
-
-console.log('< JS >')
-
+var red   = '\033[31m'
 var config = {}
 
-fs.readFile( POINTS.js , 'utf8', function(e, data ) { 
-	config = JSON.parse( data.replace(/^(.)*=/,'') )	
-	when( procall( config ), 
-		function yep(){ console.log('all files OK') }, // good
-		function nope(){ console.log( red + 'error') } // wrong
-	).then( reconfig )
+function parseLESS() {
+	var parser = new(less.Parser)({
+	    paths: [ POINTS.cssdir ] // Specify search paths for @import directives
+	    // filename: 'style.less' // Specify a filename, for better error messages
+	})
+	fs.readFile( POINTS.less , 'utf8', function(e, data ) { 
+		parser.parse( data, function (err, tree) {
+			console.log( '< CSS >')
+		    if (err) { 
+		    	console.log('</ CSS >')
+		    	return console.error( red + 'Error processing less file '+err)
+		    }
+	    	
+		    // console.log( tree.toCSS().length )
+			fs.writeFile( POINTS.css , tree.toCSS(), 'utf8', function(curr, prev) {})
+			console.log('OK')
+			console.log('</ CSS >')
+		})
+	})
+}
+
+function parseJS() {
+	console.log('< JS >')
+	fs.readFile( POINTS.js , 'utf8', function(e, data ) { 
+		config = JSON.parse( data.replace(/^(.)*=/,'') )	
+		when( procall( config ), 
+			function yep(){ console.log('all files OK') }, // good
+			function nope(){ console.log( red + 'error') } // wrong
+		).then( function(){ 
+			reconfig()
+			console.log('</ JS >')
+		})
+	})
+}
+
+parseLESS()
+parseJS()
+
+fs.watchFile( POINTS.less, function() {
+	console.info('LESS CHANGED')
+	parseLESS()
 })
 
 function reconfig() {
 	console.log( config )
 	var chunk = 'window.filesWithVersion = ' + JSON.stringify( config )
 	fs.writeFile( POINTS.js , chunk, 'utf8', function(curr, prev) {})
-	console.log('</ JS >')
 }
 
 function procall( list ) {
 	var promises = []
-	for( var k in list) 
+	for( var k in list) {
 		promises.push( procfile( k ) )
+		watchJSfile( k )
+	}
 	return when.all( promises )
+}
+
+function watchJSfile( filename ) {
+	var path = POINTS.jsdir + filename		
+
+	fs.watchFile( path, function() {
+		console.info( path + ' CHANGED')
+		fs.stat( path ,function( a, b ) {
+			minify( filename, b.mtime.getTime() )
+		})
+		reconfig()
+	})
 }
 
 function procfile( filename ) {
 	var deferred = when.defer()
-	var path = POINTS.jsdir + filename,
-		pathmin = POINTS.jsdir + filename.replace('js','min.js')
+	var path = POINTS.jsdir + filename
 
 	fs.exists( path, function(boo) { 
 		
@@ -71,23 +96,30 @@ function procfile( filename ) {
 			deferred.reject(new Error( 'no such file '+ path ))
 			return deferred.promise
 		}
-		fs.stat( path ,function(a,b) {
+		fs.stat( path ,function( a, b ) {
 			// console.log(  b.mtime.getTime() , config[''+filename] )
 			if( b.mtime.getTime() != config[''+filename] ) {
 				console.log( 'file changed, ', path )
-				new compressor.minify({
-				    type: 'uglifyjs',
-				    fileIn: path,
-				    fileOut: pathmin,
-				    callback: function(err) {
-				        if(err) console.log( red + err)
-				    }
-				})
-				config[''+filename] = b.mtime.getTime()
+				minify( filename, b.mtime.getTime() )
 			}
 			deferred.resolve()
 		})
 		
 	})
 	return deferred.promise
+}
+
+function minify( filename, timestamp ) {
+	var path = POINTS.jsdir + filename,
+		pathmin = POINTS.jsdir + filename.replace('js','min.js')
+
+	new compressor.minify({
+	    type: 'uglifyjs',
+	    fileIn: path,
+	    fileOut: pathmin,
+	    callback: function(err) {
+	        if(err) console.log( red + err)
+	    }
+	})
+	config[''+filename] = timestamp
 }
