1e2447177baa9dc5626d469e6278470f6212271d
green <green@mail.ru>
2012-10-11 12:56:07 +0400

    dark: debug panel

--- dark/data/debug/panel.php
+++ dark/data/debug/panel.php
@@ -23,9 +23,26 @@ $debug->add('memory', sprintf('%s Mb', round(memory_get_peak_usage() / 1048576, 
 
 if (!\App::request()->isXmlHttpRequest()) {
 ?>
-    <div draggable="true" ondblclick="$(this).remove()" style="position: fixed; top: 24px; left: 2px; width: 260px; overflow: hidden; z-index: 999; background: #000000; color: #00ff00; opacity: 0.8; padding: 4px 6px; border-radius: 5px; font-size: 10px; font-family: Courier New; box-shadow: 0 0 10px rgba(0,0,0,0.5);">
+    <div draggable="true" style="position: absolute; top: 24px; left: 2px; width: 260px; overflow: hidden; z-index: 999; background: #000000; color: #00ff00; opacity: 0.8; padding: 4px 6px; border-radius: 5px; font-size: 10px; font-family: Courier New; box-shadow: 0 0 10px rgba(0,0,0,0.5);">
+        <span onclick="$(this).parent().remove()" style="cursor: pointer; font-size: 16px; color: #ffffff;">&times;</span>
+        <span onclick="var el = $(this).parent(); if (el.height() > 40) { el.animate({height: 16}, 200); } else { el.animate({height: 300}, 200); }" style="cursor: pointer; font-size: 16px; color: #ffffff;">&minus;</span>
+        <span onclick="var el = $(this).parent(); if (el.width() > 260) { el.animate({width: 260}, 200); } else { el.animate({width: 1024}, 200); }" style="cursor: pointer; font-size: 16px; color: #ffffff;">+</span>
+        <br />
+
     <? foreach ($debug->getAll() as $item) { ?>
-        <?= $item['name'] ?>: <? if (\Debug\Collector::TYPE_ERROR == $item['type']) { ?><span style="color: #ff0000;"><?= $item['value'] ?></span><? } else { ?><?= $item['value'] ?><? } ?>
+        <?
+            $isHidden = mb_strlen($item['value']) > 40;
+            if ($isHidden) $item['value'] = '<pre>' . $item['value'] . '</pre>';
+        ?>
+        <?= $item['name'] ?>:
+
+        <? if ($isHidden) { ?>
+        <span onclick="var el = $(this).next(); el.is(':hidden') ? el.show() : el.hide()" style="cursor: pointer;">...</span>
+        <? }?>
+
+        <span<? if ($isHidden) { ?> style="display: none;" <? } ?>>
+            <? if (\Debug\Collector::TYPE_ERROR == $item['type']) { ?><span style="color: #ff0000;"><?= $item['value'] ?></span><? } else { ?><?= $item['value'] ?><? } ?>
+        </span>
         <br />
     <? } ?>
     </div>
