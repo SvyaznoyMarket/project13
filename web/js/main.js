1e8dc7059c94a81c5342f461c9c43845ffffacc9
Alex Zaycev <13rentgen@gmail.com>
2012-10-15 17:44:46 +0400

    SITE-174 Испрвил верстку селектора модификатора товара. Убрал из js применение стилей к этому селектору.

--- web/js/main.js
+++ web/js/main.js
@@ -491,17 +491,6 @@ $(document).ready(function(){
 	}
 	
 	/* Search */
-	$('input:[name="q"]').bind(
-		{
-			'focusin': function() {
-				if ( $(this).val() == 'Поиск среди 30 000 товаров' ) $(this).val( '' );
-			},
-			'blur': function() {
-				if ( $(this).val() == '' ) $(this).val( 'Поиск среди 30 000 товаров' );
-			}
-		}
-	)
-
 	$('.search-form').bind('submit', function(e) {
 		e.preventDefault()
 		var form = $(this)
