f4d503ec570719a334865abbc266cad492505e44
green <green@mail.ru>
2012-10-17 10:12:14 +0400

    SITE-78 selected filter fix

--- dark/data/template/product-category/_selectedFilter.php
+++ dark/data/template/product-category/_selectedFilter.php
@@ -1,7 +1,4 @@
 <?php
-
-use \Model\Product\Filter\Entity as FilterEntity;
-
 /**
  * @var $page          \View\DefaultLayout
  * @var $productFilter \Model\Product\Filter
@@ -9,88 +6,8 @@ use \Model\Product\Filter\Entity as FilterEntity;
 ?>
 
 <?php
-/**
- * @param  int  $filterId
- * @param  null $value
- * @return string
- */
-$getUrl = function($filterId, $value = null) use ($page, $productFilter) {
-    $data = $productFilter->getValues();
-    if (array_key_exists($filterId, $data)) {
-        if (null == $value) {
-            unset($data[$filterId]);
-        } else foreach ($data[$filterId] as $k => $v) {
-            if ($v == $value) {
-                unset($data[$filterId][$k]);
-            }
-        }
-    }
-
-    return $page->url('product.category', array(
-        'categoryPath'                  => $productFilter->getCategory()->getPath(),
-        \View\Product\FilterForm::$name => $data
-    ));
-};
-
-/**
- * @param $first
- * @param $second
- * @return bool
- */
-$isEqualNumeric = function($first, $second) use ($page) {
-    $first = $page->helper->clearZeroValue((float)$first);
-    $second = $page->helper->clearZeroValue((float)$second);
-
-    return $first == $second;
-};
-
-$list = array();
-foreach ($productFilter->getFilterCollection() as $filter) {
-    $value = $productFilter->getValue($filter);
-    switch ($filter->getTypeId()) {
-        case FilterEntity::TYPE_SLIDER:
-        case FilterEntity::TYPE_NUMBER:
-            if (empty($value['from']) && empty($value['to'])) continue;
-            $name = array();
-            if (!($isEqualNumeric($value['from'], $filter->getMin()))) $name[] = sprintf('от %d', $value['from']);
-            if (!($isEqualNumeric($value['to'], $filter->getMax()))) $name[] = sprintf('до %d', $value['to']);
-            if (!$name) continue;
-            if ($filter->getFilterId() == 'price') $name[] .= 'р.';
-            $list[] = array(
-                'type' => $filter->getFilterId() == 'brand' ? 'creator' : 'parameter',
-                'name'  => implode(' ', $name),
-                'url'   => $getUrl($filter->getFilterId()),
-                'title' => $filter->getName(),
-            );
-            break;
-        case FilterEntity::TYPE_BOOLEAN:
-            if (!is_array($value) || count($value) == 0) continue;
-            foreach ($value as $v) {
-                $list[] = array(
-                    'type'  => $filter->getFilterId() == 'brand' ? 'creator' : 'parameter',
-                    'name'  => $filter->getName() . ': ' . ($v == 1 ? 'да' : 'нет'),
-                    'url'   => $getUrl($filter->getFilterId(), $v),
-                    'title' => $filter->getName(),
-                );
-            }
-            break;
-        case FilterEntity::TYPE_LIST:
-            if (!is_array($value) || count($value) == 0) continue;
-            foreach ($filter->getOption() as $option) {
-                if (in_array($option->getId(), $value)) {
-                    $list[] = array(
-                        'type'  => $filter->getFilterId() == 'brand' ? 'creator' : 'parameter',
-                        'name'  => $option->getName(),
-                        'url'   => $getUrl($filter->getFilterId(), $option->getId()),
-                        'title' => $filter->getName(),
-                    );
-                }
-            }
-            break;
-        default:
-            continue;
-    }
-}
+$formFilter = new \View\Product\FilterForm($productFilter);
+$list = $formFilter->getSelected();
 ?>
 
 <? if ((bool)$list): ?>
--- dark/lib/View/Product/FilterForm.php
+++ dark/lib/View/Product/FilterForm.php
@@ -2,6 +2,104 @@
 
 namespace View\Product;
 
+use \Model\Product\Filter\Entity as FilterEntity;
+
 class FilterForm {
     public static $name = 'f';
+    /** @var \Model\Product\Filter */
+    private $productFilter;
+    /** @var \View\Layout */
+    private $page;
+
+    public function __construct(\Model\Product\Filter $productFilter) {
+        $this->productFilter = $productFilter;
+        $this->page = new \View\Layout();
+    }
+
+    public function getSelected() {
+        $return = array();
+        foreach ($this->productFilter->getFilterCollection() as $filter) {
+            $value = $this->productFilter->getValue($filter);
+            switch ($filter->getTypeId()) {
+                case FilterEntity::TYPE_SLIDER:
+                case FilterEntity::TYPE_NUMBER:
+                    if (empty($value['from']) && empty($value['to'])) continue;
+                    $name = array();
+                    if (!($this->isEqualNumeric($value['from'], $filter->getMin()))) $name[] = sprintf('от %d', $value['from']);
+                    if (!($this->isEqualNumeric($value['to'], $filter->getMax()))) $name[] = sprintf('до %d', $value['to']);
+                    if (!$name) continue;
+                    if ($filter->getFilterId() == 'price') $name[] .= 'р.';
+                    $return[] = array(
+                        'type' => $filter->getFilterId() == 'brand' ? 'creator' : 'parameter',
+                        'name'  => implode(' ', $name),
+                        'url'   => $this->getUrl($filter->getFilterId()),
+                        'title' => $filter->getName(),
+                    );
+                    break;
+                case FilterEntity::TYPE_BOOLEAN:
+                    if (!is_array($value) || count($value) == 0) continue;
+                    foreach ($value as $v) {
+                        $return[] = array(
+                            'type'  => $filter->getFilterId() == 'brand' ? 'creator' : 'parameter',
+                            'name'  => $filter->getName() . ': ' . ($v == 1 ? 'да' : 'нет'),
+                            'url'   => $this->getUrl($filter->getFilterId(), $v),
+                            'title' => $filter->getName(),
+                        );
+                    }
+                    break;
+                case FilterEntity::TYPE_LIST:
+                    if (!is_array($value) || count($value) == 0) continue;
+                    foreach ($filter->getOption() as $option) {
+                        if (in_array($option->getId(), $value)) {
+                            $return[] = array(
+                                'type'  => $filter->getFilterId() == 'brand' ? 'creator' : 'parameter',
+                                'name'  => $option->getName(),
+                                'url'   => $this->getUrl($filter->getFilterId(), $option->getId()),
+                                'title' => $filter->getName(),
+                            );
+                        }
+                    }
+                    break;
+                default:
+                    continue;
+            }
+        }
+
+        return $return;
+    }
+
+    /**
+     * @param  int  $filterId
+     * @param  null $value
+     * @return string
+     */
+    private function getUrl($filterId, $value = null) {
+        $data = $this->productFilter->getValues();
+        if (array_key_exists($filterId, $data)) {
+            if (null == $value) {
+                unset($data[$filterId]);
+            } else foreach ($data[$filterId] as $k => $v) {
+                if ($v == $value) {
+                    unset($data[$filterId][$k]);
+                }
+            }
+        }
+
+        return $this->page->url('product.category', array(
+            'categoryPath'                  => $this->productFilter->getCategory()->getPath(),
+            \View\Product\FilterForm::$name => $data,
+        ));
+    }
+
+    /**
+     * @param $first
+     * @param $second
+     * @return bool
+     */
+    private function isEqualNumeric($first, $second) {
+        $first = $this->page->helper->clearZeroValue((float)$first);
+        $second = $this->page->helper->clearZeroValue((float)$second);
+
+        return $first == $second;
+    }
 }
