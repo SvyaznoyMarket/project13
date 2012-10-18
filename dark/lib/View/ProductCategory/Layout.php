9caf4719b71dc3c8eb8eb9d2159442e6d9f6bb45
green <green@mail.ru>
2012-10-18 09:41:34 +0400

    SITE-78 seo

--- dark/lib/View/ProductCategory/Layout.php
+++ dark/lib/View/ProductCategory/Layout.php
@@ -6,12 +6,21 @@ class Layout extends \View\DefaultLayout {
     public function prepare() {
         /** @var $category \Model\Product\Category\Entity */
         $category = $this->getParam('category') instanceof \Model\Product\Category\Entity ? $this->getParam('category') : null;
+        if (!$category) {
+            return;
+        }
+        /** @var $productPager \Iterator\EntityPager */
+        $productPager = $this->getParam('productPager') instanceof \Iterator\EntityPager ? $this->getParam('productPager') : null;
+        /** @var $regionName string */
+        $regionName = \App::user()->getRegion()->getName();
 
+        // content title
         if (!$this->getParam('title')) {
-            $this->setParam('title', $category ? $category->getName() : '');
+            $this->setParam('title', $category->getName());
         }
 
-        if (!$this->hasParam('breadcrumbs') && $category) {
+        // breadcrumbs
+        if (!$this->hasParam('breadcrumbs')) {
             $breadcrumbs = array();
             foreach ($category->getAncestor() as $ancestor) {
                 $breadcrumbs[] = array(
@@ -25,6 +34,49 @@ class Layout extends \View\DefaultLayout {
             );
 
             $this->setParam('breadcrumbs', $breadcrumbs);
+        }
+
+        // seo: page meta
+        if ($productPager && ($productPager->getPage() > 1)) {
+            $categoryNames = array();
+            foreach ($category->getAncestor() as $ancestor) {
+                $categoryNames[] = $ancestor->getName();
+            }
+            $categoryNames[] = $category->getName();
+
+            $this->setTitle(sprintf('%s - страница %d из %d - интернет-магазин Enter.ru - %s',
+                implode(' - ', $categoryNames),
+                $productPager->getPage(),
+                $productPager->getLastPage(),
+                $regionName
+            ));
+        } else {
+            // title
+            if (!$category->getSeoTitle()) {
+                $category->setSeoTitle(''
+                    . $category->getName()
+                    . ($category->getRoot() ? (' - ' . $category->getRoot()->getName()) : '')
+                    . ' - ' . $regionName
+                    . ' - ENTER.ru'
+                );
+            }
+            // description
+            if (!$category->getSeoDescription()) {
+                $category->setSeoDescription(''
+                    . $category->getName()
+                    . ' в ' . $regionName
+                    . ' с ценами и описанием.'
+                    . ' Купить в магазине Enter'
+                );
+            }
+            // keywords
+            if (!$category->getSeoKeywords()) {
+                $category->setSeoKeywords($category->getName() . ' магазин продажа доставка ' . $regionName . ' enter.ru');
+            }
+
+            $this->setTitle($category->getSeoTitle());
+            $this->addMeta('description', $category->getSeoDescription());
+            $this->addMeta('keywords', $category->getSeoKeywords());
         }
     }
 
