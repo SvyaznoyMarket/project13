3ec321200869fc0c4db8091da927b54d66297c65
Gray <ssapego@maxus.ru>
2012-10-18 17:44:37 +0400

    SITE-68 Перенос страницы товара на plain: подправлена работа с корзиной

--- dark/lib/Session/Cart.php
+++ dark/lib/Session/Cart.php
@@ -6,11 +6,39 @@ class Cart {
 
     /** @var string */
     private $sessionName = 'userCart';
-
+    /** @var \Http\Session */
     private $storage;
 
     public function __construct() {
         $this->storage = \App::session();
+        $session = $this->storage->all();
+
+        /**
+         * Если человек впервые - заводим ему пустую корзину
+         */
+        if(!array_key_exists($this->sessionName, $session)){
+            $this->storage->set($this->sessionName, array('productList' => array(), 'serviceList' => array(), 'warrantyList' => array()));
+            return;
+        }
+
+        if (!array_key_exists('productList', $session[$this->sessionName])) {
+            $data = $this->storage->get($this->sessionName);
+            $data['productList'] = array();
+            $this->storage->set($this->sessionName, $data);
+        }
+
+        if (!array_key_exists('serviceList', $session[$this->sessionName])) {
+            $data = $this->storage->get($this->sessionName);
+            $data['serviceList'] = array();
+            $this->storage->set($this->sessionName, $data);
+        }
+
+        if(!array_key_exists('warrantyList', $session[$this->sessionName])){
+            $data = $this->storage->get($this->sessionName);
+            $data['warrantyList'] = array();
+            $this->storage->set($this->sessionName, $data);
+        }
+
     }
 
     public function hasProduct($productId) {
