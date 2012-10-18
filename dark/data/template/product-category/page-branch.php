5b02f15291e04f99506563216fee0cda9e6355ef
green <green@mail.ru>
2012-10-18 12:24:38 +0400

    dark: doc fix

--- dark/data/template/product-category/page-branch.php
+++ dark/data/template/product-category/page-branch.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @var $page                    \View\Layout
+ * @var $page                    \View\ProductCategory\BranchPage
  * @var $category                \Model\Product\Category\Entity
  * @var $productFilter           \Model\Product\Filter
  * @var $productPagersByCategory \Iterator\EntityPager[]
