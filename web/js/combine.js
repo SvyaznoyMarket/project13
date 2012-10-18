b37a4284d38839cd9c052b387e043eeb45b600a2
Ivn Local <ikotov@maxus.ru>
2012-10-18 19:09:22 +0400

    suborders fix

--- web/js/combine.js
+++ web/js/combine.js
@@ -1,1 +1,2 @@
 window.filesWithVersion = {"app.order.v4.js":1350480427,"ports.js":1350480605,"app.oneclick.js":1350480410,"infopages.js":1342714085,"library.js":1350480427,"app.cart.js":1349447327,"bigjquery.min.js":1350480427,"welcome.js":1349783191,"main.js":1350480427,"dash.js":1350480427,"app.order.js":1349683659,"app.product.comment.list.js":1349436197,"app.product.js":1349783191,"app.shop.js":1350480410}
+
