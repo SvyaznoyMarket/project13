9d1b239623cb20c79a5833f6a1271d4145baed26
Alex Zaycev <13rentgen@gmail.com>
2012-10-19 19:18:12 +0400

    SITE-63 Рабочая версия новой анимации в каталоге

--- dark/data/template/product/show/_compact.php
+++ dark/data/template/product/show/_compact.php
@@ -13,89 +13,39 @@ $hasModel = (isset($hasModel) ? $hasModel : true) && $product->getModel() && (bo
 ?>
 
 <div class="goodsbox"<? if ($isHidden): ?> style="display:none;"<? endif ?>>
-
-    <div class="photo">
+    <div class="goodsbox__inner">
+    	<div class="photo">
+	        <a href="<?= $product->getLink() ?>">
+	            <? if (!empty($kit) && $kit->getCount()): ?>
+	                <div class="bLabelsQuantity" src="/images/quantity_shild.png"><?= $kit->getCount(); ?> шт.</div>
+	            <? endif ?>
+	
+	            <? if ($label = $product->getLabel()): ?>
+	                <img class="bLabels" src="<?= $label->getImageUrl() ?>" alt="<?= $label->getName() ?>"/>
+	            <? endif ?>
+	
+	            <img class="mainImg" src="<?= $product->getImageUrl(2) ?>" alt="<?= $product->getNameWithCategory() ?>" title="<?= $product->getNameWithCategory() ?>" width="160" height="160"/>
+	        </a>
+	    </div>
+	    <div class="goodsbox__rating rate<?= round($product->getRating())?>">
+	    	<div class="fill"></div>
+	    </div>
+	    <h3><a href="<?= $product->getLink() ?>"><?= $product->getName() ?></a></h3>
+		<? if ($product->getIsBuyable()): ?>
+        <div class="goodsbar mSmallBtns mR">
+            <?= $page->render('cart/_button', array('product' => $product)) ?>
+        </div>
+        <? endif ?>
+	    <div class="font18 pb10 mSmallBtns">
+            <span class="price"><?= $page->helper->formatPrice($product->getPrice()) ?></span> <span class="rubl">p</span>
+        </div>
+	    <? if ($hasModel): ?>
         <a href="<?= $product->getLink() ?>">
-            <? if (!empty($kit) && $kit->getCount()): ?>
-                <div class="bLabelsQuantity" src="/images/quantity_shild.png"><?= $kit->getCount(); ?> шт.</div>
-            <? endif ?>
-
-            <? if ($label = $product->getLabel()): ?>
-                <img class="bLabels" src="<?= $label->getImageUrl() ?>" alt="<?= $label->getName() ?>"/>
-            <? endif ?>
-
-            <img class="mainImg" src="<?= $product->getImageUrl(2) ?>" alt="<?= $product->getNameWithCategory() ?>" title="<?= $product->getNameWithCategory() ?>" width="160" height="160"/>
-        </a>
-    </div>
-
-    <?= str_repeat('<span class="ratingview" style="width:13px;vertical-align:middle;display:inline-block;"></span>', round($product->getRating())) ?>
-    <?= str_repeat('<span class="ratingview" style="width:13px;vertical-align:middle;display:inline-block;background-position:-51px 0;"></span>', 5 - round($product->getRating())) ?>
-
-    <h3><a href="<?= $product->getLink() ?>"><?= $product->getName() ?></a></h3>
-
-    <div class="font18 pb10 mSmallBtns">
-        <span class="price"><?= $page->helper->formatPrice($product->getPrice()) ?></span> <span class="rubl">p</span>
-    </div>
-
-    <? if ($hasModel): ?>
-    <a href="<?= $product->getLink() ?>">
-        <div class="bListVariants">
-            Доступно в разных вариантах<br>
-            (<?= $product->getModel()->getVariations() ?>)
-        </div>
-    </a>
-    <? endif ?>
-
-    <!-- Hover -->
-    <div class="boxhover"<? if ($product->getIsBuyable()): ?> ref="<?= $product->getToken() ?>"<? endif ?>>
-        <b class="rt"></b><b class="lb"></b>
-
-        <div class="rb">
-            <div class="lt" data-url="<?= $product->getLink() ?>">
-                <!--<a href="" class="fastview">Быстрый просмотр</a>-->
-                <div class="photo">
-                    <a href="<?= $product->getLink() ?>">
-                        <? if (!empty($kit) && $kit->getCount()): ?>
-                        <div class="bLabelsQuantity" src="/images/quantity_shild.png">
-                            <?= $kit->getCount(); ?> шт.
-                        </div>
-                        <? endif ?>
-
-                        <? if ($label = $product->getLabel()): ?>
-                            <img class="bLabels" src="<?= $label->getImageUrl() ?>" alt="<?= $label->getName() ?>"/>
-                        <? endif ?>
-
-                        <img class="mainImg" src="<?= $product->getImageUrl(2) ?>" alt="<?= $product->getNameWithCategory() ?>" title="<?= $product->getNameWithCategory() ?>" width="160" height="160"/>
-                    </a>
-                </div>
-
-                <?= str_repeat('<span class="ratingview" style="width:13px;vertical-align:middle;display:inline-block;"></span>', round($product->getRating())) ?>
-                <?= str_repeat('<span class="ratingview" style="width:13px;vertical-align:middle;display:inline-block;background-position:-51px 0;"></span>', 5 - round($product->getRating())) ?>
-
-                <h3><a href="<?= $product->getLink() ?>"><?= $product->getName() ?></a></h3>
-
-                <? if ($product->getIsBuyable()): ?>
-                <div class="goodsbar mSmallBtns mR">
-                    <?= $page->render('cart/_button', array('product' => $product)) ?>
-                </div>
-                <? endif ?>
-
-                <div class="font18 pb10 mSmallBtns">
-                    <span class="price"><?= $page->helper->formatPrice($product->getPrice()) ?></span> <span class="rubl">p</span>
-                </div>
-
-                <? if ($hasModel): ?>
-                <a href="<?= $product->getLink() ?>">
-                    <div class="bListVariants">
-                        Доступно в разных вариантах<br>
-                        (<?= $product->getModel()->getVariations() ?>)
-                    </div>
-                </a>
-                <? endif ?>
-
+            <div class="bListVariants">
+                Доступно в разных вариантах<br>
+                (<?= $product->getModel()->getVariations() ?>)
             </div>
-        </div>
+        </a>
+        <? endif ?>
     </div>
-    <!-- /Hover -->
-
 </div>
