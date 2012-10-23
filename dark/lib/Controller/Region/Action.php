9ca082a7be620e15d39ac9511cf2aa068962f42e
green <green@mail.ru>
2012-10-23 14:47:24 +0400

    dark: region change

--- dark/lib/Controller/Region/Action.php
+++ dark/lib/Controller/Region/Action.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Controller\User;
+namespace Controller\Region;
 
 class Action {
     public function init(\Http\Request $request) {
@@ -8,7 +8,22 @@ class Action {
     }
 
     public function change(\Http\Request $request) {
+        $regionId = (int)$request->get('region');
 
+        if (!$regionId) {
+            return new \Http\RedirectResponse(\App::router()->generate('homepage'));
+        }
+
+        $response = new \Http\RedirectResponse($request->headers->get('referer') ?: \App::router()->generate('homepage'));
+
+        $region = \RepositoryManager::getRegion()->getEntityById($regionId);
+        if (!$region) {
+            throw new \Exception\NotFoundException(sprintf('Region #%s not found', $regionId));
+        }
+
+        \App::user()->setRegion($region, $response);
+
+        return $response;
     }
 
     public function autocomplete(\Http\Request $request) {
