1b90b36add4e57f86f4b4ce4e8f4cb081794c369
Ivn Local <ikotov@maxus.ru>
2012-10-24 16:53:16 +0400

    popup buy botton fix, SITE-231

--- web/js/combine.js
+++ web/js/combine.js
@@ -8,7 +8,7 @@ window.filesWithVersion = {
 "bigjquery.min.js":1350480427,
 "welcome.js":1349783191,
 "main.js":1351064914,
-"dash.js":1351068695,
+"dash.js":1351083086,
 "app.order.js":1351076309,
 "app.product.comment.list.js":1349436197,
 "app.product.js":1351074932,
