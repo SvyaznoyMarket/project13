f53758de5ac96af315bade262c8a0686a7d47359
Ivn Local <ikotov@maxus.ru>
2012-10-25 10:30:06 +0400

    where to buy, IE, SITE-223

--- web/js/combine.js
+++ web/js/combine.js
@@ -1,9 +1,9 @@
 window.filesWithVersion = {
 "app.order.v4.js":1351086087,
 "ports.js":1351086087,
-"app.oneclick.js":1351086078,
+"app.oneclick.js":1351146483,
 "infopages.js":1342714085,
-"library.js":1350480427,
+"library.js":1351146274,
 "app.cart.js":1351086078,
 "bigjquery.min.js":1350480427,
 "welcome.js":1349783191,
