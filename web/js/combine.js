63e184ff439dc09f981e32999e1c4c0a8719332b
Alex Zaycev <13rentgen@gmail.com>
2012-10-26 13:02:52 +0400

    SITE-209 Навесил скрипт на слайдер

--- web/js/combine.js
+++ web/js/combine.js
@@ -1,1 +1,16 @@
-window.filesWithVersion = {"app.order.v4.js":1350893775,"ports.js":1350896095,"app.oneclick.js":1350893775,"infopages.js":1342714085,"library.js":1350480427,"app.cart.js":1350893775,"bigjquery.min.js":1350480427,"welcome.js":1349783191,"main.js":1350893936,"dash.js":1350893797,"app.order.js":1350893797,"app.product.comment.list.js":1349436197,"app.product.js":1350893775,"app.shop.js":1350480410}
+window.filesWithVersion = {
+"app.order.v4.js":1351166359,
+"ports.js":1351089985,
+"app.oneclick.js":1351166359,
+"infopages.js":1348130598,
+"library.js":1351166359,
+"app.cart.js":1351166359,
+"bigjquery.min.js":1350660911,
+"welcome.js":1350660911,
+"main.js":1351240783,
+"dash.js":1351242018,
+"app.order.js":1351166359,
+"app.product.comment.list.js":1348731365,
+"app.product.js":1351166359,
+"app.shop.js":1348130595
+}
--- web/js/dash.js
+++ web/js/dash.js
@@ -1,4 +1,18 @@
 $(document).ready(function(){
+	/*страница /catalog/ */
+	if($('.indexCatalog__slideMenu').length){
+		var slMenu = $('.indexCatalog__slideMenu');
+		var startVal = slMenu.offset().top;
+		$(window).scroll(function(){
+			if ($(window).scrollTop() > startVal){
+				slMenu.offset({top:$(window).scrollTop()})
+			}
+			else{
+				slMenu.removeAttr('style');
+			}
+		});
+	}
+	
 	var carturl = $('.lightboxinner .point2').attr('href')
 
 	var shortinfo = '/user/shortinfo'
