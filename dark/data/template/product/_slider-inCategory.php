01ae58d63e7b3e7a816ab51c87a83fd4481f369f
green <green@mail.ru>
2012-11-01 11:55:46 +0400

    SITE-111 изменил ссылки на категории с учетом фильтров

--- dark/data/template/product/_slider-inCategory.php
+++ dark/data/template/product/_slider-inCategory.php
@@ -1,25 +1,37 @@
 <?php
 /**
  * @var $page          \View\Layout
+ * @var $request       \Http\Request
  * @var $category      \Model\Product\Category\Entity
  * @var $pager         \Iterator\EntityPager
  * @var $product       \Model\Product\Entity
  * @var $itemsInSlider int
  */
+?>
+
+<?
 $view = $category->getHasLine() ? 'line' : 'compact';
+
+$categoryLink = $category->getLink();
+// если запрос содержит фильтры
+if ($filterData = $request->get(\View\Product\FilterForm::$name)) {
+    if (is_array($filterData) && !empty($filterData)) {
+        $categoryLink .= '?' . http_build_query($request->query->all());
+    }
+}
 ?>
 
 <!-- Carousel -->
 <div class="carouseltitle">
     <div class="rubrictitle">
         <h2>
-            <a href="<?= $category->getLink() ?>" class="underline"><?= $category->getName()?></a>
+            <a href="<?= $categoryLink ?>" class="underline"><?= $category->getName()?></a>
         </h2>
     </div>
 
     <? if ($pager->count() > 3) { ?>
     <div class="scroll">
-        <span><a href='<?= $category->getLink() ?>' class='srcoll_link'>посмотреть все</a></span>
+        <span><a href='<?= $categoryLink ?>' class='srcoll_link'>посмотреть все</a></span>
         <span class="jshm">( <?= $pager->count() ?> )</span>
         <a href="javascript:void(0)" data-url="<?= $page->url('product.category.slider', array('categoryPath' => $category->getPath())) ?>" class="srcoll_link_button back disabled" title="Предыдущие 3"></a>
         <a href="javascript:void(0)" data-url="<?= $page->url('product.category.slider', array('categoryPath' => $category->getPath())) ?>" class="srcoll_link_button forvard" title="Следующие 3"></a>
