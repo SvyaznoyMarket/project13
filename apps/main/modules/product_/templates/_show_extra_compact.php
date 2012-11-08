584059ecbc293482d99f30b0eddf9c7546d842df
Gray <ssapego@maxus.ru>
2012-11-08 19:00:39 +0300

    SITE-287 добавлены трекеры GA для слайдеров SE

--- apps/main/modules/product_/templates/_show_extra_compact.php
+++ apps/main/modules/product_/templates/_show_extra_compact.php
@@ -4,25 +4,27 @@
  * @var $ii
  * @var $maxPerPage
  * @var $marker string
+ * @var $gaEvent   string
  */
 
 $title = $item->getName();
 if($main = $item->getMainCategory())
   $title .= ' - '.$main->getName();
 $marker = isset($marker) ? $marker : null;
+$gaEvent = isset($gaEvent) ? $gaEvent : null;
 ?>
 
 <div class="goodsbox"<?php echo (isset($ii) && $ii > $maxPerPage) ? ' style="display:none;"' : '' ?>>
     <div class="goodsbox__inner" ref="<?php echo $item->getToken() ?>">
     	<div class="photo">
-	        <a href="<?php echo $item->getLink().(!empty($marker) ? $marker : '') ?>">
+	        <a href="<?php echo $item->getLink().(!empty($marker) ? $marker : '') ?>"<?php if (!empty($gaEvent)) echo ' data-event="'.$gaEvent.'" class="gaEvent"'; ?>>
       			<img class="mainImg" src="<?php echo $item->getMediaImageUrl() ?>" alt="<?php echo $title ?>" title="<?php echo $title ?>" width="119" height="120"/>
 			</a>
 	    </div>
 		<div class="goodsbox__rating rate<?= round($item->getRating())?>">
 	    	<div class="fill"></div>
 	    </div>
-	    <h3><a href="<?php echo $item->getLink().(!empty($marker) ? $marker : '') ?>"><?php echo $item->getName() ?></a></h3>
+	    <h3><a href="<?php echo $item->getLink().(!empty($marker) ? $marker : '') ?>"<?php if (!empty($gaEvent)) echo ' data-event="'.$gaEvent.'" class="gaEvent"'; ?>><?php echo $item->getName() ?></a></h3>
 		<div class="goodsbar mSmallBtns mR">
 			<?php render_partial('cart_/templates/_buy_button.php', array('item' => $item)) ?>
 		</div>
