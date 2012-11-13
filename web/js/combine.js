7c8c2f9a3dd9039be945674a4f7823dd248d1205
Gray <ssapego@maxus.ru>
2012-11-13 16:21:53 +0300

    SITE-287 добаисправлен трекер клика кнопки купить

--- web/js/combine.js
+++ web/js/combine.js
@@ -1,16 +1,16 @@
 window.filesWithVersion = {
-"app.order.v4.js":1352386065,
+"app.order.v4.js":1352469156,
 "ports.js":1352187111,
-"app.oneclick.js":1352386065,
-"infopages.js":1341404096,
-"library.js":1352386065,
-"app.cart.js":1352386065,
-"bigjquery.min.js":1352386065,
+"app.oneclick.js":1352469156,
+"infopages.js":1352727803,
+"library.js":1352812826,
+"app.cart.js":1352469156,
+"bigjquery.min.js":1352469156,
 "welcome.js":1350560037,
-"main.js":1352391940,
-"dash.js":1352386065,
+"main.js":1352812560,
+"dash.js":1352469156,
 "app.order.js":1352383851,
 "app.product.comment.list.js":1349186382,
 "app.product.js":1351857261,
-"app.shop.js":1352386065
+"app.shop.js":1352469156
 }
