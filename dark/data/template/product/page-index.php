b930741b203e8cd4d8d7437e8a4947ef7331518c
Alex Zaycev <13rentgen@gmail.com>
2012-11-19 16:21:02 +0400

    SITE-52 Закрыл теги img в карточке товара

--- dark/data/template/product/page-index.php
+++ dark/data/template/product/page-index.php
@@ -393,7 +393,7 @@
 <div id="ajaxgoods" class="popup width230" style="display: none">
   <div  style="padding: 15px 15px 20px 5px">
     <div class="bNavLoader__eIco">
-      <img src="/images/ajar.gif">
+      <img src="/images/ajar.gif" />
     </div>
     <div class="bNavLoader__eM">
       <p class="bNavLoader__eText">Подождите немного</p>
