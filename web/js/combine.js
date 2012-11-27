2eb557192add9fb497774d2fd93fd04bdc8b6dcf
Gray <ssapego@maxus.ru>
2012-11-27 17:05:41 +0300

    SITE-388 Убрал дебаг

--- web/js/combine.js
+++ web/js/combine.js
@@ -1,16 +1,16 @@
 window.filesWithVersion = {
-"app.order.v4.js":1353932793,
+"app.order.v4.js":1354024122,
 "ports.js":1352187111,
-"app.oneclick.js":1353932793,
+"app.oneclick.js":1354024122,
 "infopages.js":1352727803,
-"library.js":1353932793,
-"app.cart.js":1353932793,
-"bigjquery.min.js":1353932793,
+"library.js":1354024122,
+"app.cart.js":1354024122,
+"bigjquery.min.js":1354024122,
 "welcome.js":1350560037,
-"main.js":1353932793,
-"dash.js":1354019703,
+"main.js":1354024122,
+"dash.js":1354025102,
 "app.order.js":1352383851,
 "app.product.comment.list.js":1349186382,
 "app.product.js":1351857261,
-"app.shop.js":1353932793
+"app.shop.js":1354024122
 }
