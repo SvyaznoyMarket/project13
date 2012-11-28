5d195551a9c03c7b5b7bc2398adb72865aacb653
green <green@mail.ru>
2012-11-28 13:04:46 +0400

    SITE-383 Поправить неправильное окончание товара/товаров в счетчике товаров

--- dark/data/template/search/_title.php
+++ dark/data/template/search/_title.php
@@ -12,11 +12,11 @@
 
     <? if (false == $forceMean) { // если принудительный поиск не был использован ?>
         Ура! Нашли <span class="orange">&quot;<?= $searchQuery ?>&quot;</span>
-        <?= $page->helper->formatNumberChoice('{n: n > 10 && n < 20}%count% товаров|{n: n % 10 == 1}%count% товар|{n: n % 10 > 1 && n % 10 < 5}%count% товара|(1,+Inf]%count% товаров', array('%count%' => $count), $count) ?>
+        <?= $count . ' ' . $page->helper->numberChoice($count, array('товар', 'товара', 'товаров')) ?>
 
         <? } else { // ...иначе, если принудительный поиск использован ?>
         Вы искали <span class="orange">&quot;<?= $meanQuery ?>&quot;</span> ?<br />
-        Мы нашли <?= $page->helper->formatNumberChoice('{n: n > 10 && n < 20}%count% товаров|{n: n % 10 == 1}%count% товар|{n: n % 10 > 1 && n % 10 < 5}%count% товара|(1,+Inf]%count% товаров', array('%count%' => $count), $count) ?> :)
+        Мы нашли <?= $count . ' ' . $page->helper->numberChoice($count, array('товар', 'товара', 'товаров')) ?> :)
 
         <p class="font16" style="font-family: Tahoma;"><strong><?= $searchQuery ?></strong> мы не нашли. Уточните, пожалуйста, запрос</p>
     <? } ?>
