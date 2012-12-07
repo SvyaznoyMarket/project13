8841beeee4d7ff84d50b88220c0ea8c03ac86c8a
Alex Zaycev <13rentgen@gmail.com>
2012-12-07 16:38:07 +0400

    SITE-438 Переделана форма с выбором города

--- dark/data/template/_regionSelection.php
+++ dark/data/template/_regionSelection.php
@@ -16,34 +16,113 @@ for ($i = 0; $i < $rowCount; $i++) {
 }
 ?>
 
-<div class="popup popupRegion" style="display:none">
+<div class="popup popupRegion clearfix" style="display:none">
     <a href="#" class="close">Закрыть</a>
-
-    <h2 class="pouptitle">Привет! Укажите, из какого вы города.</h2>
-
-    <div class="hidden">
-        <p>скрытый блок</p>
-    </div>
+    <h2 class="popuptitle">Из какого вы города?</h2>
+    <p class="font16">Цены товаров и доставки зависят от региона.</p>
     <form class="ui-css">
-        <input id="jscity" data-url-autocomplete="<?= $page->url('region.autocomplete') ?>" placeholder="Введите свой город" class="bBuyingLine__eText mInputLong"/>
+        <input id="jscity" data-url-autocomplete="<?= $page->url('region.autocomplete') ?>" placeholder="Введите свой город" class="bBuyingLine__eText font18"/>
         <a class="inputClear" href="#">x</a>
         <input id="jschangecity" type="submit" value="Сохранить" class="button bigbutton mDisabled"/>
 
         <div id="jscities" style="position:relative"></div>
     </form>
-    <div class="cityInline">
-        <a href="<?= $page->url('region.change', array('regionId' => 14974)) ?>">Москва</a>
-        <a href="<?= $page->url('region.change', array('regionId' => 108136)) ?>">Санкт-Петербург</a>
+    <div class="cityInline font14 clearfix">
+        <div class="cityItem"><a href="<?= $page->url('region.change', array('regionId' => 14974)) ?>">Москва</a></div>
+        <div class="cityItem"><a href="<?= $page->url('region.change', array('regionId' => 108136)) ?>">Санкт-Петербург</a></div>
     </div>
-
-    <? $offset = 0; foreach ($columnCount as $count): ?>
-        <div class="colomn">
-            <? for ($i = 0; $i < $count; $i++) { ?>
-            <? $region = $regions[$offset + $i] ?>
-            <a href="<?= $page->url('region.change', array('regionId' => $region->getId())) ?>"><?= $region->getName(); ?></a>
-            <? } ?>
+    <div class="BlackArrow fl leftArr"></div>
+    <div class="regionSlidesWrap fl">
+        <div class="regionSlides">
+            <!-- 2 слайда по 4 колонки -->
+            <div class="regionSlides_slide">
+                <? $offset = 0; foreach ($columnCount as $count): ?>
+                    <div class="colomn font14">
+                        <? for ($i = 0; $i < $count; $i++) { ?>
+                        <? $region = $regions[$offset + $i] ?>
+                        <a href="<?= $page->url('region.change', array('regionId' => $region->getId())) ?>"><?= $region->getName(); ?></a>
+                        <? } ?>
+                    </div>
+                <? $offset += $count; endforeach ?>
+                <div class="colomn font14 clearfix">
+                    <a href="/region/change/119597">Батайск</a>
+                    <a href="/region/change/13241">Белгород</a>
+                    <a href="/region/change/83210">Брянск</a>
+                    <a href="/region/change/96423">Владимир</a>
+                    <a href="/region/change/18074">Воронеж</a>
+                    <a href="/region/change/1965">Долгопрудный</a>
+                    <a href="/region/change/3323">Егорьевск</a>
+                    <a href="/region/change/93751">Екатеринбург</a>
+                    <a href="/region/change/58490">Елец</a>
+                    <a href="/region/change/93747">Иваново</a>
+                    <a href="/region/change/1922">Климовск</a>
+                    <a href="/region/change/124190">Краснодар</a>
+                    <a href="/region/change/74562">Курск</a>
+                </div>
+            </div>
+            <div class="regionSlides_slide">
+                <div class="colomn font14 clearfix">
+                    <a href="/region/change/119597">Батайск</a>
+                    <a href="/region/change/13241">Белгород</a>
+                    <a href="/region/change/83210">Брянск</a>
+                    <a href="/region/change/96423">Владимир</a>
+                    <a href="/region/change/18074">Воронеж</a>
+                    <a href="/region/change/1965">Долгопрудный</a>
+                    <a href="/region/change/3323">Егорьевск</a>
+                    <a href="/region/change/93751">Екатеринбург</a>
+                    <a href="/region/change/58490">Елец</a>
+                    <a href="/region/change/93747">Иваново</a>
+                    <a href="/region/change/1922">Климовск</a>
+                    <a href="/region/change/124190">Краснодар</a>
+                    <a href="/region/change/74562">Курск</a>
+                </div>
+                <div class="colomn font14 clearfix">
+                    <a href="/region/change/119597">Батайск</a>
+                    <a href="/region/change/13241">Белгород</a>
+                    <a href="/region/change/83210">Брянск</a>
+                    <a href="/region/change/96423">Владимир</a>
+                    <a href="/region/change/18074">Воронеж</a>
+                    <a href="/region/change/1965">Долгопрудный</a>
+                    <a href="/region/change/3323">Егорьевск</a>
+                    <a href="/region/change/93751">Екатеринбург</a>
+                    <a href="/region/change/58490">Елец</a>
+                    <a href="/region/change/93747">Иваново</a>
+                    <a href="/region/change/1922">Климовск</a>
+                    <a href="/region/change/124190">Краснодар</a>
+                    <a href="/region/change/74562">Курск</a>
+                </div>
+                <div class="colomn font14 clearfix">
+                    <a href="/region/change/119597">Батайск</a>
+                    <a href="/region/change/13241">Белгород</a>
+                    <a href="/region/change/83210">Брянск</a>
+                    <a href="/region/change/96423">Владимир</a>
+                    <a href="/region/change/18074">Воронеж</a>
+                    <a href="/region/change/1965">Долгопрудный</a>
+                    <a href="/region/change/3323">Егорьевск</a>
+                    <a href="/region/change/93751">Екатеринбург</a>
+                    <a href="/region/change/58490">Елец</a>
+                    <a href="/region/change/93747">Иваново</a>
+                    <a href="/region/change/1922">Климовск</a>
+                    <a href="/region/change/124190">Краснодар</a>
+                    <a href="/region/change/74562">Курск</a>
+                </div>
+                <div class="colomn font14 clearfix">
+                    <a href="/region/change/119597">Батайск</a>
+                    <a href="/region/change/13241">Белгород</a>
+                    <a href="/region/change/83210">Брянск</a>
+                    <a href="/region/change/96423">Владимир</a>
+                    <a href="/region/change/18074">Воронеж</a>
+                    <a href="/region/change/1965">Долгопрудный</a>
+                    <a href="/region/change/3323">Егорьевск</a>
+                    <a href="/region/change/93751">Екатеринбург</a>
+                    <a href="/region/change/58490">Елец</a>
+                    <a href="/region/change/93747">Иваново</a>
+                    <a href="/region/change/1922">Климовск</a>
+                    <a href="/region/change/124190">Краснодар</a>
+                    <a href="/region/change/74562">Курск</a>
+                </div>
+            </div>
         </div>
-    <? $offset += $count; endforeach ?>
-
-    <div class="clear"></div>
+    </div>
+    <div class="BlackArrow fl rightArr"></div>
 </div>
