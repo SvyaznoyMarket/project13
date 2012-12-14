01e2dc02a01428bce3737df649c9f4e3167832fb
Alex Zaycev <13rentgen@gmail.com>
2012-12-14 18:01:27 +0400

    SITE-480 Убраны поп-апы вызываемые ошибками ajax. Теперь они логируются в GA

--- web/js/combine.js
+++ web/js/combine.js
@@ -1,16 +1,16 @@
 window.filesWithVersion = {
-"app.order.v4.js":1355314881,
-"ports.js":1354786359,
-"app.oneclick.js":1355314881,
+"app.order.v4.js":1355492972,
+"ports.js":1355492957,
+"app.oneclick.js":1355492972,
 "infopages.js":1351763597,
 "library.js":1355413418,
 "app.cart.js":1354099186,
 "bigjquery.min.js":1354085035,
 "welcome.js":1351500454,
-"main.js":1355413418,
-"dash.js":1354609639,
-"app.order.js":1355483850,
+"main.js":1355493321,
+"dash.js":1355491283,
+"app.order.js":1355492972,
 "app.product.comment.list.js":1351763596,
-"app.product.js":1355143273,
+"app.product.js":1355492972,
 "app.shop.js":1355413418
 }
