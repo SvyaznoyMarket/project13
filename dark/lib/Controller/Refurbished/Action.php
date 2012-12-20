fd53177e428a179d022392f29274b87387bcf100
Gray <ssapego@maxus.ru>
2012-12-20 16:46:42 +0400

    SITE-342 добавлена форма подписки

--- dark/lib/Controller/Refurbished/Action.php
+++ dark/lib/Controller/Refurbished/Action.php
@@ -3,6 +3,8 @@ namespace Controller\Refurbished;
 
 class Action {
 
+    private $channelId = 3;
+
     public function execute(\Http\Request $request) {
         \App::logger()->debug('Exec ' . __METHOD__);
 
@@ -37,8 +39,30 @@ class Action {
 
 
         if ($form->isValid()) {
-            $response = array('success' => true);
-            return new \Http\JsonResponse($response);
+            try {
+                $name = explode(" ", $form->getName());
+                $response = \App::coreClientV2()->query('user/callback-create', array(), array(
+                    'channel_id' => $this->channelId,
+                    'first_name' => isset($name[0]) ? $name[0] : null,
+                    'last_name' =>  isset($name[1]) ? $name[1] : null,
+                    'email' => $form->getEmail(),
+                    'theme' => 'Подписка на уцененные товары',
+                    'text' => $form->getName() . ' хочет получать списки уцененных товаров на адрес ' . $form->getEmail(),
+                ));
+
+                if (!isset($response['confirmed']) || !$response['confirmed']) {
+                    throw new \Exception('Не удалось сохранить форму');
+                }
+
+                $response = array('success' => true);
+                return new \Http\JsonResponse($response);
+            } catch (\Exception $e) {
+                \App::exception()->remove($e);
+                \App::logger()->error($e);
+
+                $form->setError('global', 'Не удалось сохранить форму');
+            }
+
         }
 
         $response = array(
