5c1fad0bb3c1d0ad0a30d4624fda4316d9e6f5b0
Alex Zaycev <13rentgen@gmail.com>
2012-12-20 18:25:02 +0400

    SITE-275 Убрано плавное перемещение карты при переходе к другому городу

--- web/js/combine.js
+++ web/js/combine.js
@@ -1,16 +1,16 @@
 window.filesWithVersion = {
-"app.order.v4.js":1355928392,
+"app.order.v4.js":1356012602,
 "ports.js":1355928392,
 "app.oneclick.js":1355993654,
 "infopages.js":1351763597,
-"library.js":1355922727,
+"library.js":1356013178,
 "app.cart.js":1354099186,
 "bigjquery.min.js":1354085035,
-"welcome.js":1355928398,
-"main.js":1355928398,
-"dash.js":1355491283,
-"app.order.js":1355998565,
+"welcome.js":1356012602,
+"main.js":1356012611,
+"dash.js":1356012602,
+"app.order.js":1356012602,
 "app.product.comment.list.js":1351763596,
 "app.product.js":1355492972,
-"app.shop.js":1355922727
+"app.shop.js":1356013185
 }
