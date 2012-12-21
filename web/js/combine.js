053e4edaeefaf5cea96ff16146fadf6bfd9130a5
Alex Zaycev <13rentgen@gmail.com>
2012-12-21 17:19:59 +0400

    SITE-501 При выборе конкретного города остальные города скрываются. Выбранный город прилипает к верху и не прокручивается

--- web/js/combine.js
+++ web/js/combine.js
@@ -1,16 +1,16 @@
 window.filesWithVersion = {
-"app.order.v4.js":1356012602,
+"app.order.v4.js":1356085967,
 "ports.js":1355928392,
 "app.oneclick.js":1355993654,
 "infopages.js":1351763597,
-"library.js":1356013178,
+"library.js":1356080304,
 "app.cart.js":1354099186,
 "bigjquery.min.js":1354085035,
-"welcome.js":1356012602,
-"main.js":1356012611,
-"dash.js":1356012602,
-"app.order.js":1356012602,
+"welcome.js":1356085967,
+"main.js":1356085967,
+"dash.js":1356085967,
+"app.order.js":1356080304,
 "app.product.comment.list.js":1351763596,
 "app.product.js":1355492972,
-"app.shop.js":1356013185
+"app.shop.js":1356095656
 }
