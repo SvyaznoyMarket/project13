5162423d28085e347bd7d483531da4a08c18c2b6
Gray <ssapego@maxus.ru>
2012-12-25 19:32:13 +0400

    SITE-533 В списке услуг отображается только min цена

--- dark/data/template/service/page-category.php
+++ dark/data/template/service/page-category.php
@@ -40,10 +40,10 @@
                             <? else: ?>
                                 &mdash; <strong>бесплатно</strong>
                             <? endif ?>
-                        </div>
-                        <? elseif (!is_null($service->getPricePercent())): ?>
-                            <div class="font12 mInlineBlock">
-                                &mdash; <strong><?= $service->getPricePercent() ?>%</strong> от стоимости товара<?php if (!is_null($service->getPriceMin())): ?>, но не менее <strong><span class="price"><?= $page->helper->formatPrice($service->getPriceMin()) ?></span> <span class="rubl">p</span></strong><?php endif ?>
+                            </div>
+                        <? elseif (!is_null($service->getPriceMin())): ?>
+                            <div class="font16 mInlineBlock">
+                                &mdash; от <strong><?= $page->helper->formatPrice($service->getPriceMin()) ?>&nbsp;<span class="rubl">p</span></strong>
                             </div>
                         <? endif ?>
                     </div>
