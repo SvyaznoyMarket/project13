1357e5014f65efd46a93e1fb495c901d4790a970
green <green@mail.ru>
2013-01-17 14:21:39 +0400

    SITE-607 Стили при оформлении кредита - Ренесcанс

--- dark/lib/View/Order/CompletePage.php
+++ dark/lib/View/Order/CompletePage.php
@@ -4,6 +4,16 @@ namespace View\Order;
 
 class CompletePage extends Layout {
     public function prepare() {
+        $orders = is_array($this->getParam('orders')) ? $this->getParam('orders') : array();
+        /** @var $order \Model\Order\Entity */
+        $order = reset($orders);
+        if ($order) {
+            // если банк Ренессанс
+            if (($order->getCredit() instanceof \Model\Order\Credit\Entity) && (\Model\CreditBank\Entity::PROVIDER_DIRECT_CREDIT == $order->getCredit()->getBankProviderId())) {
+                $this->addStylesheet('http://direct-credit.ru/widget/style.css');
+            }
+        }
+
         $this->setTitle('Оформление заказа - Enter');
     }
 
