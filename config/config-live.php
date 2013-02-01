ce240eadbe236b0026d28a8079da00d705f98a68
green <green@mail.ru>
2013-02-01 18:13:53 +0400

    config

--- config/config-live.php
+++ config/config-live.php
@@ -9,7 +9,7 @@ $c->coreV2['url'] = 'http://api.enter.ru/v2/';
 
 $c->wordpress['url'] = 'http://content.enter.ru/';
 
-$c->dataStore['url'] = 'http://api.enter.ru/';
+$c->dataStore['url'] = 'http://www.enter.ru/';
 
 $c->analytics['enabled'] = true;
 $c->googleAnalytics['enabled'] = true;
