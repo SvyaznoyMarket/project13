a60d8a4fa9af3362610b06a351b3f11225f3437e
Alex Zaycev <13rentgen@gmail.com>
2013-02-12 18:43:59 +0400

    SITE-705 Сверстал лэндинг для регистрации

--- main/template/user/page-login.php
+++ main/template/user/page-login.php
@@ -5,16 +5,61 @@
  */
 ?>
 
-<div class="registerbox">
+<div class="bPageLogin clearfix">
+	<div class="bPageLogin_eLogo fl"></div>
+	<div class="bPageLogin_eContent bLoginPageForms fl">
+		<h1 class="bLoginPageForms_eTitle">хотите быть в курсе новостей от enter?</h1>
+		<div class="clear"></div>
+		<p class="bLoginPageForms_eDesc">Зарегистрируйтесь и получайте актуальную информацию<br/>
+			о новых поступлениях, акциях и распродажах!</p>
+		
+		<form method="post" class="form" action="/register" id="register-form">
+			<input type="hidden" value="/login" name="redirect_to">
 
-    <?= $page->render('form-forgot') ?>
+			<div class="bLoginForm width327 fl">
+				<h2 class="bLoginForm_eTitle">Я новый пользователь</h2>
+				<a class="bLoginForm_eUr orange underline" href="/corporate-register">Регистрация юридического лица</a>
+				<div class="pb5">Как к вам обращаться?</div>
+				<div class="pb5">
+					<input type="text" tabindex="5" value="" name="register[first_name]" class="text width315 mb10" id="register_first_name" required="required">
+				</div>
+				<div class="pb5 clearfix">
+					<span class="registerAnotherWay">Ваш e-mail</span>:<a href="#" class="registerAnotherWayBtn font10 fr">У меня нет e-mail</a>
+				</div>
+				<div class="pb5">
+					<span class="registerPhonePH">+7</span>
+					<input type="text" tabindex="6" value="" name="register[username]" class="text width315 mb10" id="register_username" required="required">
+				</div>
+				<label class="bSubscibe fl checked" style="display: inline;">
+					<b></b> Хочу знать об интересных<br>предложениях
+					<input type="checkbox" checked="checked" class="subscibe hiddenCheckbox" autocomplete="off" value="1" name="subscribe">
+				</label>
+				<input type="submit" tabindex="10" value="Регистрация" class="fr button bigbutton mDisabled">
+				<div class="clear"></div>
+				<p>Нажимая кнопку «Регистрация», я подтверждаю свое согласие с <a href="/terms" target="_blank" class="underline">Условиями продажи...</a></p>
+			</div>
+			<img class="fl mr20" src="http://content.enter.ru/wp-content/uploads/2013/02/loginFormImg.jpg"/>
+		</form>
+		<div class="clear"></div>
+		<a id="hideLoginform" class="font18 dashed" href="#">У меня уже есть логин и пароль</a>
+		<form id="loginForm" method="post" class="form hf" action="/login" id="login-form">
+			<input type="hidden" value="http://Developer:dEl23sTOas@sasha.ent3.ru/login" name="redirect_to">
+			<div class="width327 bLoginForm clearfix">
+				<h2 class="bLoginForm_eTitle">У меня есть логин и пароль</h2>
+				<div class="pb5">E-mail или мобильный телефон:</div>
+				<div class="pb5">
+					<input type="text" name="signin[username]" value="" tabindex="1" class="text width315 mb10" id="signin_username" required="required">
+				</div>
+				<div class="pb5">
+					<a class="fr orange underline" href="/request-password" id="forgot-pwd-trigger">Забыли пароль?</a>
+					Пароль:
+				</div>
+				<div class="pb5">
+					<input type="password" tabindex="2" name="signin[password]" class="text width315 mb10" id="signin_password" required="required">
+				</div>
+				<input type="submit" tabindex="3" value="Войти" class="fr button bigbutton">
+			</div>
+		</form>
 
-    <?= $page->render('form-login', array('form' => $form instanceof \View\User\LoginForm ? $form : null)) ?>
-
-    <?= $page->render('form-register', array('form' => $form instanceof \View\User\RegistrationForm ? $form : null)) ?>
-
-    <div class="clear"></div>
+	</div>
 </div>
-
-
-<br />
--- main/view/User/LoginPage.php
+++ main/view/User/LoginPage.php
@@ -3,7 +3,7 @@
 namespace View\User;
 
 class LoginPage extends \View\DefaultLayout {
-    protected $layout = 'layout-oneColumn';
+    protected $layout = 'layout-login';
 
     public function prepare() {
         $this->setTitle('Авторизация - Enter');
@@ -22,5 +22,20 @@ class LoginPage extends \View\DefaultLayout {
 
     public function slotBodyDataAttribute() {
         return 'infopage';
+    }
+    
+    public function slotFooter() {
+        $client = \App::contentClient();
+
+        try {
+            $response = $client->query('footer_compact');
+        } catch (\Exception $e) {
+            \App::exception()->add($e);
+            \App::logger()->error($e);
+
+            $response = array('content' => '');
+        }
+
+        return $response['content'];
     }
 }
