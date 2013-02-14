ea2fba267b106b948189ad45577b9d9685d55264
Alex Zaycev <13rentgen@gmail.com>
2013-02-14 12:43:59 +0400

    SITE-705 Лэйаут для лэндинга регистрации и модуль со стилями

---
+++ main/template/layout-login.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @var $page \View\DefaultLayout
+ */
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <?= $page->slotMeta() ?>
+    <title><?= $page->getTitle() ?></title>
+    <link rel="shortcut icon" href="/favicon.ico"/>
+    <?= $page->slotStylesheet() ?>
+    <?= $page->slotRelLink() ?>
+    <?= $page->slotGoogleAnalytics() ?>
+    <?= $page->slotMetaOg() ?>
+</head>
+<body class="<?= $page->slotBodyClassAttribute() ?>" data-template="<?= $page->slotBodyDataAttribute() ?>" data-id="<?= \App::$id ?>">
+    <div class="allpage" id="page">
+        <div class="adfoxWrapper" id="adfoxbground"></div>
+
+        <div class="allpageinner">
+
+            <?= $page->slotContent() ?>
+
+            <div class="clear"></div>
+        </div>
+        <div class="clear"></div>
+    </div>
+
+    <?= $page->render('order/_footer') ?>
+    <?= $page->slotFooter() ?>
+
+    <?= $page->slotRegionSelection() ?>
+    <?= $page->slotJavascript() ?>
+    <?= $page->slotInnerJavascript() ?>
+    <?= $page->slotAuth() ?>
+    <?= $page->slotYandexMetrika() ?>
+    <?= $page->slotAdvanceSeoCounter() ?>
+    <?= $page->slotMyThings() ?>
+
+    <? if (\App::config()->analytics['enabled']): ?>
+        <div id="gooReMaCategories" class="jsanalytics"></div>
+        <div id="luxupTracker" class="jsanalytics"></div>
+    <? endif ?>
+  
+    <?= $page->slotAdriver() ?>
+  
+</body>
+</html>
