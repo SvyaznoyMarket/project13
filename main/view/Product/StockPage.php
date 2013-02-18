70f7251277da22b1346339e99d680047f1de22a4
Gray <ssapego@maxus.ru>
2013-02-18 17:59:24 +0400

    SITE-710 добавлен ретаргетинг на некоторые страницы карточки товара

--- main/view/Product/StockPage.php
+++ main/view/Product/StockPage.php
@@ -50,4 +50,18 @@ class StockPage extends \View\DefaultLayout {
     public function slotBodyDataAttribute() {
         return 'product_stock';
     }
+
+    public function slotInnerJavascript() {
+        /** @var $product \Model\Product\Entity */
+        $product = $this->getParam('product') instanceof \Model\Product\Entity ? $this->getParam('product') : null;
+        $categories = $product->getCategory();
+        $category = array_pop($categories);
+
+        return ''
+            . ($product ? $this->render('product/_odinkod', array('product' => $product)) : '')
+            . "\n\n"
+            . (bool)$product ? $this->render('_remarketingGoogle', ['tag_params' => ['prodid' => $product->getId(), 'pagetype' => 'product', 'pname' => $product->getName(), 'pcat' => ($category) ? $category->getToken() : '', 'value' => $product->getPrice()]]) : ''
+            . "\n\n"
+            . $this->render('_innerJavascript');
+    }
 }
