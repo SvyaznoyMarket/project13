ebc38bf2774acba17ca6370148433ac991e1d8be
Alex Zaycev <13rentgen@gmail.com>
2013-02-25 12:52:44 +0400

    SITE-771 Немного изменил верстку блока карт F1

--- main/template/cart/form-certificate.php
+++ main/template/cart/form-certificate.php
@@ -4,18 +4,22 @@
  * @var $user \Session\User
  */
 ?>
-
-<? if ((bool)$user->getCart()->getCertificates()): ?>
-    <? // TODO: верстка примененного сертификата ?>
-
-<? else: ?>
-    <div class="bF1SaleCard">
-        <div class="pl35">
+<div class="clear"></div>
+    <div class="bF1SaleCard">	
+		<div class="pl35">
             <h3 class="bF1SaleCard_eTitle ">Скидка по карте «Под защитой F1»</h3>
-            <p class="font11">Введите серийный номер карты «Под защитой F1» для скидки на услуги:</p>
-            <input class="mr20 width370" type="text"/><input class="yellowbutton button" type="button" value="Получить скидку"/>
+            <? if ((bool)$user->getCart()->getCertificates()): ?>
+		    <? // TODO: верстка примененного сертификата ?>
+		    <div class="bF1SaleCard_eComplete">
+		    	
+		    </div>
+			<? else: ?>
+			<div class="bF1SaleCard_eForm">
+	            <p class="font11">Введите серийный номер карты «Под защитой F1» для скидки на услуги:</p>
+	            <input class="mr20 width370" type="text"/><input class="yellowbutton button" type="button" value="Получить скидку"/>
+	        </div>
+	         <? endif ?>
         </div>
         <div class="line mt32 pb30"></div>
     </div>
 
-<? endif ?>
