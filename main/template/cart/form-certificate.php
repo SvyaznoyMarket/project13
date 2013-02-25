e524d12498b5dc4015012082b53cfa6e12b0e12b
Alex Zaycev <13rentgen@gmail.com>
2013-02-25 13:28:56 +0400

    SITE-771 Сделал верстку активированной карты

--- main/template/cart/form-certificate.php
+++ main/template/cart/form-certificate.php
@@ -10,9 +10,9 @@
             <h3 class="bF1SaleCard_eTitle ">Скидка по карте «Под защитой F1»</h3>
             <? if ((bool)$user->getCart()->getCertificates()): ?>
 		    <? // TODO: верстка примененного сертификата ?>
-		    <div class="bF1SaleCard_eComplete">
-		    	
-		    </div>
+			<div class="bF1SaleCard_eComplete mGold">
+				<p class="font18">Ваша скидка на услуги F1 — <b>10%, 90р</b></p>
+			</div>
 			<? else: ?>
 			<div class="bF1SaleCard_eForm">
 	            <p class="font11">Введите серийный номер карты «Под защитой F1» для скидки на услуги:</p>
@@ -22,4 +22,3 @@
         </div>
         <div class="line mt32 pb30"></div>
     </div>
-
