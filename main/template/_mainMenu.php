aa5c7ecbf0e17631da0627d1544aa6f29af259ec
green <green@mail.ru>
2013-02-27 15:37:44 +0400

    resolve conflicts

--- main/template/_mainMenu.php
+++ main/template/_mainMenu.php
@@ -8,11 +8,23 @@
 
 <?
 $level = isset($level) ? $level : 1;
+$count = count($menu);
 ?>
 
 <ul class="bMainMenuLevel-<?= $level ?>">
-<? foreach ($menu as $iMenu): ?>
-    <li <li class="bMainMenuLevel-<?= $level ?>__eItem clearfix <? if (\Model\Menu\Entity::ACTION_SEPARATOR === $iMenu->getAction()): ?> bMainMenuLevel-<?= $level ?>__eSeparator<? endif ?>">
+<? $i = 1; foreach ($menu as $iMenu): ?>
+<?
+    $class = '';
+    if (\Model\Menu\Entity::ACTION_SEPARATOR === $iMenu->getAction()) {
+        $class .= ' bMainMenuLevel-' . $level . '__eSeparator';
+    }
+    if ((1 == $level) && ($i == $count)) {
+        $class .= ' mMore';
+    }
+    $class = trim($class);
+?>
+
+    <li class="bMainMenuLevel-<?= $level ?>__eItem<? if ($class) echo ' ' . $class ?>">
         <? if ($iMenu->getLink()): ?>
             <a class="bMainMenuLevel-<?= $level ?>__eTitle" href="<?= $iMenu->getLink() ?>"><?= $iMenu->getName() ?></a>
         <? elseif ($iMenu->getName()): ?>
@@ -23,5 +35,5 @@ $level = isset($level) ? $level : 1;
             <?= $page->render('_mainMenu', ['menu' => $iMenu->getChild(), 'level' => $level + 1]) ?>
         <? endif ?>
     </li>
-<? endforeach ?>
+<? $i++; endforeach ?>
 </ul>
