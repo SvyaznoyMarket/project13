a2297357f8e31ab9eb22639f97098b553bc1b51b
Alex Zaycev <13rentgen@gmail.com>
2013-03-12 15:37:33 +0400

    SITE-902 Убрал сезонный блок с главной

--- main/template/layout-main.php
+++ main/template/layout-main.php
@@ -30,28 +30,7 @@
     <div class="adfoxWrapper" id="adfox980"></div>
 
     <?= $page->slotHeader() ?>
-    <div class="bPromoCategoryWrap clearfix">
-        <div id="ogorod_1feb" class="bPromoCategory fl">
-            <div class="bPromoCategory_eArrow"></div>
-            <a class="bPromoCategory_eIcon fl" href="/catalog/do_it_yourself/tovari-dlya-sada-311/?from=enter_internal_promo_season"><img src="/css/mainPage/img/bPromoCategory_ogorodGreen.gif"/></a>
-            <div class="fl">
-                <h2 class="bPromoCategory_eTitle">Скоро дачный сезон!</h2>
-                <a class="bPromoCategory_eLink" href="/catalog/do_it_yourself/tovari-dlya-sada-311/?from=enter_internal_promo_season">Товары для сада и огорода</a>
-                <a class="bPromoCategory_eLink" href="catalog/doityourself/ruchnie-sadovie-instrumenti-tovari-dlya-rassadi-1564/?from=enter_internal_promo_season">Товары для рассады</a>
-                <a class="bPromoCategory_eLink" href="/catalog/household/dekor-interera-gorshki-i-kashpo-1130/?from=enter_internal_promo_season">Горшки и кашпо</a>
-            </div>
-        </div>
-        <div id="jew_1feb" class="bPromoCategory fl">
-            <div class="bPromoCategory_eArrow"></div>
-            <a class="bPromoCategory_eIcon fl" href="/catalog/jewel/ukrasheniya-ko-dnyu-vlyublennih-2826/?from=enter_internal_promo_season"><img src="/css/mainPage/img/bPromoCategory_jewelPink.gif"/></a>
-            <div class="fl">
-                <h2 class="bPromoCategory_eTitle">Подарки к весенним праздникам</h2>
-                <a class="bPromoCategory_eLink" href="/catalog/ukrasheniya-k-8-marta-2922/?from=enter_internal_promo_season">Украшения к 8 марта</a>
-                <p class="bPromoCategory_eLink"><a href="/catalog/podarki-na-8-marta-1522/?from=enter_internal_promo_season">Подарки на 8 марта</a></p>
-                <a class="bPromoCategory_eLink" href="/catalog/parfyumeriya-i-kosmetika/podarochnie-nabori-2591/?from=enter_internal_promo_season">Подарочные наборы парфюмерии</a>
-            </div>
-        </div>
-    </div>
+    
     <noindex>
         <div id="mainPageSearch" class="searchbox">
             <?= $page->render('search/form-main') ?>
