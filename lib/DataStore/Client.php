278e91f1f63623715d245485b465ace865b706d1
Sergey Sapego <ssv@enter.ru>
2013-03-15 12:48:30 +0400

    SITE-849 исправлена обработка эксепшена в DataStoreClient

--- lib/DataStore/Client.php
+++ lib/DataStore/Client.php
@@ -37,27 +37,25 @@ class Client {
         \App::logger()->info('Start data-store request ' . $path);
 
         $response = null;
-        try {
-            // локальный файл
-            if (0 === strpos($path, '/')) {
-                $file = \App::config()->dataDir . '/data-store' . $path;
-                $response = is_file($file) ? json_decode(file_get_contents($file), true) : null;
-                \Util\RequestLogger::getInstance()->addLog($file, [], 0, 'unknown');
-            // http-ресурс
-            } else {
-                //$response = $this->curl->query($this->config['url'] . $path, [], $this->config['timeout']);
-                $response = '';
-                $this->curl->addQuery($this->config['url'] . $path, [], function($data) use (&$response) {
-                    $response = $data;
-                }, null, $this->config['timeout']);
-                $this->curl->execute(\App::config()->coreV2['retryTimeout']['tiny'], \App::config()->coreV2['retryCount']);
-            }
-            $spend = \Debug\Timer::stop('data-store');
-            \App::logger()->info('End data-store request ' . $path . ' in ' . $spend);
-        } catch (\Exception $e) {
-            $spend = \Debug\Timer::stop('data-store');
-            \App::exception()->remove($e);
-            \App::logger()->info('Fail data-store request ' . $path . ' in ' . $spend . ' with ' . $e);
+        // локальный файл
+        if (0 === strpos($path, '/')) {
+            $file = \App::config()->dataDir . '/data-store' . $path;
+            $response = is_file($file) ? json_decode(file_get_contents($file), true) : null;
+            \Util\RequestLogger::getInstance()->addLog($file, [], 0, 'unknown');
+        // http-ресурс
+        } else {
+            //$response = $this->curl->query($this->config['url'] . $path, [], $this->config['timeout']);
+            $response = '';
+            $this->curl->addQuery($this->config['url'] . $path, [], function($data) use (&$response, $path) {
+                $response = $data;
+                $spend = \Debug\Timer::stop('data-store');
+                \App::logger()->info('End data-store request ' . $path . ' in ' . $spend);
+            }, function($e) use ($path) {
+                $spend = \Debug\Timer::stop('data-store');
+                \App::exception()->remove($e);
+                \App::logger()->info('Fail data-store request ' . $path . ' in ' . $spend . ' with ' . $e);
+            }, $this->config['timeout']);
+            $this->curl->execute(\App::config()->coreV2['retryTimeout']['tiny'], \App::config()->coreV2['retryCount']);
         }
 
         return $response;
