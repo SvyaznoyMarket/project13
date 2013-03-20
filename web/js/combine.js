f32a2175f78e9fe061ecb9c612198817f7621de2
Alex Zaycev <13rentgen@gmail.com>
2013-03-20 12:51:42 +0400

    SITE-887 Минифицированный JS

--- web/js/combine.js
+++ web/js/combine.js
@@ -1,16 +1,16 @@
 window.filesWithVersion = {
-"app.order.v4.js":1363098305,
-"ports.js":1363098305,
-"app.oneclick.js":1363270711,
+"app.order.v4.js":1363754668,
+"ports.js":1363754694,
+"app.oneclick.js":1363270967,
 "infopages.js":1363087640,
 "library.js":1363087640,
 "app.cart.js":1363087640,
 "bigjquery.min.js":1363087640,
 "welcome.js":1363087640,
-"main.js":1363098305,
-"dash.js":1363270753,
+"main.js":1363754668,
+"dash.js":1363754668,
 "app.order.js":1363098305,
 "app.product.comment.list.js":1351763596,
-"app.product.js":1363268777,
+"app.product.js":1363350603,
 "app.shop.js":1363087640
 }
