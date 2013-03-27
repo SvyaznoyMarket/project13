3e8b71c841fc2baacd8895f2248b6a24d994185c
Alex Zaycev <13rentgen@gmail.com>
2013-03-27 19:19:02 +0400

    SITE-969 Починил срабатывание кнопки Enter на suggest'е

--- web/js/combine.js
+++ web/js/combine.js
@@ -1,16 +1,16 @@
 window.filesWithVersion = {
-"app.order.v4.js":1364380324,
+"app.order.v4.js":1364388632,
 "ports.js":1364293128,
 "app.oneclick.js":1360754818,
-"infopages.js":1364380896,
+"infopages.js":1364388632,
 "library.js":1360754818,
-"app.cart.js":1360754818,
-"bigjquery.min.js":1364380324,
+"app.cart.js":1364388632,
+"bigjquery.min.js":1364388632,
 "welcome.js":1360754713,
-"main.js":1364380324,
-"dash.js":1364380331,
+"main.js":1364397038,
+"dash.js":1364388632,
 "app.order.js":1364194169,
 "app.product.comment.list.js":1360754713,
-"app.product.js":1364380324,
+"app.product.js":1364388632,
 "app.shop.js":1360754713
 }
