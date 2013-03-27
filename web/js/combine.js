8fb3c50cf4e036b630f54ddc03a86a67adffdeda
Alex Zaycev <13rentgen@gmail.com>
2013-03-27 20:14:42 +0400

    SITE-968 Перекомпилил JS, Enter Type в выпадалке

--- web/js/combine.js
+++ web/js/combine.js
@@ -1,16 +1,16 @@
 window.filesWithVersion = {
-"app.order.v4.js":1364388632,
+"app.order.v4.js":1364399478,
 "ports.js":1364293128,
 "app.oneclick.js":1360754818,
-"infopages.js":1364388632,
+"infopages.js":1364399478,
 "library.js":1360754818,
 "app.cart.js":1364388632,
-"bigjquery.min.js":1364388632,
+"bigjquery.min.js":1364399478,
 "welcome.js":1360754713,
-"main.js":1364397038,
-"dash.js":1364388632,
+"main.js":1364400277,
+"dash.js":1364399478,
 "app.order.js":1364194169,
 "app.product.comment.list.js":1360754713,
-"app.product.js":1364388632,
+"app.product.js":1364399478,
 "app.shop.js":1360754713
 }
