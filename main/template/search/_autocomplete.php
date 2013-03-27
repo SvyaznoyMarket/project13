e4ec393c71d46767fd69caba5fd2675889115c79
Alex Zaycev <13rentgen@gmail.com>
2013-03-27 17:05:45 +0400

    SITE-965 Вывод в саджесте заголовков "Категории" и "Товары"

--- main/template/search/_autocomplete.php
+++ main/template/search/_autocomplete.php
@@ -8,7 +8,7 @@
 <div class="bSearchSuggest">
     <? if ((bool)$categories): ?>
     <div class="bSearchSuggest__eCategoryList">
-        <p class="bSearchSuggest__eListLine"><span class="bSearchSuggest__eListTitle">Категория</span></p>
+        <p class="bSearchSuggest__eListLine"><span class="bSearchSuggest__eListTitle">Категории</span></p>
         <? foreach ($categories as $category): ?>
         <a class="bSearchSuggest__eCategoryRes bSearchSuggest__eRes" href="<?= $category->getLink() ?>">
             <?= preg_replace('/'.$searchQuery.'/iu', '<span class="bSearchSuggest__eSelected">' . htmlspecialchars('$0', ENT_QUOTES, \App::config()->encoding) . '</span>', $category->getName()) ?>
@@ -18,7 +18,7 @@
     <? endif ?>
     <? if ((bool)$products): ?>
     <div class="bSearchSuggest__eProductList">
-        <p class="bSearchSuggest__eListLine"><span class="bSearchSuggest__eListTitle">Товар</span></p>
+        <p class="bSearchSuggest__eListLine"><span class="bSearchSuggest__eListTitle">Товары</span></p>
         <? foreach ($products as $product): ?>
         <a class="bSearchSuggest__eGoodRes bSearchSuggest__eRes clearfix" href="<?= $product->getLink()?>">
             <img class="bSearchSuggest__eGoodImgRes fl" src="<?= $product->getImageUrl(1) ?>" width="48" height="48"/>
