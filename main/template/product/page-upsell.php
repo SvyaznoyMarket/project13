f2b92dd0ad5f6228a3a11d9b5c04f03cccee7e55
Sergey Sapego <ssv@enter.ru>
2013-03-28 14:17:05 +0400

    SITE-955 добавлена страница upsell с данными

--- main/template/product/page-upsell.php
+++ main/template/product/page-upsell.php
@@ -1,8 +1,10 @@
 <?php
 /**
- * @var $page    \View\Product\StockPage
- * @var $user    \Session\User
- * @var $product \Model\Product\Entity
+ * @var $page           \View\Product\StockPage
+ * @var $user           \Session\User
+ * @var $product        \Model\Product\Entity
+ * @var $accessories    \Model\Product\Entity[]
+ * @var $related        \Model\Product\Entity[]
  */
 ?>
 
@@ -24,12 +26,16 @@
     <div class="bUpsaleRedirectBlock fr">
         <h2 class="bUpsaleRedirectBlock__eTitle">В корзине <strong id="upsaleCounter" class="bUpsaleRedirectBlock__eCounter"></strong> товара на сумму <strong class="bUpsaleRedirectBlock__ePrice"><span id="upsalePrice"></span> <span class="rubl">p</span></strong></h2>
         <div class="clearfix">
-            <a class="bUpsaleRedirectBlock__eLink fl" href="">Перейти в корзину</a>
-            <a class="bUpsaleRedirectBlock__eLink fr mFullOrange" href="">Оформить заказ ></a>
+            <a class="bUpsaleRedirectBlock__eLink fl" href="<?= $page->url('cart') ?>">Перейти в корзину</a>
+            <a class="bUpsaleRedirectBlock__eLink fr mFullOrange" href="<?= $page->url('order.create') ?>">Оформить заказ ></a>
         </div>
     </div>
 </div>
 
-<? if ($showAccessoryUpper && (bool)$accessories && \App::config()->product['showAccessories']): ?>
+<? if ((bool)$accessories && \App::config()->product['showAccessories']): ?>
     <?= $page->render('product/_slider', array('product' => $product, 'productList' => array_values($accessories), 'totalProducts' => count($product->getAccessoryId()), 'itemsInSlider' => \App::config()->product['itemsInSlider'], 'page' => 1, 'title' => 'Аксессуары', 'url' => $page->url('product.accessory', array('productToken' => $product->getToken())), 'gaEvent' => 'Accessorize')) ?>
+<? endif ?>
+
+<? if ((bool)$related && \App::config()->product['showRelated']): ?>
+    <?= $page->render('product/_slider', array('product' => $product, 'productList' => array_values($related), 'totalProducts' => count($product->getRelatedId()), 'itemsInSlider' => \App::config()->product['itemsInSlider'], 'page' => 1, 'title' => 'С этим товаром также покупают', 'url' => $page->url('product.related', array('productToken' => $product->getToken())))) ?>
 <? endif ?>
