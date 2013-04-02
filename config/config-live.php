a04115ec2d53eb080422cdd22798bc42ebb0204d
green <green@mail.ru>
2013-04-02 16:11:22 +0400

    config: включил регистрацию юр. лиц на проде

--- config/config-live.php
+++ config/config-live.php
@@ -58,7 +58,7 @@ $c->database['host']     = 'site-db'; // был 10.20.33.2
 
 $c->smartEngine['cert'] = $c->dataDir . '/cert/gsorganizationvalg2.crt';
 
-$c->user['corporateRegister'] = false;
+$c->user['corporateRegister'] = true;
 
 $c->abtest['bestBefore'] = '2013-04-23';
 $c->abtest['enabled']    = true;
