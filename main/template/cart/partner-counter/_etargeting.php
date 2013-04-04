b4f72cba9501f0e8d4038c84a62edf9a9894d9a9
green <green@mail.ru>
2013-04-04 10:44:32 +0400

    SITE-943 Etargeting: доработки для счетчиков

---
+++ main/template/cart/partner-counter/_etargeting.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @var $user \Session\User
+ */
+?>
+
+<? $productIds = array_map(function($cartProduct) { /** @var $cartProduct \Model\Cart\Product\Entity */ return $cartProduct->getId(); }, $user->getCart()->getProducts()) ?>
+
+<? if (\App::config()->analytics['enabled'] && (bool)$productIds && ($cusId = \Partner\Counter\Etargeting::getCusId($user->getRegion()))): ?>
+    <script language="javascript">
+        var odinkod = {
+            "type": "basket",
+            "product_list":"<?= implode(',', $productIds) ?>"
+        };
+        document.write('<scr'+'ipt src="'+('https:' == document.location.protocol ? 'https://ssl.' : 'http://') +
+            'cdn.odinkod.ru/tags/<?= $cusId ?>.js"></scr'+'ipt>');
+    </script>
+<? endif ?>
