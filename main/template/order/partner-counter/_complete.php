eace4705039fb59f575a0fbd9112ae3f719a8420
green <green@mail.ru>
2013-04-05 11:06:30 +0400

    SITE-989 Запоминать только последнего партнера: исключение для eTargeting

--- main/template/order/partner-counter/_complete.php
+++ main/template/order/partner-counter/_complete.php
@@ -8,13 +8,15 @@
 ?>
 
 <? if (\App::config()->analytics['enabled']): ?>
+    <?= $page->tryRender('order/partner-counter/_etargeting-complete', array('orders' => $orders, 'productsById' => $productsById)) ?>
+
     <?
     switch (\App::partner()->getName()) {
         case \Partner\Counter\Admitad::NAME:
             echo $page->tryRender('order/partner-counter/_admitad-complete', array('orders' => $orders, 'productsById' => $productsById));
             break;
         case \Partner\Counter\Etargeting::NAME:
-            echo $page->tryRender('order/partner-counter/_admitad-complete', array('orders' => $orders, 'productsById' => $productsById));
+            //echo $page->tryRender('order/partner-counter/_etargeting-complete', array('orders' => $orders, 'productsById' => $productsById));
             break;
         case \Partner\Counter\CityAds::NAME:
             echo $page->tryRender('order/partner-counter/_cityads-complete', array('orders' => $orders));
