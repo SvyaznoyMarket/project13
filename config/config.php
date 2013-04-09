d6eec6bec28c48140d9a44848bc4b6e2916a1aa4
green <green@mail.ru>
2013-04-09 18:16:22 +0400

    config fix

--- config/config.php
+++ config/config.php
@@ -205,7 +205,7 @@ $c->abtest = [
     'test'       => [],
 ];
 
-$c->subscribe['enabled'] = false;
+$c->subscribe['enabled'] = true;
 $c->subscribe['cookieName'] = 'subscribed';
 
 $c->requestMainMenu = false;
