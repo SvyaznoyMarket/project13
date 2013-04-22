857318935e2dc67aac22b823798316bdf122aad1
green <green@mail.ru>
2013-04-22 13:18:58 +0400

    SITE-765 обработка очереди заданий для crossss

---
+++ main/controller/Command/CrossssAction.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Controller\Command;
+
+class CrossssAction {
+    public function __construct() {
+        if ('cli' !== PHP_SAPI) {
+            throw new \Exception('Действие доступно только через CLI');
+        }
+    }
+
+    /**
+     * @param int $limit
+     * @throws \Exception
+     */
+    public function execute($limit = 10000) {
+        \App::logger()->debug('Exec ' . __METHOD__);
+
+        $curl = \App::curl();
+
+        $manager = new \QueueManager(\App::config()->queue, \App::logger());
+        $manager->process('crossss.push', function($taskName, $taskData = []) use ($curl) {
+            foreach ($taskData as $data) {
+                if (empty($data['apikey'])) {
+                    //throw new \Exception(sprintf('task.%s: не содержит параметра apikey', $taskName));
+                }
+
+                $result = $curl->query(\App::config()->crossss['apiUrl'] . '?' . http_build_query($data));
+            }
+        }, $limit);
+    }
+}
