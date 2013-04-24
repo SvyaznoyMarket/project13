c8bb78f0b89a9e59e3e4b89b7b6c34cbc1bfdebf
g84 <green840429@gmail.com>
2013-04-24 11:01:43 +0400

    SITE-940 рефакторинг

--- lib/Partner/Manager.php
+++ lib/Partner/Manager.php
@@ -19,11 +19,13 @@ class Manager {
             $request = \App::request();
             $cookie = null;
 
+            $utmSource = $request->get('utm_source');
+
             // CityAds
-            if ('ca' == $request->get('ref') && ($prx = $request->get('prx'))) {
+            if (0 === strpos($utmSource, 'cityads')) {
                 $response->headers->setCookie(new \Http\Cookie(
                     'prx',
-                    $prx,
+                    $request->get('prx'),
                     time() + $this->cookieLifetime,
                     '/',
                     null,
@@ -40,83 +42,81 @@ class Manager {
                     false,
                     true
                 );
-            } else if ($utmSource = $request->get('utm_source')) {
-                // eTargeting
-                if (0 === strpos($utmSource, 'etargeting')) {
-                    $cookie = new \Http\Cookie(
-                        $this->cookieName,
-                        \Partner\Counter\Etargeting::NAME,
-                        time() + $this->cookieLifetime,
-                        '/',
-                        null,
-                        false,
-                        true
-                    );
-                // Actionpay
-                } else if (0 === strpos($utmSource, 'actionpay')) {
-                    $response->headers->setCookie(new \Http\Cookie(
-                        'actionpay',
-                        $request->get('actionpay'),
-                        time() + $this->cookieLifetime,
-                        '/',
-                        null,
-                        false,
-                        true
-                    ));
+            // eTargeting
+            } if (0 === strpos($utmSource, 'etargeting')) {
+                $cookie = new \Http\Cookie(
+                    $this->cookieName,
+                    \Partner\Counter\Etargeting::NAME,
+                    time() + $this->cookieLifetime,
+                    '/',
+                    null,
+                    false,
+                    true
+                );
+            // Actionpay
+            } else if (0 === strpos($utmSource, 'actionpay')) {
+                $response->headers->setCookie(new \Http\Cookie(
+                    'actionpay',
+                    $request->get('actionpay'),
+                    time() + $this->cookieLifetime,
+                    '/',
+                    null,
+                    false,
+                    true
+                ));
 
-                    $cookie = new \Http\Cookie(
-                        $this->cookieName,
-                        \Partner\Counter\Actionpay::NAME,
-                        time() + $this->cookieLifetime,
-                        '/',
-                        null,
-                        false,
-                        true
-                    );
-                // Admitad
-                } else if (0 === strpos($utmSource, 'admitad')) {
-                    $response->headers->setCookie(new \Http\Cookie(
-                        'admitad_uid',
-                        $request->get('admitad_uid'),
-                        time() + $this->cookieLifetime,
-                        '/',
-                        null,
-                        false,
-                        true
-                    ));
+                $cookie = new \Http\Cookie(
+                    $this->cookieName,
+                    \Partner\Counter\Actionpay::NAME,
+                    time() + $this->cookieLifetime,
+                    '/',
+                    null,
+                    false,
+                    true
+                );
+            // Admitad
+            } else if (0 === strpos($utmSource, 'admitad')) {
+                $response->headers->setCookie(new \Http\Cookie(
+                    'admitad_uid',
+                    $request->get('admitad_uid'),
+                    time() + $this->cookieLifetime,
+                    '/',
+                    null,
+                    false,
+                    true
+                ));
 
-                    $cookie = new \Http\Cookie(
-                        $this->cookieName,
-                        \Partner\Counter\Admitad::NAME,
-                        time() + $this->cookieLifetime,
-                        '/',
-                        null,
-                        false,
-                        true
-                    );
-                // Recreative
-                } else if (0 === strpos($utmSource, 'recreative')) {
-                    $cookie = new \Http\Cookie(
-                        $this->cookieName,
-                        \Partner\Counter\Recreative::NAME,
-                        time() + $this->cookieLifetime,
-                        '/',
-                        null,
-                        false,
-                        true
-                    );
-                // Reactive
-                } else if ((0 === strpos($utmSource, 'vk.com')) && (0 === strpos($request->get('utm_campaing'), 'social_target'))) {
-                    $cookie = new \Http\Cookie(
-                        $this->cookieName,
-                        \Partner\Counter\Reactive::NAME,
-                        time() + $this->cookieLifetime,
-                        '/',
-                        null,
-                        false,
-                        true
-                    );
-                }
+                $cookie = new \Http\Cookie(
+                    $this->cookieName,
+                    \Partner\Counter\Admitad::NAME,
+                    time() + $this->cookieLifetime,
+                    '/',
+                    null,
+                    false,
+                    true
+                );
+            // Recreative
+            } else if (0 === strpos($utmSource, 'recreative')) {
+                $cookie = new \Http\Cookie(
+                    $this->cookieName,
+                    \Partner\Counter\Recreative::NAME,
+                    time() + $this->cookieLifetime,
+                    '/',
+                    null,
+                    false,
+                    true
+                );
+            // Reactive
+            } else if ((0 === strpos($utmSource, 'vk.com')) && (0 === strpos($request->get('utm_campaing'), 'social_target'))) {
+                $cookie = new \Http\Cookie(
+                    $this->cookieName,
+                    \Partner\Counter\Reactive::NAME,
+                    time() + $this->cookieLifetime,
+                    '/',
+                    null,
+                    false,
+                    true
+                );
             }
 
             if ($cookie instanceof \Http\Cookie) {
