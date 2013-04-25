96a8d7bffda8c0b0f6e68d7cfd2fff0068ce3b47
g84 <green840429@gmail.com>
2013-04-25 16:15:57 +0400

    SITE-? закрыл crossss галочкой

--- config/config.php
+++ config/config.php
@@ -181,7 +181,7 @@ $c->smartengine['logEnabled'] = true;
 $c->smartengine['logDataEnabled'] = true;
 $c->smartengine['sslVerify'] = true;
 
-$c->crossss['enabled'] = true;
+$c->crossss['enabled'] = false;
 $c->crossss['apiUrl'] = 'http://crossss.com/api.ashx';
 $c->crossss['id'] = 45;
 $c->crossss['apiKey'] = '5a0bb0cb92a94f7db8a9bf4bfacdbe39';
