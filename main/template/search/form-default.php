6217f53f3a60b7702a405a0e72f46883d59a2883
Alex Zaycev <13rentgen@gmail.com>
2013-04-26 14:28:52 +0400

    SITE-963 Поиск среди десятков тысяч товаров

--- main/template/search/form-default.php
+++ main/template/search/form-default.php
@@ -12,6 +12,6 @@ $isWide = (isset($isWide) && $isWide) ? true : false;
 ?>
 
 <form class="search-form clearfix" action="<?= $page->url('search') ?>" method="get">
-    <input type="text" class="searchtext<? if ($isWide) { ?> width483<? } ?>" name="q" placeholder="<?= $searchQuery ?>" autocomplete="off"/>
+    <input type="text" class="searchtext<? if ($isWide) { ?> width483<? } ?>" name="q" placeholder="Поиск среди десятков тысяч товаров" autocomplete="off"/>
     <input type="submit" class="searchbutton" value="Найти" title="Найти" id="try-1"/>
 </form>
