5787aaccee8a7518abd8ac81379a02dd8a3b6eb9
oleg.baranov <olegbrnv@gmail.com>
2013-05-13 18:31:54 +0400

    SITE-1086 Добавлено 2 новых поля в выдачу: days, origin_date

--- main/view/Helper.php
+++ main/view/Helper.php
@@ -40,4 +40,14 @@ class Helper extends \Templating\Helper {
 
         return 'через ' . ($interval->days - 1) . ' ' . $this->numberChoice(($interval->days - 1), array('день', 'дня', 'дней')) . ' (' . $date->format('d.m.Y') . ')';
     }
+
+    public function getDaysDiff($date) {
+        $today = new \DateTime();
+        $today->settime(0, 0, 0);
+        if (!is_object($date) || !is_a($date, 'DateTime')) {
+            $date = new \DateTime($date);
+        }
+        $interval = $today->diff($date);
+        return ($interval->days-1<0)?(0):($interval->days-1);
+    }
 }
