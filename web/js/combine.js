3677be7c06d78124505bcf80c138e2c9cd100c47
Alex Zaycev <13rentgen@gmail.com>
2013-05-14 16:17:32 +0400

    1012 - Логирование события Viewed Product

--- web/js/combine.js
+++ web/js/combine.js
@@ -1,16 +1,16 @@
 window.filesWithVersion = {
-"app.order.v4.js":1368527522,
+"app.order.v4.js":1368533133,
 "ports.js":1366982974,
-"app.oneclick.js":1368527718,
+"app.oneclick.js":1368533133,
 "infopages.js":1368024975,
 "library.js":1367231842,
 "app.cart.js":1367941013,
 "bigjquery.min.js":1368024975,
 "welcome.js":1360754713,
 "main.js":1368527128,
-"dash.js":1368530030,
+"dash.js":1368533133,
 "app.order.js":1368014870,
 "app.product.comment.list.js":1360754713,
-"app.product.js":1366982974,
+"app.product.js":1368533483,
 "app.shop.js":1368014899
 }
