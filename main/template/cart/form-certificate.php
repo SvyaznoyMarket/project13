93ac925d4c7024d577417e4a7d3e7947d2a2b1db
g84 <green840429@gmail.com>
2013-05-21 14:19:37 +0400

    SITE-1132 Применение купонов в корзине: добавил радио-бутоны

--- main/template/cart/form-certificate.php
+++ main/template/cart/form-certificate.php
@@ -22,13 +22,16 @@ if (!isset($isForm)) $isForm = true;
                 <p class="font14">Для заказа действует скидка по программе «Под защитой F1» <a class="bF1SaleCard_eDel" href="#" data-url="<?= $page->url('cart.certificate.delete') ?>">отменить</a></p>
             </div>
         <? elseif ((bool)$user->getCart()->getCoupons()): ?>
+            <? foreach ($user->getCart()->getCoupons() as $coupon): ?>
             <div class="bF1SaleCard_eComplete mGold">
-                <p class="font14">Для заказа действует скидка для товаров <a class="bF1SaleCard_eDel" href="#" data-url="<?= $page->url('cart.coupon.delete') ?>">отменить</a></p>
+                <p class="font14">Для заказа действует скидка «<?= $coupon->getName() ?>» <a class="bF1SaleCard_eDel" href="#" data-url="<?= $page->url('cart.coupon.delete') ?>">отменить</a></p>
             </div>
+            <? endforeach ?>
         <? elseif ($isForm): ?>
             <div class="bF1SaleCard_eForm">
-                <p class="font11">Введите серийный номер карты «Под защитой F1» для скидки на услуги:</p>
-                <input id="F1SaleCard_number" class="mr20 width370" type="text"/><input id="F1SaleCard_btn" data-url="<?= $page->url('cart.certificate.apply') ?>" class="yellowbutton button" type="button" value="Получить скидку"/>
+                <input type="radio" data-url="<?= $page->url('cart.certificate.apply') ?>" /> Код скидки на товары
+                <input type="radio" data-url="<?= $page->url('cart.coupon.apply') ?>" /> Скидки на услуги F1 по карте «Под защитой F1»
+                <input id="F1SaleCard_number" class="mr20 width370" type="text"/><input id="F1SaleCard_btn" class="yellowbutton button" type="button" value="Применить" />
                 <p id="bF1SaleCard_eErr" class="bF1SaleCard_eErr"></p>
             </div>
         <? else: ?>
