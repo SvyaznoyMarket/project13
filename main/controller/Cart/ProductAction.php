50904b8aa291b3b19514acc0e1aaa4b530cd6ed3
g84 <green840429@gmail.com>
2013-06-03 17:43:28 +0400

    SITE-1227 Пакетный запрос на добавление товаров в корзину

--- main/controller/Cart/ProductAction.php
+++ main/controller/Cart/ProductAction.php
@@ -35,35 +35,9 @@ class ProductAction {
 
             // не учитываем является ли товар набором или нет - за это отвечает ядро
             $cart->setProduct($product, $quantity);
-
             $cartProduct = $cart->getProductById($product->getId());
+            $this->updateCartWarranty($product, $cartProduct, $quantity);
 
-            // обновить количество гарантий для товара
-            if ($cartProduct && (bool)$cartProduct->getWarranty()) {
-                try {
-                    $cartWarranties = $cartProduct->getWarranty();
-                    /** @var $cartWarranty \Model\Cart\Warranty\Entity|null */
-                    $cartWarranty = reset($cartWarranties);
-                    if (!$cartWarranty) {
-                        throw new \Exception(sprintf('Не найдена расширенная гарантия на товар #%s', $product->getId()));
-                    }
-
-                    $warranty = null;
-                    foreach ($product->getWarranty() as $iWarranty) {
-                        if ($iWarranty->getId() == $cartWarranty->getId()) {
-                            $warranty = $iWarranty;
-                            break;
-                        }
-                    }
-                    if (!$warranty) {
-                        throw new \Exception(sprintf('Не найдена расширенная гарантия #%s на товар #%s', $cartWarranty->getId(), $product->getId()));
-                    }
-
-                    $cart->setWarranty($warranty, $quantity, $product->getId());
-                } catch (\Exception $e) {
-                    \App::logger()->error($e);
-                }
-            }
             $returnRedirect = $request->headers->get('referer') ?: \App::router()->generate('homepage');
             switch (\App::abTest()->getCase()->getKey()) {
                 case 'upsell':
@@ -103,6 +77,78 @@ class ProductAction {
         }
     }
 
+
+    public function setList(\Http\Request $request) {
+        $region = \App::user()->getRegion();
+        $cart = \App::user()->getCart();
+
+        $responseData = [];
+
+        try {
+            $productData = (array)$request->get('orders');
+            if (!(bool)$productData) {
+                throw new \Exception('Не получены данные о товарах');
+            }
+
+            /** @var $productsById \Model\Product\CompactEntity[] */
+            $productsById = [];
+            /** @var $productQuantitiesById array */
+            $productQuantitiesById = [];
+            foreach ($productData as $productItem) {
+                $productId = (int)$productItem['id'];
+                if (!$productId) {
+                    \App::logger()->error('Не указан ид товара');
+                    continue;
+                }
+                $productQuantity = (int)$productItem['quantity'];
+                if (!$productQuantity) {
+                    \App::logger()->error('Не указано количество товара');
+                    continue;
+                }
+
+                $productsById[$productId] = null;
+                $productQuantitiesById[$productId] = $productQuantity;
+            }
+
+            if (!(bool)$productsById) {
+                throw new \Exception('Не собраны ид товаров');
+            }
+
+            foreach (array_chunk(array_keys($productsById), 50, true) as $productsInChunk) {
+                \RepositoryManager::product()->prepareCollectionById($productsInChunk, $region, function($data) use (&$productsById) {
+                    foreach ($data as $item) {
+                        $productsById[$item['id']] = new \Model\Product\Entity($item);
+                    }
+                });
+            }
+            \App::coreClientV2()->execute();
+
+            foreach ($productsById as $productId => $product) {
+                if (!$product) {
+                    \App::logger()->error(sprintf('Не получен товар #%s', $productId), ['cart']);
+                    continue;
+                }
+                $productQuantity = isset($productQuantitiesById[$productId]) ? $productQuantitiesById[$productId] : null;
+                if (!$productQuantity) continue;
+
+                $cart->setProduct($product, $productQuantity);
+                $cartProduct = $cart->getProductById($product->getId());
+                $this->updateCartWarranty($product, $cartProduct, $productQuantity);
+            }
+
+            $responseData = [
+                'success' => true,
+            ];
+        } catch(\Exception $e) {
+            $responseData = [
+                'success' => false,
+                'message' => 'Не удалось положить товары в корзину' . (\App::config()->debug ? sprintf(': %s', $e->getMessage()) : ''),
+            ];
+        }
+
+        return new \Http\JsonResponse($responseData);
+    }
+
     /**
      * @param $productId
      * @param \Http\Request $request
@@ -112,5 +158,34 @@ class ProductAction {
         \App::logger()->debug('Exec ' . __METHOD__);
 
         return $this->set($productId, 0, $request);
+    }
+
+    protected function updateCartWarranty(\Model\Product\Entity $product, \Model\Cart\Product\Entity $cartProduct = null, $quantity) {
+        // обновить количество гарантий для товара
+        if ($cartProduct && (bool)$cartProduct->getWarranty()) {
+            try {
+                $cartWarranties = $cartProduct->getWarranty();
+                /** @var $cartWarranty \Model\Cart\Warranty\Entity|null */
+                $cartWarranty = reset($cartWarranties);
+                if (!$cartWarranty) {
+                    throw new \Exception(sprintf('Не найдена расширенная гарантия на товар #%s', $product->getId()));
+                }
+
+                $warranty = null;
+                foreach ($product->getWarranty() as $iWarranty) {
+                    if ($iWarranty->getId() == $cartWarranty->getId()) {
+                        $warranty = $iWarranty;
+                        break;
+                    }
+                }
+                if (!$warranty) {
+                    throw new \Exception(sprintf('Не найдена расширенная гарантия #%s на товар #%s', $cartWarranty->getId(), $product->getId()));
+                }
+
+                \App::user()->getCart()->setWarranty($warranty, $quantity, $product->getId());
+            } catch (\Exception $e) {
+                \App::logger()->error($e);
+            }
+        }
     }
 }
