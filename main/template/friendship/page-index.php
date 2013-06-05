7218eaa69a53d83537dac32bbe2fcef4242124f5
g84 <green840429@gmail.com>
2013-06-05 16:52:08 +0400

    SITE-1199 Верстка landing-page

--- main/template/friendship/page-index.php
+++ main/template/friendship/page-index.php
@@ -20,7 +20,7 @@
 	<div class="content-section__form">
 		<div class="subscribe-form clearfix">
 			<div class="subscribe-form__title">Будем друзьями?</div>
-		    <input class="subscribe-form__email" placeholder="Введите ваш email"/>
+		    <input class="subscribe-form__email" placeholder="Введите ваш email" />
 		    <button class="subscribe-form__btn" data-url="<?= $page->url('subscribe.create') ?>">Да, хочу дружить</button>
 		</div>
 	</div>
@@ -28,7 +28,7 @@
 	<div class="clear"></div>
 
 	<div class="share-section clearfix">
-		<div class="share-sectio__action">Посмотреть все <a href="">wow-акции ></a></div>
+		<div class="share-sectio__action">Посмотреть все <a href="/special_offers">wow-акции ></a></div>
 		<div class="share-sectio__link">Рассказать друзьям <ul class="clearfix"><li><a target="_blank" href="http://vk.com/youcanenter"></a></li><li><a target="_blank" class="fb" href="http://www.facebook.com/enter.ru"></a></li><li><a target="_blank" class="tw" href="http://twitter.com/enter_ru"></a></li></ul></div>
 	</div>
 </div>
