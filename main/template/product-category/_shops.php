af1f770fa76dbf7b6e5edf2eeb366baf5f50801f
oleg.baranov <olegbrnv@gmail.com>
2013-06-14 15:32:05 +0400

    SITE-1243 new css, fixed radio buttons

--- main/template/product-category/_shops.php
+++ main/template/product-category/_shops.php
@@ -18,20 +18,20 @@ if (!$page->hasGlobalParam('shops')) {
     <dd style="display: <?= $shop ? 'block' : 'none' ?>;">
     <ul class="checkbox_list">
         <li onclick="document.location='<?=$page->url('product.category.shop', ['categoryPath' => $category->getPath(), 'shopid' => 0 ])?>';">
-            <input name="" type="radio" class="hiddenCheckbox"/>
-            <label for="" class="prettyCheckbox radio list <?=$shop?'':'checked';?> ">
+            <input name="shop" id="shop_0" type="radio" class="hiddenCheckbox"/>
+            <label for="shop_0" class="prettyCheckbox radio list <?=$shop?'':'checked';?> ">
                 <span class="holderWrap" style="width: 13px; height: 13px;">
                     <span class="holder" style="width: 13px; "></span>
                 </span>
-                Неважно
+                Не важно
             </label>
         </li>
         <?
-        foreach ($shops as $singleShop) {
+        foreach ($shops as $i => $singleShop) {
             ?>
             <li onclick="document.location='<?=$page->url('product.category.shop', ['categoryPath' => $category->getPath(), 'shopid' => $singleShop->getId() ])?>';">
-                <input name="" type="radio" class="hiddenCheckbox"/>
-                <label for="" class="prettyCheckbox radio list <?=($shop&&$shop->getId() == $singleShop->getId())?'checked':'';?>">
+                <input name="shop" id="shop_<?=$i+1?>" type="radio" class="hiddenCheckbox"/>
+                <label for="shop_<?=$i+1?>" class="prettyCheckbox radio list <?=($shop&&$shop->getId() == $singleShop->getId())?'checked':'';?>">
                     <span class="holderWrap" style="width: 13px; height: 13px;">
                         <span class="holder" style="width: 13px; "></span>
                     </span>
