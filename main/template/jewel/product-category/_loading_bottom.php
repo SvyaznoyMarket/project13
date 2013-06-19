9b238e49c5504800406bc7dde507a3127b876738
Anton <trushkevich@gmail.com>
2013-06-19 10:59:20 +0300

    новый аякс лоадер для пандоры

--- main/template/jewel/product-category/_loading_bottom.php
+++ main/template/jewel/product-category/_loading_bottom.php
@@ -1,5 +1,5 @@
 <div id="ajaxgoods" class="bNavLoader hf">
-  <div class="bNavLoader__eIco"><img src="/images/ajar.gif"></div>
+  <div class="bNavLoader__eIco"><img src="/images/ajar_b.gif"></div>
   <div class="bNavLoader__eM">
     <p class="bNavLoader__eText">Подождите немного</p>
     <p class="bNavLoader__eText">Идет загрузка</p>
