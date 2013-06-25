d89f52182619d0d36c7c4b69bdc05efcdcc4b6e0
oleg.baranov <olegbrnv@gmail.com>
2013-06-25 10:12:18 +0400

    SITE-1247 показать все.

--- main/template/product/_inshop.php
+++ main/template/product/_inshop.php
@@ -9,7 +9,12 @@ $shop = $page->getGlobalParam('shop') ? $page->getGlobalParam('shop') : null;
 $renderInfo = isset($renderInfo) ? $renderInfo : true;
 if (!$shop) return;
 ?>
-Только товары из магазина <b><?=$shop->getAddress()?></b><br><br>
+<div style="float: left">
+    <img style="background: url('/css/newProductCard/img/ico.png') no-repeat 0 1px; padding-left: 25px; margin: 0 0 12px;">Только товары из магазина <b><?=$shop->getAddress()?></b>
+</div>
+<div style="float: right">
+    <a>показать все.</a>
+</div>
 <? if (!$count && $renderInfo): ?>
     <br>нет товаров
 <? endif ?>
