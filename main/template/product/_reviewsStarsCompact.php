60c2a4a8ce7eaba1fc57796f442038db3175031c
Anton <trushkevich@gmail.com>
2013-06-27 18:59:16 +0300

    отзывы в карточке пандоры

--- main/template/product/_reviewsStarsCompact.php
+++ main/template/product/_reviewsStarsCompact.php
@@ -4,7 +4,7 @@
 ?>
 <div class="reviewSection reviewSection12 pt10 pb10 clearfix">
   <? if(!empty($avgStarScore)) { ?>
-      <div class="reviewSection12__star reviewSection__star"><?= $page->render('product/_starsFive', ['score' => $avgStarScore]) ?></div>
+      <div class="reviewSection12__star reviewSection__star"><?= $page->render('product/_starsFive', ['score' => $avgStarScore, 'layout' => empty($layout) ? false : $layout]) ?></div>
       <div class="reviewSection12__link reviewSection__link"><span class="gray">(<?= $numReviews ?>)</span></div>
   <? } else { ?>
       <img src="" height="16" width="1">
