dc80075515000bc375e149d0eaebe377fda9699d
Таня Бурлакова <ta.burlakova@gmail.com>
2013-07-01 13:14:13 +0400

    SITE-1240, Переверстала отзывы. Правки после мержа.

--- main/template/product/_reviews.php
+++ main/template/product/_reviews.php
@@ -1,5 +1,5 @@
 <ul class="bReviewsTabs clearfix">
-  <li class="bReviewsTabs__eTab bReviewsTabs__eUser<?= !empty($reviewsData['review_list']) ? ' active' : ' hfImportant' ?>" data-container="reviewsUser" data-reviews-type="user"><span>Отзывы пользователей</span></li>
+  <li class="bReviewsTabs__eTab bReviewsTabs__eUser user<?= !empty($reviewsData['review_list']) ? ' active' : ' hfImportant' ?>" data-container="reviewsUser" data-reviews-type="user"><span>Отзывы пользователей</span></li>
   <?
     if(!empty($reviewsData['review_list']) && !empty($reviewsDataPro['review_list'])) {
       $tabProClass = '';
@@ -9,7 +9,7 @@
       $tabProClass = ' hfImportant';
     }
   ?>
-  <li class="bReviewsTabs__eTab bReviewsTabs__ePro<?= $tabProClass ?>" data-container="reviewsPro" data-reviews-type="pro"><span>Обзоры экспертов</span></li>
+  <li class="bReviewsTabs__eTab bReviewsTabs__ePro pro<?= $tabProClass ?>" data-container="reviewsPro" data-reviews-type="pro"><span>Обзоры экспертов</span></li>
 
   <li class="bReviewsTabs__eTab bReviewsTabs__eLast">
     <span onclick="$('.newReviewPopupLink').click()">Оставить отзыв</span>
@@ -18,22 +18,22 @@
 
 <? if(!empty($reviewsData['review_list'])) { ?>
 
-  <div class="bReviewsContent bReviewsContent__mUser">
+  <div class="bReviewsContent bReviewsContent__mUser reviewsUser">
     <? foreach ($reviewsData['review_list'] as $key => $review) { ?>
       <?= $page->render('product/_review', ['review' => $review, 'last' => empty($reviewsData['review_list'][$key + 1]), 'layout' => empty($layout) ? false : $layout]) ?>
     <? } ?>
   </div>
 
-  <div class="bReviewsContent bReviewsContent__mPro hf"></div>
+  <div class="bReviewsContent bReviewsContent__mPro reviewsPro hf"></div>
 
   <? $showMore = !(empty($reviewsData['review_list']) || (!empty($reviewsData['review_list']) && $reviewsData['page_count'] == 1)); ?>
   <? $showMoreText = 'Показать другие отзывы...' ?>
 
 <? } elseif (!empty($reviewsDataPro['review_list'])) { ?>
 
-  <div class="bReviewsContent bReviewsContent__mUser hf"></div>
+  <div class="bReviewsContent bReviewsContent__mUser reviewsUser hf"></div>
 
-  <div class="bReviewsContent bReviewsContent__mPro">
+  <div class="bReviewsContent bReviewsContent__mPro reviewsPro">
     <? foreach ($reviewsDataPro['review_list'] as $key => $review) { ?>
       <?= $page->render('product/_review', ['review' => $review, 'last' => empty($reviewsDataPro['review_list'][$key + 1]), 'layout' => empty($layout) ? false : $layout]) ?>
     <? } ?>
