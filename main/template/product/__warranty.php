a2aa88963a9826fadca24bde1e907da6a9bbb8dd
g84 <green840429@gmail.com>
2013-07-02 18:22:36 +0400

    SITE-? conflict fix

--- main/template/product/__warranty.php
+++ main/template/product/__warranty.php
@@ -1,11 +1,7 @@
 <?php
 
 return function (
-<<<<<<< HEAD
-    \Model\Product\BasicEntity $product,
-=======
     \Model\Product\Entity $product,
->>>>>>> 958b93985d8e751e20c1f798432667f15b7bdff3
     \Helper\TemplateHelper $helper
 ) { ?>
 
@@ -16,68 +12,6 @@ return function (
     </div>
 
     <ul class="bWidgetService__eInputList">
-<<<<<<< HEAD
-        <li>
-            <input id="id4" name="name1" type="radio" hidden />
-            <label class="bCustomInput" for="id4">
-                <div class="bCustomInput__eText">
-                    <span class="dotted">Black: 2 годa</span>
-
-                    <div class="bHint">
-                        <a class="bHint_eLink">Разрешение дисплея</a>
-                        <div class="bHint_ePopup popup">
-                            <div class="close"></div>
-                            <div class="bHint-text">
-                                <p>конвертировать видео, запустится ли игра. И это тот случай, когда чем больше – тем лучше.</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="bCustomInput__ePrice"><strong>1 490</strong> <span class="rubl">p</span></div>
-                </div>
-            </label>
-        </li>
-
-        <li>
-            <input id="id3" name="name1" type="radio" hidden />
-            <label class="bCustomInput" for="id3">
-                <div class="bCustomInput__eText">
-                    <span class="dotted">Gold: 2,5 годa</span>
-
-                    <div class="bHint">
-                        <a class="bHint_eLink">Разрешение дисплея</a>
-                        <div class="bHint_ePopup popup">
-                            <div class="close"></div>
-                            <div class="bHint-text">
-                                <p>конвертировать видео, запустится ли игра. И это тот случай, когда чем больше – тем лучше.</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="bCustomInput__ePrice"><strong>1 490</strong> <span class="rubl">p</span></div>
-                </div>
-            </label>
-            <div style="display: none;" class="bDeSelect"><a href="">Отменить</a></div>
-        </li>
-
-        <li>
-            <input id="id2" name="name1" type="radio" hidden />
-            <label class="bCustomInput" for="id2">
-                <div class="bCustomInput__eText">
-                    <span class="dotted">Platinum: 3 годa</span>
-
-                    <div class="bHint">
-                        <a class="bHint_eLink">Разрешение дисплея</a>
-                        <div class="bHint_ePopup popup">
-                            <div class="close"></div>
-                            <div class="bHint-text">
-                                <p>конвертировать видео, запустится ли игра. И это тот случай, когда чем больше – тем лучше.</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="bCustomInput__ePrice"><strong>1 490</strong> <span class="rubl">p</span></div>
-=======
     <? foreach ($product->getWarranty() as $warranty): ?>
         <li>
             <input class="<?= \View\Id::cartButtonForProductWarranty($product->getId(), $warranty->getId()) ?>" type="radio" hidden />
@@ -90,15 +24,11 @@ return function (
                     <? endif ?>
 
                     <div class="bCustomInput__ePrice"><strong><?= $helper->formatPrice($warranty->getPrice()) ?></strong> <span class="rubl">p</span></div>
->>>>>>> 958b93985d8e751e20c1f798432667f15b7bdff3
                 </div>
             </label>
             <div style="display: none;" class="bDeSelect"><a href="">Отменить</a></div>
         </li>
-<<<<<<< HEAD
-=======
     <? endforeach ?>
->>>>>>> 958b93985d8e751e20c1f798432667f15b7bdff3
     </ul>
 </div><!--/widget services -->
 
