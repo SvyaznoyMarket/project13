6256e5c21da9b7a6301cc2264e623dd782c0514b
g84 <green840429@gmail.com>
2013-07-02 16:31:02 +0400

    SITE-1256

--- main/template/product/__delivery.php
+++ main/template/product/__delivery.php
@@ -1,7 +1,58 @@
 <?php
 
-return function () { ?>
+return function (
+    \Helper\TemplateHelper $helper
+) { ?>
 
+<ul class="bWidgetBuy__eDelivery" data-value="<?= $helper->json(['url' => $helper->url('product.delivery')]) ?>">
+    <li class="bWidgetBuy__eDelivery-item bWidgetBuy__eDelivery-price">
+        <span>Доставка <strong>290</strong> <span class="rubl">p</span></span>
+        <div>Завтра, 16.05.2013</div>
+    </li>
+    <li class="bWidgetBuy__eDelivery-item bWidgetBuy__eDelivery-free">
+        <span>Самовывоз <strong>бесплатно</strong></span>
+        <div>Завтра, 16.05.2013</div>
+    </li>
 
+    <li class="bWidgetBuy__eDelivery-item bWidgetBuy__eDelivery-now click">
+        <span class="dotted">Есть в магазинах</span>
+        <div>Купить сегодня без предзаказа</div>
+    </li>
+</ul>
+
+<ul style="display: block;" class="bDeliveryFreeAddress">
+    <li>
+        м. Белорусская,<br/>
+        ул. Грузинский вал, д. 31
+    </li>
+    <li>
+        м. Ленинский проспект, <br/>
+        ул. Орджоникидзе, д. 11, стр. 10
+    </li>
+    <li>
+        м. Белорусская, <br/>
+        ул. Грузинский вал, д. 31
+    </li>
+    <li>
+        м. Ленинский проспект, <br/>
+        ул. Орджоникидзе, д. 11, стр. 10
+    </li>
+    <li>
+        м. Белорусская, <br/>
+        ул. Грузинский вал, д. 31
+    </li>
+    <li>
+        м. Ленинский проспект, <br/>
+        ул. Орджоникидзе, д. 11, стр. 10
+    </li>
+    <li>
+        м. Белорусская, <br/>
+        ул. Грузинский вал, д. 31
+    </li>
+    <li>
+        м. Ленинский проспект, <br/>
+        ул. Орджоникидзе, д. 11, стр. 10
+    </li>
+</ul><!--/выпадающий список при клике по - Есть в магазинах -->
 
 <? };
