4a151c9c03e2f1695816102e29f8ccfd117f8425
tamili <ta.burlakova@gmail.com>
2013-07-03 12:12:25 +0400

    SITE-1240. Warranty

--- main/template/product/__warranty.php
+++ main/template/product/__warranty.php
@@ -17,9 +17,9 @@ return function (
         $id = \View\Id::cartButtonForProductWarranty($product->getId(), $warranty->getId());
     ?>
         <li>
-            <input id="<?= $id ?>" class="<?= $id ?>" name="<?= $product->getId()?>" type="radio" hidden />
-            <label class="bCustomInput" for="<?= $id ?>">
-                <div class="bCustomInput__eText">
+            <input id="<?= $id ?>" class="<?= $id ?> bCustomInput" name="<?= $product->getId()?>" type="radio" hidden />
+            <label class="bCustomLabel" for="<?= $id ?>">
+                <div class="bCustomLabel__eText">
                     <span class="dotted"><?= $warranty->getName() ?></span> <?= $warranty->getPeriod() . '&nbsp;' . $helper->numberChoice($warranty->getPeriod(), ['месяц', 'месяца', 'месяцев']) ?>
 
                     <? if ($warranty->getDescription()): ?>
