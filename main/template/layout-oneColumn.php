24f150bec632e7dfb38faee0663f81f11d4c08e3
tamili <ta.burlakova@gmail.com>
2013-07-10 17:43:27 +0400

    SITE-1358

--- main/template/layout-oneColumn.php
+++ main/template/layout-oneColumn.php
@@ -3,7 +3,7 @@
  * @var $page \View\DefaultLayout
  */
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" xmlns:og="http://ogp.me/ns#" xmlns:fb="http://www.facebook.com/2008/fbml">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
