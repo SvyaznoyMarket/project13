9d323117cb5da9b8ae8e2203588ea1f367b998a4
Julian <y.misiukevich@gmail.com>
2013-07-11 17:11:29 +0300

    WOD-118 added seo-tags for main page

--- main/view/DefaultLayout.php
+++ main/view/DefaultLayout.php
@@ -13,6 +13,9 @@ class DefaultLayout extends Layout {
         $this->addMeta('viewport', 'width=900');
         $this->addMeta('title', 'Enter - это выход!');
         $this->addMeta('description', 'Enter - новый способ покупать. Любой из ' . \App::config()->product['totalCount'] . ' товаров нашего ассортимента можно купить где угодно, как угодно и когда угодно. Наша миссия: дарить время для настоящего. Честно. С любовью. Как для себя.');
+        /* Meta and Title могут быть переопределены в методе prepare() в /main/controller/Main/IndexAction.php
+            — загружаются там из json для главной страницы, например.
+        */
 
         $this->addStylesheet('/css/global.min.css');
 
