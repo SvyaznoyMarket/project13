ef7edfb808a236b16b37d40d2451e84ccbe4ca8a
g84 <green840429@gmail.com>
2013-07-17 10:55:13 +0400

    SITE-1241 Нятягивание нового шаблона карточки товара

--- main/template/__spinner.php
+++ main/template/__spinner.php
@@ -1,10 +1,10 @@
 <?php
 
-return function($id, $value = 1, $disabled = false) { ?>
+return function($id, $value = 1) { ?>
 
-<div class="bCountSection clearfix<? if ($disabled): ?> mDisabled<? endif ?>" data-spinner-for="<?= $id ?>">
+<div class="bCountSection clearfix" data-spinner-for="<?= $id ?>">
     <button class="bCountSection__eM">-</button>
-    <input class="bCountSection__eNum" type="text" value="<?= $value ?>" <? if ($disabled): ?>disabled="disabled"<? endif ?> />
+    <input class="bCountSection__eNum" type="text" value="<?= $value ?>" />
     <button class="bCountSection__eP">+</button>
     <span>шт.</span>
 </div><!--/counter -->
