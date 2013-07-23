49ea411dca595f72fce7dba06d9a47657744ac3f
Alex Zaycev <13rentgen@gmail.com>
2013-07-23 16:00:20 +0400

    SITE-1549 ActionPay

--- main/template/order/partner-counter/_actionpay-complete.php
+++ main/template/order/partner-counter/_actionpay-complete.php
@@ -8,6 +8,5 @@
 ?>
 
 <? if ($link = \Partner\Counter\Actionpay::getOrderCompleteLink($orders, $productsById)): ?>
-    <img src="http://n.actionpay.ru/ok/4207.png?<?= $link ?>" height="1" width="1" />
     <img src="http://n.actionpay.ru/ok/3781.png?<?= $link ?>" height="1" width="1" />
 <? endif ?>
