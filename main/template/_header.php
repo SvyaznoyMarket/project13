3e5b55d22b82574e646a9fcc46d5280b78c54352
tamili <ta.burlakova@gmail.com>
2013-07-23 09:48:41 +0400

    SITE-1512, SITE-1532. три телефона в шапке, отсутствующий товар в листинге.

--- main/template/_header.php
+++ main/template/_header.php
@@ -16,12 +16,18 @@
     <div class="bRegion">
         <a class="fl" href="<?= $page->url('region.change', ['regionId' => $user->getRegion()->getId()]) ?>" id="jsregion" data-url="<?= $page->url('region.init') ?>" data-region-id="<?= $user->getRegion()->getId() ?>" data-autoresolve-url="<?= $page->url('region.autoresolve') ?>"><?= ((mb_strlen($user->getRegion()->getName()) > 20) ? (mb_substr($user->getRegion()->getName(), 0, 20) . '...') : $user->getRegion()->getName()) ?></a>
         
-        <div class="headerContactPhone fl" >
+        <? /*<div class="headerContactPhone fl" >
             <p class="fl headerContactPhone__eTitle">Контакт-cENTER</p>
             <p class="fl headerContactPhone__ePhones"><?= \App::config()->company['phone'] ?>
                 <? if (14974 == $user->getRegion()->getId() || 83 == $user->getRegion()->getParentId()): ?>
                 <br/><?= \App::config()->company['moscowPhone'] ?>
                 <? endif ?>
+            </p>
+        </div> */ ?>
+
+        <div class="headerContactPhone fl" >
+            <p class="fl headerContactPhone__ePhones">
+                8 (800) 700-00-09, 8 (495) 775-00-06, 8 (812) 703-77-30
             </p>
         </div>
 
@@ -29,7 +35,7 @@
             <a class="bCall" onclick="typeof(_gaq)=='undefined'?'':_gaq.push(['_trackEvent', 'Zingaya', 'ButtonClick']);typeof(_gat)=='undefined'?'':_gat._getTrackerByName()._setAllowLinker(true); window.open(typeof(_gat)=='undefined'?this.href+'?referrer='+escape(window.location.href):_gat._getTrackerByName()._getLinkerUrl(this.href+'?referrer='+escape(window.location.href)), '_blank', 'width=236,height=220,resizable=no,toolbar=no,menubar=no,location=no,status=no'); return false" href="http://zingaya.com/widget/e990d486d664dfcff5f469b52f6bdb62">Позвонить онлайн</a>
         <? endif ?>
 
-        <a class="headerShopLink" href="<?= $page->url('shop') ?>">Магазины Enter</a>
+        <a class="headerShopLink fr" href="<?= $page->url('shop') ?>">Магазины Enter</a>
         <div class="bSubscribeLightboxPopupNotNow mFl"></div>
     </div>
     <noindex>
