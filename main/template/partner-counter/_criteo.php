b00e219a94383f8d06b8b8c36fd1a6ddc9f639fa
Julian <y.misiukevich@gmail.com>
2013-07-23 16:48:58 +0300

    CRITEO IN IE9!

--- main/template/partner-counter/_criteo.php
+++ main/template/partner-counter/_criteo.php
@@ -1,12 +1,13 @@
 <? if ( \App::config()->partners['criteo']['enabled'] ): ?>
-    <? if ( !empty($criteoData) ):  ?>
+    <? if ( !empty($criteoData) && is_array($criteoData) ):  ?>
 
-        <div id="criteo-data" data-value="<?= $page->json($criteoData) ?>"></div>
+        <? /* <div id="criteo-data" data-value="<?= $page->json($criteoData) ?>"></div> */ ?>
 
         <script src="//static.criteo.net/js/ld/ld.js" async="true"></script> <? /* // https: and http: — works */ ?>
 
         <script>
             window.criteo_q = window.criteo_q || [];
+            <? /* // ### This variant not works in IE9 :-(
 
             var criteo_arr = $('#criteo-data').data('value');
 
@@ -18,26 +19,23 @@
 
                 }
             }
+            */ ?>
 
 
-
-            <? /* //old: //for (var i in criteo_arr){ ] */
-            /* //old:
             window.criteo_q.push(
                 <?
-                //$echrows = [];
-                //foreach($criteoData as $row) $echrows[] = $page->helper->stringRowsParams4js($row);
-                //echo implode(','.PHP_EOL, $echrows);
+                $echrows = [];
+                foreach($criteoData as $row) $echrows[] = $page->helper->stringRowsParams4js($row);
+                echo implode(','.PHP_EOL, $echrows);
                 ?>
             );
-            */
 
-            /* //old:
-            //example:
+
+            /* // example:
             { event: "setAccount", account: 10442 },
-            { event: "setCustomerId", id: "<?= $userId ?>" },
+            { event: "setCustomerId", id: " $userId" },
             { event: "setSiteType", type: "m for mobile or t for tablet or d for desktop" },
-            { event: "<?= $routeName ?>"},
+            { event: "$routeName"},
             { event: "viewItem", item: "Your item id" },
             */
             ?>
