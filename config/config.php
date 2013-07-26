483f310bfae8063bef64ac610d7825a0b7d5cb86
Alexander A. Afonyashin <alexander.afonyashin@enter.ru>
2013-07-26 12:59:47 +0400

    Enabled notification

--- config/config.php
+++ config/config.php
@@ -181,7 +181,7 @@ $c->product['globalListEnabled']      = true;
 $c->product['showAveragePrice']       = false;
 $c->product['allowBuyOnlyInshop']     = false;
 $c->product['reviewEnabled']          = true;
-$c->product['lowerPriceNotification'] = false;
+$c->product['lowerPriceNotification'] = true;
 $c->product['furnitureConstructor']   = true;
 // jewel
 $c->product['itemsPerPageJewel']     = 24;
