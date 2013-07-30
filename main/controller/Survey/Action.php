6b6ced81cdbe914240c202288412ab2405595bae
g84 <green840429@gmail.com>
2013-07-30 12:05:30 +0400

    SITE-1470

--- main/controller/Survey/Action.php
+++ main/controller/Survey/Action.php
@@ -12,29 +12,39 @@ class Action {
 
         $outCsvDelimiter = "\t";
 
-        $question = $request->get('question');
-        $answer = $request->get('answer');
-        $kmId = $request->get('kmId');
-        $userId = \App::user()->getEntity() ? \App::user()->getEntity()->getId() : '';
-        $sessionId = $request->cookies->get('enter');
-        $date = date('Y-m-d H:i:s');
+        try {
+            $question = $request->get('question');
+            $answer = $request->get('answer');
+            $kmId = $request->get('kmId');
+            $userId = \App::user()->getEntity() ? \App::user()->getEntity()->getId() : '';
+            $sessionId = $request->cookies->get('enter');
+            $date = date('Y-m-d H:i:s');
 
-        // передав true в качестве параметра, получаем версию опроса,
-        // кэшированную с момента открытия страницы - чтобы выходной файл соответствовал опросу
-        $survey = \RepositoryManager::survey()->getEntity(true);
+            // передав true в качестве параметра, получаем версию опроса,
+            // кэшированную с момента открытия страницы - чтобы выходной файл соответствовал опросу
+            $survey = \RepositoryManager::survey()->getEntity(true);
 
-        $outCsvFilePath = is_object($survey) ? $survey->getOutputFile() : \App::config()->surveyDir . '/survey.csv';
-        if(!is_dir(\App::config()->surveyDir)) mkdir(\App::config()->surveyDir);
-        if(!is_file($outCsvFilePath)) {
-            touch($outCsvFilePath);
-            file_put_contents($outCsvFilePath, 'Вопрос'.$outCsvDelimiter.'Ответ'.$outCsvDelimiter.'KM ID'.$outCsvDelimiter.'User ID'.$outCsvDelimiter.'Session ID'.$outCsvDelimiter.'Date'."\n");
+            $outCsvFilePath = is_object($survey) ? $survey->getOutputFile() : \App::config()->surveyDir . '/survey.csv';
+            if (!is_dir(\App::config()->surveyDir)) mkdir(\App::config()->surveyDir);
+            if (!is_file($outCsvFilePath)) {
+                touch($outCsvFilePath);
+                file_put_contents($outCsvFilePath, 'Вопрос'.$outCsvDelimiter.'Ответ'.$outCsvDelimiter.'KM ID'.$outCsvDelimiter.'User ID'.$outCsvDelimiter.'Session ID'.$outCsvDelimiter.'Date'."\n");
+            }
+            file_put_contents($outCsvFilePath, $question.$outCsvDelimiter.$answer.$outCsvDelimiter.$kmId.$outCsvDelimiter.$userId.$outCsvDelimiter.$sessionId.$outCsvDelimiter.$date."\n", FILE_APPEND);
+
+            $responseData = [
+                'success' => true,
+            ];
+        } catch (\Exception $e) {
+            $responseData = [
+                'success' => false,
+                'error'   => [
+                    'code'    => $e->getCode(),
+                    'message' => $e->getMessage(),
+                ],
+            ];
         }
-        file_put_contents($outCsvFilePath, $question.$outCsvDelimiter.$answer.$outCsvDelimiter.$kmId.$outCsvDelimiter.$userId.$outCsvDelimiter.$sessionId.$outCsvDelimiter.$date."\n", FILE_APPEND);
 
-        $result = [
-            'success' => true,
-        ];
-
-        return new \Http\JsonResponse($result);
+        return new \Http\JsonResponse($responseData);
     }
 }
