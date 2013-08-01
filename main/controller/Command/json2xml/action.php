9432fd0a9c6f80fb6106636d6dfc9886cba89574
Julian <y.misiukevich@gmail.com>
2013-08-01 17:39:01 +0300

    Добавил проверки и методы

--- main/controller/Command/json2xml/action.php
+++ main/controller/Command/json2xml/action.php
@@ -1,97 +1,17 @@
-<pre>
 <?
 
-// config
-$json_path = '../../../cms.enter.ru/';
-$json_filename = "product-export.json";
-$xml_filename = 'market.xml';
+// for debug
+@error_reporting(E_ALL);
+@ini_set('display_errors', TRUE);
 
 
+include_once "json2xmlClass.php";
 
-//
-$json_filename = $json_path . $json_filename;
+$json2xml = new \Controller\Command\json2xml;
 
+$json2xml->execute();
 
-if ( file_exists($json_filename) ) {
-    echo 'файл открыт';
-}else{
-    echo 'файл ' . $json_filename . ' не найден';
-}
-
-
-$json_file = fopen($json_filename, "r");
-$i = 0;
-
-$xml = new SimpleXMLElement( '<?xml version="1.0" encoding="utf-8" ?><xml_catalog></xml_catalog>' );
-
-
-$xml->addChild('shop');
-$xml->shop->addChild('offers');
-$xml->shop->offers->addChild('offer');
-
-$offers = &$xml->shop->offers;
-//$offer = &$xml->shop->offers->addChild('offer');
-
-
-
-while ( !feof($json_file) and $i<2 ) {
-    $i++;
-    $buffer = fgets($json_file);
-    $json_line = json_decode( $buffer );
-
-    $params = [];
-
-    $params['id'] = $json_line->id;
-    $params['link'] = $json_line->link;
-    $params['media_image'] = $json_line->media_image;
-    $params['price'] = $json_line->geo->{1}->price;
-    $params['vendor'] = $json_line->brand->name ?: $json_line->name_web;
-    $params['description'] = $json_line->description;
-
-    foreach ( $json_line->categories as $cat ) {
-        // Находим главную категорию товара и выходим
-        if ( $cat->is_main ) {
-            $categoryId = $cat->category_id;
-            break;
-        }
-    }
-
-    $offers->addChild( 'offer' );
-    $offer = $offers->offer;
-
-    $offer->addAttribute('id', $id);
-    $offer->addAttribute('available', 'true');
-
-    foreach( $params as $name => $value ) {
-        $offer->addChild( $name , $value );
-    }
-
-    print_r($json_line);
-    //print_r($json_line->link);
-}
-
-//есть ли дубликаты товаров в исходном файле?
-
-fclose($json_file);
-
-
-
-
-// Add stuff to it
-//$xml->option1->addAttribute( 'first_name', 'billy' );
-//$xml->option1->addAttribute( 'middle_name', 'bob' );
-//$xml->option1->addAttribute( 'last_name', 'thornton' );
-//$xml->addChild( 'option2' );
-//$xml->option2->addAttribute( 'fav_dessert', 'cookies' );
-
-// Save
-if ( file_exists($xml_filename) ) {
-    unlink($xml_filename);
-}
-$xml->asXML( $xml_filename );
-
-//echo htmlspecialchars($xml->asXML());
+$json2xml = null;
 
 
 ?>
-</pre>
---
+++ main/controller/Command/json2xml/json2xmlClass.php
@@ -0,0 +1,205 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: juljan
+ * Date: 1.8.13
+ * Time: 16.00
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace Controller\Command;
+
+
+class json2xml
+{
+    private $log_file_name = 'json2xml_log.txt';
+    private $log_file;
+    private $log_format = 'console'
+    ;
+
+
+    function __construct() {
+        define("MAX_IERATIONS", 100);
+        if ( file_exists( $this->log_file_name ) ) unlink( $this->log_file_name );
+        $this->log_file = fopen($this->log_file_name, "w");
+    }
+
+    function __destruct() {
+        fclose( $this->log_file );
+    }
+
+
+    function file_log( $var ) {
+        fputs( $this->log_file,  "$var" );
+    }
+
+
+    private function echlog($var)
+    {
+        $res = '';
+
+        if (is_string($var)) $res .= '### ';
+        $res .= print_r($var, 1);
+
+        if ( $this->log_format != 'console' ) {
+            $res = '<pre>' . $res . '</pre>' . "\n";
+        }
+        $res .= PHP_EOL;
+
+        echo $res;
+    }
+
+
+
+
+
+    public function execute()
+    {
+
+        $time_start = time();
+
+        // config
+        $json_path = '../../../cms.enter.ru/';
+        $json_filename = "product-export.json";
+        $xml_filename = 'market.xml';
+
+        $json_filename = $json_path . $json_filename;
+
+
+        if (file_exists($json_filename)) {
+            self::echlog('Файл ' . $json_filename .' открыт');
+        } else {
+            self::echlog('Файл ' . $json_filename . ' не найден');
+        }
+        $json_file = fopen($json_filename, "r");
+
+
+        $xml = new \SimpleXMLElement('<?xml version="1.0" encoding="utf-8" ?><xml_catalog></xml_catalog>');
+        $xml->addChild('shop');
+        $offers = $xml->shop->addChild('offers');
+
+        $file_size = filesize($json_filename);
+
+        $i = 0;
+        $readed = 0;
+        while ( !feof($json_file) && (MAX_IERATIONS<0 || $i < MAX_IERATIONS) ) {
+            $i++;
+            $buffer = fgets($json_file);
+            $buf_size = strlen($buffer);
+            $readed += $buf_size;
+            $json_line = json_decode($buffer);
+
+
+            IF ( ISSET( $json_line->id ) ) { // у каждого товара должен быть ID, если нету, видимо пустая строка в файле
+
+                $params = [];
+
+                $params['id'] = $json_line->id;
+                $params['link'] = self::addIfIsset( $json_line->link );
+                $params['media_image'] = self::addIfIsset( $json_line->media_image );
+
+                $params['price'] = self::addOneOfElems(
+                    self::addIfIsset( $json_line->geo),
+                    'price'
+                );
+
+                $arr = [];
+                if ( isset( $json_line->brand->name ) )
+                    $arr[] = self::addIfIsset( $json_line->brand->name );
+
+                if ( isset( $json_line->name_web ) )
+                    $arr[] = self::addIfIsset( $json_line->name_web );
+
+                $params['vendor'] = self::addOneOfElems( $arr );
+
+                $params['description'] = self::addIfIsset( $json_line->description );
+                //$params['price'] = $json_line->geo->{1}->price; // old variant, price in Moscow
+
+
+                foreach ($json_line->categories as $cat) {
+                    // Находим главную категорию товара и выходим
+                    if ($cat->is_main) {
+                        $categoryId = $cat->category_id;
+                        break;
+                    }
+                }
+
+
+                $progressbar = ' Readed ' . $readed . ' from '. $file_size. '; '. round( ($readed/$file_size)*100 , 2 ) . '%' ;
+
+
+                //self::echlog($json_line); // log // all product-info FROM JSON file
+
+
+                self::echlog ( 'ProductID ' . $params['id'] . '; ' . $progressbar  );
+                //self::file_log ( 'ProductID ' . $params['id'] . '; ' . $progressbar  );
+
+                //self::echlog( $params ); // log // all product-info FOR XML file
+
+
+
+
+
+                $offer = $offers->addChild('offer');
+                $offer->addAttribute('id', $params['id']);
+                $offer->addAttribute('available', 'true');
+
+                foreach ($params as $name => $value) {
+                    //$offer->addChild($name, $value); // achtung! witch warnings!
+                    $offer->{$name} = $value; // ok! without warnings
+                } // end foreach
+
+
+            } // END IF
+
+
+        } // end while
+
+
+        //TDo?есть ли дубликаты товаров в исходном файле?
+
+
+        fclose($json_file);
+        self::echlog('Файл ' . $json_filename .' закрыт');
+
+
+        // Save
+        if (file_exists($xml_filename)) unlink($xml_filename);
+        $return = $xml->asXML($xml_filename);
+        $xml = null;
+        self::echlog('Файл ' . $xml_filename .' сохранён');
+
+
+        $time_end = time();
+
+        $time_exe = ($time_end - $time_start);
+
+        return $return;
+    }
+
+
+
+    private function addIfIsset( &$var ) {
+        if ( isset($var) ) return $var;
+        return false;
+    }
+
+
+
+    private function addOneOfElems ( $arr, $attrib = null ) {
+
+        foreach ( $arr as $elem ) {
+
+            if ( !$attrib ) $ret = self::addIfIsset( $elem );
+                else $ret = self::addIfIsset( $elem->{$attrib} );
+
+            if ( $ret ) return $ret;
+
+        }
+
+        return false;
+    }
+
+
+
+}
