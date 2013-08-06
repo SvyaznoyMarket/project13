8577c65813f5469f4adfb74a9ac758fb65f122b5
Alex Zaycev <13rentgen@gmail.com>
2013-08-06 14:06:11 +0400

    SITE-1606 Рендеринг DeliveryTypes. Обработчик кнопок выбора способа доставки.

--- main/template/order/page-new.php
+++ main/template/order/page-new.php
@@ -5,3 +5,23 @@
  */
 ?>
 
+<!-- общая обертка оформления заказа -->
+<div>
+
+	<!-- выбор вариантов доставки -->
+	<div data-bind="css: { mLoader: !prepareData() }">
+
+		<!-- отображение типов доставки -->
+		<ul data-bind="foreach: { data: deliveryTypes }">
+			<li>
+				<a href="#" data-bind="
+										text: $data.name,
+										states: $data.states,
+										click: $root.chooseDeliveryTypes">
+				</a>
+			</li>
+		</ul>
+
+	</div>
+
+</div>
