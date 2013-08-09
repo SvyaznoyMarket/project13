52c0ec9310b49d19772418d408befa0aca02fc18
Julian <y.misiukevich@gmail.com>
2013-08-09 16:11:22 +0300

    RetailRocket: комменты в коде

--- main/view/Partners/RetailRocket.php
+++ main/view/Partners/RetailRocket.php
@@ -13,6 +13,7 @@ class RetailRocket
         $this->returnData['routeName'] = $routeName;
     }
 
+
     /**
      * @param \Model\Order\Entity[] $orders
      * @return mixed
@@ -23,25 +24,32 @@ class RetailRocket
 
         //$orderSum = 0;
         foreach ($orders as $order) {
+
             //$orderSum += $order->getPaySum();
             if ($order instanceof \Model\Order\Entity) {
-                $data['transaction'] = $order->getNumber(); // count($orders) по идеее не может быть > 1.
-                // transaction — свойство $order и по смыслу у всего $orders должно быть одно
-                foreach ($order->getProduct() as $prod) {
-                    $data['items'][] = [
-                        //'id' => $prod->getArticle(), // несущуствующий метод! // see comment to task SITE-1572
-                        'id' => $prod->getId(),
-                        'qnt' => $prod->getQuantity(),
-                        'price' => $prod->getPrice(),
-                    ];
-                }
+                continue;
             }
-        }
+            $data['transaction'] = $order->getNumber(); // count($orders) по идеее не может быть > 1.
+            // transaction — свойство $order и по смыслу у всего $orders должно быть одно
+            foreach ($order->getProduct() as $prod) {
+                $data['items'][] = [
+                    //'id' => $prod->getArticle(), // несущуствующий метод! // see comment to task SITE-1572
+                    'id' => $prod->getId(),
+                    'qnt' => $prod->getQuantity(),
+                    'price' => $prod->getPrice(),
+                ];
+            } // end of foreach (products)
+
+        } // end of foreach ($orders)
 
         return $this->returnData;
     }
 
 
+    /**
+     * @param \Model\Product\Entity $product
+     * @return mixed
+     */
     public function product(\Model\Product\Entity $product)
     {
         $data = & $this->returnData['sendData'];
