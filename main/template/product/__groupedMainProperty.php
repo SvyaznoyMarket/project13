4c55c4b9b75420beef8a62940a4e51ccb13328a5
Julian <y.misiukevich@gmail.com>
2013-08-12 16:06:36 +0300

    небольшие комменты

--- main/template/product/__groupedMainProperty.php
+++ main/template/product/__groupedMainProperty.php
@@ -1,7 +1,8 @@
 <?php
 
+/* Файл не используется пока нигде, но может понадобиться в будущем. Правда, возможно стоит прорефакторить либо удалить */
 
-return function(
+return function (
     \Helper\TemplateHelper $helper,
     \Model\Product\Entity $product,
     \Model\Product\ExpandedEntity $productExpanded = null
@@ -21,38 +22,38 @@ return function(
 
         $HtmlOut = '';
         foreach ($group['properties'] as $property):
-            /** @var $property \Model\Product\Property\Entity  */
-            if ( in_array($property->getId(), $mainProperties) ):
+            /** @var $property \Model\Product\Property\Entity */
+            if (in_array($property->getId(), $mainProperties)):
 
                 $dd = $property->getName();
-                if ($property->getHint()){
+                if ($property->getHint()) {
                     $dd .= $helper->render('__hint', ['name' => $property->getName(), 'value' => $property->getHint()]);
                 }
-                $dd = $helper->wrap( $dd , '', 'span');
-                $dd = $helper->wrap( $dd , '', 'dd');
+                $dd = $helper->wrap($dd, '', 'span');
+                $dd = $helper->wrap($dd, '', 'dd');
 
                 $dt = $property->getStringValue();
                 if ($property->getValueHint()) {
                     $dt .= $helper->render('__hint', ['name' => $property->getStringValue(), 'value' => $property->getValueHint()]);
                 }
-                $dt = $helper->wrap( $dt , '', 'dt');
+                $dt = $helper->wrap($dt, '', 'dt');
 
                 $HtmlOut = $dd . $dt;
-                $HtmlOut = $helper->wrap( $HtmlOut, 'bSpecifications__eList clearfix', 'dl' );
+                $HtmlOut = $helper->wrap($HtmlOut, 'bSpecifications__eList clearfix', 'dl');
 
             endif; // ins_array()
 
         endforeach; // $group['properties']
 
-        if ( !empty($HtmlOut) ) $HtmlGroupedProperties .= '<div class="bSpecifications__eHead">' .$groupName . '</div>' . $HtmlOut ;
+        if (!empty($HtmlOut)) $HtmlGroupedProperties .= '<div class="bSpecifications__eHead">' . $groupName . '</div>' . $HtmlOut;
 
     endforeach; // $groupedProperties
 
 
-?>
-<h3 id="productspecification" class="bHeadSection">Характеристики</h3>
-<div class="bSpecifications">
-    <?= $HtmlGroupedProperties ?>
-</div><!--/product specifications section -->
+    ?>
+    <h3 id="productspecification" class="bHeadSection">Характеристики</h3>
+    <div class="bSpecifications">
+        <?= $HtmlGroupedProperties ?>
+    </div><!--/product specifications section -->
 
 <? }; //end function
