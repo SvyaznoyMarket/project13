8e670605f8b127ccd0615e6d2922bb070c50b074
g84 <green840429@gmail.com>
2013-08-14 18:49:10 +0400

    SITE-1694

--- main/controller/Order/PaypalAction.php
+++ main/controller/Order/PaypalAction.php
@@ -8,23 +8,44 @@ class PaypalAction {
 
         $client = \App::coreClientV2();
 
-        $token = trim((string)$request->get('token'));
-        if (!$token) {
-            throw new \Exception\NotFoundException('Не передан параметр token');
+        try {
+            $token = trim((string)$request->get('token'));
+            if (!$token) {
+                throw new \Exception\NotFoundException('Не передан параметр token');
+            }
+
+            $payerId = trim((string)$request->get('PayerID'));
+            if (!$token) {
+                throw new \Exception\NotFoundException('Не передан параметр PayerID');
+            }
+
+            $result = null;
+            $client->addQuery(
+                'payment/paypal-get-info',
+                [
+                    'token'   => $token,
+                    'PayerID' => $payerId,
+                ],
+                [],
+                function($data) use (&$result) { $result = $data; },
+                function(\Exception $e) use (&$result) { $result = $e; }
+            );
+            $client->execute();
+
+            if ($result instanceof \Exception) {
+                throw $result;
+            }
+
+            \App::debug()->add('core.response', $result, 200, \Debug\Collector::TYPE_INFO);
+
+            $page = new \View\Order\PaymentSuccessPage();
+        } catch(\Exception $e) {
+            \App::debug()->add('core.response', $e, 200, \Debug\Collector::TYPE_ERROR);
+
+            $page = new \View\Order\PaymentFailPage();
         }
 
-        $payerId = trim((string)$request->get('PayerID'));
-        if (!$token) {
-            throw new \Exception\NotFoundException('Не передан параметр PayerID');
-        }
-
-        $result = null;
-        $client->addQuery('payment/paypal-get-info', [], [], function($data) use (&$result) {
-            $result = $data;
-        });
-        $client->execute();
-
-        die(var_dump($result));
+        return new \Http\Response($page->show());
     }
 
     public function fail(\Http\Request $request) {
