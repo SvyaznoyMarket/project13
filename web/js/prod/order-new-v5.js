898a9fc5eab8a973a7559c08a337408cfc314ffa
Alex Zaycev <13rentgen@gmail.com>
2013-08-16 16:54:14 +0400

    SITE-1606 Маркировка полей ошибками

--- web/js/prod/order-new-v5.js
+++ web/js/prod/order-new-v5.js
@@ -689,6 +689,152 @@ OrderDictionary.prototype.getProductById = function( productId ) {
  */
  
  
+	/* Sertificate */
+;(function( global ) {
+	if( !$('#paymentMethod-10').length ) {
+		console.warn('нет метода оплаты сертификатом');
+
+		return false;
+	}
+
+	var sertificateWrap = $('#paymentMethod-10').parent(),
+		code = sertificateWrap.find('.mCardNumber'),
+		pin = sertificateWrap.find('.mCardPin'),
+		fieldsWrap = sertificateWrap.find('.bPayMethodAction'),
+		urlCheck = fieldsWrap.attr('data-url');
+	// end of vars
+
+	console.log(sertificateWrap)
+	console.log(code)
+	console.log(pin)
+	console.log(fieldsWrap)
+	console.log(urlCheck)
+
+
+	var SertificateCard = (function() {
+
+		var checked = false,
+			processTmpl = 'processBlock';
+		// end of vars
+
+		var getCode = function getCode() {
+				return code.val().replace(/[^0-9]/g,'');
+			},
+
+			getPIN = function getPIN() {
+				return pin.val().replace(/[^0-9]/g,'');
+			},
+
+			getParams = function getParams() {
+				return { code: getCode() , pin: getPIN() };
+			},
+
+			isActive = function isActive() {
+				if( checked && ( getCode() !== '' ) && getCode().length === 14 && ( getPIN() !== '' ) && getPIN().length === 4) {
+					return true;
+				}
+
+				return false;
+			},
+
+			checkCard = function checkCard() {
+				if ( pin.val().length !== 4) {
+					console.warn('пин код мал');
+					return false;
+				}
+				
+				if ( !code.val().length ) {
+					console.warn('номер карты не введен');
+					return false;
+				}
+
+				setProcessingStatus( 'orange', 'Проверка по номеру карты' );
+				$.post( urlCheck, getParams(), function( data ) {
+					if( !('success' in data) ) {
+						return false;
+					}
+
+					if( !data.success ) {
+						var err = ( typeof(data.error) !== 'undefined' ) ? data.error : 'ERROR';
+
+						setProcessingStatus( 'red', err );
+
+						return false;
+					}
+
+					setProcessingStatus( 'green', data.data );
+				});
+				// pin.focus()
+			},
+
+			setProcessingStatus = function setProcessingStatus( status, data ) {
+				console.info('setProcessingStatus');
+
+				var blockProcess = $('.process').first(),
+					options = { typeNum: status };
+
+				if( !blockProcess.hasClass('picked') ) {
+					blockProcess.remove();
+				}
+
+				switch( status ) {
+					case 'mOrange':
+						options.text = data;
+						checked = false;
+
+						break;
+					case 'mRed':
+						options.text = 'Произошла ошибка: ' + data;
+						checked = false;
+
+						break;
+					case 'mGreen':
+						if( 'activated' in data ) {
+							options.text = 'Карта '+ data.code + ' на сумму ' + data.sum + ' активирована!';
+						}
+						else {
+							options.text = 'Карта '+ data.code + ' имеет номинал ' + data.sum;
+						}
+
+						checked = true;
+
+						break;
+				}
+
+				fieldsWrap.after( tmpl( processTmpl, options) );
+
+				if ( typeof data['activated']  !== 'undefined' ) {
+					$('.process').first().addClass('picked');
+				}
+			};
+		// end of function
+
+		return {
+			checkCard: checkCard,
+			setProcessingStatus: setProcessingStatus,
+			isActive: isActive,
+			getCode: getCode,
+			getPIN: getPIN
+		};
+	})();
+
+	code.bind('change',function() {
+		SertificateCard.checkCard();
+	});
+
+	pin.bind('change',function() {
+		SertificateCard.checkCard();
+	});
+
+	pin.mask('9999', { completed: SertificateCard.checkCard, placeholder: '*' } );
+}(this));
+ 
+ 
+/** 
+ * NEW FILE!!! 
+ */
+ 
+ 
 /**
  * Валидация формы. Отправка на сервер. Аналитика
  */
