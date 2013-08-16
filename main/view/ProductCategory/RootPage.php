e1f2b80ca9041e5f85aadba6e07570801ce9a756
g84 <green840429@gmail.com>
2013-08-16 15:51:14 +0400

    SITE-1780 изменил layout

--- main/view/ProductCategory/RootPage.php
+++ main/view/ProductCategory/RootPage.php
@@ -3,6 +3,12 @@
 namespace View\ProductCategory;
 
 class RootPage extends Layout {
+    public function prepare() {
+        if (\App::config()->productCategory['newShow']) {
+            $this->layout = 'layout-oneColumn';
+        }
+    }
+
     public function slotContent() {
         return $this->render(\App::config()->productCategory['newShow'] ? 'product-category/page-root-new' : 'product-category/page-root', $this->params);
     }
