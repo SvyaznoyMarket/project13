579698b053ddfeb3e309493c841707daf1a813c2
tamili <ta.burlakova@gmail.com>
2013-08-19 17:23:31 +0400

    SITE-1784. Верстка каталога.

--- main/template/product-category/page-branch-new.php
+++ main/template/product-category/page-branch-new.php
@@ -8,4 +8,169 @@
  */
 ?>
 
-Новый дизайн
+<div class="bCatalog">
+
+	<ul class="bBreadcrumbs clearfix">
+		<li class="bBreadcrumbs__eItem"><a class="bBreadcrumbs__eLink" href="">Товары на каждый день</a></li>
+		<li class="bBreadcrumbs__eItem mLast"><a class="bBreadcrumbs__eLink" href="">Товары для дома</a></li>
+	</ul>
+
+	<h1  class="bTitlePage">Товары для дома</h1>
+
+	<ul class="bCatalogList clearfix">
+		<li class="bCatalogList__eItem">
+			<a class="bCatalogList__eLink" href="">
+				<span class="bCategoriesImg">
+					<img class="bCategoriesImg__eImg" src="http://fs10.enter.ru/6/1/163/70/214936.jpg" alt="" />
+				</span>
+
+				<span class="bCategoriesName">Освежители воздуха</span>
+			</a>
+		</li>
+
+		<li class="bCatalogList__eItem">
+			<a class="bCatalogList__eLink" href="">
+				<span class="bCategoriesImg">
+					<img class="bCategoriesImg__eImg" src="http://fs10.enter.ru/6/1/163/70/214936.jpg" alt="" />
+				</span>
+
+				<span class="bCategoriesName">Освежители воздуха</span>
+			</a>
+		</li>
+
+		<li class="bCatalogList__eItem mLast">
+			<a class="bCatalogList__eLink" href="">
+				<span class="bCategoriesImg">
+					<img class="bCategoriesImg__eImg" src="http://fs10.enter.ru/6/1/163/70/214936.jpg" alt="" />
+				</span>
+
+				<span class="bCategoriesName">Освежители воздуха</span>
+			</a>
+		</li>
+
+		<li class="bCatalogList__eItem">
+			<a class="bCatalogList__eLink" href="">
+				<span class="bCategoriesImg">
+					<img class="bCategoriesImg__eImg" src="http://fs10.enter.ru/6/1/163/70/214936.jpg" alt="" />
+				</span>
+
+				<span class="bCategoriesName">Млющие средства для посуды и освежители воздуха</span>
+			</a>
+		</li>
+
+		<li class="bCatalogList__eItem">
+			<a class="bCatalogList__eLink" href="">
+				<span class="bCategoriesImg">
+					<img class="bCategoriesImg__eImg" src="http://fs10.enter.ru/6/1/163/70/214936.jpg" alt="" />
+				</span>
+
+				<span class="bCategoriesName">Освежители воздуха</span>
+			</a>
+		</li>
+
+		<li class="bCatalogList__eItem mLast">
+			<a class="bCatalogList__eLink" href="">
+				<span class="bCategoriesImg">
+					<img class="bCategoriesImg__eImg" src="http://fs10.enter.ru/6/1/163/70/214936.jpg" alt="" />
+				</span>
+
+				<span class="bCategoriesName">Освежители воздуха</span>
+			</a>
+		</li>
+
+		<li class="bCatalogList__eItem">
+			<a class="bCatalogList__eLink" href="">
+				<span class="bCategoriesImg">
+					<img class="bCategoriesImg__eImg" src="http://fs10.enter.ru/6/1/163/70/214936.jpg" alt="" />
+				</span>
+
+				<span class="bCategoriesName">Освежители воздуха</span>
+			</a>
+		</li>
+	</ul>
+ 
+	<div class="bFilter clearfix">
+		<div class="bFilterHead">
+			<a class="bFilterToggle mOpen" href=""><span class="bToggleText">Бренды и параметры</span></a>
+
+			<div class="bFilterPrice">
+			</div>
+		</div>
+	</div>
+
+	<div class="bFilter clearfix">
+		<div class="bFilterHead">
+			<a class="bFilterToggle mOpen" href=""><span class="bToggleText">Бренды и параметры</span></a>
+
+			<div class="bFilterPrice">
+			</div>
+		</div>
+
+		<div class="bFilterCont">
+			<ul class="bFilterParams">
+				<li class="bFilterParams__eItem mActive"><span class="bParamName">Бренд</span></li>
+				<li class="bFilterParams__eItem"><span class="bParamName">Тип</span></li>
+				<li class="bFilterParams__eItem"><span class="bParamName">Для кого</span></li>
+				<li class="bFilterParams__eItem"><span class="bParamName">Объем</span></li>
+				<li class="bFilterParams__eItem"><span class="bParamName">Для какой кожи по возрасту</span></li>
+				<li class="bFilterParams__eItem"><span class="bParamName">Страна-производитель</span></li>
+				<li class="bFilterParams__eItem"><span class="bParamName">Периодичность использования</span></li>
+			</ul>
+
+			<div class="bFilterValues">
+				<div class="bFilterValuesItem clearfix">
+
+					<div class="bFilterValuesCol">
+						<input class="bInputHidden" type="checkbox" id="check1" name="" hidden />
+						<label class="bFilterCheckbox" for="check1">
+							Ahava
+						</label>
+					</div>
+
+					<div class="bFilterValuesCol">
+						<input class="bInputHidden" type="checkbox" id="check2" name="" hidden />
+						<label class="bFilterCheckbox" for="check2">
+							Ahava
+						</label>
+					</div>
+
+					<div class="bFilterValuesCol">
+						<input class="bInputHidden" type="checkbox" id="check3" name="" hidden />
+						<label class="bFilterCheckbox" for="check3">
+							Ahava
+						</label>
+					</div>
+
+					<div class="bFilterValuesCol">
+						<input class="bInputHidden" type="checkbox" id="check4" name="" hidden />
+						<label class="bFilterCheckbox" for="check4">
+							Ahava
+						</label>
+					</div>
+
+					<div class="bFilterValuesCol">
+						<input class="bInputHidden" type="checkbox" id="check5" name="" hidden />
+						<label class="bFilterCheckbox" for="check5">
+							Ahava
+						</label>
+					</div>
+
+					<div class="bFilterValuesCol">
+						<input class="bInputHidden" type="checkbox" id="check6" name="" hidden />
+						<label class="bFilterCheckbox" for="check6">
+							Ahava
+						</label>
+					</div>
+
+					<div class="bFilterValuesCol">
+						<input class="bInputHidden" type="checkbox" id="check7" name="" hidden />
+						<label class="bFilterCheckbox" for="check7">
+							Ahava
+						</label>
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
