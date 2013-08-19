7f96b5cd884c3a3c625004a70fc46b4bd5e01a03
g84 <green840429@gmail.com>
2013-08-19 12:36:57 +0400

    SITE-1784 шаблон для branch-категории

---
+++ main/template/product-category/page-branch-new.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @var $page                    \View\ProductCategory\BranchPage
+ * @var $category                \Model\Product\Category\Entity
+ * @var $productFilter           \Model\Product\Filter
+ * @var $productPagersByCategory \Iterator\EntityPager[]
+ * @var $productVideosByProduct  array
+ */
+?>
+
+Новый дизайн
--- main/view/ProductCategory/BranchPage.php
+++ main/view/ProductCategory/BranchPage.php
@@ -3,7 +3,13 @@
 namespace View\ProductCategory;
 
 class BranchPage extends Layout {
+    public function prepare() {
+        if (\App::config()->productCategory['newShow']) {
+            $this->layout = 'layout-oneColumn';
+        }
+    }
+
     public function slotContent() {
-        return $this->render('product-category/page-branch', $this->params);
+        return $this->render(\App::config()->productCategory['newShow'] ? 'product-category/page-branch-new' : 'product-category/page-branch', $this->params);
     }
 }
