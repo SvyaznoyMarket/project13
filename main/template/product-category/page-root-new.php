466eff14f1f078d225b9416a717cfb4d3b1eab93
tamili <ta.burlakova@gmail.com>
2013-08-19 12:11:23 +0400

    SITE-1780. Верстка рутовой категории

--- main/template/product-category/page-root-new.php
+++ main/template/product-category/page-root-new.php
@@ -5,4 +5,70 @@
  */
 ?>
 
-новый каталог
+<h1  class="bTitlePage">Товары для дома</h1>
+
+<ul class="bCatalogRoot clearfix">
+	<li class="bCatalogRoot__eItem">
+		<a class="bCatalogRoot__eImgLink" href=""><img class="bCatalogRoot__eImg" src="http://fs01.enter.ru/6/1/163/e5/213900.jpg" /></a>
+
+		<a class="bCatalogRoot__eNameLink" href="">Моющие средства для посуды</a>
+
+		<div class="bCatalogRoot__eCount">1240 товаров</div>
+	</li>
+
+	<li class="bCatalogRoot__eItem">
+		<a class="bCatalogRoot__eImgLink" href=""><img class="bCatalogRoot__eImg" src="http://fs10.enter.ru/6/1/163/70/214936.jpg" /></a>
+
+		<a class="bCatalogRoot__eNameLink" href="">Моющие средства для посуды</a>
+
+		<div class="bCatalogRoot__eCount">1240 товаров</div>
+	</li>
+
+	<li class="bCatalogRoot__eItem">
+		<a class="bCatalogRoot__eImgLink" href=""><img class="bCatalogRoot__eImg" src="http://fs01.enter.ru/6/1/163/e5/213900.jpg" /></a>
+
+		<a class="bCatalogRoot__eNameLink" href="">Моющие средства для посуды</a>
+
+		<div class="bCatalogRoot__eCount">1240 товаров</div>
+	</li>
+
+	<li class="bCatalogRoot__eItem mLast">
+		<a class="bCatalogRoot__eImgLink" href=""><img class="bCatalogRoot__eImg" src="http://fs10.enter.ru/6/1/163/70/214936.jpg" /></a>
+
+		<a class="bCatalogRoot__eNameLink" href="">Моющие средства для посуды средства для посуды</a>
+
+		<div class="bCatalogRoot__eCount">1240 товаров</div>
+	</li>
+
+	<li class="bCatalogRoot__eItem">
+		<a class="bCatalogRoot__eImgLink" href=""><img class="bCatalogRoot__eImg" src="http://fs01.enter.ru/6/1/163/e5/213900.jpg" /></a>
+
+		<a class="bCatalogRoot__eNameLink" href="">Моющие средства для посуды</a>
+
+		<div class="bCatalogRoot__eCount">1240 товаров</div>
+	</li>
+
+	<li class="bCatalogRoot__eItem">
+		<a class="bCatalogRoot__eImgLink" href=""><img class="bCatalogRoot__eImg" src="http://fs01.enter.ru/6/1/163/e5/213900.jpg" /></a>
+
+		<a class="bCatalogRoot__eNameLink" href="">Моющие средства для посуды</a>
+
+		<div class="bCatalogRoot__eCount">1240 товаров</div>
+	</li>
+
+	<li class="bCatalogRoot__eItem">
+		<a class="bCatalogRoot__eImgLink" href=""><img class="bCatalogRoot__eImg" src="http://fs01.enter.ru/6/1/163/e5/213900.jpg" /></a>
+
+		<a class="bCatalogRoot__eNameLink" href="">Моющие средства для посуды</a>
+
+		<div class="bCatalogRoot__eCount">1240 товаров</div>
+	</li>
+
+	<li class="bCatalogRoot__eItem mLast">
+		<a class="bCatalogRoot__eImgLink" href=""><img class="bCatalogRoot__eImg" src="http://fs10.enter.ru/6/1/163/70/214936.jpg" /></a>
+
+		<a class="bCatalogRoot__eNameLink" href="">Моющие средства для посуды</a>
+
+		<div class="bCatalogRoot__eCount">1240 товаров</div>
+	</li>
+</ul>
