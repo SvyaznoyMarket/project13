c6fe23a63b12590b134a1a227bce55259e6d6e0f
g84 <green840429@gmail.com>
2013-08-20 16:07:00 +0400

    SITE-1784 шаблон для leaf-категории

---
+++ main/template/product-category/page-leaf-new.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @var $page                   \View\ProductCategory\LeafPage
+ * @var $category               \Model\Product\Category\Entity
+ * @var $productFilter          \Model\Product\Filter
+ * @var $productPager           \Iterator\EntityPager
+ * @var $productSorting         \Model\Product\Sorting
+ * @var $productView            string
+ * @var $productVideosByProduct array
+ */
+?>
+
+новый дизайн
--- main/view/ProductCategory/LeafPage.php
+++ main/view/ProductCategory/LeafPage.php
@@ -3,9 +3,15 @@
 namespace View\ProductCategory;
 
 class LeafPage extends Layout {
+    public function prepare() {
+        if (\App::config()->productCategory['newShow']) {
+            $this->layout = 'layout-oneColumn';
+        }
+    }
+
     public function slotContent() {
         $this->params['request'] = \App::request();
 
-        return $this->render('product-category/page-leaf', $this->params);
+        return $this->render(\App::config()->productCategory['newShow'] ? 'product-category/page-leaf-new' : 'product-category/page-leaf', $this->params);
     }
 }
