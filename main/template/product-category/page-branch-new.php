f649aa75ef4c86c9ea71050c0e314b822194cc82
tamili <ta.burlakova@gmail.com>
2013-08-22 18:38:52 +0400

    SITE-1784 Верстка категорий

--- main/template/product-category/page-branch-new.php
+++ main/template/product-category/page-branch-new.php
@@ -91,10 +91,19 @@
  
 	<div class="bFilter clearfix">
 		<div class="bFilterHead">
-			<a class="bFilterToggle mOpen" href=""><span class="bToggleText">Бренды и параметры</span></a>
+			<a class="bFilterToggle mClose" href=""><span class="bToggleText">Бренды и параметры</span></a>
 
 			<div class="bFilterPrice">
 			</div>
+
+			<ul class="bPopularSection">
+				<li class="bPopularSection__eItem mTitle">Популярные бренды</li>
+				<li class="bPopularSection__eItem"><strong class="bPopularSection__eText">Samsung</strong></li>
+				<li class="bPopularSection__eItem"><strong class="bPopularSection__eText">Nokia</strong></li>
+				<li class="bPopularSection__eItem"><strong class="bPopularSection__eText">Roga und Koppentenganger</strong></li>
+				<li class="bPopularSection__eItem"><strong class="bPopularSection__eText">Dr. Buchman</strong></li>
+				<li class="bPopularSection__eItem"><strong class="bPopularSection__eText"></strong></li>
+			</ul>
 		</div>
 	</div>
 
@@ -169,9 +178,71 @@
 						</label>
 					</div>
 				</div>
-			</div>
 
-			<div class="bBtnPick mBtnGrey "><a href="">Подобрать</a></div>
+				<div class="bBtnPick clearfix"><a class="bBtnPick__eLink mBtnGrey" href="">Подобрать</a></div>
+			</div>
 		</div>
+
+		<div class="bFilterFoot">
+			<ul class="bFilterCheckedParams clearfix">
+				<li class="bFilterCheckedParams__eItem mTitle">Цена</li>
+
+				<li class="bFilterCheckedParams__eItem mParams"><a class="bDelete" href=""></a><span class="bParamsName">от 2 000p</span></li>
+
+				<li class="bFilterCheckedParams__eItem mParams"><a class="bDelete" href=""></a><span class="bParamsName">до 1 000 000p</span></li>
+			</ul>
+
+			<ul class="bFilterCheckedParams clearfix mLast">
+				<li class="bFilterCheckedParams__eItem mTitle">Бренд</li>
+
+				<li class="bFilterCheckedParams__eItem mParams"><a class="bDelete" href=""></a><span class="bParamsName">Ahava</span></li>
+
+				<li class="bFilterCheckedParams__eItem mParams"><a class="bDelete" href=""></a><span class="bParamsName">Bubchen</span></li>
+
+				<li class="bFilterCheckedParams__eItem mParams"><a class="bDelete" href=""></a><span class="bParamsName">Агентство старинных развлечений "Работорцы"</span></li>
+
+				<li class="bFilterCheckedParams__eItem mParams mClearAll"><a class="bDelete" href=""><strong class="bParamsName">Очистить все</strong></a></li> <!-- Добаялется только в списке идущем по очереди последним -->
+			</ul>
+		</div>
+	</div>
+
+	<div class="bSortingLine clearfix">
+		<ul class="bSortingList mSorting">
+			<li class="bSortingList__eItem mTitle">Сортировать</li>
+
+			<li class="bSortingList__eItem mSortItem mActive"><a class="bSortingList__eLink" href="">Автоматически</a></li>
+			<li class="bSortingList__eItem mSortItem"><a class="bSortingList__eLink" href="">Лидеры продаж</a></li>
+			<li class="bSortingList__eItem mSortItem"><a class="bSortingList__eLink" href="">Новинки</a></li>
+			<li class="bSortingList__eItem mSortItem"><a class="bSortingList__eLink" href="">Сначала недорогие</a></li>
+			<li class="bSortingList__eItem mSortItem"><a class="bSortingList__eLink" href="">Сначала дорогие</a></li>
+		</ul>
+
+		<ul class="bSortingList mViewer">
+			<li class="bSortingList__eItem mTitle">Вид</li>
+
+			<li class="bSortingList__eItem mSortItem mActive"><a class="bSortingList__eLink mTable" href=""><span class="bIco mIcoTable"></span></a></li>
+			<li class="bSortingList__eItem mSortItem"><a class="bSortingList__eLink mLine" href=""><span class="bIco mIcoLine"></span></a></li>
+		</ul>
+
+		<ul class="bSortingList mPager">
+			<li class="bSortingList__eItem mTitle">Страницы</li>
+
+			<li class="bSortingList__eItem mSortItem mActive"><a class="bSortingList__eLink" href="">123</a></li>
+			<li class="bSortingList__eItem mSortItem"><a class="bSortingList__eLink" href="">&#8734;</a></li>
+		</ul>
+	</div>
+
+	<div class="bSortingLine clearfix">
+		<ul class="bSortingList mPagerBottom">
+			<li class="bSortingList__eItem mTitle">Страницы</li>
+
+			<li class="bSortingList__eItem mSortItem mActive"><a class="bSortingList__eLink" href="">1</a></li>
+			<li class="bSortingList__eItem mSortItem"><a class="bSortingList__eLink" href="">2</a></li>
+			<li class="bSortingList__eItem mSortItem"><a class="bSortingList__eLink" href="">3</a></li>
+			<li class="bSortingList__eItem mSortItem"><a class="bSortingList__eLink" href="">4</a></li>
+			<li class="bSortingList__eItem mSortItem"><a class="bSortingList__eLink" href="">8</a></li>
+			<li class="bSortingList__eItem mSortItem">&#8230;</li>
+			<li class="bSortingList__eItem mSortItem"><a class="bSortingList__eLink" href="">&#8734;</a></li>
+		</ul>
 	</div>
 </div>
