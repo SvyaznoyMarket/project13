d6e99a6d1c22855cca33a03618dabd3577234beb
Anton <trushkevich@gmail.com>
2013-08-22 17:56:48 +0300

    SITE-1814 изменен провайдер лайков-шарилок на Pluso

--- main/template/product/__likeButtons.php
+++ main/template/product/__likeButtons.php
@@ -1,17 +1,11 @@
 <?php
 return function () {
+?>
 
-/* // LikeButtons (www.addthis.com)
-<!-- AddThis Button BEGIN --> */ ?>
-<div class="addthis_toolbox addthis_default_style mt15 ">
-    <a class="addthis_button_preferred_1"></a>
-    <a class="addthis_button_preferred_2"></a>
-    <? /* <a class="addthis_button_preferred_3"></a> */ ?>
-    <a class="addthis_button_compact"></a>
-    <a class="addthis_counter addthis_bubble_style"></a>
-</div>
-<script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
-<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-51b040940ada4cd1"></script>
-<? /* <!-- AddThis Button END --> */
+<script type="text/javascript">(function()
+{ if (window.pluso) if (typeof window.pluso.start == "function") return; var d = document, s = d.createElement('script'), g = 'getElementsByTagName'; s.type = 'text/javascript'; s.charset='UTF-8'; s.async = true; s.src = ('https:' == window.location.protocol ? 'https' : 'http') + '://share.pluso.ru/pluso-like.js'; var h=d[g]('head')[0] || d[g]('body')[0]; h.appendChild(s); }
+)()
+</script>
+<div class="pluso" data-background="none;" data-options="small,square,line,horizontal,counter,sepcounter=1,theme=14" data-services="facebook,vkontakte,twitter,google,email"></div>
 
-};
+<? };
