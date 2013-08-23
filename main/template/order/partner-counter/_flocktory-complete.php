f2db288da57e49715fd0d496a34f5a5251e7dd73
g84 <green840429@gmail.com>
2013-08-23 15:01:32 +0400

    SITE-1836 - Рефакторин flocktory на order.complete

--- main/template/order/partner-counter/_flocktory-complete.php
+++ main/template/order/partner-counter/_flocktory-complete.php
@@ -1,46 +1,34 @@
-<? if (!empty($userForm) && $userForm instanceof \View\Order\Form && !empty($order)) { ?>
-    <?
-    $productsCounts = [];
-    foreach ($order->getProduct() as $key => $product) {
-        $productsCounts[$product->getId()] = $product->getQuantity();
-    }
-    $products = \RepositoryManager::product()->getCollectionById(array_map(function ($product) {
-        return $product->getId();
-    }, $order->getProduct()));
-    ?>
+<?php
+/**
+ * @var $page         \View\Layout
+ * @var $order        \Model\Order\Entity
+ * @var $productsById \Model\Product\Entity[]
+ */
+?>
 
-    <script type="text/javascript">
-        //<![CDATA[
-        var _flocktory = window._flocktory = _flocktory || [];
-        _flocktory.push({
-            "order_id": "<?= $order->getId() ?>",
-            "email": "<?= $emailOrPhone = $userForm->getEmail() ? $userForm->getEmail() : $userForm->getMobilePhone().'@mail.ru' ?>",
-            "name": "<?= implode(' ', [$userForm->getFirstName(), $userForm->getLastName()]) ?>",
-            "sex": "<?= $userForm->getFirstName() && preg_match('/[аяa]$/', $userForm->getFirstName()) ? 'f' : 'm' ?>",
-            "price":        <?= $order->getProductSum() ?>,
-            "custom_field": "<?= $order->getNumber() ?>",
-            "items": [
-                <? foreach ($products as $key => $product) { ?>
-                {
-                    "id": "<?= $product->getArticle() ?>",
-                    "title": "<?= str_replace('"', "'", $product->getName()) ?>",
-                    "price":  <?= $product->getPrice() ?>,
-                    "image": "<?= $product->getImageUrl() ?>",
-                    "count":  <?= $productsCounts[$product->getId()] ?>
-                }
-                <?= $key < count($products) - 1 ? ',' : '' ?>
-                <? } ?>
-            ]
-        });
+<? if (!empty($userForm) && $userForm instanceof \View\Order\Form && !empty($order)): ?>
 
-        (function () {
-            var s = document.createElement('script');
-            s.type = 'text/javascript';
-            s.async = true;
-            s.src = "//api.flocktory.com/1/hello.2.js";
-            var l = document.getElementsByTagName('script')[0];
-            l.parentNode.insertBefore(s, l);
-        })();
-        //]]>
-    </script>
-<? } ?>
+<div id="jsOrderFlocktory" data-value="<?= $page->json([
+    'order_id'     => $order->getId(),
+    'email'        => $userForm->getEmail() ? $userForm->getEmail() : $userForm->getMobilePhone().'@mail.ru',
+    'name'         => implode(' ', [$userForm->getFirstName(), $userForm->getLastName()]),
+    'sex'          => $userForm->getFirstName() && preg_match('/[аяa]$/', $userForm->getFirstName()) ? 'f' : 'm',
+    'price'        => $order->getProductSum(),
+    'custom_field' => $order->getNumber(),
+    'items'        => array_map(function(\Model\Order\Product\Entity $orderProduct) use (&$productsById) {
+        /** @var $product \Model\Product\Entity|null */
+        $product = isset($productsById[$orderProduct->getId()]) ? $productsById[$orderProduct->getId()] : null;
+
+        if ($product) {
+            return [
+                'id'    => $product->getArticle(),
+                'title' => $product->getName(),
+                'price' => $product->getPrice(),
+                'image' => $product->getImageUrl(),
+                'count' => $orderProduct->getQuantity(),
+            ];
+        }
+    }, $order->getProduct()),
+]) ?>"></div>
+
+<? endif ?>
