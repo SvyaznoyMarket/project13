e7c0baacca19c1e0077968fea6397540140f8d4c
Anton <trushkevich@gmail.com>
2013-09-03 18:13:37 +0300

    SITE-1906 импортер 3д моделей мебели

--- main/controller/Command/Img3dImportAction.php
+++ main/controller/Command/Img3dImportAction.php
@@ -24,149 +24,54 @@ class Img3dImportAction {
         if (!$pathToCms) $pathToCms = \App::config()->img3d['cmsFolder'];
         if (!is_dir($pathToCms)) throw new \RuntimeException('Указан не правильный путь до cms');
 
-echo '>>>>>>>>>>>' . PHP_EOL;
-echo $pathToData . PHP_EOL;
-echo $pathToCms . PHP_EOL;
+        print PHP_EOL . PHP_EOL;
+
+        $copiedCount = 0;
+        $inFilesCount = 0;
+        $noOutFilesCount = 0;
 
         foreach (scandir($pathToData) as $inJsonFile) {
-            if(is_file($pathToData.$inJsonFile)) {
-                $inJson = json_decode(file_get_contents($pathToData.$inJsonFile));
-echo json_encode($inJson) . PHP_EOL . PHP_EOL;
-            }
-        }
+            $inJsonFilePath = $pathToData . $inJsonFile;
+            $outJsonFilePath = $pathToCms . $inJsonFile;
 
-return;
-
-        $i = 0;
-        foreach ($models as $model) {
-            /** @var $model \SimpleXMLElement */
-            if (!isset($model->mtd_id)) continue;
-            $allowedModels[(string)$model->mtd_id][] = [
-                'ean' => (string)$model->value,
-                'file_name' => (string)$model->mtd_filename,
-            ];
-            $eans[(string)$model->value] = true;
-            if ($i == 50) {
-                $eans = array_keys($eans);
-                \RepositoryManager::product()->prepareCollectionByEan($eans, null, function($data) use (&$productsByEan) {
-                    foreach ($data as $item) {
-                        if (isset($item['ean']) && is_array($item['ean'])) {
-                            foreach ($item['ean'] as $ean) {
-                                if (isset($productsByEan[(string)$ean]) && $productsByEan[(string)$ean]->getId() != (int)$item['id']) {
-                                    \App::logger()->error("Same EAN codes on products id:  {$productsByEan[(string)$ean]->getId()} and {$item['id']}");
-                                }
-                                $productsByEan[(string)$ean] = new \Model\Product\BasicEntity($item);
-                            }
-                        }
-                    }
-                }, function(\Exception $e) {
-                    \App::exception()->remove($e);
-                    \App::logger()->error('Fail maybe3d get models');
-                });
-                $client->execute();
-                $i = 0;
-                $eans = [];
-            } else $i++;
-        }
-
-        if (!count($productsByEan)) throw new \RuntimeException('Нет ни одной модели для обновления!');
-
-        $completeCount = 0;
-        $noSwfCount = 0;
-        $newestCount = 0;
-
-        foreach ($allowedModels as $mtd_id => $allowedModel) {
-            if (!is_array($allowedModel)) continue;
-            foreach ($allowedModel as $allowedSingleModel) {
-                if (isset($productsByEan[$allowedSingleModel['ean']])) {
-                    $swfUrl = \App::config()->maybe3d['swfUrl'].$allowedSingleModel['file_name'].'/'.$allowedSingleModel['file_name'].'.swf';
-                    $fileHeaders = @get_headers($swfUrl);
-                    if ($fileHeaders[0] != 'HTTP/1.1 404 Not Found') {
-                        $file_name = rtrim($pathToCms, '\\') . '\\' .$productsByEan[$allowedSingleModel['ean']]->getId() . '.json';
-                        if (file_exists($file_name)) {
-                            try {
-                                $json = file_get_contents($file_name);
-                                try {
-                                    /** @var  $product array */
-                                    $product = json_decode($json, true);
-                                    if (isset($product[0])) {
-                                        $product = $product[0];
-                                    } else \App::logger()->error("Empty json file: {$file_name}");
-                                    if (!isset($product['content'])) {
-                                        $product['content'] = '';
-                                    }
-                                    $product['maybe3d'] = $swfUrl;
-                                    $jsonInsert = json_encode([$product], JSON_PRETTY_PRINT);
-                                    try {
-                                        file_put_contents($file_name, $jsonInsert);
-                                    } catch (\Exception $e) {
-                                        \App::logger()->error("Fail save json to file: {$file_name}");
-                                    }
-                                    $completeCount++;
-                                    $completeProducts[$productsByEan[$allowedSingleModel['ean']]->getId()] = true;
-                                } catch (\Exception $e) {
-                                    \App::logger()->error("Fail json decode file: {$file_name}");
-                                }
-                            } catch (\Exception $e) {
-                                \App::logger()->error("Fail open file: {$file_name}");
-                            }
-                        } else {
-                            $json = json_encode([['content' => '', 'maybe3d' => $swfUrl]], JSON_PRETTY_PRINT);
-                            try {
-                                file_put_contents($file_name, $json);
-                            } catch (\Exception $e) {
-                                \App::logger()->error("Fail save json to file: {$file_name}");
-                            }
-                            $completeCount++;
-                            $newestCount++;
-                            $completeProducts[$productsByEan[$allowedSingleModel['ean']]->getId()] = true;
-                        }
-                    } else {
-                        $noSwfCount++;
-                        \App::logger()->error(".SWF not exists for model: ean => {$allowedSingleModel['ean']}, mtd => {$mtd_id}, name => {$allowedSingleModel['file_name']}, product => {$productsByEan[$allowedSingleModel['ean']]->getId()}");
-                    }
-                }
-            }
-        }
-
-        $deleteCount=0;
-        $productFiles = glob($pathToCms.'*.{json,JSON}', GLOB_BRACE);
-        if (!count($productFiles)) throw new \RuntimeException("В папке {$pathToCms} нет ни одного json для обновления!");
-        foreach ($productFiles as $productFile) {
-            $baseName = str_ireplace(['.JSON','.json'], '', basename($productFile));
-            if ((int)$baseName &&  !isset($completeProducts[(int)$baseName])) {
-                if (file_exists($productFile)) {
+            if (is_file(($inJsonFilePath)) && file_exists($inJsonFilePath)) {
+                $inFilesCount++;
+                if(file_exists($outJsonFilePath)) {
                     try {
-                        $json = file_get_contents($productFile);
+                        $inJson = file_get_contents($inJsonFilePath);
+                        $outJson = file_get_contents($outJsonFilePath);
+
                         try {
-                            /** @var  $product array */
-                            $product = json_decode($json, true);
-                            if (isset($product[0])) {
-                                if (isset($product[0]['maybe3d'])) {
-                                    unset($product[0]['maybe3d']);
-                                    $json = json_encode($product, JSON_PRETTY_PRINT);
-                                    try {
-                                        file_put_contents($productFile, $json);
-                                    } catch (\Exception $e) {
-                                        \App::logger()->error("Fail save json to file (for delete maybe3d url): {$productFile}");
-                                    }
-                                    $deleteCount++;
+                            /** @var  $productJson array */
+                            $inProductJson = json_decode($inJson, true);
+                            $outProductJson = json_decode($outJson, true);
+
+                            if (isset($outProductJson[0])) {
+                                $outProductJson[0]['img3d'] = $inProductJson;
+                                $outJson = json_encode($outProductJson, JSON_PRETTY_PRINT);
+                                try {
+                                    file_put_contents($outJsonFilePath, $outJson);
+                                } catch (\Exception $e) {
+                                    \App::logger()->error("Fail save json to file: {$outJsonFilePath}");
                                 }
+                                $copiedCount++;
                             }
                         } catch (\Exception $e) {
-                            \App::logger()->error("Fail decode json from file (for delete maybe3d url): {$productFile}");
+                            \App::logger()->error("Fail decode json from one of files: {$inJsonFilePath} or {$outJsonFilePath}");
                         }
                     } catch (\Exception $e) {
-                        \App::logger()->error("Fail open file (for delete maybe3d url): {$productFile}");
+                        \App::logger()->error("Fail open file one of files: {$inJsonFilePath} or {$outJsonFilePath} ");
                     }
+                } else {
+                    $noOutFilesCount++;
                 }
+
+            } elseif(is_file(($inJsonFilePath)) && file_exists($inJsonFilePath)) {
             }
         }
 
-        print "All count: ".count($allowedModels)."\n";
-        print "Added models: {$completeCount}\n";
-        print "Not existing .SWF: {$noSwfCount}\n";
-        print "Deleted models: {$deleteCount}";
-
+        print "Total source files: $inFilesCount" . PHP_EOL;
+        print "Copied data from source files: $copiedCount" . PHP_EOL;
+        print "Output files not found: $noOutFilesCount" . PHP_EOL;
     }
 }
