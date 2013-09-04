be7bac2e0713ca11246c7748a698af3061102539
g84 <green840429@gmail.com>
2013-09-04 16:58:44 +0400

    SITE-? изменены телефоны для call-центра

--- config/config.php
+++ config/config.php
@@ -104,8 +104,9 @@ $c->dataStore['retryTimeout'] = [
     'forever' => 0,
 ];
 
-$c->company['phone'] = '8 (800) 700-00-09';
-$c->company['moscowPhone'] = '8 (495) 775-00-06';
+//$c->company['phone'] = '8 (800) 700-00-09';
+//$c->company['moscowPhone'] = '8 (495) 775-00-06';
+$c->company['phone'] = '8 (495) 785-00-06';
 $c->company['icq'] = '648198963';
 
 $c->jsonLog['enabled'] = true;
