e2e61055e7aafd164d8ff5b3018298703178e67f
g84 <green840429@gmail.com>
2013-09-05 12:57:19 +0400

    SITE-1698 добавил в форму данные о пользователе из PayPal

--- main/controller/Order/Paypal/NewAction.php
+++ main/controller/Order/Paypal/NewAction.php
@@ -44,6 +44,18 @@ class NewAction {
             // форма
             $form = $this->getForm();
 
+            if (!$cartProduct->getDeliverySum()) {
+                if (!empty($result['payment_user_email'])) {
+                    $form->setEmail($result['payment_user_email']);
+                }
+                if (!empty($result['payment_user_firstname'])) {
+                    $form->setFirstName($result['payment_user_firstname']);
+                }
+                if (!empty($result['payment_user_lastname'])) {
+                    $form->setLastName($result['payment_user_lastname']);
+                }
+            }
+
             /** @var $productsById \Model\Product\Entity[] */
             $productsById = [];
             \RepositoryManager::product()->prepareCollectionById([$cartProduct->getId()], $region, function($data) use(&$productsById) {
