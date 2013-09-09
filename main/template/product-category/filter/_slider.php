cfbcbb14b28944d653cdd94c1b8d76368c75df17
Julian <y.misiukevich@gmail.com>
2013-09-09 17:55:55 +0300

    SITE-1740 округление до десятых, если не крайние значения фильтра

--- main/template/product-category/filter/_slider.php
+++ main/template/product-category/filter/_slider.php
@@ -8,6 +8,15 @@
  * @var $index         int
  * @var $formName      string
  */
+
+$from = $page->helper->clearZeroValue($productFilter->getValueMin($filter));
+$to = $page->helper->clearZeroValue($productFilter->getValueMax($filter));
+
+$min_from = $page->helper->clearZeroValue($filter->getMin());
+$max_to = $page->helper->clearZeroValue($filter->getMax());
+
+if ($from > $min_from) $from = round($from,1);
+if ($to< $max_to) $to = round($to,1);
 ?>
 
 <dt<? if (5 > $index) { ?> class="<?= ((1 == $index) ? ' first' : '') ?>"<? } ?>>
@@ -16,15 +25,15 @@
 
 <dd style="display: <?= $isOpened ? 'block' : 'none' ?>;">
     <div class="bSlide">
-        <input type="hidden" name="<?= $formName ?>[<?= $filter->getId() ?>][from]" value="<?= $page->helper->clearZeroValue($productFilter->getValueMin($filter)) ?>" id="f_<?= $filter->getId()?>_from"/>
-        <input type="hidden" name="<?= $formName ?>[<?= $filter->getId() ?>][to]" value="<?= $page->helper->clearZeroValue($productFilter->getValueMax($filter)) ?>" id="f_<?= $filter->getId()?>_to"/>
+        <input type="hidden" name="<?= $formName ?>[<?= $filter->getId() ?>][from]" value="<?= $from ?>" id="f_<?= $filter->getId()?>_from"/>
+        <input type="hidden" name="<?= $formName ?>[<?= $filter->getId() ?>][to]" value="<?= $to ?>" id="f_<?= $filter->getId()?>_to"/>
 
         <div class="sliderbox">
             <div id="slider-<?= uniqid()?>" class="filter-range"></div>
         </div>
         <div class="pb5">
-            <input class="slider-from" type="hidden" disabled="disabled" value="<?= $page->helper->clearZeroValue($filter->getMin())?>"/>
-            <input class="slider-to" type="hidden" disabled="disabled" value="<?= $page->helper->clearZeroValue($filter->getMax())?>"/>
+            <input class="slider-from" type="hidden" disabled="disabled" value="<?= $min_from ?>"/>
+            <input class="slider-to" type="hidden" disabled="disabled" value="<?= $max_to ?>"/>
             <span class="slider-interval"  <?php if ($filter->getStepType()): ?>  data-step="<?= $page->json($filter->getStepByFilter()) ?>" <?php endif ?>  ></span>
 
             <?if ($filter->getId() == 'price') { ?>
