4db8d626a70b23549e75777a4da587e45a0e38e4
g84 <green840429@gmail.com>
2013-09-12 11:00:22 +0400

    SITE-1897 разбивка на партиалы

--- main/template/product-category/__filter.php
+++ main/template/product-category/__filter.php
@@ -2,7 +2,6 @@
 
 return function(
     \Helper\TemplateHelper $helper,
-    \Model\Product\Category\Entity $category,
     \Model\Product\Filter $productFilter
 ) {
     /** @var $filters \Model\Product\Filter\Entity[] */
@@ -70,29 +69,7 @@ return function(
         </div>
         <!-- /Фильтр по выбранным параметрам -->
 
-        <!-- Списоки выбранных параметров -->
-        <div class="bFilterFoot">
-            <ul class="bFilterCheckedParams clearfix">
-                <li class="bFilterCheckedParams__eItem mTitle">Цена</li>
-
-                <li class="bFilterCheckedParams__eItem mParams"><a class="bDelete" href=""></a><span class="bParamsName">от 2 000p</span></li>
-
-                <li class="bFilterCheckedParams__eItem mParams"><a class="bDelete" href=""></a><span class="bParamsName">до 1 000 000p</span></li>
-            </ul>
-
-            <ul class="bFilterCheckedParams clearfix mLast">
-                <li class="bFilterCheckedParams__eItem mTitle">Бренд</li>
-
-                <li class="bFilterCheckedParams__eItem mParams"><a class="bDelete" href=""></a><span class="bParamsName">Ahava</span></li>
-
-                <li class="bFilterCheckedParams__eItem mParams"><a class="bDelete" href=""></a><span class="bParamsName">Bubchen</span></li>
-
-                <li class="bFilterCheckedParams__eItem mParams"><a class="bDelete" href=""></a><span class="bParamsName">Агентство старинных развлечений "Работорцы"</span></li>
-
-                <li class="bFilterCheckedParams__eItem mParams mClearAll"><a class="bDelete" href=""><strong class="bParamsName">Очистить все</strong></a></li> <!-- Добаялется только в списке идущем по очереди последним -->
-            </ul>
-        </div>
-        <!-- /Списоки выбранных параметров -->
+        <?= $helper->render('product-category/__selectedFilter', ['productFilter' => $productFilter]) ?>
     </div>
 
 
