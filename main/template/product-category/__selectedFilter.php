289ef23d7181ebb6e038483665ad983815486f13
g84 <green840429@gmail.com>
2013-09-20 17:57:23 +0400

    SITE-1773

--- main/template/product-category/__selectedFilter.php
+++ main/template/product-category/__selectedFilter.php
@@ -5,18 +5,13 @@ return function(
     \Model\Product\Filter $productFilter,
     $baseUrl
 ) {
-    $data = (new \View\ProductCategory\SelectedFilterAction())->execute(
-        $helper,
-        $productFilter,
-        $baseUrl
-    );
-
-    if (empty($data['filters'])) {
-        return;
-    }
 ?>
     <div class="bFilterFoot">
-        <?= $helper->renderWithMustache('product-category/_selectedFilter', $data) ?>
+        <?= $helper->renderWithMustache('product-category/_selectedFilter', (new \View\ProductCategory\SelectedFilterAction())->execute(
+            $helper,
+            $productFilter,
+            $baseUrl
+        )) ?>
     </div>
     <script id="tplSelectedFilter" type="text/html" data-partial="<?= $helper->json([]) ?>">
         <?= file_get_contents(\App::config()->templateDir . '/product-category/_selectedFilter.mustache') ?>
--- main/template/product/__pagination.php
+++ main/template/product/__pagination.php
@@ -5,7 +5,10 @@ return function(
     \Iterator\EntityPager $pager
 ) { ?>
 
-    <?= $helper->renderWithMustache('_pagination', (new \View\PaginationAction())->execute($helper, $pager)) ?>
+    <?= $helper->renderWithMustache('_pagination', (new \View\PaginationAction())->execute(
+        $helper,
+        $pager
+    )) ?>
 
     <script id="tplPagination" type="text/html" data-partial="">
         <?= file_get_contents(\App::config()->templateDir . '/_pagination.mustache') ?>
