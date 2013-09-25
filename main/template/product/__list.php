e25155d36f0e933c0003fde1feb07f88b6191f3b
Vitaly Shaposhnik <vitaly.shaposhnik@gmail.com>
2013-09-25 17:09:14 +0300

    SITE-2129 Серии товаров в новых листингах

--- main/template/product/__list.php
+++ main/template/product/__list.php
@@ -3,13 +3,14 @@
 return function(
     \Helper\TemplateHelper $helper,
     \Iterator\EntityPager $pager,
-    array $productVideosByProduct
+    array $productVideosByProduct,
+    $view
 ) { ?>
     <ul class="bListing clearfix">
-        <?= $helper->renderWithMustache('product/list/_compact', (new \View\Product\ListAction())->execute($helper, $pager, $productVideosByProduct)) ?>
+        <?= $helper->renderWithMustache('product/list/' . ($view == 'line' ? '_line' : '_compact'), (new \View\Product\ListAction())->execute($helper, $pager, $productVideosByProduct)) ?>
     </ul>
 
     <script id="listing_compact_tmpl" type="text/html" data-partial="<?= $helper->json(['cart/_button-product' => file_get_contents(\App::config()->templateDir . '/cart/_button-product.mustache')]) ?>">
-        <?= file_get_contents(\App::config()->templateDir . '/product/list/_compact.mustache') ?>
+        <?= file_get_contents(\App::config()->templateDir . '/product/list/' . ($view == 'line' ? '_line.mustache' : '_compact.mustache')) ?>
     </script>
 <? };
