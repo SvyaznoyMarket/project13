430353e33851561185a41399dd725660fc600a4d
vit <vit@vit-System-Product-Name.Dlink>
2013-10-01 14:28:18 +0300

    SITE-2106. Не выводить 'дата с' для самовывоза и забрать сегодня

--- main/template/order/page-new.php
+++ main/template/order/page-new.php
@@ -122,11 +122,11 @@ foreach (array_reverse($productsById) as $product) {
 					</div>
 
 					<div class="bSelectWrap mFastInpSmall" data-bind="if: box.choosenDate().intervals.length, visible: box.choosenDate().intervals.length">
-						<span class="bSelectWrap_eText" data-bind="text: 'c '+ box.choosenInterval().start + ' до ' + box.choosenInterval().end"></span>
+						<span class="bSelectWrap_eText" data-bind="text: (box.state !== 'self' && box.state !== 'now' ? 'c ' + box.choosenInterval().start + ' ' : '') + 'до ' + box.choosenInterval().end"></span>
 						<select class="bSelect" data-bind="options: box.choosenDate().intervals,
 															value: box.choosenInterval,
 															optionsText: function(item) {
-																return 'c '+ item.start + ' до ' + item.end;
+																return (box.state !== 'self' && box.state !== 'now' ? 'c ' + box.choosenInterval().start + ' ' : '') + 'до ' + item.end;
 															}">
 						</select>
 					</div>
