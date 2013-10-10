248761a0d86945b9df4f18324b945dfc7ae59118
Vitaly Shaposhnik <vitaly.shaposhnik@gmail.com>
2013-10-10 19:53:13 +0300

    SITE-2283 В поисковой строке должно сохраняться искомое слово

--- main/template/search/form-default.php
+++ main/template/search/form-default.php
@@ -12,13 +12,12 @@
 
 // По запросу Гертмана. Согласовано с Мостицким.
 // https://jira.enter.ru/browse/SITE-963
-$searchQuery = 'Поиск среди десятков тысяч товаров';
 $isWide = (isset($isWide) && $isWide) ? true : false;
 ?>
 
 <form class="search-form clearfix" action="<?= $page->url('search') ?>" method="get">
     <span class="searchtextWrapper">
-      <input type="text" class="searchtext<? if ($isWide) { ?> width483<? } ?>" name="q" placeholder="<?= $searchQuery ?>" autocomplete="off"/>
+      <input type="text" class="searchtext<? if ($isWide) { ?> width483<? } ?>" name="q" placeholder="Поиск среди десятков тысяч товаров" value="<?= $searchQuery ?>" autocomplete="off"/>
     </span>
     <input type="submit" class="searchbutton" value="Найти" title="Найти" id="try-1"/>
 </form>
