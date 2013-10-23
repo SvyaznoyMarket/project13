9396bcfe1426961ae23de6c85c4c6b55ad7dd7d5
Alex Zaycev <13rentgen@gmail.com>
2013-10-23 16:38:08 +0400

    SITE-2189 Доработка валидатора

--- main/template/form-login.php
+++ main/template/form-login.php
@@ -15,12 +15,12 @@ if (!isset($form)) $form = new \View\User\LoginForm();
         <legend class="bFormLogin__ePlaceTitle">У меня есть логин и пароль</legend>
 
         <label class="bFormLogin__eLabel">E-mail или мобильный телефон:</label>
-        <input class="text bFormLogin__eInput jsSigninUsername" type="text" value="<?= $form->getUsername() ?>" name="signin[username]" />
+        <div><input class="text bFormLogin__eInput jsSigninUsername" type="text" value="<?= $form->getUsername() ?>" name="signin[username]" /></div>
 
         <label class="bFormLogin__eLabel">Пароль:</label>
         <a class="bFormLogin__eLinkHint mForgotPassword jsForgotPwdTrigger" href="javascript:void(0)">Забыли пароль?</a>
 
-        <input class="text bFormLogin__eInput jsSigninPassword" type="password" name="signin[password]" />
+        <div><input class="text bFormLogin__eInput jsSigninPassword" type="password" name="signin[password]" /></div>
 
         <input type="submit" class="bigbutton bFormLogin__eBtnSubmit jsSubmit" data-loading-value="Вхожу..." value="Войти" />
     </fieldset>
