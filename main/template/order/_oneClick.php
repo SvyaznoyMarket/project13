870ae5f94f959d6618526494fcc1f344fb72382a
Julian <y.misiukevich@gmail.com>
2013-11-04 14:34:57 +0300

    Исправление конфликтов мержа branch 'master' of into misiukevich-site2365-GA
    Conflicts:
    	main/template/order/_oneClick.php
    	web/js/dev/oneclick/oneclick.js

--- main/template/order/_oneClick.php
+++ main/template/order/_oneClick.php
@@ -7,6 +7,20 @@
  * @var $orderProduct       \Model\Order\Product\Entity
  * @var $product            \Model\Product\Entity
  */
+
+$analyticsData = null;
+$categoryName = false;
+$orderSum = str_replace(',', '.', $order->getSum()); // Полная сумма заказа (дроби через точку)
+
+if ($orderProduct && $product){
+    $categories = $product->getCategory();
+    /** @var $rootCategory \Model\Product\Category\Entity */
+    $rootCategory = array_shift($categories);
+    /** @var $category \Model\Product\Category\Entity */
+    $category = array_pop($categories);
+    $categoryName = $rootCategory ? ($rootCategory->getName() . ($category ? (' - ' . $category->getName()) : '')) : '';
+}
+
 ?>
 
 <div style="width: 900px;">
@@ -19,93 +33,104 @@
         <div class="line"></div>
         <p class="font14">Спасибо за размещение заказа! Оператор подтвердит его звонком или смс на Ваш контактный номер!</p>
     </div>
+    <?
 
-    <? if (\App::config()->analytics['enabled']): ?>
+    if (\App::config()->analytics['enabled']) {
+
+        ?>
         <div id="adriverOrder" data-vars="<?= $page->json($orderData) ?>" class="jsanalytics"></div>
         <div id="adblenderOrder" data-vars="<?= $page->json($orderData) ?>" class="jsanalytics"></div>
         <!-- Efficient Frontiers -->
         <img src="http://pixel.everesttech.net/3252/t?ev_Orders=0&amp;ev_Revenue=0&amp;ev_Quickorders=1&amp;ev_Quickrevenue=<?= $order->getSum() ?>&amp;ev_transid=<?= $order->getNumber() ?>" width="1" height="1" />
         <img src="http://rs.mail.ru/g632.gif" style="width:0;height:0;position:absolute;" alt=""/>
-
         <? /* adriverOrder: */ ?>
         <noscript>
             <img src="http://ad.adriver.ru/cgi-bin/rle.cgi?sid=182615&sz=order&bt=55&pz=0&rnd=1086697038&custom=150=<?= $order->getNumber() ?>" border="0" width="1" height="1" alt="" />
         </noscript>
-    <? endif ?>
+        <?
 
 
-    <script type="text/javascript">
-        function runAnalitics() {
-            <? if (\App::config()->googleAnalytics['enabled']): ?>
-            if (typeof(_gaq) !== 'undefined') {
-                _gaq.push(['_addTrans',
-                    '<?= $order->getNumber() . '_F' ?>', // Номер заказа
-                    '<?= $shop ? $page->escape($shop->getName()) : '' ?>', // Название магазина (Необязательно)
-                    '<?= str_replace(',', '.', $order->getSum()) ?>', // Полная сумма заказа (дроби через точку)
-                    '0', // Стоимость доставки (дроби через точку)
-                    '<?= $order->getCity() ? $page->escape($order->getCity()->getName()) : '' ?>', // Город доставки (Необязательно)
-                    '', // Область (необязательно)
-                    '' // Страна (нобязательно)
-                ]);
-                _gaq.push(['_trackEvent', 'QuickOrder', 'Success']);
+        if ( \App::config()->googleAnalytics['enabled'] ) {
 
-                <? if ($orderProduct && $product): ?>
-                <?
-                    $categories = $product->getCategory();
-                    /** @var $rootCategory \Model\Product\Category\Entity */
-                    $rootCategory = array_shift($categories);
-                    /** @var $category \Model\Product\Category\Entity */
-                    $category = array_pop($categories);
-                    $categoryName = $rootCategory ? ($rootCategory->getName() . ($category ? (' - ' . $category->getName()) : '')) : '';
+            // code for _gaq.push( ['_addTrans'], )
+            $analyticsData = [
+                '_addTrans',
+                $order->getNumber() . '_F', //  ID транзакции, Номер заказа
+                $shop ? $page->escape( $shop->getName() ) : '', // Название магазина (Необязательно)
+                $orderSum, // Полная сумма заказа (дроби через точку)
+                //'0',                  // old // Стоимость доставки (дроби через точку)
+                $orderSum*0.18,         // new // Налог. Общая сумма из пукнта выше умноженная на 0,18.
+                $order->getCity() ? $page->escape( $order->getCity()->getName() ) : '', // Город доставки (Необязательно)
+                '', // Область (необязательно)
+                '' // Страна (нобязательно)
+            ];
+            ?>
+            <div id="GA_addTransJS" class="" data-vars="<?= $page->json( $analyticsData ) ?>"></div>
+            <?
+
+
+
+            if ( $categoryName ) {
+                // code for _gaq.push( ['_addItem'], )
+                $analyticsData = [
+                    '_addItem',
+                    $order->getNumber() . '_F', // Номер заказа
+                    $product->getArticle(), // Артикул
+                    $page->escape( $product->getName() ), // Название товара
+                    $page->escape( $categoryName ), // Категория товара
+                    str_replace( ',', '.', $orderProduct->getPrice() ), // Стоимость 1 единицы товара
+                    str_replace( ',', '.', $orderProduct->getQuantity() ), // Количество товара
+                ];
                 ?>
-                _gaq.push(['_addItem',
-                    '<?= $order->getNumber() . '_F' ?>', // Номер заказа
-                    '<?= $product->getArticle() ?>', // Артикул
-                    '<?= $page->escape($product->getName()) ?>', // Название товара
-                    '<?= $page->escape($categoryName) ?>', // Категория товара
-                    '<?= str_replace(',', '.', $orderProduct->getPrice()) ?>', // Стоимость 1 единицы товара
-                    '<?= str_replace(',', '.', $orderProduct->getQuantity()) ?>'               // Количество товара
-                ]);
-                <? endif ?>
-
-                _gaq.push(['_trackTrans']);
+                <div id="GA_addItemJS" class="" data-vars="<?= $page->json( $analyticsData ) ?>"></div>
+                <?
             }
-            <? endif ?>
 
-            <? if (\App::config()->yandexMetrika['enabled']): ?>
-            var yaParams = {
-                order_id:'<?= $order->getNumber() ?>',
-                order_price: <?= str_replace(',', '.', $order->getSum()) ?>,
-                currency:'RUR',
-                exchange_rate:1,
-                goods:[
-                    <? if ($orderProduct && $product): ?>
-                    {
-                        id:'<?= $product->getArticle() ?>',
-                        name:'<?= $page->escape($product->getName()) ?>',
-                        price: <?= str_replace(',', '.', $orderProduct->getPrice()) ?>,
-                        quantity: <?= $orderProduct->getQuantity() ?>
-                    }
-                    <? endif ?>
-                ]
-            };
-            if (typeof(yaCounter10503055) !== 'undefined')  yaCounter10503055.reachGoal('QORDER', yaParams);
-            <? endif ?>
+        } // end of If googleAnalytics enabled
 
-            <? if (\App::config()->analytics['enabled']): ?>
-            if (typeof(window.adBelnder) != 'undefined') window.adBelnder.addOrder(<?= str_replace(',', '.', $order->getSum()) ?>);
 
-            <? endif ?>
-        }
-    </script>
 
-    <!-- <div class="line"></div> -->
 
+
+        if ( \App::config()->yandexMetrika['enabled'] ) {
+            $analyticsData = [
+                'order_id' => $order->getNumber(),
+                'order_price' => $orderSum,
+                'currency' => 'RUR',
+                'exchange_rate' => 1,
+            ];
+            if ( $orderProduct && $product ) {
+                $analyticsData['goods'] = [
+                    'id' => $product->getArticle(),
+                    'name' => $page->escape( $product->getName() ),
+                    'price' => str_replace( ',', '.', $orderProduct->getPrice() ),
+                    'quantity' => $orderProduct->getQuantity(),
+                ];
+            }
+            ?>
+            <div id="YA_paramsJS" class="" data-vars="<?= $page->json( $analyticsData ) ?>"></div>
+            <?
+        } // end of If yandexMetrika enabled
+
+
+
+
+        ?>
+        <div id="adBelnderJS" class="" data-vars="<?= $page->json( $orderSum ) ?>"></div>
+        <?
+
+
+    }// end of If analytics enabled
+
+
+    /* <!-- <div class="line"></div> --> */
+    ?>
     <div class="bFormB2">
         <div class="ac mb25">
             <a class="bBigOrangeButton" href="<?= $page->url('homepage') ?>" onclick="$('#order1click-container-new').trigger('close'); return false">Продолжить покупки</a>
         </div>
     </div>
 
-    <?= $page->tryRender('order/partner-counter/_reactive-oneClick', ['orderSum' => str_replace(',', '.', $order->getSum()), 'orderNum' => $order->getNumber()]) ?>
+    <?= $page->tryRender('order/partner-counter/_reactive-oneClick', ['orderSum' => $orderSum, 'orderNum' => $order->getNumber()]) ?>
 </div>
+<?
