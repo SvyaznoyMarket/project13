6abf159803bad9a6e4da6e526742a293cb98f8c8
tamili <ta.burlakova@gmail.com>
2013-11-07 13:57:05 +0400

    SITE-2530 Верстка Пандоролистинков

--- main/template/product-category/page-leaf-new.php
+++ main/template/product-category/page-leaf-new.php
@@ -18,34 +18,38 @@
     if ($productFilter->getShop()) $page->setGlobalParam('shop', $productFilter->getShop());
 ?>
 
-<div class="bCatalog">
+<div class="bCatalog mCustomCss">
 
     <?= $helper->render('product-category/__breadcrumbs', ['category' => $category]) // хлебные крошки ?>
 
-	<h1  class="bTitlePage"><?= $title ?></h1>
+    <div class="bCustomFilter" style="background-image: url(http://content.enter.ru/wp-content/uploads/2013/07/main.jpg); background-color: #4c635b;">
+        
+    	<h1 class="bTitlePage"><?= $title ?></h1>
 
-    <? if (\App::config()->adFox['enabled']): ?>
-    <!-- Баннер --><div id="adfox683sub" class="adfoxWrapper bBannerBox"></div><!--/ Баннер -->
-    <? endif ?>
+        <? if (\App::config()->adFox['enabled']): ?>
+        <!-- Баннер --><div id="adfox683sub" class="adfoxWrapper bBannerBox"></div><!--/ Баннер -->
+        <? endif ?>
 
-    <? if (!empty($promoContent)): ?>
-        <?= $promoContent ?>
-    <? else: ?>
-        <?= $helper->render('product-category/__children', ['category' => $category]) // дочерние категории ?>
-    <? endif ?>
+        <? if (!empty($promoContent)): ?>
+            <?= $promoContent ?>
+        <? else: ?>
+            <?= $helper->render('product-category/__children', ['category' => $category]) // дочерние категории ?>
+        <? endif ?>
 
-    <?= $helper->render('product-category/__filter', [
-        'baseUrl'       => $helper->url('product.category', ['categoryPath' => $category->getPath()]),
-        'countUrl'      => $helper->url('product.category.count', ['categoryPath' => $category->getPath()]),
-        'productFilter' => $productFilter,
-        'hotlinks'      => $hotlinks,
-        'openFilter'    => false,
-    ]) // фильтры ?>
+        <?= $helper->render('product-category/__filter', [
+            'baseUrl'       => $helper->url('product.category', ['categoryPath' => $category->getPath()]),
+            'countUrl'      => $helper->url('product.category.count', ['categoryPath' => $category->getPath()]),
+            'productFilter' => $productFilter,
+            'hotlinks'      => $hotlinks,
+            'openFilter'    => false,
+        ]) // фильтры ?>
+    
 
-    <?= $helper->render('product/__listAction', [
-        'pager'          => $productPager,
-        'productSorting' => $productSorting,
-    ]) // сортировка, режим просмотра, режим листания ?>
+        <?= $helper->render('product/__listAction', [
+            'pager'          => $productPager,
+            'productSorting' => $productSorting,
+        ]) // сортировка, режим просмотра, режим листания ?>
+    </div>
 
     <?= $helper->render('product/__list', [
         'pager'                  => $productPager,
