ac2ea4a93f0e104d5711cc97c8b7cb2bb0da99a3
Vitaly Shaposhnik <vitaly.shaposhnik@gmail.com>
2013-11-11 12:14:28 +0200

    SITE-2453 Добавил disabledFilter.values в ответ ajax-запроса фильтрации

--- main/controller/ProductCategory/Action.php
+++ main/controller/ProductCategory/Action.php
@@ -304,10 +304,30 @@ class Action {
         // запрашиваем дерево категорий
         \RepositoryManager::productCategory()->prepareEntityBranch($category, $region);
 
+        // получаем фильтры из http-запроса
+        $filterUrl = $this->getFilterFromUrl($request);
+
+        // заполняем параметр filters для запроса к ядру
+        $filterParams = [];
+        if (!empty($filterUrl)) {
+            foreach ($filterUrl as $name => $values) {
+                if (isset($values['from']) || isset($values['to'])) {
+                    $filterParams[] = [
+                        $name,
+                        2,
+                        isset($values['from']) ? $values['from'] : null,
+                        isset($values['to']) ? $values['to'] : null
+                    ];
+                } else {
+                    $filterParams[] = [$name, 1, $values];
+                }
+            }
+        }
+
         // запрашиваем фильтры
         /** @var $filters \Model\Product\Filter\Entity[] */
         $filters = [];
-        \RepositoryManager::productFilter()->prepareCollectionByCategory($category, $region, function($data) use (&$filters) {
+        \RepositoryManager::productFilter()->prepareCollectionByCategory($category, $region, $filterParams, function($data) use (&$filters) {
             foreach ($data as $item) {
                 $filters[] = new \Model\Product\Filter\Entity($item);
             }
@@ -745,21 +765,18 @@ class Action {
 
         // ajax
         if ($request->isXmlHttpRequest() && 'true' == $request->get('ajax')) {
-            $selectedFilter = (new \View\ProductCategory\SelectedFilterAction())->execute(
-                \App::closureTemplating()->getParam('helper'),
-                $productFilter,
-                \App::router()->generate('product.category', ['categoryPath' => $category->getPath()])
-            );
-
-            return new \Http\JsonResponse([
+            $data = [
                 'list'           => (new \View\Product\ListAction())->execute(
                     \App::closureTemplating()->getParam('helper'),
                     $productPager,
                     $productVideosByProduct,
                     !empty($catalogJson['bannerPlaceholder']) ? $catalogJson['bannerPlaceholder'] : []
                 ),
-                'selectedFilter' => $selectedFilter,
-                'disabledFilter' => ['values' => isset($selectedFilter['values']) ? $selectedFilter['values'] : []],
+                'selectedFilter' => (new \View\ProductCategory\SelectedFilterAction())->execute(
+                    \App::closureTemplating()->getParam('helper'),
+                    $productFilter,
+                    \App::router()->generate('product.category', ['categoryPath' => $category->getPath()])
+                ),
                 'pagination'     => (new \View\PaginationAction())->execute(
                     \App::closureTemplating()->getParam('helper'),
                     $productPager
@@ -771,7 +788,17 @@ class Action {
                 'page'          => [
                     'title'     => $this->getPageTitle()
                 ],
-            ]);
+            ];
+
+            // если установлена настройка что бы показывать фасеты, то в ответ добавляем "disabledFilter"
+            if (true === \App::config()->sphinx['showFacets']) {
+                $data['disabledFilter'] = (new \View\ProductCategory\DisabledFilterAction())->execute(
+                    \App::closureTemplating()->getParam('helper'),
+                    $productFilter
+                );
+            }
+
+            return new \Http\JsonResponse($data);
         }
 
         $page->setParam('productPager', $productPager);
@@ -809,22 +836,10 @@ class Action {
     }
 
     /**
-     * @param \Model\Product\Filter\Entity[] $filters
-     * @param \Model\Product\Category\Entity $category
-     * @param \Model\Brand\Entity|null $brand
      * @param \Http\Request $request
-     * @param \Model\Shop\Entity|null $shop
-     * @return \Model\Product\Filter
+     * @return array
      */
-    public function getFilter(array $filters, \Model\Product\Category\Entity $category = null, \Model\Brand\Entity &$brand = null, \Http\Request $request, $shop = null) {
-        // флаг глобального списка в параметрах запроса
-        $isGlobal = self::isGlobal();
-        //
-        $inStore = self::inStore();
-
-        // регион для фильтров
-        $region = $isGlobal ? null : \App::user()->getRegion();
-
+    public function getFilterFromUrl(\Http\Request $request) {
         // добывание фильтров из http-запроса
         $requestData = ('POST'== $request->getMethod()) ? $request->request : $request->query;
 
@@ -848,6 +863,29 @@ class Action {
             // TODO: SITE-2218 сделать однотипные фильтры для ювелирки и неювелирки
             $values = (array)$request->get(\View\Product\FilterForm::$name, []);
         }
+
+        return $values;
+    }
+
+    /**
+     * @param \Model\Product\Filter\Entity[] $filters
+     * @param \Model\Product\Category\Entity $category
+     * @param \Model\Brand\Entity|null $brand
+     * @param \Http\Request $request
+     * @param \Model\Shop\Entity|null $shop
+     * @return \Model\Product\Filter
+     */
+    public function getFilter(array $filters, \Model\Product\Category\Entity $category = null, \Model\Brand\Entity &$brand = null, \Http\Request $request, $shop = null) {
+        // флаг глобального списка в параметрах запроса
+        $isGlobal = self::isGlobal();
+        //
+        $inStore = self::inStore();
+
+        // регион для фильтров
+        $region = $isGlobal ? null : \App::user()->getRegion();
+
+        // добывание фильтров из http-запроса
+        $values = $this->getFilterFromUrl($request);
 
         if ($isGlobal) {
             $values['global'] = 1;
