a02c66477f788e848c1e2a8a9926000bbb44fb25
g84 <green840429@gmail.com>
2013-11-11 15:51:22 +0400

    SITE-? изменил ид типа доставки для ПЖ

--- config/config.php
+++ config/config.php
@@ -369,7 +369,7 @@ $c->sphinx['showListingSearchBar'] = false;
 $c->lifeGift['enabled'] = false;
 $c->lifeGift['regionId'] = 144756;
 $c->lifeGift['labelId'] = 18;
-$c->lifeGift['deliveryTypeId'] = 3;
+$c->lifeGift['deliveryTypeId'] = 1065;
 
 // настройки для АБ-тестов могут быть переопределены в json
 $c->abtest['cookieName'] = 'switch';
