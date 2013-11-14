dd90162255889da97d136b9151741e36aa79cee2
tamili <ta.burlakova@gmail.com>
2013-11-14 11:58:43 +0400

    SITE-2306 Новые иконки сторов в футере

--- main/template/layout-oneColumn.php
+++ main/template/layout-oneColumn.php
@@ -97,14 +97,14 @@
 
             <ul class="applist">
                 <li class="applist__item mTitle">Мобильные приложения</li>
-                <li class="applist__item"><a target="_blank" href="https://itunes.apple.com/ru/app/enter/id486318342?mt=8"><img src="/styles/footer/img/apple_new.png" /></a></li>
+                <li class="applist__item"><a target="_blank" href="https://itunes.apple.com/ru/app/enter/id486318342?mt=8"><img src="/styles/footer/img/apple.png" /></a></li>
                 
                 <li class="applist__item">
                     <a target="_blank" href="https://play.google.com/store/apps/details?id=ru.enter">
-                      <img alt="Get it on Google Play" src="/styles/footer/img/google_new.png" />
+                      <img alt="Get it on Google Play" src="/styles/footer/img/google.png" />
                     </a>
                 </li>
-                <li class="applist__item"><a target="_blank" href="https://itunes.apple.com/ru/app/enter/id486318342?mt=8"><img src="/styles/footer/img/windows_new.png" /></a></li>
+                <li class="applist__item"><a target="_blank" href="https://itunes.apple.com/ru/app/enter/id486318342?mt=8"><img src="/styles/footer/img/wind.png" /></a></li>
                 
             </ul>
         </div>
