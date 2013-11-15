ba65c110c388b73d21105f851e408f0ded063f8e
Julian <y.misiukevich@gmail.com>
2013-11-15 16:21:11 +0300

    SITE-1917 add method trustfactorQuery in Trait

--- main/controller/Product/TrustfactorsTrait.php
+++ main/controller/Product/TrustfactorsTrait.php
@@ -13,45 +13,30 @@ trait TrustfactorsTrait
 
         $trustfactorExcludeToken = empty($catalogJson['trustfactor_exclude_token']) ? [] : $catalogJson['trustfactor_exclude_token'];
         $excludeIntersectTokens = array_intersect($productCategoryTokens, $trustfactorExcludeToken);
+
         if(empty($excludeIntersectTokens)) {
+
+            $contentClient = \App::contentClient();
+            $doQuery = false;
+
             if(!empty($catalogJson['trustfactor_top'])) $trustfactorTop = $catalogJson['trustfactor_top'];
             if(!empty($catalogJson['trustfactor_main'])) {
-                \App::contentClient()->addQuery(
-                    trim((string)$catalogJson['trustfactor_main']),
-                    [],
-                    function($data) use (&$trustfactorMain) {
-                        if (!empty($data['content'])) {
-                            $trustfactorMain = $data['content'];
-                        }
-                    },
-                    function(\Exception $e) {
-                        \App::logger()->error(sprintf('Не получено содержимое для промо-страницы %s', \App::request()->getRequestUri()));
-                        \App::exception()->add($e);
-                    }
-                );
-                \App::contentClient()->execute();
+                $this->trustfactorQuery($contentClient, $catalogJson['trustfactor_main'], $trustfactorMain);
+                $doQuery = true;
             }
+
             if(!empty($catalogJson['trustfactor_right'])) {
                 if(!is_array($catalogJson['trustfactor_right'])) $catalogJson['trustfactor_right'] = [$catalogJson['trustfactor_right']];
                 $i = 0;
                 foreach ($catalogJson['trustfactor_right'] as $trustfactorRightToken) {
-                    \App::contentClient()->addQuery(
-                        trim((string)$trustfactorRightToken),
-                        [],
-                        function($data) use (&$trustfactorRight, $i) {
-                            if (!empty($data['content'])) {
-                                $trustfactorRight[$i] = $data['content'];
-                            }
-                        },
-                        function(\Exception $e) {
-                            \App::logger()->error(sprintf('Не получено содержимое для промо-страницы %s', \App::request()->getRequestUri()));
-                            \App::exception()->add($e);
-                        }
-                    );
-                    $i++;
+                    $this->trustfactorQuery($contentClient, $trustfactorRightToken, $trustfactorRight[$i++]);
                 }
-                \App::contentClient()->execute();
-                ksort($trustfactorRight);
+                if ($i > 0) $doQuery = true;
+            }
+
+            if ($doQuery) {
+                $contentClient->execute();
+                if ($trustfactorRight) ksort($trustfactorRight);
             }
         }
 
@@ -60,6 +45,23 @@ trait TrustfactorsTrait
             'main'  => $trustfactorMain,
             'right'  =>  $trustfactorRight,
         ];
+    }
+
+
+    private function trustfactorQuery(&$contentClient, &$source, &$elem) {
+        $contentClient->addQuery(
+            trim((string)$source),
+            [],
+            function($data) use (&$trustfactorRight, &$elem) {
+                if (!empty($data['content'])) {
+                    $elem = $data['content'];
+                }
+            },
+            function(\Exception $e) {
+                \App::logger()->error(sprintf('Не получено содержимое для промо-страницы %s', \App::request()->getRequestUri()));
+                \App::exception()->add($e);
+            }
+        );
     }
 
 }
