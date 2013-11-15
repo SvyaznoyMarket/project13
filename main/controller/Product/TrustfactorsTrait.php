2a2c5906c6df378005c8c9a3583c14dbfa32ca67
Julian <y.misiukevich@gmail.com>
2013-11-15 20:00:01 +0300

    SITE-1917 add prepare trustfactors

--- main/controller/Product/TrustfactorsTrait.php
+++ main/controller/Product/TrustfactorsTrait.php
@@ -5,8 +5,14 @@ namespace Controller\Product;
 trait TrustfactorsTrait
 {
 
+    /**
+     * Tрастфакторы: Подготовим. Проверим. Покажем.
+     *
+     * @param $catalogJson
+     * @param $productCategoryTokens
+     * @return array
+     */
     protected function getTrustfactors(&$catalogJson, $productCategoryTokens) {
-        // трастфакторы
         $trustfactorTop = null;
         $trustfactorMain = null;
         $trustfactorRight = [];
@@ -14,24 +20,39 @@ trait TrustfactorsTrait
         $trustfactorExcludeToken = empty($catalogJson['trustfactor_exclude_token']) ? [] : $catalogJson['trustfactor_exclude_token'];
         $excludeIntersectTokens = array_intersect($productCategoryTokens, $trustfactorExcludeToken);
 
-        if(empty($excludeIntersectTokens)) {
+
+        if(empty($excludeIntersectTokens)) { // Нет исключений из выбранных категорий глобально
 
             $contentClient = \App::contentClient();
             $doQuery = false;
 
-            if(!empty($catalogJson['trustfactor_top'])) $trustfactorTop = $catalogJson['trustfactor_top'];
+            if(!empty($catalogJson['trustfactor_top'])) {
+                $tfPrepared = $this->prepareTrustfactor($catalogJson['trustfactor_top'], $productCategoryTokens);
+                if ($tfPrepared) $trustfactorTop = $tfPrepared;
+            }
+
             if(!empty($catalogJson['trustfactor_main'])) {
-                $this->trustfactorQuery($contentClient, $catalogJson['trustfactor_main'], $trustfactorMain);
-                $doQuery = true;
+                $tfPrepared = $this->prepareTrustfactor($catalogJson['trustfactor_main'], $productCategoryTokens);
+                if ($tfPrepared) {
+                    $this->trustfactorQuery($contentClient, $tfPrepared, $trustfactorMain);
+                    $doQuery = true;
+                }
             }
 
             if(!empty($catalogJson['trustfactor_right'])) {
-                if(!is_array($catalogJson['trustfactor_right'])) $catalogJson['trustfactor_right'] = [$catalogJson['trustfactor_right']];
-                $i = 0;
-                foreach ($catalogJson['trustfactor_right'] as $trustfactorRightToken) {
-                    $this->trustfactorQuery($contentClient, $trustfactorRightToken, $trustfactorRight[$i++]);
+                $tfPrepared = $this->prepareTrustfactor($catalogJson['trustfactor_right'], $productCategoryTokens);
+
+                if ($tfPrepared) {
+                    if( !is_array($tfPrepared) ) {
+                        $tfPrepared = array($tfPrepared);
+                    }
+                    $i = 0;
+                    foreach ($tfPrepared as $trustfactorRightToken) {
+                        $this->trustfactorQuery($contentClient, $trustfactorRightToken, $trustfactorRight[$i++]);
+                    }
+                    if ($i > 0) $doQuery = true;
                 }
-                if ($i > 0) $doQuery = true;
+
             }
 
             if ($doQuery) {
@@ -48,7 +69,16 @@ trait TrustfactorsTrait
     }
 
 
+    /**
+     * Запрос к ядру, чтобы получить содержимое трастфактора
+     *
+     * @param $contentClient
+     * @param $source
+     * @param $elem
+     */
     private function trustfactorQuery(&$contentClient, &$source, &$elem) {
+        if (!is_string($source)) return;
+
         $contentClient->addQuery(
             trim((string)$source),
             [],
@@ -57,11 +87,49 @@ trait TrustfactorsTrait
                     $elem = $data['content'];
                 }
             },
-            function(\Exception $e) {
-                \App::logger()->error(sprintf('Не получено содержимое для промо-страницы %s', \App::request()->getRequestUri()));
-                \App::exception()->add($e);
+            function(\Exception $e) use ($source) {
+                \App::logger()->error(sprintf('Не получено содержимое трастфактора от урла %s для страницы %s', $source, \App::request()->getRequestUri()));
+                //\App::exception()->add($e);
+                \App::exception()->remove($e);
             }
         );
+    }
+
+
+    /**
+     * Преобразовывает трастфактор, проверяет и возвращает его, если можно показывать
+     *
+     * @param $tfFields
+     * @param $productCategoryTokens
+     * @return array|null
+     */
+    private function prepareTrustfactor($tfFields, &$productCategoryTokens) {
+        if ( !is_array($tfFields) ) {
+            return $tfFields;
+        }
+
+        $hasExclude = isset($tfFields['exclude_tokens']);
+        $hasSrc = isset($tfFields['src']);
+        $excludeIntersectTokens = null;
+
+        if ( $hasExclude || $hasSrc ) {
+
+            if ( $hasExclude ) {
+                $excludeIntersectTokens = array_intersect( $productCategoryTokens, $tfFields['exclude_tokens'] );
+                unset($tfFields['exclude_tokens']);
+            }
+
+            if ( empty($excludeIntersectTokens) ) { // Нет исключений из выбранных категорий локально
+                if ( $hasSrc ) {
+                    $tfFields = $tfFields['src'];
+                }
+            }else{
+                return null; // Есть локально исключениe, не показываем трастфактор
+            }
+
+        }
+
+        return $tfFields;
     }
 
 }
