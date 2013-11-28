5cd56d6d7e0ea59520686aa0d637434ab17f3dab
Julian <y.misiukevich@gmail.com>
2013-11-28 18:01:39 +0300

    SITE-2648 перенос сетов на новые листинги

--- main/controller/Product/SetAction.php
+++ main/controller/Product/SetAction.php
@@ -13,13 +13,22 @@ class SetAction {
      */
     public function execute($productBarcodes, \Http\Request $request) {
         \App::logger()->debug('Exec ' . __METHOD__);
-
-        $client = \App::coreClientV2();
-
+        $limit = \App::config()->product['itemsPerPage'];
+        $pageNum = (int)$request->get('page', 1);
         $productBarcodes = explode(',', $productBarcodes);
+
         if (!(bool)$productBarcodes) {
             throw new \Exception\NotFoundException('Не передано ни одного баркода товара');
         }
+
+        if ($pageNum < 1) {
+            throw new \Exception\NotFoundException(sprintf('Неверный номер страницы "%s"', $pageNum));
+        }
+
+        $productVideosByProduct = []; // todo
+        $productView = \Model\Product\Category\Entity::PRODUCT_VIEW_COMPACT; // вид товаров
+        $client = \App::coreClientV2();
+
 
         // подготовка 1-го запроса
 
@@ -46,15 +55,71 @@ class SetAction {
         // выполнение 1-го запроса
         $client->execute();
 
-        $pager = new \Iterator\EntityPager($products, count($products));
-        $pager->setPage(1);
-        $pager->setMaxPerPage(100);
+        $countProducts = count($products);
+        if ($countProducts < $limit) {
+            $limit = $countProducts;
+        }
 
+
+        $productPager = new \Iterator\EntityPager($products, $countProducts);
+        $productPager->setPage($pageNum);
+        $productPager->setMaxPerPage($limit);
+
+
+        // проверка на максимально допустимый номер страницы
+        if (($productPager->getPage() - $productPager->getLastPage()) > 0) {
+            throw new \Exception\NotFoundException(sprintf('Неверный номер страницы "%s".', $productPager->getPage()));
+            /*return new \Http\JsonResponse([
+                'list' => [
+                    'products' => [],
+                    'productCount' => 0,
+                ]
+            ]);*/
+        }
+
+
+        // сортировка
+        $productSorting = new \Model\Product\Sorting();
+        list($sortingName, $sortingDirection) = array_pad(explode('-', $request->get('sort')), 2, null);
+        $productSorting->setActive($sortingName, $sortingDirection);
+
+
+        // ajax
+        if ($request->isXmlHttpRequest() && 'true' == $request->get('ajax')) {
+
+            $templating = \App::closureTemplating();
+            $helper = $templating->getParam('helper');
+            /** @var $helper \Helper\TemplateHelper */
+
+            return new \Http\JsonResponse([
+                'list'           => (new \View\Product\ListAction())->execute(
+                        $helper,
+                        $productPager,
+                        $productVideosByProduct,
+                        !empty($catalogJson['bannerPlaceholder']) ? $catalogJson['bannerPlaceholder'] : []
+                    ),
+                //'selectedFilter' => $selectedFilter,
+                'pagination'     => (new \View\PaginationAction())->execute(
+                        $helper,
+                        $productPager
+                    ),
+                'sorting'        => (new \View\Product\SortingAction())->execute(
+                        $templating->getParam('helper'),
+                        $productSorting
+                    ),
+                /*'page'           => [
+                    //'title'      => 'Тег «'.$tag->getName() . '»' . ( $selectedCategory ? ( ' — ' . $selectedCategory->getName() ) : '' )
+                ],*/
+            ]);
+        }
 
         $page = new \View\Product\SetPage();
-        $page->setParam('pager', $pager);
+        $page->setParam('productPager', $productPager);
         $page->setParam('products', $productsForRetargeting);
         $page->setParam('categoriesById', $categoriesById);
+        $page->setParam('productView', $productView);
+        $page->setParam('productSorting', $productSorting);
+        $page->setParam('productVideosByProduct', $productVideosByProduct);
 
         return new \Http\Response($page->show());
     }
