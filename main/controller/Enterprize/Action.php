fadf02a25f5d85d4eeabdee0ca33c692fb0b2566
Vitaly Shaposhnik <vitaly.shaposhnik@gmail.com>
2013-11-28 15:53:44 +0200

    SITE-2695 Создать обработчик для получения купона

--- main/controller/Enterprize/Action.php
+++ main/controller/Enterprize/Action.php
@@ -32,81 +32,109 @@ class Action {
     public function create(\Http\Request $request) {
         \App::logger()->debug('Exec ' . __METHOD__);
 
+        if (!$request->isXmlHttpRequest()) {
+            throw new \Exception\NotFoundException('Request is not xml http request');
+        }
+
         // secure
         if (!\App::user()->getToken()) {
-           throw new \Exception\AccessDeniedException('Пользователь не авторизован');
+            throw new \Exception\AccessDeniedException('Пользователь не авторизован');
+        }
+
+        $couponRequest = (array)$request->get('coupon_request');
+        if (!$couponRequest) {
+            throw new \Exception\Exception('Не переданы данные запроса на получение купона');
         }
 
         $client = \App::coreClientV2();
 
-
-        $responseData = [
-            'success' => false,
-        ];
-
+        $responseData = [];
         try {
-            $form = array_merge([
-                'name'              => null,
-                'phonenumber'       => null,
-                'email'             => null,
-                'sclub_card_number' => null,
-                'agreed'            => null,
-                'coupon_type'       => null,
-            ], (array)$request->get('coupon_request'));
-
-            try {
-                $result = $client->query(
-                    'coupon/enter-prize',
-                    [
-                        'token' => \App::user()->getToken(),
-                    ],
-                    [
-                        'name'                      => $form['name'],
-                        'phone'                     => $form['phonenumber'],
-                        'email'                     => $form['email'],
-                        'svyaznoy_club_card_number' => $form['sclub_card_number'],
-                        'guid'                      => $form['coupon_type'],
-                        'agree'                     => (bool)$form['agreed'],
-                    ],
-                    3
-                );
-
-                die(var_dump($result));
-            } catch (\Curl\Exception $e) {
-                \App::logger()->error(['code' => $e->getCode(), 'message' => $e->getMessage(), 'content' => $e->getContent()], ['enterprize']);
-
-                if (422 === $e->getCode()) {
-                    $errorContent = $e->getContent();
-                    $fieldErrors = isset($errorContent['errors']) ? (array)$errorContent['errors'] : [];
-                    if (!empty($fieldErrors['name'])) {
-                        $form['name'] = new \Exception('Неверно указано имя');
-                    }
-                    if (!empty($fieldErrors['phone'])) {
-                        $form['phonenumber'] = new \Exception('Неверно указано телефон');
-                    }
-                    if (!empty($fieldErrors['email'])) {
-                        $form['email'] = new \Exception('Неверно указан email');
-                    }
-                    if (!empty($fieldErrors['svyaznoy_club_card_number'])) {
-                        $form['sclub_card_number'] = new \Exception('Неверно указан номер карты "Связной клуб"');
-                    }
-                    if (!empty($fieldErrors['guid'])) {
-                        $form['coupon_type'] = new \Exception('Неверно указан тип купона');
-                    }
-                    if (!empty($fieldErrors['agree'])) {
-                        $form['agreed'] = new \Exception('Не приняты условия');
-                    }
-
-                    throw new \Exception('Форма заполнена неверно');
-                }
-
-                throw new \Exception('Произошла ошибка при получении купона');
+            $name = trim((string)$couponRequest['name']);
+            if (!$name) {
+                throw new \Exception('Не указано имя участника');
             }
 
-            $responseData['success'] = true;
-        } catch (\Exception $e) {
-            $responseData['success'] = false;
-            $responseData['error'] = ['code' => $e->getCode(), 'message' => $e->getMessage()];
+            $phone = trim((string)$couponRequest['phone']);
+            if (!$phone) {
+                throw new \Exception('Не указан номер телефона');
+            }
+
+            $email = trim((string)$couponRequest['email']);
+            if (!$email) {
+                throw new \Exception('Не указан e-mail');
+            }
+
+//            $svyaznoy_club_card_number = trim((string)$couponRequest['svyaznoy_club_card_number']);
+//            if (!$svyaznoy_club_card_number) {
+//                throw new \Exception('Не указан номер карты "Связной-Клуб"');
+//            }
+
+            $guid = trim((string)$couponRequest['guid']);
+            if (!$guid) {
+                throw new \Exception('Не указан идентификатор серии купона');
+            }
+
+            $agree = (bool)$couponRequest['agree'];
+            if (!$agree) {
+                throw new \Exception('Не отмечено согласие с условиями оферты');
+            }
+
+            $requestData = array_merge([
+                'name'                      => null,
+                'phone'                     => null,
+                'email'                     => null,
+                'svyaznoy_club_card_number' => null,
+                'guid'                      => null,
+                'agree'                     => null,
+            ], $couponRequest);
+
+            $result = [];
+            $client->addQuery(
+                'coupon/enter-prize',
+                [
+                    'client_id' => \App::config()->coreV2['client_id'],
+                    'token'     => \App::user()->getToken(),
+                ],
+                [
+                    'name'                      => $requestData['name'],
+                    'phone'                     => $requestData['phone'],
+                    'email'                     => $requestData['email'],
+                    'svyaznoy_club_card_number' => $requestData['svyaznoy_club_card_number'],
+                    'guid'                      => $requestData['guid'],
+                    'agree'                     => (bool)$requestData['agree'],
+                ],
+                function ($data) use (&$result) {
+                    $result = $data;
+                },
+                function(\Exception $e) use (&$result) {
+                    \App::exception()->remove($e);
+                    $result = $e;
+                }
+            );
+            $client->execute();
+
+            if ($result instanceof \Exception) {
+                throw $result;
+            }
+
+            $responseData = [
+                'error'  => null,
+                'notice' => ['message' => $result['message'], 'type' => 'info']
+            ];
+        } catch (\Curl\Exception $e) {
+            $errorData = $e->getContent();
+            $formError = [];
+            if (isset($errorData['detail'])) {
+                foreach ($errorData['detail'] as $field => $message) {
+                    $formError[] = ['code' => 0, 'field' => $field, 'message' => $message];
+                }
+            }
+
+            $responseData = [
+                'error' => ['code' => $e->getCode(), 'message' => $e->getMessage()],
+                'form'  => $formError,
+            ];
         }
 
         return new \Http\JsonResponse($responseData);
