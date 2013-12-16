1db9a4860f37cd901d35f4a172641f6e4a899141
vit <vitaly.shaposhnik@gmail.com>
2013-12-16 18:02:15 +0200

    SITE-2863 Поправить валидацию полей при сохранении формы в личном кабинете

--- main/template/user/page-edit-enterprize.php
+++ main/template/user/page-edit-enterprize.php
@@ -14,47 +14,40 @@
     <p class="green"><?= $message ?></p>
 <? endif ?>
 
-<form class="userInfoEdit clearfix" action="<?= $page->url('user.edit') ?>" class="form" method="post">
+<form class="userInfoEdit clearfix jsUserEditEnterprizeForm" action="<?= $page->url('user.edit') ?>" class="form" method="post">
     <div class="fl width430">
         <input type="hidden" name="redirect_to" value="<?= $redirect ?>">
 
         <input type="hidden" name="user[enterprize_coupon]" value="<?= $form->getEnterprizeCoupon() ?>">
 
         <label class="userInfoEdit__label" for="user_first_name">Имя*:</label>
-
-        <input type="text" id="user_first_name" value="<?= $form->getFirstName() ?>" name="user[first_name]" class="text width418 mb10" />
+        <div><input type="text" id="user_first_name" value="<?= $form->getFirstName() ?>" name="user[first_name]" class="text width418 mb10 jsFirstName" /></div>
 
         <label class="userInfoEdit__label" for="user_middle_name">Отчество:</label>
-
-        <input type="text" id="user_middle_name" value="<?= $form->getMiddleName() ?>" name="user[middle_name]" class="text width418 mb10" />
+        <div><input type="text" id="user_middle_name" value="<?= $form->getMiddleName() ?>" name="user[middle_name]" class="text width418 mb10 jsMiddleName" /></div>
 
         <label class="userInfoEdit__label" for="user_last_name">Фамилия*:</label>
-
-        <input type="text" id="user_last_name" value="<?= $form->getLastName() ?>" name="user[last_name]" class="text width418 mb10" />
+        <div><input type="text" id="user_last_name" value="<?= $form->getLastName() ?>" name="user[last_name]" class="text width418 mb10 jsLastName" /></div>
 
         <label class="userInfoEdit__label" for="user_mobile_phone">Номер телефона*:</label>
-
-        <input type="text" id="user_mobile_phone" value="<?= $form->getMobilePhone() ?>" name="user[mobile_phone]" class="text" />
+        <div><input type="text" id="user_mobile_phone" value="<?= $form->getMobilePhone() ?>" name="user[mobile_phone]" class="text jsMobilePhone" /></div>
 
         <label class="userInfoEdit__label" for="user_email">E-mail*:</label>
-
-        <input type="text" id="user_email" value="<?= $form->getEmail() ?>" name="user[email]" class="text width418 mb10" />
+        <div><input type="text" id="user_email" value="<?= $form->getEmail() ?>" name="user[email]" class="text width418 mb10 jsEmail" /></div>
 
         <label class="userInfoEdit__label" for="user_card_number">Номер карты Связной-Клуб:</label>
-
-        <input type="text" id="user_card_number" value="<?= $form->getCardNumber() ?>" name="user[card_number]" class="text" />
+        <div><input type="text" id="user_card_number" value="<?= $form->getCardNumber() ?>" name="user[card_number]" class="text jsCardNumber" /></div>
 
         <div class="bInputList">
-            <input type="checkbox" id="user_agree" name="user[coupon_agree]" value="1" autocomplete="off" class="bCustomInput mCustomCheckbox" <?= $form->getCouponAgree() ? 'checked="checked"' : '' ?> />
-            <label class="bCustomLabel" for="user_agree">Ознакомлен с <a href="http://www.enter.ru/reklamnaya-akcia-enterprize" target="blank">правилами ENTER PRIZE</a></label>
+            <input type="checkbox" id="user_agree" name="user[coupon_agree]" value="1" autocomplete="off" class="bCustomInput mCustomCheckbox jsAgree" <?= $form->getCouponAgree() ? 'checked="checked"' : '' ?> />
+            <label class="bCustomLabel" for="user_agree">Ознакомлен с <a href="http://www.enter.ru/reklamnaya-akcia-enterprize" target="blank">правилами ENTER PRIZE</a>*</label>
         </div>
 
         <div class="bInputList">
-            <input type="checkbox" id="user_is_subscribe" name="user[is_subscribe]" value="1" autocomplete="off" class="bCustomInput mCustomCheckbox" <?= $form->getIsSubscribed() ? 'checked="checked"' : '' ?> />
-            <label class="bCustomLabel" for="user_is_subscribe">Согласен получать рекламную рассылку</label>
+            <input type="checkbox" id="user_is_subscribe" name="user[is_subscribe]" value="1" autocomplete="off" class="bCustomInput mCustomCheckbox jsSubscribe" <?= $form->getIsSubscribed() ? 'checked="checked"' : '' ?> />
+            <label class="bCustomLabel" for="user_is_subscribe">Согласен получать рекламную рассылку*</label>
         </div>
 
-        <input type="submit" value="Сохранить изменения" id="bigbutton" class="btnSave button bigbutton">
-
+        <input type="submit" value="Сохранить изменения" id="bigbutton" class="btnSave button bigbutton jsEnterprizeFormSubmit">
     </div>
 </form>
--- web/js/combine.js
+++ web/js/combine.js
@@ -1,1 +1,1 @@
-window.release = { "version":"t63-04-133-gdba229f"}
+window.release = { "version":"t63-04-141-ga7f6f25"}
