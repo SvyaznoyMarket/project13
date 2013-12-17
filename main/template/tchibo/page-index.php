e8115dd0101c5048f3df81b69e9300fe5e0dd321
tamili <ta.burlakova@gmail.com>
2013-12-17 17:00:13 +0400

    SITE-2844 Чибо слайдеры и пр.

--- main/template/tchibo/page-index.php
+++ main/template/tchibo/page-index.php
@@ -4,4 +4,92 @@
  */
 ?>
 
-tchibo
+<div class="navTchibo clearfix">
+	<div class="navTchiboTitle">Миры Tchibo</div>
+
+	<ul class="navTchiboList">
+		<li class="navTchiboList__item">
+			<a class="navTchiboList__link" href="">
+				<img class="img" src="/styles/tchibo/img/ImgCat.png" />
+				<span class="title">Мужчины</span>
+			</a>
+		</li>
+
+		<li class="navTchiboList__item">
+			<a class="navTchiboList__link" href="">
+				<img class="img" src="/styles/tchibo/img/ImgCat.png" />
+				<span class="title">Женщины</span>
+			</a>
+		</li>
+
+		<li class="navTchiboList__item">
+			<a class="navTchiboList__link" href="">
+				<img class="img" src="/styles/tchibo/img/ImgCat.png" />
+				<span class="title">Дети</span>
+			</a>
+		</li>
+
+		<li class="navTchiboList__item">
+			<a class="navTchiboList__link" href="">
+				<img class="img" src="/styles/tchibo/img/ImgCat.png" />
+				<span class="title">Дом</span>
+			</a>
+		</li>
+
+		<li class="navTchiboList__item">
+			<a class="navTchiboList__link" href="">
+				<img class="img" src="/styles/tchibo/img/ImgCat.png" />
+				<span class="title">Спорт</span>
+			</a>
+		</li>
+
+		<li class="navTchiboList__item">
+			<a class="navTchiboList__link" href="">
+				<img class="img" src="/styles/tchibo/img/ImgCat.png" />
+				<span class="title">Кофе</span>
+			</a>
+		</li>
+	</ul>
+</div>
+
+<div class="bPromoCatalogSlider mTchiboSlider">
+    <a href="#" class="bPromoCatalogSlider_eArrow mArLeft"></a>
+    <a href="#" class="bPromoCatalogSlider_eArrow mArRight"></a>
+    <div class="bPromoCatalogSliderWrap clearfix" style="width: 7710px;">         
+    	<div class="bPromoCatalogSliderWrap_eSlide">             
+    		<a class="bPromoCatalogSliderWrap_eSlideLink topLifted" href="">                 
+    			<div class="bottomLifted">
+    				<img src="/styles/tchibo/img/Img.png" alt="">
+    				<ul class="bCatLine">
+    					<li class="bCatLine__eItem">Дом</li>
+    					<li class="bCatLine__eItem">Дизайн для ванной и души</li>
+    				</ul>
+    			</div>             
+    		</a>         
+    	</div>             
+    	<div class="bPromoCatalogSliderWrap_eSlide">             
+    	 	<a class="bPromoCatalogSliderWrap_eSlideLink topLifted" href="">                 
+    	 		<div class="bottomLifted"><img src="/styles/tchibo/img/Img.png" alt=""></div>             
+    	 	</a>         
+    	</div>              
+    	<div class="bPromoCatalogSliderWrap_eSlide">             
+    	 	<a class="bPromoCatalogSliderWrap_eSlideLink topLifted" href="">                 
+    	 		<div class="bottomLifted"><img src="/styles/tchibo/img/Img.png" alt=""></div>             
+    	 	</a>         
+    	</div>              
+    	<div class="bPromoCatalogSliderWrap_eSlide">             
+    	 	<a class="bPromoCatalogSliderWrap_eSlideLink topLifted" href="">                 
+			    <div class="bottomLifted"><img src="/styles/tchibo/img/Img.png" alt=""></div>             
+			</a>         
+		</div>             
+	</div>
+</div>
+
+<div id="promoCatalogPaginator" class="bPaginator mTchiboPaginator clearfix">
+	<div class="bPaginator_eWrap clearfix">
+		<a class="bPaginator_eLink" href="">1</a>
+		<a class="bPaginator_eLink" href="">2</a>
+		<a class="bPaginator_eLink active" href="">3</a>
+		<a class="bPaginator_eLink" href="">4</a>
+	</div>
+</div>
