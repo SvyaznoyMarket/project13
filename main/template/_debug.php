7948749aec3e78bc6cc856849014606c25920640
g84 <green840429@gmail.com>
2014-01-10 12:43:35 +0400

    SITE-1094 улучшен вывод настроек

--- main/template/_debug.php
+++ main/template/_debug.php
@@ -63,6 +63,29 @@
     </tr>
 </script>
 
+
+<script id="tplDebugFirstLevelConfig" type="text/html">
+    <tr>
+        <td style="background-image: url({{iconUrl}});" class="property-name">
+            <a class="jsExpandValue" href="#">{{name}}</a>
+        </td>
+        <td class="property-value">
+            <div class="jsExpandedValue property-value-expanded" style="display:none;">
+                <table>
+                    <tbody>
+                    {{#value}}
+                    <tr>
+                        <td class="query-cell">{{name}}</td>
+                        <td class="query-cell"><pre>{{value}}</pre></td>
+                    </tr>
+                    {{/value}}
+                    </tbody>
+                </table>
+            </div>
+        </td>
+    </tr>
+</script>
+
 <script id="tplDebugFirstLevelTimer" type="text/html">
     <tr>
         <td style="background-image: url({{iconUrl}});" class="property-name">
