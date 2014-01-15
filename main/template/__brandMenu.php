c8796e512e3db986c5e6820f12a30db2d25ec7b9
Ju1jan <y.misiukevich@gmail.com>
2014-01-16 02:09:43 +0300

    SITE-2987 Отображение полоски со связанной категорией исходя из json

--- main/template/__brandMenu.php
+++ main/template/__brandMenu.php
@@ -1,18 +1,34 @@
 <?php
 
-return function()
-{
+return function(
+    \Helper\TemplateHelper $helper,
+    $css = '',
+    $brandLogo = '',
+    $categoryName,
+    array $categoryList = []
+) {
+
+    if (!empty($brandLogo)) {
+        $brandLogo = sprintf(' style="background-image: url(%s);" ', $brandLogo);
+    }
 ?>
-
-
-
 <div class="brandMenuWrap">
-    <div class="brandMenu">
-        <div class="brandLogo">Дом</div>
+    <div class="brandMenu"<?= $css ? "style='$css'": '' ?>>
+
+        <div class="brandLogo"<?= $brandLogo ?>>
+            <?= $categoryName ?>
+        </div>
 
         <div class="slideCategory">
             <div class="slideCategory__inner">
+                <? if (!empty($categoryList)): ?>
                 <ul class="categoryList">
+                    <? foreach ($categoryList as $categoryItem) { ?>
+                    <li class="categoryList__item">
+                        <a class="categoryList__link" href="<?= $categoryItem['link'] ?>"><?= $categoryItem['name'] ?></a>
+                    </li>
+                    <? } ?>
+                    <?/*
                     <li class="categoryList__item"><a class="categoryList__link" href="">Чистота сама по себе</a></li>
                     <li class="categoryList__item"><a class="categoryList__link" href="">Уход за одеждой и шитье</a></li>
                     <li class="categoryList__item"><a class="categoryList__link" href="">Дизайн для ванной и душа</a></li>
@@ -21,11 +37,13 @@ return function()
                     <li class="categoryList__item"><a class="categoryList__link" href="">Идеальный завтрак</a></li>
                     <li class="categoryList__item"><a class="categoryList__link" href="">Чистота сама по себе</a></li>
                     <li class="categoryList__item"><a class="categoryList__link" href="">Уход за одеждой и шитье</a></li>
+                    */?>
                 </ul>
+                <? endif; ?>
             </div>
         </div>
+
     </div>
 </div>
 
-<?
-} ?>
+<? } ?>
