3e41d2a8cc0e7c12b370ecc9b01837443b33c823
vit <vitaly.shaposhnik@gmail.com>
2014-02-05 14:30:38 +0200

    SITE-3144 Бага в Thibo меню

--- main/template/product-category/__sibling-list.php
+++ main/template/product-category/__sibling-list.php
@@ -17,21 +17,21 @@ return function(
 
         <ul class="tchiboNav__list clearfix">
         <? foreach ($categories as $category):
-            $active = $currentCategory && ($category->getId() === $currentCategory->getParentId()) && !$currentCategory->getHasChild() ? true : false; ?>
+            $active = $currentCategory && in_array($category->getId(), [$currentCategory->getParentId(), $currentCategory->getId()]) ? true : false; ?>
 
             <li class="item jsItemListTchibo<? if ($active): ?> active<? endif ?>">
                 <a class="link" href="<?= $category->getLink() ?>">
                     <span class="itemText"><?= $category->getName() ?></span>
                 </a>
 
-                <? if ((bool)$category->getChild()): ?>
-                <ul class="tchiboNav__sublist<? if ($active): ?> active<? endif ?>">
-                <? foreach ($category->getChild() as $child):
-                    $activeChild = $currentCategory && ($child->getId() === $currentCategory->getId()) ? true : false; ?>
+                <? if ((bool)$category->getChild() && ($active || !$currentCategory)): ?>
+                    <ul class="tchiboNav__sublist<? if ($active): ?> active<? endif ?>">
+                    <? foreach ($category->getChild() as $child):
+                        $activeChild = $currentCategory && ($child->getId() === $currentCategory->getId()) ? true : false; ?>
 
-                    <li class="sublistItem jsItemListTchibo<? if ($activeChild): ?> mActive<? endif ?>"><a class="link" href="<?= $child->getLink() ?>"><?= $child->getName() ?></a></li>
-                <? endforeach ?>
-                </ul>
+                        <li class="sublistItem jsItemListTchibo<? if ($activeChild): ?> mActive<? endif ?>"><a class="link" href="<?= $child->getLink() ?>"><?= $child->getName() ?></a></li>
+                    <? endforeach ?>
+                    </ul>
                 <? endif ?>
             </li>
         <? endforeach ?>
