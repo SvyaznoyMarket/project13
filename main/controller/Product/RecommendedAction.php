59f3f49abe8085b9650789860d3ebdd0fd179dd4
vit <vitaly.shaposhnik@gmail.com>
2014-02-12 13:23:56 +0200

    SITE-3187 Оптимизация-рефакторинг рекоммендашек

--- main/controller/Product/RecommendedAction.php
+++ main/controller/Product/RecommendedAction.php
@@ -11,27 +11,91 @@ class RecommendedAction {
     public function execute(\Http\Request $request, $productId) {
         \App::logger()->debug('Exec ' . __METHOD__);
 
+        $client = \App::retailrocketClient();
+        $region = \App::user()->getRegion();
+
+        $recommend = [
+            'alsoBought' => null,
+            'similar'    => null,
+            'alsoViewed' => null
+        ];
+        $productsCollection = $recommend;
+        $ids = $recommend;
+        $controller = array_merge($recommend, [
+            'alsoBought' => new \Controller\Product\UpsaleAction(),
+            'similar'    => new \Controller\Product\SimilarAction(),
+            'alsoViewed' => new \Controller\Product\AlsoViewedAction()
+        ]);
+
         $responseData = [];
-
         try {
-            $recommend = [
-                'alsoBought' => null,
-                'similar'    => null,
-                'alsoViewed' => null
-            ];
+            $product = \RepositoryManager::product()->getEntityById($productId);
 
-            foreach ($recommend as $type => $item) {
-                switch ($type) {
-                    case 'alsoBought':
-                        $recommend[$type] = (new \Controller\Product\UpsaleAction())->getResponseData($productId, $request);
-                        break;
-                    case 'similar':
-                        $recommend[$type] = (new \Controller\Product\SimilarAction())->getResponseData($productId, $request);
-                        break;
-                    case 'alsoViewed':
-                        $recommend[$type] = (new \Controller\Product\AlsoViewedAction())->getResponseData($productId, $request);
-                        break;
+            if (!$product) {
+                throw new \Exception(sprintf('Товар #%s не найден', $productId));
+            }
+
+            $key = \App::abTest()->getCase()->getKey();
+            \App::logger()->info(sprintf('abTest.key=%s, response.cookie.switch=%s', $key, $request->cookies->get('switch')));
+
+            $productId = $product->getId();
+
+            // запрашиваем $ids продуктов для всех типов рекоммендаций
+            $allIds = [];
+            foreach ($ids as $type => $item) {
+                $ids[$type] = $client->query('Recomendation/' . $controller[$type]->getRetailrocketMethodName(), $productId);
+
+                // для блока "С этим товаром также покупают" к $ids добавляем связные товары
+                if ('alsoBought' === $type) {
+                    $ids[$type] = array_unique(array_merge($product->getRelatedId(), $ids[$type]));
                 }
+
+                $allIds = array_unique(array_merge($allIds, $ids[$type]));
+            }
+
+            // запрашиваем $collection товаров для всех типов рекоммендаций
+            $collection = [];
+            \RepositoryManager::product()->prepareCollectionById($allIds, $region, function($data) use(&$collection) {
+                foreach ($data as $value) {
+                    $entity = new \Model\Product\Entity($value);
+                    $collection[$entity->getId()] = $entity;
+                }
+            });
+            \App::coreClientV2()->execute(\App::config()->coreV2['retryTimeout']['medium']);
+
+            // разбиваем товары по типам рекомендаций
+            foreach ($productsCollection as $type => $item) {
+                foreach ($ids[$type] as $id) {
+                    $productsCollection[$type][] = $collection[$id];
+                }
+            }
+
+            // подготавливаем контент для всех типов рекомендаций
+            foreach ($recommend as $type => $item) {
+                if ('alsoBought' === $type) {
+                    // SITE-2818 Из блока "С этим товаром также покупают" убраем товары, которые есть только в магазинах ("Резерв" и витринные)
+                    foreach ($productsCollection[$type] as $key => $value) {
+                        if ($value->isInShopOnly() || $value->isInShopStockOnly()) {
+                            unset($productsCollection[$type][$key]);
+                        }
+                    }
+
+                    $products = array_slice($productsCollection[$type], 0, \App::config()->product['itemsInSlider'] * 2);
+                } else {
+                    $products = $this->prepareProducts($productsCollection[$type], $client::NAME, $controller[$type]->getName());
+                }
+
+                if ( !is_array($products) ) {
+                    throw new \Exception(sprintf('Not found products data in response. ActionType: %s', $controller[$type]->getActionType()));
+                }
+
+                $recommend[$type] = [
+                    'success' => true,
+                    'content' => \App::closureTemplating()->render('product/__slider', [
+                        'title' => $controller[$type]->getActionTitle(),
+                        'products' => $products,
+                    ]),
+                ];
             }
 
             $responseData = [
@@ -46,5 +110,43 @@ class RecommendedAction {
         }
 
         return new \Http\JsonResponse($responseData);
+    }
+
+
+    /**
+     * @param $products
+     * @param $senderName
+     * @return mixed
+     * @throws \Exception
+     */
+    protected function prepareProducts($products, $senderName, $recommendName) {
+        if (!(bool)$products) {
+            throw new \Exception('Рекомендации не получены');
+        }
+
+        foreach ($products as $i => $product) {
+            /* @var product Model\Product\Entity */
+
+            if (!$product->getIsBuyable())  {
+                unset($products[$i]);
+                continue;
+            }
+
+            $link = $product->getLink();
+            $link = $link . (false === strpos($link, '?') ? '?' : '&') . 'sender=' . $senderName . '|' . $product->getId();
+
+            if ('upsale' == $recommendName) {
+                $link = $link . (false === strpos($link, '?') ? '?' : '&') . 'from=cart_rec';
+                $product->setIsUpsale(true);
+            }
+
+            $product->setLink($link);
+        }
+
+        if (!(bool)$products) {
+            throw new \Exception('Нет товаров');
+        }
+
+        return $products;
     }
 }
