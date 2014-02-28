ecbff240066a8f31f25acc7ff409083bb73620f2
tamili <ta.burlakova@gmail.com>
2014-02-28 15:49:36 +0400

    SITE-3255 Чёрный ENTER PRIZE

--- main/template/enterprize/page-form.php
+++ main/template/enterprize/page-form.php
@@ -13,22 +13,18 @@
     <input type="hidden" name="user[guid]" value="<?= $form->getEnterprizeCoupon() ?>" />
 
     <fieldset class="formDefault__fields">
-        <div>
+ 
             <label class="formDefault__label">Имя:</label>
             <input class="formDefault__inputText jsName" type="text" name="user[name]" value="<?= $form->getName() ?>" />
-        </div>
 
-        <div>
             <label class="formDefault__label">Мобильный телефон:</label>
             <input class="formDefault__inputText jsMobile" type="text" name="user[mobile]" value="<?= $form->getMobile() ?>" />
-        </div>
 
-        <div>
             <label class="formDefault__label">E-mail:</label>
             <input class="formDefault__inputText jsEmail" type="text" name="user[email]" value="<?= $form->getEmail() ?>" />
-        </div>
+   
 
-        <ul class="bInputList">
+        <ul class="bInputList mEnterPrizeSubscr">
             <li class="bInputList__eListItem ">
                 <input class="jsCustomRadio bCustomInput mCustomCheckBig jsSubscribe" id="subscribe" name="user[subscribe]" type="checkbox" checked="checked" />
                 <label class="bCustomLabel mCustomLabelBig mChecked" for="subscribe">Получить рекламную рассылку</label>
