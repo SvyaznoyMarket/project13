a609e4b26701c59f7bdbc03605c2a8a0b1abd7ca
vit <vitaly.shaposhnik@gmail.com>
2014-03-03 10:33:25 +0200

    SITE-3244 Убераем продублированную кнопку 'Новый код'

--- main/template/enterprize/page-confirm-phone.php
+++ main/template/enterprize/page-confirm-phone.php
@@ -16,7 +16,7 @@
 <? endif ?>
 
 <div class="enterprizeConfirm">
-    <p class="textConfirm"><?//= $data['mobile'] ? $data['mobile'] : '' ?><?//= preg_replace('/(\d{1,3})(\d{1,3})(\d{1,2})(\d{1,2})/i', '+7 ($1) $2-$3-$4', $userEntity->getEntity()) // должен быть формат +7 999 777-11-22 ?></strong></p>
+    <p class="textConfirm"><?= isset($data['mobile']) ? $data['mobile'] : '' ?><?//= preg_replace('/(\d{1,3})(\d{1,3})(\d{1,2})(\d{1,2})/i', '+7 ($1) $2-$3-$4', $userEntity->getEntity()) // должен быть формат +7 999 777-11-22 ?></strong></p>
 
     <form action="<?= $page->url('enterprize.confirmPhone.check') ?>" method="post">
         <input type="hidden" name="enterprizeToken" value="<?= $enterpizeCoupon ? $enterpizeCoupon->getToken() : null ?>" />
