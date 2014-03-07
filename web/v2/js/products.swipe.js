dd96af06dbba73cf0c9c3de49a215418d6ad9370
tamili <ta.burlakova@gmail.com>
2014-03-07 09:24:32 +0400

    Responsive

--- web/v2/js/products.swipe.js
+++ web/v2/js/products.swipe.js
@@ -1,73 +1,53 @@
-$(function(){
-   $.fn.touchwipe = function(settings) {
-     var config = {
-    		min_move_x: 20,
-    		min_move_y: 20,
- 			wipeLeft: function() { },
- 			wipeRight: function() { },
- 			wipeUp: function() { },
- 			wipeDown: function() { },
-			preventDefaultEvents: true
-	 };
-     
-     if (settings) $.extend(config, settings);
- 
-     this.each(function() {
-    	 var startX;
-    	 var startY;
-		 var isMoving = false;
+$(function() {
+  $.fn.swipe = function( callback ) {
+    var touchDown = false,
+        originalPosition = null,
+        $el = $( this );
 
-    	 function cancelTouch() {
-    		 this.removeEventListener('touchmove', onTouchMove);
-    		 startX = null;
-    		 isMoving = false;
-    	 }	
-    	 
-    	 function onTouchMove(e) {
-    		 if(config.preventDefaultEvents) {
-    			 e.preventDefault();
-    		 }
-    		 if(isMoving) {
-	    		 var x = e.touches[0].pageX;
-	    		 var y = e.touches[0].pageY;
-	    		 var dx = startX - x;
-	    		 var dy = startY - y;
-	    		 if(Math.abs(dx) >= config.min_move_x) {
-	    			cancelTouch();
-	    			if(dx > 0) {
-	    				config.wipeLeft();
-	    			}
-	    			else {
-	    				config.wipeRight();
-	    			}
-	    		 }
-	    		 else if(Math.abs(dy) >= config.min_move_y) {
-		    			cancelTouch();
-		    			if(dy > 0) {
-		    				config.wipeDown();
-		    			}
-		    			else {
-		    				config.wipeUp();
-		    			}
-		    		 }
-    		 }
-    	 }
-    	 
-    	 function onTouchStart(e)
-    	 {
-    		 if (e.touches.length == 1) {
-    			 startX = e.touches[0].pageX;
-    			 startY = e.touches[0].pageY;
-    			 isMoving = true;
-    			 this.addEventListener('touchmove', onTouchMove, false);
-    		 }
-    	 }    	 
-    	 if ('ontouchstart' in document.documentElement) {
-    		 this.addEventListener('touchstart', onTouchStart, false);
-    	 }
-     });
- 
-     return this;
-   };
- 
+    function swipeInfo( event ) {
+      var x = event.originalEvent.pageX,
+          y = event.originalEvent.pageY,
+          dx, dy;
+
+      dx = ( x > originalPosition.x ) ? "right" : "left";
+      dy = ( y > originalPosition.y ) ? "down" : "up";
+
+      return {
+        direction: {
+          x: dx,
+          y: dy
+        },
+        offset: {
+          x: x - originalPosition.x,
+          y: originalPosition.y - y
+        }
+      };
+    }
+
+    $el.on( "touchstart mousedown", function ( event ) {
+      touchDown = true;
+      originalPosition = {
+        x: event.originalEvent.pageX,
+        y: event.originalEvent.pageY
+      };
+    } );
+
+    $el.on( "touchend mouseup", function () {
+      touchDown = false;
+      originalPosition = null;
+    } );
+
+    $el.on( "touchmove mousemove", function ( event ) {
+      if ( !touchDown ) { return;}
+      var info = swipeInfo( event );
+      callback( info.direction, info.offset );
+    } );
+
+    return true;
+  };
+
+  $("body").swipe(function( direction, offset ) {
+      console.log( "Moving", direction.x, "and", direction.y );
+      console.log( "Touch moved by", offset.x, "horizontally and", offset.y, "vertically" );    
+    });
 });
