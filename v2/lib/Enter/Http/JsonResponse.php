f9ba4aae7a6d2548e381b93806e7d567720538c6
g84 <green840429@gmail.com>
2014-03-11 18:26:09 +0400

    SITE-2945 http.jsonResponse

--- v2/lib/Enter/Http/JsonResponse.php
+++ v2/lib/Enter/Http/JsonResponse.php
@@ -3,7 +3,21 @@
 namespace Enter\Http;
 
 class JsonResponse extends Response {
-    public function __construct(array $data = null, $statusCode = self::STATUS_OK) {
+    /** @var array */
+    public $data = [];
 
+    public function __construct(array $data = null, $statusCode = self::STATUS_OK) {
+        parent::__construct(null, $statusCode);
+
+        $this->headers['Content-Type'] = 'application/json';
+    }
+
+    /**
+     * @return $this
+     */
+    public function sendContent() {
+        $this->content = json_encode($this->data, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_AMP | JSON_HEX_QUOT);
+
+        return parent::sendContent();
     }
 }
