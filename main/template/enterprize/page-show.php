64397ad12317dd1a11cee7692267af2469a40779
tamili <ta.burlakova@gmail.com>
2014-03-11 10:45:42 +0400

    SITE-3325 Стили кнопочки - Получить

--- main/template/enterprize/page-show.php
+++ main/template/enterprize/page-show.php
@@ -5,4 +5,4 @@
  */
 ?>
 
-<a href="<?= $page->url('enterprize.form.show', ['enterprizeToken' => $enterpizeCoupon->getToken()]) ?>">Получить</a>
+<a style="margin: -15px 0 30px 10px; width: 100px;" class="formDefault__btnSubmit mBtnOrange" href="<?= $page->url('enterprize.form.show', ['enterprizeToken' => $enterpizeCoupon->getToken()]) ?>">Получить</a>
