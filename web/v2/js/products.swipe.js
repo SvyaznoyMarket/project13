23a75d79bd973a2b8b0f284d6890dc57cc33aac5
tamili <ta.burlakova@gmail.com>
2014-03-17 17:19:33 +0400

    slider

--- web/v2/js/products.swipe.js
+++ web/v2/js/products.swipe.js
@@ -3,90 +3,93 @@ $(function() {
 
   //$('.productDescImgList__item').css({'height': imgHeight});
   $(".productDescImgList").css({'height': imgHeight});
-
-  $.fn.swipe = function( callback ) {
-    var touchDown = false,
-        originalPosition = null,
-        $el = $( this );
-
-    function swipeInfo( event ) {
-      var x = event.originalEvent.pageX,
-          y = event.originalEvent.pageY,
-          dx, dy;
-
-      dx = ( x > originalPosition.x ) ? "right" : "left";
-      dy = ( y > originalPosition.y ) ? "down" : "up";
-
-      return {
-        direction: {
-          x: dx,
-          y: dy
-        },
-        offset: {
-          x: x - originalPosition.x,
-          y: originalPosition.y - y
-        }
-      };
-    }
-
-    $el.on( "touchstart mousedown", function ( event ) {
-      touchDown = true;
-      originalPosition = {
-        x: event.originalEvent.pageX,
-        y: event.originalEvent.pageY
-      };
-    } );
-
-    $el.on( "touchend mouseup", function () {
-      touchDown = false;
-      originalPosition = null;
-    } );
-
-    var draw = function draw ( offset ) {
-
-      
-    };
-
-    $el.on( "touchmove mousemove", function ( event, offset ) {
-      if ( !touchDown ) { return;}
-      var info = swipeInfo( event );
-
-      callback( info.direction, info.offset );
-
-      if ( info.offset.x > 0 ) {
-
-        event.preventDefault();
-
-        $('.productDescImgList__item.page-current').css({transform: 'translate(' + info.offset.x + 'px, 0)'});
-
-        if ( info.offset.x >= 50 ) {
-        $('.productDescImgList__item.page-current').removeClass('page-current').addClass('page-left');
-        $('.productDescImgList__item.page-right').removeClass('page-right').addClass('page-current page-animating');
-        $('.productDescImgList__item.page-current').next('.productDescImgList__item').addClass('page-right');
-        $('.productDescImgList__item.page-current').prev('.productDescImgList__item').addClass('page-left');
-
+  $('.productDescImgList__item:first').addClass('page-current');
+  $('.productDescImgList__item.page-current').next('.productDescImgList__item').addClass('page-right');
+  $('.productDescImgList__item.page-current').prev('.productDescImgList__item').addClass('page-left');
+ 
+  $.fn.swipe = function( options ) {
+    // Default thresholds & swipe functions
+    options = $.extend(true, {}, $.fn.swipe.options, options);
+     
+    return this.each(function() {
+     
+      var self = this,
+          originalCoord = { 'x': 0, 'y': 0 },
+          finalCoord = { 'x': 0, 'y': 0 };
+       
+      // Screen touched, store the initial coordinates
+      function touchStart( event ) {
+        var touch = event.originalEvent.targetTouches[0];
+        originalCoord.x = touch.pageX;
+        originalCoord.y = touch.pageY;
       }
-      };
-    } );
+       
+      // Store coordinates as finger is swiping
+      function touchMove( event ) {
+        var touch = event.originalEvent.targetTouches[0];
+        finalCoord.x = touch.pageX; // Updated X,Y coordinates
+        finalCoord.y = touch.pageY;
+        event.preventDefault();
+      }
+       
+      // Done swiping
+      // Swipe should only be on X axis, ignore if swipe on Y axis
+      // Calculate if the swipe was left or right
+      function touchEnd() {
+        var changeY = originalCoord.y - finalCoord.y,
+            changeX,
+            threshold = options.threshold,
+            y = threshold.y,
+            x = threshold.x;
 
-    return true;
+        if ( changeY < y && changeY > (- y) ) {
+          changeX = originalCoord.x - finalCoord.x;
+
+          if ( changeX > x ) {
+            options.swipeLeft.call(self);
+          } else if ( changeX < (- x) ) {
+            options.swipeRight.call(self);
+          }
+        }
+      }
+       
+      // Swipe was canceled
+      function touchCancel() {
+      //console.log('Canceling swipe gesture…')
+      }
+       
+      // Add gestures to all swipable areas
+      $(self).bind({
+        'touchstart.swipe': touchStart,
+        'touchmove.swipe': touchMove,
+        'touchend.swipe': touchEnd,
+        'touchcancel.swipe': touchCancel
+      });
+    });
+     
+  };
+     
+  $.fn.swipe.options = {
+    'threshold': {
+      'x': 30,
+      'y': 50
+    },
+
+    'swipeLeft': function() {
+      $('.productDescImgList__item.page-current').removeClass('page-current').addClass('page-left');
+      $('.productDescImgList__item.page-right').removeClass('page-right').addClass('page-current page-animating');
+      $('.productDescImgList__item.page-current').next('.productDescImgList__item').addClass('page-right');
+      $('.productDescImgList__item.page-current').prev('.productDescImgList__item').addClass('page-left');
+    },
+
+    'swipeRight': function() {
+      $('.productDescImgList__item.page-current').removeClass('page-current').addClass('page-right');
+      $('.productDescImgList__item.page-left').removeClass('page-left').addClass('page-current page-animating');
+      $('.productDescImgList__item.page-current').prev('.productDescImgList__item').addClass('page-left');
+    }
   };
 
-  $('.productDescImgList__item:first').addClass('page-current');
-  $('.productDescImgList__item.page-current').prev('.productDescImgList__item').addClass('page-left');
-  $('.productDescImgList__item.page-current').next('.productDescImgList__item').addClass('page-right');
-  $('.productDescImgList__item.page-current').removeClass('page-animating');
-
-  $(".productDescImgList__img").swipe(function( direction, offset ) {
-      console.log( "Moving", direction.x, "and", direction.y );
-      console.log( "Touch moved by", offset.x, "horizontally and", offset.y, "vertically" ); 
-      
+  $('.productDescImgList__item').swipe();
 
 
-      
-
-      // else if ( offset.x < '0' ){
-      //   //alert('left');
-      // }
-    });
 });
