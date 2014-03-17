1c66804a0e00fc08a7026498dd7e7d90e7aad78b
tamili <ta.burlakova@gmail.com>
2014-03-17 17:49:04 +0400

    SITE-3391 Если фишек больше нет, то фишка должна исчезать с витрины с фишками.

--- main/template/enterprize/page-show.php
+++ main/template/enterprize/page-show.php
@@ -7,7 +7,7 @@
 ?>
 
 <? if (is_numeric($limit) && 0 === $limit): ?>
-    <p>К сожалению, эти фишки закончились. <a href="<?= $page->url('enterprize') ?>">Посмотреть другие фишки</a></p>
+    <p class="lastFishka">К сожалению, эти фишки закончились. <a class="lastFishka__link" href="<?= $page->url('enterprize') ?>">Посмотреть другие фишки</a></p>
 <? else: ?>
     <a class="formDefault__btnSubmit getCuponEP mBtnOrange" href="<?= $page->url('enterprize.form.show', ['enterprizeToken' => $enterpizeCoupon->getToken()]) ?>">Получить</a>
 <? endif ?>
