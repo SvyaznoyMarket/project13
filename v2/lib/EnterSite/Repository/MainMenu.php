e913bbef80a6bf72dbeaff2c5594a0ee2679a4ae
g84 <green840429@gmail.com>
2014-03-18 22:00:13 +0400

    SITE-3216 Меню: продолжение

--- v2/lib/EnterSite/Repository/MainMenu.php
+++ v2/lib/EnterSite/Repository/MainMenu.php
@@ -40,37 +40,54 @@ class MainMenu {
             unset($categoryItem);
         };
         $walkByCategoryData($categoryData);
-        //die(var_dump(array_keys($categoryItemsById)));
 
-        $walkByMenuElementItem = function($elementItems, Model\MainMenu\Element $parentElement = null) use (&$menu, &$walkByMenuElementItem) {
-            foreach ($elementItems as $elementItem) {
-                $menuElement = new Model\MainMenu\Element($elementItem);
+        $walkByMenuElementItem = function($elementItems, Model\MainMenu\Element $parentElement = null) use (&$menu, &$walkByMenuElementItem, &$categoryItemsById) {
+            $elementItem = null;
+            foreach ($elementItems as &$elementItem) {
+                $element = null;
+                if (!isset($elementItem['children'])) {
+                    $elementItem['children'] = [];
+                }
 
                 $source = (!empty($elementItem['source']) && is_scalar($elementItem['source'])) ? trim((string)$elementItem['source'], '/') : null;
                 if ($source) {
                     $params = [];
+                    parse_str(parse_url($source, PHP_URL_QUERY), $params);
+
                     if ((0 === strpos($source, 'category/get')) && !empty($params['id']) && isset($categoryItemsById[$params['id']])) {
-                        $menuElement->name = (string)$categoryItemsById[$params['id']]['name'];
-                        $menuElement->url = rtrim((string)$categoryItemsById[$params['id']]['link'], '/');
+                        $element = new Model\MainMenu\Element($elementItem);
+                        $element->name = (string)$categoryItemsById[$params['id']]['name'];
+                        $element->url = rtrim((string)$categoryItemsById[$params['id']]['link'], '/');
                     } else if ((0 === strpos($source, 'category/tree')) && !empty($params['root_id']) && isset($categoryItemsById[$params['root_id']])) {
-                        //die(var_dump($categoryItemsById[$params['root_id']]));
+                        $element = $parentElement;
+                        $parentElement = null;
+                        $categoryItem = null;
+                        foreach ($categoryItemsById[$params['root_id']]['children'] as &$categoryItem) {
+                            $elementItem['children'][] = [
+                                'source' => 'category/get?id=' . $categoryItem['id'],
+                            ];
+                        }
+                        unset($categoryItem);
                     }
 
                     if (isset($elementItem['children'][0])) {
-                        $walkByMenuElementItem($elementItem['children'], $menuElement);
+                        $walkByMenuElementItem($elementItem['children'], $element);
                     }
                 }
 
+                if (!$element) continue;
+
                 if ($parentElement) {
-                    $parentElement->children[] = $menuElement;
+                    $parentElement->children[] = $element;
                 } else {
-                    $menu->elements[] = $menuElement;
+                    $menu->elements[] = $element;
                 }
             }
+            unset($elementItem);
         };
         $walkByMenuElementItem($menuData['items']);
 
-        //die(var_dump($menu));
+        //die(json_encode($menu->elements, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
 
         return $menu;
     }
