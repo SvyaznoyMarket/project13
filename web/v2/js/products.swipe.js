0d6ad63a30ff5b250d942a8c4a9cbd2bf1a76910
tamili <ta.burlakova@gmail.com>
2014-03-26 08:36:32 +0400

    v2

--- web/v2/js/products.swipe.js
+++ web/v2/js/products.swipe.js
@@ -1,6 +1,28 @@
 $(function() {
-  $('.jsSlides').slidesjs({
-    width: 940,
-    height: 528
+
+  var swipeWrap = $('.productDescImg'),
+      swipeItem = $('.productDescImg productDescImgList_img'),
+      swipeAction = $('.jsSlides'),
+  /*
+   * Функция изменяет высоту врапера swipe блока
+   */
+  getImageSize = function getImageSize() {
+
+    swipeItem.each(function() {
+      console.log(imgHeight);
+      var $this = $(this),
+          imgHeight = $this.height();
+
+      swipeWrap.css({
+        'height': imgHeight
+      });
+    });
+  };
+
+  swipeAction.slidesjs({
+    width: 350,
+    height: 350
   });
+
+  $(window).on('resize load', getImageSize);
 });
