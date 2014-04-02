7a62a6625f4c86bec1026b6f49d1b9ba7e925769
zhukovra <zhukov.roman@gmail.com>
2014-04-02 18:24:54 +0400

    Ответ завернут в result

--- main/controller/DebugAction.php
+++ main/controller/DebugAction.php
@@ -11,15 +11,15 @@ class DebugAction {
     public function info(\Http\Request $request) {
         \App::logger()->debug('Exec ' . __METHOD__);
 
+        $data = [];
+
         $gitData = [
             'version' => trim(shell_exec(sprintf('cd %s && git rev-parse --abbrev-ref HEAD', realpath(\App::config()->appDir)))),
             'tag'     => trim(shell_exec(sprintf('cd %s && git describe --always --tag', realpath(\App::config()->appDir)))),
         ];
         $gitData['url'] = 'https://github.com/SvyaznoyMarket/project13/tree/' . $gitData['version'];
 
-        $data = [
-            'git' => $gitData
-        ];
+        $data['result']['git'] = $gitData;
 
         return new \Http\JsonResponse($data);
     }
