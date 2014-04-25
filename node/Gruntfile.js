862660689e8188a2157c5b5c733fc502ddf82bb5
Alexandr Zaytcev <13rentgen@gmail.com>
2014-04-25 14:13:44 +0400

    SITE-? Node_modules в gitignore. Изменения в Gruntfile.js

--- node/Gruntfile.js
+++ node/Gruntfile.js
@@ -562,16 +562,7 @@ module.exports = function( grunt ) {
 
 	});
 
-	grunt.loadNpmTasks('grunt-contrib-less');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-qunit');
-	grunt.loadNpmTasks('grunt-contrib-connect');
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-exec');
-	grunt.loadNpmTasks('grunt-jsdoc');
-
+	require('load-grunt-tasks')(grunt);
 
 	/**
 	 * Генерация xml файла с полигонами для яндекс карт
