f507bd7107f8529f0c17df9e6c466bea579b2b2b
g84 <green840429@gmail.com>
2014-05-05 14:33:45 +0400

    SITE-3718 API для терминалов: Layout

---
+++ v2/lib/EnterTerminal/Model/Layout.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace EnterTerminal\Model {
+    use EnterSite\Model;
+
+    class Layout {
+        /** @var Model\MainMenu */
+        public $mainMenu;
+        /** @var Model\Region */
+        public $region;
+    }
+}
