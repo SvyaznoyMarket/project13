dce1a5033253de9bc6dc10ab1f44be1d04e94b8d
g84 <green840429@gmail.com>
2014-05-05 13:29:31 +0400

    SITE-3216 js: разбивка на модули: начало

---
+++ web/v2/js/module/enter.cart.common.js
@@ -0,0 +1,17 @@
+modules.define(
+    'enter.cart.common',
+    ['enter.config'],
+    function (provideModule, config) {
+        var module = (function () {
+            var init = function (config) {
+
+            }
+
+            return {
+                init: init
+            };
+        }());
+
+        provideModule(module);
+    }
+);
