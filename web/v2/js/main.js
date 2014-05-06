ef0046d05149eedf409db740f5e2eb168dff578b
g84 <green840429@gmail.com>
2014-05-06 16:26:39 +0400

    SITE-3216 js: разбивка на модули: продолжение

--- web/v2/js/main.js
+++ web/v2/js/main.js
@@ -1,14 +1,47 @@
-(function(window, document, modules) {
-    /**
-     * Расширяем стандартные функции YM Modules
-     */
-    modules.require(['loader'], function(loader) {
-        modules.setOptions({
-            loader: loader
-        });
+
+    requirejs.config({
+        baseUrl: "/v2/js",
+        paths: {
+            'jquery'           : 'vendor/jquery-1.8.3',
+            'jquery.cookie'    : 'vendor/jquery/jquery.cookie-1.4.0',
+            'jquery.touchwipe' : 'vendor/jquery/jquery.touchwipe-1.6.5',
+
+            'underscore'    : 'vendor/underscore-1.6.0',
+            'html5'         : 'vendor/html5-3.6.2',
+            'mustache'      : 'vendor/mustache-0.8.2'
+        },
+
+        shim: {
+            'mustache': {
+                exports: 'mustache'
+            },
+            'jquery': {
+                exports: '$'
+            },
+            'jquery.cookie': {
+                deps: ['jquery']
+            },
+            'jquery.touchwipe': {
+                deps: ['jquery'],
+                exports: '$.fn.touchwipe'
+            }
+        }
     });
-}(
-    this,
-    this.document,
-    this.modules
-));
+
+    require(
+        ['jquery', 'jquery.touchwipe'],
+        function($) {
+            console.log(typeof $.touchwipe);
+        }
+    );
+
+    //var moduleName = 'module/' + (document.getElementById('js-enter-module').getAttribute('content') || 'default');
+
+    /*
+    require(
+        ['jquery', moduleName],
+        function($, module) {
+            console.info(module);
+        }
+    );
+    */
--- web/v2/js/vendor/jquery/jquery.cookie-1.4.0.js
+++ web/v2/js/vendor/jquery/jquery.cookie-1.4.0.js
@@ -8,7 +8,7 @@
 (function (factory) {
     if (typeof define === 'function' && define.amd) {
         // AMD
-        define(['jquery'], factory);
+        define([''], factory);
     } else if (typeof exports === 'object') {
         // CommonJS
         factory(require('jquery'));
