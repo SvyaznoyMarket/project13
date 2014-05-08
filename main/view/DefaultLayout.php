0983fa13cec543b337e58fdbd7a6163ba9984e0b
zhukovra <zhukov.roman@gmail.com>
2014-05-08 16:05:07 +0400

    SITE-3650 Добавить категорию Tсhibo в фид для Sociomantic Для главной категории Чибо

--- main/view/DefaultLayout.php
+++ main/view/DefaultLayout.php
@@ -487,7 +487,10 @@ class DefaultLayout extends Layout {
                 $return .= $this->render($smantic_path . 'smanticPage', ['cart_prods' => $cart_prods]);
             }
 
-        }/* else if ($routeName == 'order.complete') {
+        } else if ($routeName == 'tchibo') {
+            $return .= $this->render($smantic_path . 'smanticPage', ['prod_cats' => ['Tchibo']]);
+        }
+        /* else if ($routeName == 'order.complete') {
 
             // !!! На этих страницах подключается через js — /web/js/dev/order/order.js
 
