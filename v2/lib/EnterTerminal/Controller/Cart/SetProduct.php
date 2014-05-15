31b4ac9a9ec7ba0363d2b331a4e6af66e07713a7
g84 <green840429@gmail.com>
2014-05-15 16:38:46 +0400

    SITE-3718 API для терминалов: методы корзины

---
+++ v2/lib/EnterTerminal/Controller/Cart/SetProduct.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace EnterTerminal\Controller\Cart;
+
+use Enter\Http;
+use EnterSite\ConfigTrait;
+use EnterSite\CurlClientTrait;
+use EnterSite\LoggerTrait;
+use EnterSite\SessionTrait;
+use EnterSite\Curl\Query;
+use EnterSite\Model;
+use EnterSite\Repository;
+use EnterTerminal\Controller;
+
+class SetProduct {
+    use ConfigTrait, LoggerTrait, CurlClientTrait, SessionTrait {
+        ConfigTrait::getConfig insteadof LoggerTrait, CurlClientTrait, SessionTrait;
+        LoggerTrait::getLogger insteadof CurlClientTrait, SessionTrait;
+    }
+
+    /**
+     * @param Http\Request $request
+     * @throws \Exception
+     * @return Http\JsonResponse
+     */
+    public function execute(Http\Request $request) {
+        $config = $this->getConfig();
+        $curl = $this->getCurlClient();
+        $session = $this->getSession();
+        $cartRepository = new Repository\Cart();
+
+        // корзина из сессии
+        $cart = $cartRepository->getObjectByHttpSession($session);
+
+        // товара для корзины
+        $cartProduct = $cartRepository->getProductObjectByHttpRequest($request);
+        if (!$cartProduct) {
+            throw new \Exception('Товар не получен');
+        }
+
+        // добавление товара в корзину
+        $cartRepository->setProductForObject($cart, $cartProduct);
+
+        // ид магазина
+        $shopId = (new \EnterTerminal\Repository\Shop())->getIdByHttpRequest($request); // FIXME
+
+        // запрос магазина
+        $shopItemQuery = new Query\Shop\GetItemById($shopId);
+        $curl->prepare($shopItemQuery);
+
+        $curl->execute(1, 2);
+
+        // магазин
+        $shop = (new Repository\Shop())->getObjectByQuery($shopItemQuery);
+        if (!$shop) {
+            throw new \Exception(sprintf('Магазин #%s не найден', $shopId));
+        }
+
+        $productItemQuery = new Query\Product\GetItemById($cartProduct->id, $shop->regionId);
+        $curl->prepare($productItemQuery);
+
+        // токен пользователя
+        $userToken = (new Repository\User)->getTokenByHttpSession($session);
+
+        // запрос пользователя
+        $userItemQuery = $userToken ? new Query\User\GetItemByToken($userToken) : null;
+        if ($userItemQuery) {
+            $curl->prepare($userItemQuery);
+        }
+
+        // запрос корзины
+        $cartItemQuery = new Query\Cart\GetItem($cart, $shop->regionId);
+        $curl->prepare($cartItemQuery);
+
+        $curl->execute(1, 2);
+
+        // корзина из ядра
+        $cart = $cartRepository->getObjectByQuery($cartItemQuery);
+
+        // сохранение корзины в сессию
+        $cartRepository->saveObjectToHttpSession($session, $cart);
+
+        // response
+        return (new Controller\Cart())->execute($request);
+    }
+}
