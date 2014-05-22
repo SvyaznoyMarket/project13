f861038faa3013097243abc0787e93dddebabfb2
g84 <green840429@gmail.com>
2014-05-22 16:43:17 +0400

    SITE-3798 листинги: moreLink: обработка повторного нажатия

--- web/v2/js/module/product.catalog.js
+++ web/v2/js/module/product.catalog.js
@@ -16,28 +16,37 @@ define(
                     dataValue = $container.data('value')
                 ;
 
-                console.info('loadMoreProduct', $el, $container);
+                console.info('loadMoreProduct', $el.data('disabled'), $el, $container);
 
-                url && $.get(url, dataValue).done(function(response) {
-                    if (_.isObject(response.result) && dataValue && $container.length) {
-                        console.info(response.result);
-                        dataValue.page = response.result.page;
-                        dataValue.count = response.result.count;
+                if (url && (true !== $el.data('disabled'))) {
+                    $.get(url, dataValue)
+                        .done(function(response) {
+                            if (_.isObject(response.result) && dataValue && $container.length) {
+                                console.info(response.result);
+                                dataValue.page = response.result.page;
+                                dataValue.count = response.result.count;
 
-                        if (dataValue.count <= dataValue.page * dataValue.limit) {
-                            $el.hide();
-                        }
+                                if (dataValue.count <= dataValue.page * dataValue.limit) {
+                                    $el.hide();
+                                }
 
-                        _.each(response.result.productCards, function(content) {
-                            $container.append(content);
-                        });
+                                _.each(response.result.productCards, function(content) {
+                                    $container.append(content);
+                                });
 
-                        if (_.isObject(response.result.widgets)) {
-                            $body.data('widget', response.result.widgets);
-                            $body.trigger('render');
-                        }
-                    }
-                });
+                                if (_.isObject(response.result.widgets)) {
+                                    $body.data('widget', response.result.widgets);
+                                    $body.trigger('render');
+                                }
+                            }
+                        })
+                        .always(function() {
+                            $el.data('disabled', false);
+                        })
+                    ;
+
+                    $el.data('disabled', true);
+                }
 
                 e.preventDefault();
             }
