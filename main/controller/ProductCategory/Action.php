6bc01168f16bcd75df80b9162f7e1e998ada77ac
vit <vitaly.shaposhnik@gmail.com>
2014-05-23 10:45:59 +0300

    SITE-3832 Перевести рейтинг товаров на ui

--- main/controller/ProductCategory/Action.php
+++ main/controller/ProductCategory/Action.php
@@ -124,9 +124,18 @@ class Action {
                     $products[] = new \Model\Product\Entity($item);
                 }
             });
+        }
+        \App::coreClientV2()->execute(\App::config()->coreV2['retryTimeout']['medium']);
 
-            $scoreData = [];
-            \RepositoryManager::review()->prepareScoreCollection($productIds, function($data) use (&$scoreData) {
+        $scoreData = [];
+        if ((bool)$products) {
+            $productUIs = [];
+            foreach ($products as $product) {
+                if (!$product instanceof \Model\Product\BasicEntity) continue;
+                $productUIs[] = $product->getUi();
+            }
+
+            \RepositoryManager::review()->prepareScoreCollectionByUi($productUIs, function($data) use (&$scoreData) {
                 if (isset($data['product_scores'][0])) {
                     $scoreData = $data;
                 }
@@ -892,9 +901,18 @@ class Action {
                         $products[] = new \Model\Product\Entity($item);
                     }
                 });
+            }
+            \App::coreClientV2()->execute(\App::config()->coreV2['retryTimeout']['medium']);
 
-                $scoreData = [];
-                \RepositoryManager::review()->prepareScoreCollection($productIds, function($data) use (&$scoreData) {
+            $scoreData = [];
+            if ((bool)$products) {
+                $productUIs = [];
+                foreach ($products as $product) {
+                    if (!$product instanceof \Model\Product\BasicEntity) continue;
+                    $productUIs[] = $product->getUi();
+                }
+
+                \RepositoryManager::review()->prepareScoreCollectionByUi($productUIs, function($data) use (&$scoreData) {
                     if (isset($data['product_scores'][0])) {
                         $scoreData = $data;
                     }
@@ -980,9 +998,18 @@ class Action {
                         $products[] = new \Model\Product\Entity($item);
                     }
                 });
+            }
+            \App::coreClientV2()->execute(\App::config()->coreV2['retryTimeout']['medium']);
 
-                $scoreData = [];
-                \RepositoryManager::review()->prepareScoreCollection($productIds, function($data) use (&$scoreData) {
+            $scoreData = [];
+            if ((bool)$products) {
+                $productUIs = [];
+                foreach ($products as $product) {
+                    if (!$product instanceof \Model\Product\BasicEntity) continue;
+                    $productUIs[] = $product->getUi();
+                }
+
+                \RepositoryManager::review()->prepareScoreCollectionByUi($productUIs, function($data) use (&$scoreData) {
                     if (isset($data['product_scores'][0])) {
                         $scoreData = $data;
                     }
