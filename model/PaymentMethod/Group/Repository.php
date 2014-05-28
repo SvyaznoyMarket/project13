fc1c964f30170b50763160bc7c4f8e259a5a1409
Zhukov Roman <zhukov.roman@gmail.com>
2014-05-28 16:47:35 +0400

    SITE-3854 Допилить метод payment-method/get-group

--- model/PaymentMethod/Group/Repository.php
+++ model/PaymentMethod/Group/Repository.php
@@ -16,10 +16,11 @@ class Repository {
     /**
      * @param \Model\Region\Entity $region
      * @param array $filters
+     * @param array $data
      * @param $done
      * @param null $fail
      */
-    public function prepareCollection(\Model\Region\Entity $region = null, array $filters = [], $done, $fail = null) {
+    public function prepareCollection(\Model\Region\Entity $region = null, array $filters = [], array $data = [], $done, $fail = null) {
         \App::logger()->debug('Exec ' . __METHOD__ . ' ' . json_encode(func_get_args(), JSON_UNESCAPED_UNICODE));
 
         $param = [
@@ -31,6 +32,6 @@ class Repository {
             $param = array_merge($param, $filters);
         }
 
-        $this->client->addQuery('payment-method/get-group', $param, [], $done, $fail);
+        $this->client->addQuery('payment-method/get-group', $param, $data, $done, $fail);
     }
 }
