7bae1218f31f06781402dfe1c2dc20770b795237
g84 <green840429@gmail.com>
2014-06-03 15:47:45 +0400

    SITE-3795 fix live config

---
+++ v2/config/terminal/config-live.php
@@ -0,0 +1,9 @@
+<?php
+
+return function(\EnterTerminal\Config\Application $config) {
+    /** @var \Closure $handler */
+    $handler = include __DIR__ . '/config.php';
+    $handler($config);
+
+    // live config
+};
