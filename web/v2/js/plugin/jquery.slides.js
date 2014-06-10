5ef8c1361050ccf1d54c55bd26fd33230ab7a3e3
tamili <ta.burlakova@gmail.com>
2014-06-10 11:00:25 +0400

    mobile site: slidebox plugin

--- web/v2/js/plugin/jquery.slides.js
+++ web/v2/js/plugin/jquery.slides.js
@@ -1,112 +1,131 @@
-; (function( $ ){
-	var 
-	    body = $('body'),
-	    slides = $('.js-slides-img'),
-	    slidesItem = slides.find('.js-slides-img-item'),
-	    slidesImg = slidesItem.find('.js-slides-img-cont')
+(function( $ ){
+	$.fn.slidesbox = function( params ) {
+		var 
+		    w = $(window);
+	    // end of vars
 
-	    sliderPag = $('.js-slides-img-pag'),
-	    slidesImgCount = slidesItem.length,
+		return this.each(function() {
+			var
+				options = $.extend(
+							{},
+							$.fn.slidesbox.defaults,
+							params ),
+				$self = $(this),
 
-	    pager = $('.js-slides-img-pag'),
+				leftBtn = $self.find(options.leftBtnControl),
+				rightBtn = $self.find(options.rightBtnControl),
+				item = $self.find(options.itemControl),
+				cont = $self.find(options.slidesContControl),
+				pager = $self.find(options.pagerControl),
 
-	    curSlide = 1,
+				slidesCount = item.length,
 
-	    btnNext = $('.js-slides-img-right'),
-	    btnPrev = $('.js-slides-img-left');
-    // end of vars
+				curSlides = 1;
+			// end of vars
 
-	var 
-		slidesResize = function slidesResize() {
-			slidesImgH = slidesImg.height();
+			var
+				slidesResize = function slidesResize() {
+					var slidesH = cont.height();
 
-			slides.css({ 'height' : slidesImgH });
-		},
+					$self.css({ 'height' : slidesH });
+				},
 
-	  	nextSlides = function nextSlides() {
-	  		curSlide++;
+			  	nextSlides = function nextSlides() {
+			  		curSlides++;
 
-			slidesImgCenter = slides.find('.slidesImg_item-center');
+					slidesCenter = $self.find('.slidesImg_item-center');
 
-	  		slidesImgCenter.removeClass('slidesImg_item-center').addClass('slidesImg_item-left');
-	  		slidesImgCenter.next().removeClass('slidesImg_item-right').addClass('slidesImg_item-center');
+			  		slidesCenter.removeClass('slidesImg_item-center').addClass('slidesImg_item-left');
+			  		slidesCenter.next().removeClass('slidesImg_item-right').addClass('slidesImg_item-center');
 
-	  		pagerCustom();
-	  	},
+			  		pagerCustom();
+			  	},
 
-	  	prevSlides = function prevSlides() {
-	  		curSlide--;
-	  		
-			slidesImgCenter = slides.find('.slidesImg_item-center');
+			  	prevSlides = function prevSlides() {
+			  		curSlides--;
+			  		
+					slidesCenter = $self.find('.slidesImg_item-center');
 
-	  		slidesImgCenter.removeClass('slidesImg_item-center').addClass('slidesImg_item-right');
-	  		slidesImgCenter.prev().removeClass('slidesImg_item-left').addClass('slidesImg_item-center');
+			  		slidesCenter.removeClass('slidesImg_item-center').addClass('slidesImg_item-right');
+			  		slidesCenter.prev().removeClass('slidesImg_item-left').addClass('slidesImg_item-center');
 
-	  		pagerCustom();
-	  	},
+			  		pagerCustom();
+			  	},
 
-	  	pagerCustom = function pagerCustom() {
-	  		var pager = $('.js-slides-img-pag'),
-			    pagerItem = pager.find('.js-slides-img-pag-item'),
-			    pagerItemData = pagerItem.data('slide-index');
+			  	pagerCustom = function pagerCustom() {
+			  		var pager = $('.js-slides-img-pag'),
+					    pagerItem = pager.find('.js-slides-img-pag-item'),
+					    pagerItemData = pagerItem.data('slide-index');
 
-			pagerItem.first().addClass('slidesImg_pager_item-active');
+					pagerItem.first().addClass('slidesImg_pager_item-active');
 
-	  		pagerItem.each(function () {
-	  			$(this).removeClass('slidesImg_pager_item-active');
+			  		pagerItem.each(function () {
+			  			$(this).removeClass('slidesImg_pager_item-active');
 
-	  			if ( curSlide == $(this).data('slide-index') ) {
-	  				$(this).addClass('slidesImg_pager_item-active');
-	  			}
-	  		});
-	  	},
+			  			if ( curSlides == $(this).data('slide-index') ) {
+			  				$(this).addClass('slidesImg_pager_item-active');
+			  			}
+			  		});
+			  	},
 
-	  	slidesImgNext = function slidesImgNext() {
-	  		if( curSlide <= slidesImgCount - 1 ) {
-	  			nextSlides();
-	  		}
-	  	},
+			  	slidesImgNext = function slidesImgNext() {
+			  		if( curSlides <= slidesCount - 1 ) {
+			  			nextSlides();
+			  		}
+			  	},
 
-	  	slidesImgPrev = function slidesImgPrev() {
-	  		if( curSlide >= 2 ) {
-	  			prevSlides();
-	  		}
-	  	},
+			  	slidesImgPrev = function slidesImgPrev() {
+			  		if( curSlides >= 2 ) {
+			  			prevSlides();
+			  		}
+			  	},
 
-	  	addPagination = function addPagination( i ) {
-	  		var pagerHtml = '';
+			  	addPager = function addPager() {
+			  		var pagerHtml = '';
 
-	  		sliderPager = $('<div class="js-slides-img-pag slidesImg_pager" />');
+			  		sliderPager = $('<div class="js-slides-img-pag slidesImg_pager" />');
 
-	  		if ( slidesImgCount > 1 ) {
-	  			slides.append(sliderPager);
-	  		};
+			  		if ( slidesCount > 1 ) {
+			  			$self.append(sliderPager);
+			  		};
 
-			for ( var i = 1; i <= slidesImgCount; i++ ) {
-				pagerHtml += '<div class="js-slides-img-pag-item slidesImg_pager_item" data-slide-index="' + i + '" />';
-			}; 
+					for ( var i = 1; i <= slidesCount; i++ ) {
+						pagerHtml += '<div class="js-slides-img-pag-item slidesImg_pager_item" data-slide-index="' + i + '" />';
+					}; 
 
-			sliderPager.html(pagerHtml);
-	  	};
-	//end of functions
+					sliderPager.html(pagerHtml);
+			  	};
+			//end of functions
 
-	addPagination();
-	pagerCustom();
+			addPager();
+			pagerCustom();
 
-	$('.js-slides-img-pag').css({'margin-left' : - $('.js-slides-img-pag').width() / 2 })
+			$('.js-slides-img-pag').css({'margin-left' : - $('.js-slides-img-pag').width() / 2 })
 
-	slides.touchwipe({
-	    wipeLeft : function() {
-	    	slidesImgNext();
-	    },
-	    wipeRight : function() {
-	    	slidesImgPrev();
-	    }
-	});
+			$self.touchwipe({
+			    wipeLeft : function() {
+			    	slidesImgNext();
+			    },
+			    wipeRight : function() {
+			    	slidesImgPrev();
+			    }
+			});
 
-	btnNext.on('click', slidesImgNext);
-	btnPrev.on('click', slidesImgPrev);
-	$(window).on('resize', slidesResize);
-  	slidesResize();
+			rightBtn.on('click', slidesImgNext);
+			leftBtn.on('click', slidesImgPrev);
+			w.on('resize', slidesResize);
+		  	slidesResize();
+		});
 
+	};
+
+	$.fn.slidesbox.defaults = {
+		leftBtnControl: '.js-slides-img-left',
+		rightBtnControl: '.js-slides-img-right',
+		pagerControl: '.js-slides-img-pag',
+		itemControl: '.js-slides-img-item',
+		slidesContControl: '.js-slides-img-cont'
+	};
+
+	$('.slidesImg').slidesbox();
 })( jQuery );
