6acc3ccd9e0f9b3a7a7e8a43c341cd3ffa81b8cd
g84 <green840429@gmail.com>
2014-06-10 13:26:00 +0400

    SITE-3949 Новый источник для отзывов

--- config/config.php
+++ config/config.php
@@ -69,7 +69,7 @@ $c->corePrivate['retryTimeout'] = [
     'forever' => 0,
 ];
 
-$c->reviewsStore['url']          = 'http://admin.enter.ru/reviews/';
+$c->reviewsStore['url']          = 'http://scms.enter.ru/reviews/';
 $c->reviewsStore['retryCount']   = 2;
 $c->reviewsStore['timeout']      = 0.36;
 $c->reviewsStore['retryTimeout'] = [
