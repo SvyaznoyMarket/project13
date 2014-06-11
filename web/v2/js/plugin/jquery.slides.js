55dd9c86989f989fa21bac0a019dc9f69e107a65
tamili <ta.burlakova@gmail.com>
2014-06-11 13:10:07 +0400

    mobile slides

--- web/v2/js/plugin/jquery.slides.js
+++ web/v2/js/plugin/jquery.slides.js
@@ -12,63 +12,96 @@
 							params ),
 				$self = $(this),
 
+				leftBtn = $self.find(options.leftBtnControl),
+				rightBtn = $self.find(options.rightBtnControl),
+				slides = $self.find(options.slidesControl),
 				item = $self.find(options.itemControl),
 				cont = $self.find(options.slidesContControl),
 				pager = $self.find(options.pagerControl),
 
+				leftClass = 'slidesImg_item-left',
+				centerClass = 'slidesImg_item-center',
+				rightClass = 'slidesImg_item-right',
 				changeClass = 'slidesImg_item-change'
 
 				slidesData = $self.data('value'),
 				slidesDataLength = $self.data('value').length,
 
-				curSlides = 0;
+				curSlides = 0,
+				index = 0;
 			// end of vars
 
 			var
-				/**
-				 * Изменение размеров блока слайдера, при ресайзе окна.
-				 */
+				setData = function setData() {
+					item.each(function () {
+						itemUrl = $self.data('value')[index].url;
+						contSrc = $self.data('value')[index].image;
+						$(this).attr('href' , itemUrl);
+						$(this).find(cont).attr('src' , contSrc);
+
+						index++;
+					})
+				},
+
+				setDataD = function setDataD() {
+					itemUrl = $self.data('value')[curSlides].url;
+					contSrc = $self.data('value')[curSlides].image;
+					$(this).attr('href' , itemUrl);
+					$(this).find(cont).attr('src' , contSrc);
+
+					console.log(curSlides);
+				},
+
 				slidesResize = function slidesResize() {
 					slidesH = cont.height();
 					$self.css({ 'height' : slidesH });
+					slides.css({ 'height' : slidesH });
 				},
 
-				/**
-				 * Получение данные баннера, переключение анимационного класса.
-				 */
-				setData = function setData() {
-					itemUrl = $self.data('value')[curSlides].url;
-					contSrc = $self.data('value')[curSlides].image;
-					item.attr('href' , itemUrl);
-					cont.attr('src' , contSrc);
-
-					item.addClass(changeClass).delay(500).queue(function() {
-                       $(this).removeClass(changeClass);
-                       $(this).dequeue();
-                   });
-				},
-
-				/**
-				 * Переключение на следующий слайд.
-				 */
 			  	nextSlides = function nextSlides() {
-			  		curSlides++;
-			  		setData();
-			  		pagerCustom();
+					curSlides++;
+					setDataD();
+
+					slidesImgCenter = $self.find('.slidesImg_item-center');
+					slidesImgCenter.removeClass(centerClass).addClass(leftClass);
+					slidesImgCenter.next().removeClass(rightClass).addClass(centerClass);
+
+					slides.append('<a href="'+ itemUrl +'" class="js-slides-img-item slidesImg_item slidesImg_item-right"><img src="'+ contSrc +'" class="js-slides-img-cont slidesImg_cont"></a>');
+					$('.slidesImg_item-left').prev().remove();
+
+					pagerCustom();
+
+					console.log(curSlides);
+				},
+
+				prevSlides = function prevSlides() {
+					curSlides--;
+					setDataD();
+
+					slidesImgCenter = $self.find('.slidesImg_item-center');
+					slidesImgCenter.removeClass(centerClass).addClass(rightClass);
+					slidesImgCenter.prev().removeClass(leftClass).addClass(centerClass);
+
+					slides.prepend('<a href="'+ itemUrl +'" class="js-slides-img-item slidesImg_item slidesImg_item-left"><img src="'+ contSrc +'" class="js-slides-img-cont slidesImg_cont"></a>');
+					$('.slidesImg_item-right').next().remove();
+					
+					pagerCustom();
+
+					console.log(curSlides);
+				},
+
+			  	slidesImgNext = function slidesImgNext() {
+			  		if( curSlides <= slidesDataLength ) {
+			  			nextSlides();
+			  		}
 			  	},
 
-			  	/**
-				 * Переключение на предыдущий слайд.
-				 */
-			  	prevSlides = function prevSlides() {
-			  		curSlides--;
-			  		setData();
-			  		pagerCustom();
+			  	slidesImgPrev = function slidesImgPrev() {
+			  		if( curSlides >= 0 ) {
+			  			prevSlides();
+			  		}
 			  	},
 
-			  	/**
-				 * Добавляем пагинатор.
-				 */
 			  	addPager = function addPager() {
 			  		var pagerHtml = '';
 
@@ -78,16 +111,13 @@
 			  			$self.append(sliderPager);
 			  		};
 
-					for ( var i = 0; i <= slidesDataLength - 1; i++ ) {
+					for ( var i = 0; i <= slidesDataLength; i++ ) {
 						pagerHtml += '<div class="js-slides-img-pag-item slidesImg_pager_item" data-slide-index="' + i + '" />';
 					}; 
 
 					sliderPager.html(pagerHtml);
 			  	},
 
-			  	/**
-				 * Управление активным классом пагинатора.
-				 */
 			  	pagerCustom = function pagerCustom() {
 			  		pagerItem = $self.find('.js-slides-img-pag-item'),
 					pagerItemData = pagerItem.data('slide-index');
@@ -97,12 +127,14 @@
 			  		pagerItem.each(function () {
 			  			$(this).removeClass('slidesImg_pager_item-active');
 
-			  			if ( curSlides == $(this).data('slide-index') ) {
+			  			if ( curSlides == $(this).data('slide-index')) {
 			  				$(this).addClass('slidesImg_pager_item-active');
 			  			}
 			  		});
 			  	};
 			//end of functions
+
+			console.log(slidesDataLength);
 
 			setData();
 			addPager();
@@ -114,28 +146,30 @@
 
 			$self.touchwipe({
 			    wipeLeft : function() {
-			    	if( curSlides <= slidesDataLength - 2) {
-			  			nextSlides();
-			  		}
+			    	slidesImgNext();
 			    },
 			    wipeRight : function() {
-			    	if( curSlides >= 1 ) {
-			  			prevSlides();
-			  		}
+			    	slidesImgPrev();
 			    }
 			});
 
-			w.on('load resize', slidesResize);
+			rightBtn.on('click', slidesImgNext);
+			leftBtn.on('click', slidesImgPrev);
+			w.on('resize', slidesResize);
+
 			slidesResize();
 		});
+
 	};
 
 	$.fn.slidesbox.defaults = {
-		slidesControl: '.js-slides-img',
+		leftBtnControl: '.js-slides-img-left',
+		rightBtnControl: '.js-slides-img-right',
+		slidesControl: '.js-slides-img-list',
 		pagerControl: '.js-slides-img-pag',
 		itemControl: '.js-slides-img-item',
 		slidesContControl: '.js-slides-img-cont'
 	};
 
-	$('.js-slides-img').slidesbox();
+	$('.slidesImg').slidesbox();
 })( jQuery );
