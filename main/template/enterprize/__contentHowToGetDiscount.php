55e35ffb6815253e3f27930bafa46a11a3f468fe
vit <vitaly.shaposhnik@gmail.com>
2014-06-20 15:28:01 +0300

    SITE-3930 Замена формулировок Enter Prize

---
+++ main/template/enterprize/__contentHowToGetDiscount.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @var $page \View\Layout
+ */
+?>
+
+<ul class="enterPrizeList">
+    <li class="enterPrizeList__item mBlue">
+        <strong>Сайт www.enter.ru</strong><br>
+        Входите в личный кабинет на www.enter.ru!<br>
+        При оформлении Заказа в поле КУПОН или ФИШКА вводите номер фишки!
+    </li>
+
+    <li class="enterPrizeList__item mOrange">
+        <strong>Розничные магазины ENTER</strong><br>
+        Скажите сотруднику магазина, что Вы &mdash; участник Enter Prize!<br>
+        И сообщите номер Фишки при оплате заказа!
+    </li>
+
+    <li class="enterPrizeList__item mGreen">
+        <strong>Контакт-сENTER 8 800 700 00 09</strong><br>
+        Скажите оператору Контакт-cENTER, что Вы &mdash; участник Enter Prize!<br>
+        И при оформлении заказа сообщите номер Фишки!
+    </li>
+</ul>
