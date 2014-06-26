1ef2f22e70ee6a781363593d28d0c34f6499abdf
tamili <ta.burlakova@gmail.com>
2014-06-26 17:04:17 +0400

    SITE-3973

--- main/template/user/page-index.php
+++ main/template/user/page-index.php
@@ -6,17 +6,37 @@
  */
 ?>
 
+<!-- навигация по личному кабинету -->
 <menu class="personalControl">
-    <a href="" class="personalControl_link personalControl_link-active">Заказы</a>
-    <a href="" class="personalControl_link">Избранное</a>
-    <a href="" class="personalControl_link">Сравнение</a>
-    <a href="" class="personalControl_link">Личные данные</a>
-    <a href="" class="personalControl_link">Подписки e-mail и sms</a>
-    <a href="" class="personalControl_link">Фишки Enter Prize</a>
+    <li class="personalControl_item">
+        <a href="" class="personalControl_link personalControl_link-active">Заказы</a>
+    </li>
+
+    <li class="personalControl_item">
+        <a href="" class="personalControl_link">Избранное</a>
+    </li>
+
+    <li class="personalControl_item">
+        <a href="" class="personalControl_link">Сравнение</a>
+    </li>
+
+    <li class="personalControl_item">
+        <a href="" class="personalControl_link">Личные данные</a>
+    </li>
+
+    <li class="personalControl_item">
+        <a href="" class="personalControl_link">Подписки e-mail и sms</a>
+    </li>
+
+    <li class="personalControl_item">
+        <a href="" class="personalControl_link">Фишки Enter Prize</a>
+    </li>
 </menu>
+<!-- /навигация по личному кабинету -->
 
 <div class="personalTitle">Текущие заказы <span class="personalTitle_count">3</span></div>
 
+<!-- таблица текущих заказов -->
 <div class="personalTable personalTable-border">
     <div class="personalTable_row personalTable_row-head">
         <div class="personalTable_cell">Дата</div>
@@ -29,7 +49,7 @@
         <div class="personalTable_cell"></div>
     </div>
 
-    <div class="personalTable_row">
+    <div class="personalTable_row bgGrey">
         <div class="personalTable_cell">06.06.14</div>
         <div class="personalTable_cell"><a href="">COXD-305176</a></div>
         <div class="personalTable_cell personalTable_cell-text">
@@ -39,7 +59,7 @@
                 <li><a href="">и ещё 3 товара</a></li>
             </ul>
         </div>
-        <div class="personalTable_cell personalTable_cell-right">46 740 <span class="rubl">p</span></div>
+        <div class="personalTable_cell ta-r">46 740 <span class="rubl">p</span></div>
         <div class="personalTable_cell">Доставка</div>
         <div class="personalTable_cell">23:59 11.07.14</div>
         <div class="personalTable_cell">Принят</div>
@@ -52,18 +72,20 @@
         <div class="personalTable_cell personalTable_cell-text">
             Бумажный конструктор 3 шт.
         </div>
-        <div class="personalTable_cell personalTable_cell-right">740 <span class="rubl">p</span></div>
+        <div class="personalTable_cell ta-r">740 <span class="rubl">p</span></div>
         <div class="personalTable_cell">Самовывоз</div>
         <div class="personalTable_cell"></div>
         <div class="personalTable_cell">Принят</div>
         <div class="personalTable_cell"><a class="orderCancel" href="">Отменить</a></div>
     </div>
 </div>
+<!--/ таблица текущих заказов -->
 
 <div class="personalTitle">История</div>
 
+<!-- таблица истории заказов -->
 <div class="personalTable personalTable-border">
-    <div class="personalTable_row personalTable_row-head personalTable_row-center">
+    <div class="personalTable_row personalTable_row-head">
         <div class="personalTable_cell">Дата</div>
         <div class="personalTable_cell">№ заказа</div>
         <div class="personalTable_cell">Состав</div>
@@ -73,7 +95,7 @@
         <div class="personalTable_cell"></div>
     </div>
 
-    <div class="personalTable_row">
+    <div class="personalTable_row bgGrey">
         <div class="personalTable_cell">06.06.14</div>
         <div class="personalTable_cell"><a href="">COXD-305176</a></div>
         <div class="personalTable_cell personalTable_cell-text">
@@ -83,7 +105,7 @@
                 <li><a href="">и ещё 3 товара</a></li>
             </ul>
         </div>
-        <div class="personalTable_cell personalTable_cell-right">
+        <div class="personalTable_cell ta-r">
             46 740 <span class="rubl">p</span><br/>
             <span class="textStatus">оплачено</span>
         </div>
@@ -98,7 +120,7 @@
         <div class="personalTable_cell personalTable_cell-text">
             Бумажный конструктор 3 шт.
         </div>
-        <div class="personalTable_cell personalTable_cell-right">
+        <div class="personalTable_cell ta-r">
             740 <span class="rubl">p</span><br/>
             <span class="textStatus">оплачено</span>
         </div>
@@ -106,22 +128,24 @@
         <div class="personalTable_cell">Принят</div>
         <div class="personalTable_cell"><a class="orderCancel" href="">Добавить в корзину</a></div>
     </div>
-
+    
+    <!-- отмененный заказ, к ячейком добавляем класс colorGrey, кроме последний двух-->
     <div class="personalTable_row personalTable_row-canceled">
-        <div class="personalTable_cell personalTable_cell-strong">06.06.14</div>
-        <div class="personalTable_cell personalTable_cell-strong"><a href="">COXD-305176</a></div>
-        <div class="personalTable_cell personalTable_cell-text personalTable_cell-strong">
+        <div class="personalTable_cell colorGrey">06.06.14</div>
+        <div class="personalTable_cell colorGrey"><a href="">COXD-305176</a></div>
+        <div class="personalTable_cell personalTable_cell-text colorGrey">
             <ul class="orderItem">
                 <li>Бумажный конструктор 3 шт.</li>
                 <li>Карта памяти microSDHC… 1 шт.</li>
                 <li><a href="">и ещё 3 товара</a></li>
             </ul>
         </div>
-        <div class="personalTable_cell personalTable_cell-strong personalTable_cell-right">46 740 <span class="rubl">p</span></div>
-        <div class="personalTable_cell personalTable_cell-strong">Доставка</div>
+        <div class="personalTable_cell ta-r colorGrey">46 740 <span class="rubl">p</span></div>
+        <div class="personalTable_cell colorGrey">Доставка</div>
         <div class="personalTable_cell">Отменён</div>
         <div class="personalTable_cell"><a class="orderCancel" href="">Добавить в корзину</a></div>
     </div>
+    <!--/ отмененный заказ, к ячейком добавляем класс colorGrey, кроме последний двух-->
 
     <div class="personalTable_row">
         <div class="personalTable_cell">06.06.14</div>
@@ -129,7 +153,7 @@
         <div class="personalTable_cell personalTable_cell-text">
             Бумажный конструктор 3 шт.
         </div>
-        <div class="personalTable_cell personalTable_cell-right">
+        <div class="personalTable_cell ta-r">
             40 <span class="rubl">p</span>
             <span class="textStatus">оплачено</span>
         </div>
@@ -138,45 +162,58 @@
         <div class="personalTable_cell"><a class="orderCancel" href="">Добавить в корзину</a></div>
     </div>
 </div>
+<!--/ таблица истории заказов -->
 
-<div class="personalTitle"><a href="">Текущие заказы</a> <span class="personalTitle_count">3</span></div>
+<div class="personalTitle"><a class="td-underl" href="">Текущие заказы</a> <span class="personalTitle_count">3</span></div>
 
 <div class="personalPage_left">
-    <div class="personalPage_head">
-        <h2 class="personalPage_head_left orderTitle_left-mark">Заказ COXD-305176 от 06.06.14</h2>
+    <div class="personalPage_head clearfix">
+        <h2 class="personalPage_head_left">Заказ COXD-305176 от 06.06.14</h2>
 
-        <div class="personalPage_head">К оплате: <span class="orderPrice">42 740 <span class="rubl">p</span></span></div>
+        <div class="personalPage_head_right">К оплате: <span class="orderPrice">42 740 <span class="rubl">p</span></span></div>
     </div>
+    
+    <!-- статусы заказа -->
+    <ul class="personalControl personalControl-arrow">
+        <li class="personalControl_item personalControl_item-arrow personalControl_item-active">
+            Принят
+        </li>
 
-    <ul class="personalControl">
-        <li class="personalControl_link personalControl_link-active">Принят</li>
-        <li class="personalControl_link">Отправлен</li>
-        <li class="personalControl_link">Получен</li>
+        <li class="personalControl_item personalControl_item-arrow">
+            Отправлен
+        </li>
+
+        <li class="personalControl_item">
+            Получен
+        </li>
     </ul>
-
+    <!--/ статусы заказа -->
+    
+    <!-- информация о заказе -->
     <div class="personalInfo">
-        <p>Плановый срок получения заказа <mark class="decorColor">Завтра, 15 июня, воскресенье</mark></p>
+        <p>Плановый срок получения заказа <mark class="colorBlack">Завтра, 15 июня, воскресенье</mark></p>
         
         <div class="personalTable">
             <div class="personalTable_row">
                 <div class="personalTable_cell">Магазин</div>
 
-                <div class="personalTable_cell personalTable_cell-s">
-                    <mark class="decorColor decorColor-bullet" style="color: #b1b4c2"><span class="decorColor">м. Петровско-Разумовская</span></mark>
+                <div class="personalTable_cell s">
+                    <mark class="colorBlack decorColor-bullet" style="color: #b1b4c2"><span class="colorBlack">м. Петровско-Разумовская</span></mark>
                     ул. Линии Октябрьской Железной Дороги, д. 1, стр. 2
                 </div>
 
-                <div class="personalTable_cell personalTable_cell-s">
-                    <mark class="decorColor">режим работы</mark><br/>
+                <div class="personalTable_cell s">
+                    <mark class="colorBlack">режим работы</mark><br/>
                     с 9.00 до 22.00
                 </div>
 
-                <div class="personalTable_cell"><a class="decorColor">Как добраться?</a></div>
+                <div class="personalTable_cell va-m"><a class="colorBlack">Как добраться?</a></div>
             </div>
         </div>
 
         <p>В магазине можно оплатить заказ как наличными, так и банковской картой.</p>
     </div>
+    <!--/ информация о заказе -->
 
     <div class="personalTable personalTable-border nomargin">
         <div class="personalTable_row">
@@ -189,13 +226,13 @@
                 Jazwares Minecraft Papercraft «Дружелюбные мобы»
             </div>
 
-            <div class="personalTable_cell personalTable_cell-right personalTable_cell-w60 personalTable_cell-l personalTable_cell-strong">
+            <div class="personalTable_cell personalTable_cell-w60 l colorGrey ta-r">
                 200 <span class="rubl">p</span><br/>
             </div>
 
-            <div class="personalTable_cell personalTable_cell-right personalTable_cell-w60 personalTable_cell-l personalTable_cell-strong">2 шт.</div>
+            <div class="personalTable_cell personalTable_cell-w60 l colorGrey ta-r">2 шт.</div>
 
-            <div class="personalTable_cell personalTable_cell-right personalTable_cell-l">
+            <div class="personalTable_cell l ta-r">
                 400 <span class="rubl">p</span><br/>
             </div>
         </div>
@@ -210,13 +247,13 @@
                 Jazwares Minecraft Papercraft «Дружелюбные мобы»
             </div>
 
-            <div class="personalTable_cell personalTable_cell-right personalTable_cell-w60 personalTable_cell-l personalTable_cell-strong">
+            <div class="personalTable_cell personalTable_cell-w60 l colorGrey ta-r">
                 1 200 <span class="rubl">p</span><br/>
             </div>
 
-            <div class="personalTable_cell personalTable_cell-right personalTable_cell-w60 personalTable_cell-l personalTable_cell-strong">2 шт.</div>
+            <div class="personalTable_cell personalTable_cell-w60 l colorGrey ta-r">2 шт.</div>
 
-            <div class="personalTable_cell personalTable_cell-right personalTable_cell-l">
+            <div class="personalTable_cell l ta-r">
                 400 <span class="rubl">p</span><br/>
             </div>
         </div>
@@ -231,13 +268,13 @@
                 Jazwares Minecraft Papercraft «Дружелюбные мобы»
             </div>
 
-            <div class="personalTable_cell personalTable_cell-right personalTable_cell-w60 personalTable_cell-l personalTable_cell-strong">
+            <div class="personalTable_cell personalTable_cell-w60 l colorGrey ta-r">
                 12 200 <span class="rubl">p</span><br/>
             </div>
 
-            <div class="personalTable_cell personalTable_cell-right personalTable_cell-w60 personalTable_cell-l personalTable_cell-strong">2 шт.</div>
+            <div class="personalTable_cell personalTable_cell-w60 l colorGrey ta-r">2 шт.</div>
 
-            <div class="personalTable_cell personalTable_cell-right personalTable_cell-l">
+            <div class="personalTable_cell personalTable_cell-l ta-r">
                 400 <span class="rubl">p</span><br/>
             </div>
         </div>
@@ -258,7 +295,7 @@
                 Минимальная сумма заказа 6999 руб
             </div>
 
-            <div class="personalTable_cell personalTable_cell-right personalTable_cell-l personalTable_cell-warn">
+            <div class="personalTable_cell personalTable_cell-right l colorRed ta-r">
                 - 400 <span class="rubl">p</span><br/>
             </div>
         </div>
@@ -272,13 +309,41 @@
                 Подарочный сертификат 5000 руб
             </div>
 
-            <div class="personalTable_cell personalTable_cell-right personalTable_cell-l personalTable_cell-warn">
+            <div class="personalTable_cell personalTable_cell-right l colorRed ta-r">
                 - 2 400 <span class="rubl">p</span><br/>
+            </div>
+        </div>
+
+        <div class="personalTable_row personalTable_row-total ta-r">
+            <div class="personalTable_cell">
+            </div>
+
+            <div class="personalTable_cell">
+                Самовывоз:
+            </div>
+
+            <div class="personalTable_cell">
+                Бесплатно
+            </div>
+        </div>
+
+        <div class="personalTable_row personalTable_row-total ta-r">
+            <div class="personalTable_cell">
+            </div>
+
+            <div class="personalTable_cell ">
+                Итого:
+            </div>
+
+            <div class="personalTable_cell l">
+                <span class="colorGrey td-lineth">47 580 <span class="rubl">p</span></span><br/>
+                42 740 <span class="rubl">p</span>
             </div>
         </div>
     </div>
 </div>
 
+<!-- сайдбар онлайн оплаты -->
 <aside class="personalPage_right">
     <menu class="payCommands">
         <ul class="payCommandsList">
@@ -290,6 +355,7 @@
                     <img src="/styles/personal-page/img/cards/sber.png" alt="" class="descPay_img" />
                 </span>
             </li>
+
             <li class="payCommandsList_item">
                 <button class="btnPay">Оплатить онлайн</button>
 
@@ -304,7 +370,7 @@
         </ul>
     </menu>
 </aside>
-
+<!--/ сайдбар онлайн оплаты -->
 
 
 
