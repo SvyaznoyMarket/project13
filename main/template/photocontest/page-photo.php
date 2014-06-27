0a76ac53c190fc9bef992b58ad727ec6f7028e59
Vadim Kovalenko <vadim.kovalenko@enter.ru>
2014-06-27 11:54:37 +0400

    photocontest - facebook widget fix

--- main/template/photocontest/page-photo.php
+++ main/template/photocontest/page-photo.php
@@ -47,7 +47,6 @@
 <h2 class="pc_left">Вы можете оставить свой комментарий к этой фотографии</h2>
 
  <!--Комментарии вконтакте--> 
- <!-- Put this script tag to the <head> of your page -->
 <script type="text/javascript" src="//vk.com/js/api/openapi.js?113"></script>
 <script type="text/javascript">VK.init({apiId: <?=(App::config()->debug?4432950:4432944)?>, onlyWidgets: true}); //production</script>
 
@@ -63,7 +62,7 @@
 
 <div id="comments-box">
 	<div class="bCommentSn mVk pc_left" id="vk_comments"></div>
-	<div class="bCommentSn mFb fb-comments" data-href="<?=$request->getRequestUri()?>" data-numposts="10" data-width="460"></div>
+	<div class="bCommentSn mFb fb-comments" data-href="<?=$request->getSchemeAndHttpHost().'/'.$request->getRequestUri()?>" data-numposts="10" data-width="460"></div>
 </div>
 <script type="text/javascript">VK.Widgets.Comments("vk_comments", {limit: 10, width: "460", attach: false});</script>
 
