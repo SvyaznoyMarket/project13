7bf50c8e30544ed52edd71e875db7d8a091ae0c4
Vitaly Shaposhnik <vitaly.shaposhnik@gmail.com>
2014-06-27 19:27:13 +0300

    SITE-3824 Создание механики и лэндинга для розницы в Enter Prize

--- main/controller/Enterprize/IndexAction.php
+++ main/controller/Enterprize/IndexAction.php
@@ -105,6 +105,9 @@ class IndexAction {
             return true;
         });
 
+        $enterpizeCoupon = $repository->getEntityByToken('8ca49a4e-ade1-11e3-93ea-e4115b11879a');
+        $isCouponSent = true;
+
         // получаем товары
         $products = [];
         if ($enterpizeCoupon) {
