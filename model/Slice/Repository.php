0324e28734d7ced10c625777459a02d09d98d46a
Vitaly Shaposhnik <vitaly.shaposhnik@gmail.com>
2014-07-09 18:33:23 +0300

    SITE-3382 seo слайзы редирект на основной URL

--- model/Slice/Repository.php
+++ model/Slice/Repository.php
@@ -23,4 +23,15 @@ class Repository {
 
         $this->client->addQuery(sprintf('slice/%s.json', $token), [], $done, $fail);
     }
+
+    /**
+     * @param $token
+     * @param $done
+     * @param null $fail
+     */
+    public function prepareSeoJsonByToken($token, $done, $fail = null) {
+        \App::logger()->debug('Exec ' . __METHOD__ . ' ' . json_encode(func_get_args(), JSON_UNESCAPED_UNICODE));
+
+        $this->client->addQuery(sprintf('seo/slice/%s.json', $token), [], $done, $fail);
+    }
 }
