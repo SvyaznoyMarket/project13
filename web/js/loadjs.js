13fd014529390c5f08b52a1f5e1282209d9239c2
Vadim Kovalenko <vadim.kovalenko@enter.ru>
2014-07-15 19:30:45 +0400

    - disable game slots

--- web/js/loadjs.js
+++ web/js/loadjs.js
@@ -338,8 +338,8 @@
 					.script('adfox.asyn.code.ver3.min.js')
 					.wait()
 					.script( getWithVersion('ports.js') )
-					.wait()
-					.script( getWithVersion('/js/game/slots.js') )
+//					.wait()
+//					.script( getWithVersion('/js/game/slots.js') )
 					.wait()
 					.script( logTimeAfterPartnerScript );
 			}).runQueue();
