72d29b6160364c525dc73d9e1befbe9a3aefe306
Zhukov Roman <zhukov.roman@gmail.com>
2014-07-18 11:46:08 +0400

    SITE-4148 При редиректе с мобильного сайта на полную версию передавать регион пользователя Удаление старой куки

--- web/index.php
+++ web/index.php
@@ -144,6 +144,8 @@ $response = null;
                     $domain
                 );
             }
+
+            $response->headers->clearCookie('geoshop', '/', 'www.enter.ru');
         } catch (\Exception $e) {
             \App::logger()->error($e, ['response']);
         }
