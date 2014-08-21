a4808411426cb491351ee7de55e647a3f34e7cfc
Zhukov Roman <zhukov.roman@gmail.com>
2014-08-21 15:05:21 +0400

    Fix smart address 2

--- web/js/dev/order-v3/_smart-address.js
+++ web/js/dev/order-v3/_smart-address.js
@@ -299,7 +299,7 @@
                 if (spinner) spinner.spin($('.kladr_spinner')[0]);
                 address = new Address({});
                 console.log('Определение адреса КЛАДР, запрос', $.extend(config, {limit: 1, type: $.kladr.type.city, name: $('#region-name').data('value')}));
-                $.kladr.api($.extend(config, {limit: 1, contentType: $.kladr.type.city, query: $('#region-name').data('value')}), function (data){
+                $.kladr.api($.extend(config, {'limit': 1, type: $.kladr.type.city, name: $('#region-name').data('value')}), function (data){
                     console.log('KLADR data', data);
                     var id = data.length > 0 ? data[0].id : 0;
                     if (id==0) console.error('КЛАДР не определил город, конфигурация запроса: ', $.extend(config, {limit: 1, type: $.kladr.type.city, name: $('#region-name').data('value')}));
