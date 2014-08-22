d7c2cc56dbc02205788bc5254d4c0b78e173dd7b
Vitaly Shaposhnik <vitaly.shaposhnik@gmail.com>
2014-08-23 00:59:08 +0300

    SITE-4260 Не отображать весь блок с Одноруким Бандитом, если виджет не стартанул

--- web/js/dev/main-parallax/parallax.js
+++ web/js/dev/main-parallax/parallax.js
@@ -1,34 +1,28 @@
 ;(function($) {
 	var
-		slotsWrap = $('#slotsWrapWrapper');
-	// end of vars
+		slotsWrap = $('#slotsWrapWrapper'),
 
 		parallax = {
-			"init": function() {
-				var
-					bodyParallax = $("body.parallax");
+			init: function() {
+				var bodyParallax = $("body.parallax");
 
 				bodyParallax.find(".top-parallax-block, .bannersbox").css("position", "fixed");
 				bodyParallax.find(".center-parallax-block").css({"position": "absolute", "top": "635px", "z-index": 1000, "width": "100%"});
-				bodyParallax.find(".center-parallax-block #slotsBack").css({"top": "0px !important", "background": "#000000 url(img/slot_background.png) no-repeat bottom", "opacity": 0});
-				bodyParallax.find(".center-parallax-block #slotsBottomLine").css({"background-position": "center", "background-repeat": "no-repeat"});
 				bodyParallax.find(".bottom-parallax-block").css({"position": "relative", "top": "518px", "background": "#000000", "padding-bottom": "20px"});
 				bodyParallax.find(".bottom-parallax-block .footer__main").css({"background": "black"});
 				bodyParallax.find(".bottom-parallax-block .footer__main #mainPageFooter .copy").css({"background": "black"});
-				bodyParallax.find(".bottom-parallax-block .gameBandit").css({"display": "block"});
+				bodyParallax.find(".center-parallax-block .gameBandit").css({"display": "block"});
 
 				parallax.scrollOpacityInit();
 			},
 
-			"scrollOpacityInit": function() {
-				var
-					fadeStart = 50,
-					fadeUntil = 635;
-				// end of vars
-
+			scrollOpacityInit: function() {
 				var
 					scrollHandler = function() {
-						var offset = $(document).scrollTop(),
+						var
+							fadeStart = 0,
+							fadeUntil = $("body.parallax .top-parallax-block").height() || 635,
+							offset = $(document).scrollTop(),
 							opacity = 1;
 						// end of vars
 
