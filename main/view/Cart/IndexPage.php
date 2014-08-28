4b5b84daba4a871dd0baabf3f853dbc17dfc1598
Vitaly Shaposhnik <vitaly.shaposhnik@gmail.com>
2014-08-28 17:57:14 +0300

    SITE-1919 Если в IE попытаться ввести количество товара вручную из корзины, то оно отображается некорректно

--- main/view/Cart/IndexPage.php
+++ main/view/Cart/IndexPage.php
@@ -6,6 +6,11 @@ class IndexPage extends \View\DefaultLayout {
     protected $layout  = 'layout-oneColumn';
 
     public function prepare() {
+        // TODO - Костыль для IE10. SITE-1919
+        if(preg_match('/msie 10/i', $_SERVER['HTTP_USER_AGENT'])) {
+            $this->addStylesheet('/css/basket/ie10.min.css');
+        }
+
         $this->setTitle('Корзина - Enter.ru');
         $this->setParam('title', 'Корзина');
     }
