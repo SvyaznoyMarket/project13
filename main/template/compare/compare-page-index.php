c4ca53a25b61d1fb434cbc2e9f998b80d938846b
tamili <ta.burlakova@gmail.com>
2014-09-26 13:47:46 +0400

    compare

--- main/template/compare/compare-page-index.php
+++ main/template/compare/compare-page-index.php
@@ -4,6 +4,458 @@ return function (
     \Helper\TemplateHelper $helper
 ) { ?>
 
-    CONTENT HERE
+<script>
+	$(function() {
+		$('.cmprCntW').css({'height' : $('body').height() - $('.cmprHd').height() });
+	});
+</script>
 
+    <div class="cmprHd clearfix">
+    	<div class="cmprHd_l"><a href=""><img src="/styles/compare/img/logo_cmpr.png" alt=""></a></div>
+
+    	<div class="cmprHd_r">
+    		<div class="cmprHd_t">
+    			Сравнение товаров
+    		</div>
+
+    		<ul class="cmprHd_lst">
+    			<li class="cmprHd_lst_i cmprHd_lst_i-act">Планшетные компьютеры <span class="cmprHd_lst_qn">12</span></li>
+    			<li class="cmprHd_lst_i">Автомобили и электромотоциклы <span class="cmprHd_lst_qn">99</span></li>
+    			<li class="cmprHd_lst_i">Планшетные компьютеры <span class="cmprHd_lst_qn">1200</span></li>
+    			<li class="cmprHd_lst_i">Автомобили и электромотоциклы <span class="cmprHd_lst_qn">132</span></li>
+    			<li class="cmprHd_lst_i">Сушильные машины <span class="cmprHd_lst_qn">12343</span></li>
+    			<li class="cmprHd_lst_i">Автомобили и электромотоциклы <span class="cmprHd_lst_qn">122</span></li>
+    		</ul>
+    	</div>
+    </div>
+	
+	<div class="cmprCntW">
+	<div class="cmprCntScroll">
+	<div class="cmprCnt clearfix">
+	    <div class="cmprCnt_row cmprCnt_row cmprCnt_row-fix">
+	        <div class="cmprCnt_cell cmprCnt_cell-hd">
+	            <menu class="cmprCnt_mode">
+	                <button class="cmprCnt_mode_btn btn1">Только отличия</button>
+	                <button class="cmprCnt_mode_btn btn1 cmprCnt_mode_btn-act">Все характеристики</button>
+	            </menu>
+	        </div>
+
+	        <div class="cmprCnt_cell cmprCnt_thd">
+                <a class="cmprCnt_img" href="">
+                    <img src="http://fs04.enter.ru/1/1/163/d0/202993.jpg" class="descrImg_img">
+                </a>
+				
+				<div>
+					<a href="" class="cmprCnt_cat">Планшетный компьютер</a><br/>
+		            <a href="" class="cmprCnt_n">Wexler TAB 7iD 8ГБ 3G черный</a>
+	            </div>
+				
+                <span class="cmprCnt_price">
+                    4 690
+                    <span class="rubl">p</span>
+                </span>
+					
+                <span class="cmprCnt_price">
+                    4 690
+                    <span class="rubl">p</span>
+                </span>
+
+				<div class="cmprCnt_buy"><a href="" class="cmprCnt_buy_lk btnBuy__eLink">Купить</a></div>
+	        </div>
+	    </div>
+		
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cpt cmprCnt_cell">
+                <span class="cmprCnt_cpt_t">ОС, процессор и ещё какие-то буквы</span>
+            </div>
+
+            <div class="cmprCnt_cpt cmprCnt_cell"></div>
+        </div>
+		
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cell">
+                Платформа
+            </div>
+
+            <div class="cmprCnt_cell">
+                Android
+            </div>
+		</div>
+
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cell">
+                Платформа
+            </div>
+
+            <div class="cmprCnt_cell">
+                4.1
+            </div>
+		</div>
+
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cpt cmprCnt_cell">
+                <span class="cmprCnt_cpt_t">ОС, процессор</span>
+            </div>
+
+            <div class="cmprCnt_cpt cmprCnt_cell"></div>
+        </div>
+		
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cell">
+                Платформа
+            </div>
+
+            <div class="cmprCnt_cell">
+                Android
+            </div>
+		</div>
+
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cell">
+                Платформа
+            </div>
+
+            <div class="cmprCnt_cell">
+                4.1
+            </div>
+		</div>
+
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cpt cmprCnt_cell">
+                <span class="cmprCnt_cpt_t">ОС, процессор и ещё какие-то буквы</span>
+            </div>
+
+            <div class="cmprCnt_cpt cmprCnt_cell"></div>
+        </div>
+		
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cell">
+                Платформа
+            </div>
+
+            <div class="cmprCnt_cell">
+                Android
+            </div>
+		</div>
+
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cell">
+                Платформа
+            </div>
+
+            <div class="cmprCnt_cell">
+                4.1
+            </div>
+		</div>
+
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cpt cmprCnt_cell">
+                <span class="cmprCnt_cpt_t">ОС, процессор</span>
+            </div>
+
+            <div class="cmprCnt_cpt cmprCnt_cell"></div>
+        </div>
+		
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cell">
+                Платформа
+            </div>
+
+            <div class="cmprCnt_cell">
+                Android
+            </div>
+		</div>
+
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cell">
+                Платформа
+            </div>
+
+            <div class="cmprCnt_cell">
+                4.1
+            </div>
+		</div>
+
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cpt cmprCnt_cell">
+                <span class="cmprCnt_cpt_t">ОС, процессор и ещё какие-то буквы</span>
+            </div>
+
+            <div class="cmprCnt_cpt cmprCnt_cell"></div>
+        </div>
+		
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cell">
+                Платформа
+            </div>
+
+            <div class="cmprCnt_cell">
+                Android
+            </div>
+		</div>
+
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cell">
+                Платформа
+            </div>
+
+            <div class="cmprCnt_cell">
+                4.1
+            </div>
+		</div>
+
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cpt cmprCnt_cell">
+                <span class="cmprCnt_cpt_t">ОС, процессор</span>
+            </div>
+
+            <div class="cmprCnt_cpt cmprCnt_cell"></div>
+        </div>
+		
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cell">
+                Платформа
+            </div>
+
+            <div class="cmprCnt_cell">
+                Android
+            </div>
+		</div>
+
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cell">
+                Платформа
+            </div>
+
+            <div class="cmprCnt_cell">
+                4.1
+            </div>
+		</div>
+
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cpt cmprCnt_cell">
+                <span class="cmprCnt_cpt_t">ОС, процессор и ещё какие-то буквы</span>
+            </div>
+
+            <div class="cmprCnt_cpt cmprCnt_cell"></div>
+        </div>
+		
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cell">
+                Платформа
+            </div>
+
+            <div class="cmprCnt_cell">
+                Android
+            </div>
+		</div>
+
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cell">
+                Платформа
+            </div>
+
+            <div class="cmprCnt_cell">
+                4.1
+            </div>
+		</div>
+
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cpt cmprCnt_cell">
+                <span class="cmprCnt_cpt_t">ОС, процессор</span>
+            </div>
+
+            <div class="cmprCnt_cpt cmprCnt_cell"></div>
+        </div>
+		
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cell">
+                Платформа
+            </div>
+
+            <div class="cmprCnt_cell">
+                Android
+            </div>
+		</div>
+
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cell">
+                Платформа
+            </div>
+
+            <div class="cmprCnt_cell">
+                4.1
+            </div>
+		</div>
+
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cpt cmprCnt_cell">
+                <span class="cmprCnt_cpt_t">ОС, процессор и ещё какие-то буквы</span>
+            </div>
+
+            <div class="cmprCnt_cpt cmprCnt_cell"></div>
+        </div>
+		
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cell">
+                Платформа
+            </div>
+
+            <div class="cmprCnt_cell">
+                Android
+            </div>
+		</div>
+
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cell">
+                Платформа
+            </div>
+
+            <div class="cmprCnt_cell">
+                4.1
+            </div>
+		</div>
+
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cpt cmprCnt_cell">
+                <span class="cmprCnt_cpt_t">ОС, процессор</span>
+            </div>
+
+            <div class="cmprCnt_cpt cmprCnt_cell"></div>
+        </div>
+		
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cell">
+                Платформа
+            </div>
+
+            <div class="cmprCnt_cell">
+                Android
+            </div>
+		</div>
+
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cell">
+                Платформа
+            </div>
+
+            <div class="cmprCnt_cell">
+                4.1
+            </div>
+		</div>
+
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cpt cmprCnt_cell">
+                <span class="cmprCnt_cpt_t">ОС, процессор и ещё какие-то буквы</span>
+            </div>
+
+            <div class="cmprCnt_cpt cmprCnt_cell"></div>
+        </div>
+		
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cell">
+                Платформа
+            </div>
+
+            <div class="cmprCnt_cell">
+                Android
+            </div>
+		</div>
+
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cell">
+                Платформа
+            </div>
+
+            <div class="cmprCnt_cell">
+                4.1
+            </div>
+		</div>
+
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cpt cmprCnt_cell">
+                <span class="cmprCnt_cpt_t">ОС, процессор</span>
+            </div>
+
+            <div class="cmprCnt_cpt cmprCnt_cell"></div>
+        </div>
+		
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cell">
+                Платформа
+            </div>
+
+            <div class="cmprCnt_cell">
+                Android
+            </div>
+		</div>
+
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cell">
+                Платформа
+            </div>
+
+            <div class="cmprCnt_cell">
+                4.1
+            </div>
+		</div>
+
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cpt cmprCnt_cell">
+                <span class="cmprCnt_cpt_t">ОС, процессор и ещё какие-то буквы</span>
+            </div>
+
+            <div class="cmprCnt_cpt cmprCnt_cell"></div>
+        </div>
+		
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cell">
+                Платформа
+            </div>
+
+            <div class="cmprCnt_cell">
+                Android
+            </div>
+		</div>
+
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cell">
+                Платформа
+            </div>
+
+            <div class="cmprCnt_cell">
+                4.1
+            </div>
+		</div>
+
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cpt cmprCnt_cell">
+                <span class="cmprCnt_cpt_t">ОС, процессор</span>
+            </div>
+
+            <div class="cmprCnt_cpt cmprCnt_cell"></div>
+        </div>
+		
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cell">
+                Платформа
+            </div>
+
+            <div class="cmprCnt_cell">
+                Android
+            </div>
+		</div>
+
+		<div class="cmprCnt_row">
+			<div class="cmprCnt_cell">
+                Платформа
+            </div>
+
+            <div class="cmprCnt_cell">
+                4.1
+            </div>
+		</div>
+    </div>
+    </div>
+    </div>
 <? } ?>
