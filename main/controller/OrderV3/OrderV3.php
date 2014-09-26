5f5c144a692e5a993550f53010c1d4f13ca78fa5
Zhukov Roman <zhukov.roman@gmail.com>
2014-09-26 16:42:05 +0400

    Новое оформление для любого региона доступно в debug-режиме

--- main/controller/OrderV3/OrderV3.php
+++ main/controller/OrderV3/OrderV3.php
@@ -31,6 +31,8 @@ class OrderV3 {
     public function execute(\Http\Request $request) {
         \App::logger()->debug('Exec ' . __METHOD__);
 
+        if (\App::config()->debug) return null; // чтобы можно было смотреть разбиение на тестовых площадках
+
         if (!in_array($this->user->getRegion()->getId(), [119623, 93746, 14974])) {
             return new \Http\RedirectResponse(\App::router()->generate('order'));
         }
