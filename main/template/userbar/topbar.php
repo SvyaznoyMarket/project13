ea25cf5edf6d77020c80cbf305e9a65c2ea015be
tamili <ta.burlakova@gmail.com>
2014-09-29 16:44:28 +0400

    icon ep header

--- main/template/userbar/topbar.php
+++ main/template/userbar/topbar.php
@@ -1,50 +1,50 @@
 <?php
 /**
- * @var $page \View\DefaultLayout
- * @var $user \Session\User
+ * @var $page           \View\DefaultLayout
  */
 ?>
 
-<div class="topbar clearfix">
-    <a class="topbar_loc jsChangeRegion" href="<?= $page->url('region.change', ['regionId' => $user->getRegion()->getId()]) ?>" data-url="<?= $page->url('region.init') ?>" data-region-id="<?= $user->getRegion()->getId() ?>" data-autoresolve-url="<?= $page->url('region.autoresolve', ['nocache' => 1]) ?>">
-        <?= ((mb_strlen($user->getRegion()->getName()) > 20) ? (mb_substr($user->getRegion()->getName(), 0, 20) . '...') : $user->getRegion()->getName()) ?>
-    </a>
+<div class="topbar">
+    <div class="topbar_cell topbar_cell-1">
+        <a class="topbar_loc jsChangeRegion" href="<?= $page->url('region.change', ['regionId' => $user->getRegion()->getId()]) ?>" data-url="<?= $page->url('region.init') ?>" data-region-id="<?= $user->getRegion()->getId() ?>" data-autoresolve-url="<?= $page->url('region.autoresolve', ['nocache' => 1]) ?>">
+            <?= ((mb_strlen($user->getRegion()->getName()) > 20) ? (mb_substr($user->getRegion()->getName(), 0, 20) . '...') : $user->getRegion()->getName()) ?>
+        </a>
 
-    <div class="topbar_call" itemscope itemtype="http://schema.org/Organization">
+        <div class="topbar_call" itemscope itemtype="http://schema.org/Organization">
 
-    <? if (\App::config()->onlineCall['enabled']): ?>
-        <a onclick="typeof(_gaq)=='undefined'?'':_gaq.push(['_trackEvent', 'Zingaya', 'ButtonClick']);typeof(_gat)=='undefined'?'':_gat._getTrackerByName()._setAllowLinker(true); window.open(typeof(_gat)=='undefined'?this.href+'?referrer='+escape(window.location.href):_gat._getTrackerByName()._getLinkerUrl(this.href+'?referrer='+escape(window.location.href)), '_blank', 'width=236,height=220,resizable=no,toolbar=no,menubar=no,location=no,status=no'); return false" href="http://zingaya.com/widget/e990d486d664dfcff5f469b52f6bdb62"><div class="topbar_call_t">Звонок с сайта</div></a>
-    <? endif ?>
+        <? if (\App::config()->onlineCall['enabled']): ?>
+            <a onclick="typeof(_gaq)=='undefined'?'':_gaq.push(['_trackEvent', 'Zingaya', 'ButtonClick']);typeof(_gat)=='undefined'?'':_gat._getTrackerByName()._setAllowLinker(true); window.open(typeof(_gat)=='undefined'?this.href+'?referrer='+escape(window.location.href):_gat._getTrackerByName()._getLinkerUrl(this.href+'?referrer='+escape(window.location.href)), '_blank', 'width=236,height=220,resizable=no,toolbar=no,menubar=no,location=no,status=no'); return false" href="http://zingaya.com/widget/e990d486d664dfcff5f469b52f6bdb62"><div class="topbar_call_t">Звонок с сайта</div></a>
+        <? endif ?>
 
-        <div class="topbar_call_lst">
-            <? if (108136 == $user->getRegion()->getId()): // Санкт-Петербург ?>
-                <span class="topbar_call_i" itemprop="telephone"><?= \App::config()->company['spbPhone'] ?></span>
-                <span class="topbar_call_i" itemprop="telephone"><?= \App::config()->company['phone'] ?></span>
-            <? else: ?>
+            <div class="topbar_call_lst">
                 <span class="topbar_call_i" itemprop="telephone"><?= \App::config()->company['phone'] ?></span>
                 <span class="topbar_call_i" itemprop="telephone"><?= \App::config()->company['moscowPhone'] ?></span>
-            <? endif ?>
+            </div>
+        </div>
+
+        <div class="topbar_lks">
+            <a class="topbar_lks_i" href="<?= $page->url('shop') ?>">Магазины Enter</a>
+            <a class="topbar_lks_i" href="/how_get_order">Доставка</a>
         </div>
     </div>
-
-    <div class="topbar_lks">
-        <a class="topbar_lks_i" href="<?= $page->url('shop') ?>">Магазины Enter</a>
-        <a class="topbar_lks_i" href="/how_get_order">Доставка</a>
+        
+    <div class="topbar_cell topbar_cell-2">
+        <a class="topbar_ep" href="<?= \App::router()->generate('enterprize') ?>"><span class="topbar_ep_mark">Enter</span> Prize</a>
     </div>
 
-    <div class="bSubscribeLightboxPopupNotNow mFl"></div>
+    <div class="topbar_cell topbar_cell-3">
+        <noindex>
+            <div class="topbarfix topbarfix-stc <?=('homepage'==\App::request()->attributes->get('route') || isset($scheme) && $scheme === 'homepage'?'topbarfix-home':null)?>">
 
-    <noindex>
-        <div class="topbarfix topbarfix-stc <?=('homepage'==\App::request()->attributes->get('route') || isset($scheme) && $scheme === 'homepage'?'topbarfix-home':null)?>">
+                <?= $page->render('userbar/_usercart') ?>
 
-            <?= $page->render('userbar/_usercart') ?>
+                <?= $page->render('userbar/_usercompare') ?>
 
-            <?= $page->render('userbar/_usercompare') ?>
+                <?= $page->render('userbar/_userinfo') ?>
 
-            <?= $page->render('userbar/_userinfo') ?>
-
-        </div>
-    </noindex>
-
-    <a class="topbar_ep" href="<?= \App::router()->generate('enterprize') ?>"><span class="topbar_ep_mark">Enter</span> Prize</a>
+            </div>
+        </noindex>
+    </div>
 </div>
+
+<div class="bSubscribeLightboxPopupNotNow mFl"></div>
