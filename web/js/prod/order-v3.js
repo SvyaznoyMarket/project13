d5f835dea4277b0b9a746831507a1b2f8f08d110
Mihail Haritonov <mihail.haritonov@enter.ru>
2014-10-01 13:08:12 +0400

    SITE-4403 Не применяется купон (новое разбиение)

--- web/js/prod/order-v3.js
+++ web/js/prod/order-v3.js
@@ -1581,7 +1581,8 @@
                     $('[data-block_name='+block_name+']').find('.cuponPin').show();
                 } else if (data.error_code == 743) {
                     // 743 - Сертификат не найден
-                    sendChanges('applyDiscount',{'block_name': block_name, 'number':code})
+					alert('Сертификат не найден');
+//                    sendChanges('applyDiscount',{'block_name': block_name, 'number':code})
                 }
             }).always(function(data){
                 console.log('Certificate check response',data);
