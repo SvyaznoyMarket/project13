62d34396604aa039ab31e5c36480a1fd99af2ec4
Mihail Haritonov <mihail.haritonov@enter.ru>
2014-10-14 13:32:55 +0400

    SITE-4437 В карточке товара не отображаются pdf-ки
    Conflicts:
    	web/styles/global.min.css

--- main/template/product/__trustfactors.php
+++ main/template/product/__trustfactors.php
@@ -4,20 +4,28 @@ return function (\Helper\TemplateHelper $helper, $trustfactors, $type) {
     <? foreach ((array)$trustfactors as $trustfactor): ?>
         <? if ($trustfactor['type'] === $type): ?>
             <div class="trustfactor-<?= $type ?>">
-                <? if (isset($trustfactor['link'])): ?>
-                    <a href="<?= $helper->escape($trustfactor['link']) ?>">
-                <? endif ?>
-
                 <? if ('image' === $trustfactor['media']['provider']): ?>
+                    <? if (isset($trustfactor['link'])): ?>
+                        <a href="<?= $helper->escape($trustfactor['link']) ?>">
+                    <? endif ?>
+
                     <? foreach ($trustfactor['media']['sources'] as $source): ?>
                         <? if ('original' === $source['type']): ?>
                             <img src="<?= $helper->escape($source['url']) ?>" width="<?= $helper->escape($source['width']) ?>" height="<?= $helper->escape($source['height']) ?>" alt="<?= $helper->escape($trustfactor['alt']) ?>" />
+                            <? break ?>
                         <? endif ?>
                     <? endforeach ?>
-                <? endif ?>
 
-                <? if (isset($trustfactor['link'])): ?>
-                    </a>
+                    <? if (isset($trustfactor['link'])): ?>
+                        </a>
+                    <? endif ?>
+                <? elseif ('file' === $trustfactor['media']['provider']): ?>
+                    <? foreach ($trustfactor['media']['sources'] as $source): ?>
+                        <? if ('original' === $source['type']): ?>
+                            <a href="<?= $helper->escape($source['url']) ?>" target="_blank" class="trustfactor-<?= $type ?>-file"><span><?= $helper->escape($trustfactor['alt']) ?></span></a>
+                            <? break ?>
+                        <? endif ?>
+                    <? endforeach ?>
                 <? endif ?>
             </div>
         <? endif ?>
