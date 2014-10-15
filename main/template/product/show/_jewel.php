b2b3a27d1a435f85989567ab40c0ec9fc629d4bd
Mihail Haritonov <mihail.haritonov@enter.ru>
2014-10-15 16:19:32 +0400

    SITE-4386 Изменено название галочки в блоке "Узнать о снижении цены"

--- main/template/product/show/_jewel.php
+++ main/template/product/show/_jewel.php
@@ -13,6 +13,8 @@
  * @var $deliveryData      array
  * @var $isTchibo          boolean
  * @var $addToCartJS string
+ * @var $isUserSubscribedToEmailActions boolean
+ * @var $actionChannelName string
  */
 
 $isKitPage = (bool)$product->getKit();
@@ -28,7 +30,7 @@ $isKitPage = (bool)$product->getKit();
 
         <?= $helper->render('product/__price', ['product' => $product]) // Цена ?>
 
-        <?= $helper->render('product/__notification-lowerPrice', ['product' => $product, 'isUserSubscribedToEmailActions' => $isUserSubscribedToEmailActions]) // Узнать о снижении цены ?>
+        <?= $helper->render('product/__notification-lowerPrice', ['product' => $product, 'isUserSubscribedToEmailActions' => $isUserSubscribedToEmailActions, 'actionChannelName' => $actionChannelName]) // Узнать о снижении цены ?>
 
         <?= $helper->render('product/__credit', ['product' => $product, 'creditData' => $creditData]) // Купи в кредит ?>
 
