11a54713bafc17b0bfcf54e80a4cf0c7c7aeaab1
tamili <ta.burlakova@gmail.com>
2014-10-24 14:48:57 +0400

    new topbar btn

--- main/template/product-category/_userbarContent.php
+++ main/template/product-category/_userbarContent.php
@@ -21,8 +21,8 @@ if ($category) {
 
 <div class="topbarfix_up">
     <? if ($productFilter && $productFilter->hasAlwaysShowFilters()): ?>
-        <a class="btnGrey topbarfix_upLink topbarfix_upLink-custom js-userbar-upLink" href="">
-            <em class="cornerTop">&#9650;</em> Параметры
+        <a class="topbarfix_upLink topbarfix_upLink-custom js-userbar-upLink" href="">
+            <em class="cornerTop">&#9650;</em> <span class="topbarfix_upLink_tx">Параметры</span>
         </a>
     <? else: ?>
         <a class="btnGrey topbarfix_upLink js-userbar-upLink" href="">
