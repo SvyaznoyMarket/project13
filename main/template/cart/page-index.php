a2c6441a89651b6692778630942f5af07be23a06
Zhukov Roman <zhukov.roman@gmail.com>
2014-10-27 17:53:04 +0300

    SITE-4472 АВ тест платного самовывоза

--- main/template/cart/page-index.php
+++ main/template/cart/page-index.php
@@ -62,7 +62,7 @@ $helper = new \Helper\TemplateHelper();
     <? if (\Controller\Delivery\Action::isPaidSelfDelivery()) : ?>
 
         <div class="cartInfo"
-             data-bind="visible: cartSum() < ENTER.config.pageConfig.selfDeliveryLimit"
+             data-bind="visible: cartSum() != 0 && cartSum() < ENTER.config.pageConfig.selfDeliveryLimit"
              style="display: <?= \App::config()->self_delivery['limit'] > $cart->getSum() ? 'block' : 'none' ?>">
             Для бесплатного самовывоза добавьте товаров на <strong><span data-bind="text: ENTER.config.pageConfig.selfDeliveryLimit - cartSum()"><?= \App::config()->self_delivery['limit'] - $cart->getSum()?></span> <span class="rubl">p</span></strong>
         </div>
