cda7157d093506893ee611496eaa5b6df1b5ba44
Zhukov Roman <zhukov.roman@gmail.com>
2014-10-28 16:15:13 +0300

    SITE-4472 АВ тест платного самовывоза (маленькая чистка)

--- web/js/prod/cart.js
+++ web/js/prod/cart.js
@@ -1,18 +1,9 @@
 ;(function($){
 
-	var kitPopupItems = ko.observableArray(),
-		ajaxProducts = ko.observableArray(),
-		$body = $(document.body),
-		ajaxRoute = '/cart/add-product/';
-
-	$body.on('addtocart', function(e, data) {
-		if (!data.product) return;
-		ajaxProducts.push(data);
-		$('.basketSum__price .price').text( printPrice(data.cart.full_price) );
-	});
+	var $body = $(document.body);
 
 	/* Увеличение и уменьшение товара AJAX */
-	$body.on('click', '.numerbox a, .jsCartDeleteProduct', function(e){
+	$body.on('click', '.numerbox a', function(e){
 		var $elem = $(this),
 			href = $elem.attr('href');
 
@@ -21,11 +12,29 @@
 			$.ajax({
 				url: href,
 				success: function(data){
-					console.log(data);
 					if (data.success && data.product && typeof data.product.quantity != 'undefined') {
 						ENTER.UserModel.productQuantityUpdate(data.product.id, data.product.quantity);
 					} else if (data.success && data.product && typeof data.product.quantity == 'undefined') {
 						ENTER.UserModel.removeProductByID(data.product.id);
+					}
+				}
+			})
+		}
+
+	});
+
+	/* Удаление продукта AJAX */
+	$body.on('click', '.jsCartDeleteProduct', function(e){
+		var href = $(this).attr('href');
+
+		if (href != '') {
+			e.preventDefault();
+			$.ajax({
+				url: href,
+				success: function(data){
+					if (data.success && data.product) {
+						ENTER.UserModel.removeProductByID(data.product.id);
+						$body.trigger('removeFromCart', [data.product]);
 					}
 				}
 			})
