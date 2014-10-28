1f977c87d33b17e2eb04aed81b15aa703c9b1f3b
Mihail Haritonov <mihail.haritonov@enter.ru>
2014-10-28 16:37:28 +0300

    SITE-4192 Исправлена ошибка "Undefined variable" при поиске товаров

--- main/template/product-category/_userbarContent.php
+++ main/template/product-category/_userbarContent.php
@@ -20,7 +20,7 @@ if ($category) {
 } ?>
 
 <div class="topbarfix_up">
-    <? if ($productFilter && $productFilter->hasAlwaysShowFilters()): ?>
+    <? if (isset($productFilter) && $productFilter->hasAlwaysShowFilters()): ?>
         <a class="topbarfix_upLink topbarfix_upLink-custom js-userbar-upLink" href="">
             <em class="cornerTop">&#9650;</em> <span class="topbarfix_upLink_tx">Параметры</span>
         </a>
