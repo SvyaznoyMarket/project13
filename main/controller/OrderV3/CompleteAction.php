a752201c3fc7cd92cfe1a6164466ef5e9be83ff9
Mihail Haritonov <mihail.haritonov@enter.ru>
2014-10-29 04:20:23 +0300

    SITE-? Исправлен АБтест оформления заказов

--- main/controller/OrderV3/CompleteAction.php
+++ main/controller/OrderV3/CompleteAction.php
@@ -21,10 +21,10 @@ class CompleteAction extends OrderV3 {
      * @throws \Exception
      */
     public function execute(\Http\Request $request) {
-        $controller = parent::execute($request);
-        if ($controller) {
-            return $controller;
-        }
+//        $controller = parent::execute($request);
+//        if ($controller) {
+//            return $controller;
+//        }
 
         \App::logger()->debug('Exec ' . __METHOD__);
 
