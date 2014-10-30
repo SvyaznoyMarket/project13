7669b3e39d752fc592a2dc506fe7a55ee98c59a2
g84 <green.840429@gmail.com>
2014-10-30 17:07:13 +0300

    SITE-? изменил лимит на платный самовывоз до 500 рублей

--- config/config.php
+++ config/config.php
@@ -603,7 +603,7 @@ $c->abTest = [
     ],
 ];
 
-$c->self_delivery['limit'] = 1000;
+$c->self_delivery['limit'] = 500;
 $c->self_delivery['regions'] = [119623, 93746, 14974];
 
 $c->preview = false;
