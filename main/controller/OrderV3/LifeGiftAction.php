b91dad245938e8cea27e8787371077bc82d5f44f
Zhukov Roman <zhukov.roman@gmail.com>
2014-10-30 18:20:05 +0300

    SITE-4450 Реализация страницы Подари жизнь

--- main/controller/OrderV3/LifeGiftAction.php
+++ main/controller/OrderV3/LifeGiftAction.php
@@ -17,13 +17,20 @@ class LifeGiftAction {
         \App::logger()->debug('Exec ' . __METHOD__);
         $error = null;
         $product = null;
+        $responseData = null;
 
         try {
 
             if (!\App::config()->lifeGift['enabled']) throw new \Exception('Акция отключена');
 
-            if ($request->isMethod('POST')) {
-                $this->createOrder($request, $productId);
+            if ($request->isMethod('POST') && $request->isXmlHttpRequest()) {
+                // создаем заказ
+                $createdOrder = $this->createOrder($request, $productId);
+                // получаем его из БД
+                $fullOrder = $this->getOrder($request, $createdOrder);
+                // получаем данные формы
+                $form = $this->getPaymentForm($fullOrder, $request->get('paynow') == 'card');
+                return new \Http\JsonResponse(['form' => $form]);
             }
 
             $lifegiftRegion = \RepositoryManager::region()->getEntityById(\App::config()->lifeGift['regionId']);
@@ -38,6 +45,10 @@ class LifeGiftAction {
         } catch (\Exception $e) {
             \App::exception()->remove($e);
             $error = $e->getMessage();
+            $responseData = [
+                'success' => false,
+                'error'   => ['code' => $e->getCode(), 'message' => $e->getMessage()],
+            ];
         }
 
         $page = new \View\OrderV3\LifeGiftPage();
@@ -45,15 +56,23 @@ class LifeGiftAction {
         $page->setParam('user', $this->user);
         $page->setParam('error', $error);
 
-        return new \Http\Response($page->show());
+        return $request->isXmlHttpRequest() ? new \Http\JsonResponse(['result' => $responseData]) : new \Http\Response($page->show());
     }
 
+    /**
+     * @param \Http\Request $request
+     * @param $productID
+     * @return \Model\Order\CreatedEntity|null
+     */
     private function createOrder(\Http\Request $request, $productID) {
+
+        $order = null;
 
         // общие данные заказа
         $orderData = [
             'type_id'           => \Model\Order\Entity::TYPE_ORDER, /* ОБЯЗАТЕЛЬНО */
             'geo_id'            => \App::config()->lifeGift['regionId'], /* ОБЯЗАТЕЛЬНО */
+            'shop_id'           => 172,
             'user_id'           => $this->user ? $this->user->getId() : null,
             'is_legal'          => $this->user ? $this->user->getIsCorporative() : false,
             'payment_id'        => \Model\Order\Entity::PAYMENT_TYPE_ID_CREDIT_CARD_ONLINE,
@@ -71,24 +90,70 @@ class LifeGiftAction {
         ];
 
         $params = [];
-        if ($this->user && $this->user->getToken()) {
-            $params['user_token'] = $this->user->getToken();
-        }
+
+        if ($this->user && $this->user->getToken()) $params['user_token'] = $this->user->getToken();
 
         try {
             $result = \App::coreClientV2()->query('order/create-packet2', $params, [$orderData], \App::config()->coreV2['hugeTimeout']);
+            if (is_array($result) && isset($result[0])) $order = new \Model\Order\CreatedEntity($result[0]);
+            if (!$order->getId() || !$order->getNumber()) {
+                throw new \Exception('Ошибка создания заказа');
+            }
         } catch(\Exception $e) {
-
+            $result = $e->getMessage();
         }
 
         \App::logger()->info(['action' => __METHOD__, 'core.response' => $result], ['order']);
 
-        $redirectUrl = '';
-        return new \Http\RedirectResponse($redirectUrl);
+        return $order;
     }
 
+    /**
+     * @param \Http\Request $request
+     * @param \Model\Order\CreatedEntity $order
+     * @return \Model\Order\Entity|null
+     * @throws \Exception
+     */
+    private function getOrder(\Http\Request $request, \Model\Order\CreatedEntity $order) {
+        $o = \RepositoryManager::order()->getEntityByNumberAndPhone($order->getNumber(), $request->get('user_phone'));
+        if ($o == null) throw new \Exception('Неудалось получить заказ');
+        return $o;
+    }
 
-    private function getPaymentForm(){
+    private function getPaymentForm(\Model\Order\Entity $order, $isCardPayment) {
+
+        $methodId = $isCardPayment ? 5 : 13;
+
+        $privateClient = $privateClient = \App::coreClientPrivate();
+
+        $coreResponse = $privateClient->query('site-integration/payment-config',
+            [
+                'method_id' => $methodId,
+                'order_id'  => $order->getId(),
+            ],
+            [
+                'back_ref'    => \App::router()->generate('homepage', [], true),// обратная ссылка
+            ],
+            \App::config()->coreV2['hugeTimeout']
+        );
+
+        if (!$coreResponse) throw new \Exception('Ошибка получения данных payment-config');
+
+        if ($methodId == 5) {
+            $formEntity = (new \Payment\Psb\Form());
+            $formEntity->fromArray($coreResponse['detail']);
+            $form = (new \Templating\HtmlLayout())->render('order/payment/form-psb', array(
+                'provider' => new \Payment\Psb\Provider(['payUrl' => $coreResponse['url']]),
+                'order' => $order,
+                'form' => $formEntity
+            ));
+        } else {
+            $form = (new \Templating\HtmlLayout())->render('order/payment/form-paypal', array(
+                'url' => $coreResponse['url']
+            ));
+        }
+
+        return $form;
 
     }
 
