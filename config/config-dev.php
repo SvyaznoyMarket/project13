2e89dab1520651723aaa73169975d4a7179757eb
tamili <ta.burlakova@gmail.com>
2014-11-05 14:35:23 +0300

    SITE-4192

--- config/config-dev.php
+++ config/config-dev.php
@@ -76,5 +76,6 @@ $c->paymentPsbInvoice['key']          = $c->dataDir . '/key/privkey.pem';
 $c->paymentPsbInvoice['payUrl']       = 'https://retail-tst.payment.ru/dn/Invoices/ReceiveUniversalInvoices.aspx';
 
 $c->requestMainMenu = false;
+$c->enterprize['enabled'] = true;
 
 return $c;
