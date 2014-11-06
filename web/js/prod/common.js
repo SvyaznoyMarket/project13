e035185a67af4af1eb90f6fe34a7407d7f46427f
Mihail Haritonov <mihail.haritonov@enter.ru>
2014-11-06 18:26:30 +0400

    SITE-4475 Исправлена анимация bodybar'а

--- web/js/prod/common.js
+++ web/js/prod/common.js
@@ -885,13 +885,28 @@
 // })();
 
 $(function(){
-    var $bodybar = $('.js-bodybar');
+    var $bodybar = $('.js-bodybar'),
+		timeout;
 
     $bodybar.on('mouseenter', function() {
+		timeout = setTimeout(function() {
+			$bodybar.removeClass('bodybar-hide');
+		}, 200);
+	});
+
+	$bodybar.on('mouseleave', function() {
+		clearInterval(timeout);
+	});
+			
+	$bodybar.on('click', function() {
+		clearInterval(timeout);
 		$bodybar.removeClass('bodybar-hide');
 	});
 	
-	$('.js-bodybar-hideButton', $bodybar).on('click', function() {
+	$('.js-bodybar-hideButton', $bodybar).on('click', function(e) {
+		e.preventDefault();
+		e.stopPropagation();
+		clearInterval(timeout);
 		$bodybar.addClass('bodybar-hide');
 	});
 });
