d45879e55459f5709cbd44b0308b0baa4f5df320
tamili <ta.burlakova@gmail.com>
2014-11-07 11:15:47 +0300

    SITE-4475

--- main/template/__bodybar.php
+++ main/template/__bodybar.php
@@ -6,7 +6,6 @@ return function($showBodybar) { ?>
             <label for="" class="sbscrBar_lbl">Сообщайте мне об акциях и специальных ценах</label>
             <div class="sbscrBar_itw">
                 <input type="text" name="" id="" class="sbscrBar_it js-subscribebar-email" placeholder="Ваш e-mail">
-                <div class="sbscrBar_errtx">Неверно введен email</div>
             </div>
             <input type="submit" value="Подписаться" class="sbscrBar_is js-subscribebar-subscribeButton">
             <div class="sbscrBar_tx">и получить купон на 300 руб.</div>
