0e1d0fb10257d8226b89937ca505ad1961b20d91
Zhukov Roman <zhukov.roman@gmail.com>
2014-11-11 16:59:29 +0300

    SITE-??? Улучшения GA setCustomVar

--- main/template/debug/_abtests.php
+++ main/template/debug/_abtests.php
@@ -7,12 +7,16 @@
 <ul>
     <? foreach ($tests as $test) : ?>
 
-    <li><span style="color: <?= $test->isActive() ? 'green' : 'red' ?>"><?= $test->getKey() ?></span> <span style="color: gray;">(до <?= $test->getExpireDate() ?>)</span>
+    <li><span style="color: <?= $test->isActive() ? 'green' : 'red' ?>"><?= $test->name ?> (<?= $test->getKey() ?>)</span> <span style="color: gray;">(до <?= $test->getExpireDate() ?>)</span>
+        <? if ($test->gaSlotNumber) : ?>
+            <span style="color: #00ADE0">GA: slot <?= $test->gaSlotNumber ?>, scope <?= $test->gaSlotScope ?></span>
+        <? endif; ?>
         <ul style="list-style-type: none; padding: 5px 0 10px 0;">
         <? foreach ($test->getCases() as $case) : ?>
             <li>
                 <input type="radio" class="changeCase" name="<?= $test->getKey() ?>" value="<?= $case->getKey() ?>" <?= $test->getChosenCase() == $case ? 'checked' : '' ?> <?= !$test->isActive() ? 'disabled' : '' ?> />
                 <?= $case->getName() ?> <span style="color: gray;"><?= $case->getTraffic() ?>%</span>
+                <span class="gaValue" style="color: #AAA; border: 1px solid #AAA; border-radius: 2px; font-size: 12px; padding: 0 2px; cursor: pointer" title="<?= $test->getKey().'_'.$case->getKey() ?>">GA</span>
             </li>
         <? endforeach ?>
         </ul>
@@ -43,6 +47,11 @@
 
             location.reload();
 
+        });
+
+        $(document.body).on('click', '.gaValue', function(){
+            var $this = $(this);
+            $this.text($this.attr('title'));
         })
     })
 </script>
