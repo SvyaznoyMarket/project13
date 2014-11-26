85b91bb67bc0ec7020c4833915643c4ae2e2c24c
tamili <ta.burlakova@gmail.com>
2014-11-26 12:13:56 +0300

    SITE-4673 Отслеживание обновленного поиска

--- main/template/common/_searchbar.php
+++ main/template/common/_searchbar.php
@@ -2,31 +2,60 @@
 <div class="header_c clearfix">
     <a href="/" class="header_i sitelogo"></a>
 
+    <!-- для АВ-теста строки поиска, к hdsearch подключить класс hdsearch-v2 -->
     <div class="header_i hdsearch">
         <form action="" class="hdsearch_f">
             <label class="hdsearch_lbl" for="">Все товары для жизни по выгодным ценам!</label>
             <div class="hdsearch_itw"><input type="text" class="hdsearch_it" placeholder="Поиск по товарам..."></div>
+            <?php /*
+            <div class="hdsearch_itw">
+                <div class="searchcat">
+                    <!-- нужно делать .toggleClass('searchcat_tl-act') при клике по - Все товары  -->
+                    <div class="searchcat_tl"><span class="searchcat_tl_tx">Все товары</span></div>
+
+                    <ul class="searchcat_dd">
+                        <li class="searchcat_dd_i">
+                            <a href="" class="searchcat_dd_lk"><span class="undrlh">Мебель</span></a>
+                        </li>
+                        <li class="searchcat_dd_i">
+                            <a href="" class="searchcat_dd_lk"><span class="undrlh">Товары для дома</span></a>
+                        </li>
+                        <li class="searchcat_dd_i">
+                            <a href="" class="searchcat_dd_lk"><span class="undrlh">Электроника</span></a>
+                        </li>
+                        <li class="searchcat_dd_i">
+                            <a href="" class="searchcat_dd_lk"><span class="undrlh">Детские товары</span></a>
+                        </li>
+                        <li class="searchcat_dd_i">
+                            <a href="" class="searchcat_dd_lk"><span class="undrlh">Украшение и часы</span></a>
+                        </li>
+                    </ul>
+                </div>
+
+                <input type="text" class="hdsearch_it" placeholder="Поиск по товарам...">
+            </div>
+            */?>
             <button class="hdsearch_btn btn3">Найти</button>
         </form>
 
         <!-- саджест поиска -->
-        <div class="searchdd">
+        <div class="searchdd" style="display: none;">
             <div class="searchdd_t"><span class="searchdd_t_tx">Категории</span></div>
-            <a href="" class="searchdd_lk"><span class="undlrh">Планшеты (283)</span></a>
-            <a href="" class="searchdd_lk"><span class="undlrh">Графические планшеты (12)</span></a>
+            <a href="" class="searchdd_lk"><span class="undrlh">Планшеты (283)</span></a>
+            <a href="" class="searchdd_lk"><span class="undrlh">Графические планшеты (12)</span></a>
 
             <div class="searchdd_t"><span class="searchdd_t_tx">Товары</span></div>
             <a href="" class="searchdd_lk">
                 <img src="http://fs09.enter.ru/1/1/120/15/340081.jpg" alt="" class="searchdd_img">
-                <span class="searchdd_tx"><span class="undlrh">Планшетный компьютер EXEQ P-703 4ГБ WiFi черный</span></span>
+                <span class="searchdd_tx"><span class="undrlh">Планшетный компьютер EXEQ P-703 4ГБ WiFi черный</span></span>
             </a>
             <a href="" class="searchdd_lk">
                 <img src="http://fs09.enter.ru/1/1/120/15/340081.jpg" alt="" class="searchdd_img">
-                <span class="searchdd_tx"><span class="undlrh">Планшетный компьютер EXEQ P-703 4ГБ WiFi черный</span></span>
+                <span class="searchdd_tx"><span class="undrlh">Планшетный компьютер EXEQ P-703 4ГБ WiFi черный</span></span>
             </a>
             <a href="" class="searchdd_lk">
                 <img src="http://fs09.enter.ru/1/1/120/15/340081.jpg" alt="" class="searchdd_img">
-                <span class="searchdd_tx"><span class="undlrh">Планшетный компьютер EXEQ P-703 4ГБ WiFi черный</span></span>
+                <span class="searchdd_tx"><span class="undrlh">Планшетный компьютер EXEQ P-703 4ГБ WiFi черный</span></span>
             </a>
         </div>
         <!--/ саджест поиска -->
