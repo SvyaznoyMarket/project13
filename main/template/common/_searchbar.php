a1c820bc7d910b0035265a48f50b1ca538512236
tamili <ta.burlakova@gmail.com>
2014-11-26 11:01:21 +0300

    SITE-4663 саджест поиска

--- main/template/common/_searchbar.php
+++ main/template/common/_searchbar.php
@@ -2,11 +2,35 @@
 <div class="header_c clearfix">
     <a href="/" class="header_i sitelogo"></a>
 
-    <form action="" class="header_i hdsearch">
-        <label class="hdsearch_lbl" for="">Все товары для жизни по выгодным ценам!</label>
-        <div class="hdsearch_itw"><input type="text" class="hdsearch_it" placeholder="Поиск по товарам..."></div>
-        <button class="hdsearch_btn btn3">Найти</button>
-    </form>
+    <div class="header_i hdsearch">
+        <form action="" class="hdsearch_f">
+            <label class="hdsearch_lbl" for="">Все товары для жизни по выгодным ценам!</label>
+            <div class="hdsearch_itw"><input type="text" class="hdsearch_it" placeholder="Поиск по товарам..."></div>
+            <button class="hdsearch_btn btn3">Найти</button>
+        </form>
+
+        <!-- саджест поиска -->
+        <div class="searchdd">
+            <div class="searchdd_t"><span class="searchdd_t_tx">Категории</span></div>
+            <a href="" class="searchdd_lk"><span class="undlrh">Планшеты (283)</span></a>
+            <a href="" class="searchdd_lk"><span class="undlrh">Графические планшеты (12)</span></a>
+
+            <div class="searchdd_t"><span class="searchdd_t_tx">Товары</span></div>
+            <a href="" class="searchdd_lk">
+                <img src="http://fs09.enter.ru/1/1/120/15/340081.jpg" alt="" class="searchdd_img">
+                <span class="searchdd_tx"><span class="undlrh">Планшетный компьютер EXEQ P-703 4ГБ WiFi черный</span></span>
+            </a>
+            <a href="" class="searchdd_lk">
+                <img src="http://fs09.enter.ru/1/1/120/15/340081.jpg" alt="" class="searchdd_img">
+                <span class="searchdd_tx"><span class="undlrh">Планшетный компьютер EXEQ P-703 4ГБ WiFi черный</span></span>
+            </a>
+            <a href="" class="searchdd_lk">
+                <img src="http://fs09.enter.ru/1/1/120/15/340081.jpg" alt="" class="searchdd_img">
+                <span class="searchdd_tx"><span class="undlrh">Планшетный компьютер EXEQ P-703 4ГБ WiFi черный</span></span>
+            </a>
+        </div>
+        <!--/ саджест поиска -->
+    </div>
 
     <div class="header_i hdep">
         <div class="hdep_h">Больше скидок</div>
