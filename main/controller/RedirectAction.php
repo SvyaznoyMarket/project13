9130a343f75db7a1ab6ad76bf07d3220590e613d
Zhukov Roman <roman.zhukov@enter.ru>
2014-12-02 10:57:52 +0300

    SITE-4779 Отключить scms-редиректы на preview.enter.ru

--- main/controller/RedirectAction.php
+++ main/controller/RedirectAction.php
@@ -6,7 +6,7 @@ class RedirectAction {
     /**
      * @param \Http\Request $request
      * @throws \Exception\NotFoundException
-     * @return \Http\Response
+     * @return \Http\Response|null
      */
     public function execute(\Http\Request $request) {
         \App::logger()->debug('Exec ' . __METHOD__);
@@ -14,11 +14,15 @@ class RedirectAction {
         $uri = $request->getPathInfo();
         // если главная страница, то игнорируем
         if ('/' == $uri) {
-            return;
+            return null;
+        }
+        // если preview.enter.ru, то редиректы тоже не нужны
+        if (\App::config()->preview) {
+            return null;
         }
         // если ajax-запрос, то игнорируем
         if ($request->isXmlHttpRequest()) {
-            return;
+            return null;
         }
 
         $redirectUrl = null;
@@ -43,7 +47,7 @@ class RedirectAction {
         \App::scmsSeoClient()->execute(\App::config()->scmsSeo['retryTimeout']['tiny']);
 
         if (!$redirectUrl) {
-            return;
+            return null;
         }
 
         if ((false === strpos($redirectUrl, '?')) && $request->getQueryString()) {
