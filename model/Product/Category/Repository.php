51252e67aad0607d285690d88425e726f7344cc9
Mihail Haritonov <mihail.haritonov@enter.ru>
2014-12-08 20:37:27 +0300

    SITE-4814 Перейти на новые методы листингов и фильтров

--- model/Product/Category/Repository.php
+++ model/Product/Category/Repository.php
@@ -236,7 +236,7 @@ class Repository {
     public function getRootCollection() {
         \App::logger()->debug('Exec ' . __METHOD__ . ' ' . json_encode(func_get_args(), JSON_UNESCAPED_UNICODE));
 
-        $client = \App::searchClient();
+        $client = clone \App::searchClient();
         $entityClass = $this->entityClass;
 
         // TODO: добавить регион
@@ -285,7 +285,7 @@ class Repository {
     public function getTreeCollection(\Model\Region\Entity $region = null, $maxLevel = null) {
         \App::logger()->debug('Exec ' . __METHOD__ . ' ' . json_encode(func_get_args(), JSON_UNESCAPED_UNICODE));
 
-        $client = \App::searchClient();
+        $client = clone \App::searchClient();
         $entityClass = $this->entityClass;
 
         $params = [
