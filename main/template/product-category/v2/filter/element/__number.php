ef6d278e96d513988c1835374a97d49638496b51
Mihail Haritonov <mihail.haritonov@enter.ru>
2014-12-08 16:40:08 +0300

    SITE-4658 Исправлен числовой фильтр

--- main/template/product-category/v2/filter/element/__number.php
+++ main/template/product-category/v2/filter/element/__number.php
@@ -5,10 +5,22 @@ return function(
     \Model\Product\Filter $productFilter,
     \Model\Product\Filter\Entity $filter
 ) {
+    $minValue = $helper->clearZeroValue($productFilter->getValueMin($filter));
+    $maxValue = $helper->clearZeroValue($productFilter->getValueMax($filter));
+    $placeholderMinValue = $helper->clearZeroValue($filter->getMin());
+    $placeholderMaxValue = $helper->clearZeroValue($filter->getMax());
+
+    if ($minValue === $placeholderMinValue) {
+        $minValue = '';
+    }
+
+    if ($maxValue === $placeholderMaxValue) {
+        $maxValue = '';
+    }
     ?>
     <div class="fltrBtn_ln js-category-v2-filter-element-number">
-        <span class="fltrBtn_param_lbl txmark1">от</span> <input class="fltrBtn_param_it" name="<?= \View\Name::productCategoryFilter($filter, 'from') ?>" placeholder="<?= $helper->clearZeroValue($productFilter->getValueMin($filter)) ?>" type="text" />
-        &ensp;<span class="fltrBtn_param_lbl txmark1">до</span> <input class="fltrBtn_param_it" name="<?= \View\Name::productCategoryFilter($filter, 'to') ?>" placeholder="<?= $helper->clearZeroValue($productFilter->getValueMax($filter)) ?>" type="text" />
+        <span class="fltrBtn_param_lbl txmark1">от</span> <input class="fltrBtn_param_it" name="<?= \View\Name::productCategoryFilter($filter, 'from') ?>" value="<?= $minValue ?>" placeholder="<?= $placeholderMinValue ?>" type="text" />
+        &ensp;<span class="fltrBtn_param_lbl txmark1">до</span> <input class="fltrBtn_param_it" name="<?= \View\Name::productCategoryFilter($filter, 'to') ?>" value="<?= $maxValue ?>" placeholder="<?= $placeholderMaxValue ?>" type="text" />
         <span class="fltrBtn_param_lbl txmark1"><?= $filter->getUnit() ?></span>
     </div>
 <? };
