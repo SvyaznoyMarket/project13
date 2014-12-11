4b2a2caa16692c55b486b44eeed646d22ff70061
g84 <green.840429@gmail.com>
2014-12-11 14:35:21 +0300

    SITE-? почистил небольшой мусор в index.php

--- web/index.php
+++ web/index.php
@@ -122,15 +122,6 @@ $response = null;
             (new \Debug\ShowAction())->execute($request, $response);
         }
 
-        // SITE-4040 удаление старых сессионных кук
-        try {
-            $sessionParams = session_get_cookie_params();
-            //$response->headers->clearCookie(\App::session()->getName(), $sessionParams['path']);
-            //$response->headers->clearCookie(\App::config()->region['cookieName'], '/');
-        } catch (\Exception $e) {
-            \App::logger()->error($e, ['response']);
-        }
-
         $response->send();
     }
 
