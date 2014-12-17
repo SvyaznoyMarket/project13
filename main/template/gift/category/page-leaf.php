34c0c5c1c8beff0034fe4eac6d59dc2960eb9fe8
tamili <ta.burlakova@gmail.com>
2014-12-17 13:06:50 +0300

    SITE-4885

--- main/template/gift/category/page-leaf.php
+++ main/template/gift/category/page-leaf.php
@@ -12,7 +12,7 @@
 $helper = new \Helper\TemplateHelper();
 ?>
 
-<div class="bCatalog js-gift-category <?= $isNewMainPage ? 'newMainPage' : '' ?>" id="bCatalog" data-lastpage="<?= $productPager->getLastPage() ?>">
+<div class="bCatalog js-gift-category <?= $isNewMainPage ? 'newLayout' : '' ?>" id="bCatalog" data-lastpage="<?= $productPager->getLastPage() ?>">
     <div class="bCustomFilter" styles="background-image: url('/styles/catalog/img/bg-ny-gift.jpg')">
 
         <?= $helper->render('gift/category/__filter', [
