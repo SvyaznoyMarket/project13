0c66abdc67284bd6cfb8a94ec673b1b6684963cf
tanyaten <web@mow-01-ic237.enter.lan>
2014-12-18 16:27:45 +0300

    SITE-4933 styles & layout

--- main/template/order-v3-new/page-complete_online-motivation.php
+++ main/template/order-v3-new/page-complete_online-motivation.php
@@ -21,6 +21,38 @@ return function(
     <section class="orderCnt">
 
         <!-- Заверстать все блоки тут -->
+        <!-- Блок оплата (оплата онлайн невозможна) -->
+        <div class="orderPayment">
+            <!-- Заголовок -->
+            <div class="orderPayment_head">
+                    Оформлен заказ № <a href="#" class="orderPayment_num">COXD-305127</a>
+            </div>
+            <!-- Блок в обводке -->
+            <div class="orderPayment_block orderPayment_noOnline">
+
+                <div class="orderPayment_msg orderPayment_noOnline_msg">
+                    <div class="orderPayment_msg_head">
+                        Ждем вас 27.12.2014 в магазине
+                    </div>
+                    <div class="orderPayment_msg_shop">
+                        <span class="orderPayment_msg_shop_metro">м. Петровско-Разумовская</span>
+                        <span class="orderPayment_msg_shop_addr">ул. Линии Октябрьской Железной Дороги, д. 1, стр. 2</span>
+                        <a href="#" class="orderPayment_msg_addr_link">
+                            Как добраться
+                        </a>
+                    </div>
+                    <div class="orderPayment_msg_info">
+                        Вы можете оплатить заказ при получении.
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
 
         <div class="orderCompl orderCompl_final clearfix">
             <a class="orderCompl_continue_link" href="<?= $helper->url('homepage') ?>">Вернуться на главную</a>
