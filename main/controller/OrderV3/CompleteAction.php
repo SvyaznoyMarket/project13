a7cfd3f37f42c0821bb6680938ca352db1f38f39
Zhukov Roman <roman.zhukov@enter.ru>
2014-12-29 14:56:40 +0300

    SITE-? Добавил refresh к back_ref

--- main/controller/OrderV3/CompleteAction.php
+++ main/controller/OrderV3/CompleteAction.php
@@ -183,7 +183,7 @@ class CompleteAction extends OrderV3 {
                 'order_id'  => $orderId,
             ],
             [
-                'back_ref'    => \App::router()->generate('orderV3.complete', [], true),// обратная ссылка
+                'back_ref'    => \App::router()->generate('orderV3.complete', ['refresh' => 1], true),// обратная ссылка
                 'email'       => $order->getUser() ? $order->getUser()->getEmail() : '',
 //                            'card_number' => $order->card,
                 'user_token'  => $request->cookies->get('UserTicket'),// токен кросс-авторизации. может быть передан для Связного-Клуба (UserTicket)
