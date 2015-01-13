7d4c6935811cf7357f731ab080b62c30701ff056
Zhukov Roman <roman.zhukov@enter.ru>
2015-01-13 12:22:37 +0300

    SITE-5004 Кэшировать /main_menu/recommendations в localstorage

--- web/js/prod/common.js
+++ web/js/prod/common.js
@@ -3071,79 +3071,135 @@ $(document).ready(function() {
 			$(this).next('.enterPrizeListWrap').toggle('fast');
 	});
 });
-// Simple lazy loading
-;$('nav').on('mouseenter', '.navsite2_i', function(){
-	$(this).find('.menuImgLazy').each(function(){
-		$(this).attr('src', $(this).data('src'))
-	});
-});
+;(function($){
 
-$('nav').on('mouseenter', '.navsite_i', function(){
-	var
-		$el = $(this),
-		url = $el.data('recommendUrl'),
-		xhr = $el.data('recommendXhr')
-		;
+	var $body = $(document.body),
+		$nav = $('nav'),
+		MenuStorage, storage, fillRecommendBlocks;
 
-	if (url && !xhr) {
-		xhr = $.get(url);
-		$el.data('recommendXhr', xhr);
-
-		xhr.done(function(response) {
-			if (!response.productBlocks) return;
-
-			var $containers = $el.find('.jsMenuRecommendation');
-
-			$.each(response.productBlocks, function(i, block) {
-				try {
-					if (!block.categoryId) return;
-
-					var $container = $containers.filter('[data-parent-category-id="' + block.categoryId + '"]');
-					$container.html(block.content);
-				} catch (e) { console.error(e); }
-			});
-		});
-
-		xhr.fail(function() {
-			$el.data('recommendXhr', false);
-			//$el.data('recommendXhr', true);
-		});
-	}
-});
-
-// аналитика
-$('body').on('click', '.jsRecommendedItemInMenu', function(event) {
-	console.log('jsRecommendedItemInMenu');
-
-	event.stopPropagation();
-
-	try {
-		var
-			$el = $(this),
-			link = $el.attr('href'),
-			sender = $el.data('sender')
-			;
-
-		body.trigger('TLT_processDOMEvent', [event]);
-
-		$('body').trigger('trackGoogleEvent', {
-			category: 'RR_взаимодействие',
-			action: 'Перешел на карточку товара',
-			label: sender ? sender.position : null,
-			hitCallback: function(){
-				console.log({link: link});
-
-				if (link) {
-					setTimeout(function() { window.location.href = link; }, 90);
+	/**
+	 * Конструктор хранилища данных. Возвращает либо lscache, либо объект с необходимыми свойствами (функциями)
+	 * @return {*}
+	 * @link https://github.com/pamelafox/lscache
+	 * @constructor
+	 */
+	MenuStorage = function MenuStorageF() {
+		var cKey = 'cachedData';
+		if (lscache && typeof lscache.supported == 'function' && lscache.supported()) {
+			return lscache
+		} else {
+			return {
+				'set': function(key, value, time, $el){
+					$el.data(cKey, value);
+				},
+				'get': function(key, $el) {
+					return $el.data(cKey) ? $el.data(cKey) : false;
+				},
+				'remove': function(key, $el) {
+					$el.data(cKey, false);
 				}
 			}
+		}
+	};
+
+	/**
+	 * Заполнение блоков меню "товарами дня"
+	 * @param $el
+	 * @param blocks
+	 */
+	fillRecommendBlocks = function fillRecommendBlocksF($el, blocks) {
+
+		var $containers = $el.find('.jsMenuRecommendation');
+
+		$.each(blocks, function(i, block) {
+			try {
+				if (!block.categoryId) return;
+				var $container = $containers.filter('[data-parent-category-id="' + block.categoryId + '"]');
+				$container.html(block.content);
+			} catch (e) {
+				console.error(e);
+			}
 		});
+	};
 
-		$el.trigger('TL_recommendation_clicked');
+	// объект универсального хранилища для данных "товар дня"
+	storage = new MenuStorage();
 
-	} catch (e) { console.error(e); }
-});
+	// Simple lazy loading
+	$nav.on('mouseenter', '.navsite2_i', function(){
+		$(this).find('.menuImgLazy').each(function(){
+			$(this).attr('src', $(this).data('src'))
+		});
+	});
 
+	// Товар дня
+	$nav.on('mouseenter', '.navsite_i', function(){
+
+		var	$el = $(this),
+			url = $el.data('recommendUrl'),
+			lKey = 'xhrLoading', // ключ для предотвращения дополнительного запроса на загрузку данных
+			cacheTime = 10, // время кэширования в localstorage (в минутах)
+			key, xhr;
+
+		if (typeof url == 'string' && !$el.data(lKey) === true) {
+
+			// отрезаем от url параметры для ключа в localstorage
+			key = url.substring(0, url.indexOf('?'));
+
+			if (!storage.get(key, $el)) {
+
+				xhr = $.get(url);
+				$el.data(lKey, true);
+
+				xhr.done(function(response) {
+					var data = response.productBlocks;
+					if (!data) return;
+					storage.set(key, data, cacheTime, $el);
+					fillRecommendBlocks($el, data);
+				}).fail(function() {
+					storage.remove(key, $el);
+				}).always(function(){
+					$el.data(lKey, false)
+				});
+			} else {
+				fillRecommendBlocks($el, storage.get(key, $el));
+			}
+
+		}
+	});
+
+	// аналитика
+	$body.on('click', '.jsRecommendedItemInMenu', function(event) {
+		console.log('jsRecommendedItemInMenu');
+
+		event.stopPropagation();
+
+		try {
+
+			var $el = $(this),
+				link = $el.attr('href'),
+				sender = $el.data('sender');
+
+			$body.trigger('TLT_processDOMEvent', [event]);
+
+			$body.trigger('trackGoogleEvent', {
+				category: 'RR_взаимодействие',
+				action: 'Перешел на карточку товара',
+				label: sender ? sender.position : null,
+				hitCallback: function(){
+					console.log({link: link});
+
+					if (link) {
+						setTimeout(function() { window.location.href = link; }, 90);
+					}
+				}
+			});
+
+			$el.trigger('TL_recommendation_clicked');
+
+		} catch (e) { console.error(e); }
+	})
+})(jQuery);
 ;(function($){	
 	/*paginator*/
 	var EnterPaginator = function( domID,totalPages, visPages, activePage ) {
