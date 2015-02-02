1df110da91e0332e910777238d1cf21fe73e0be1
g84 <green.840429@gmail.com>
2015-02-02 16:05:37 +0300

    SITE-5034 оптимизация запроса формы

--- main/controller/OrderV3OneClick/FormAction.php
+++ main/controller/OrderV3OneClick/FormAction.php
@@ -5,7 +5,9 @@ namespace Controller\OrderV3OneClick;
 class FormAction {
     /** Main function
      * @param \Http\Request $request
+     * @param string $productUid
      * @return \Http\Response
+     * @throws \Exception
      */
     public function execute(\Http\Request $request, $productUid) {
         \App::logger()->debug('Exec ' . __METHOD__);
@@ -14,6 +16,9 @@ class FormAction {
         $user = \App::user();
 
         // подготовка 1-го пакета запросов
+
+        /** @var \Model\Product\Entity|null $product */
+        $product = null;
 
         // запрашиваем текущий регион, если есть кука региона
         $regionConfig = [];
@@ -32,19 +37,6 @@ class FormAction {
             });
         }
 
-        // выполнение 1-го пакета запросов
-        $client->execute(\App::config()->coreV2['retryTimeout']['tiny']);
-
-        $regionEntity = $user->getRegion();
-        if ($regionEntity instanceof \Model\Region\Entity) {
-            if (array_key_exists('reserve_as_buy', $regionConfig)) {
-                $regionEntity->setForceDefaultBuy(false == $regionConfig['reserve_as_buy']);
-            }
-            $user->setRegion($regionEntity);
-        }
-
-        /** @var $product \Model\Product\Entity */
-        $product = null;
         \RepositoryManager::product()->prepareEntityByUid($productUid, function($data) use (&$product) {
             if (!is_array($data)) return;
 
@@ -55,8 +47,16 @@ class FormAction {
             }
         });
 
-        // выполнение 2-го пакета запросов
+        // выполнение 1-го пакета запросов
         $client->execute(\App::config()->coreV2['retryTimeout']['tiny']);
+
+        $regionEntity = $user->getRegion();
+        if ($regionEntity instanceof \Model\Region\Entity) {
+            if (array_key_exists('reserve_as_buy', $regionConfig)) {
+                $regionEntity->setForceDefaultBuy(false == $regionConfig['reserve_as_buy']);
+            }
+            $user->setRegion($regionEntity);
+        }
 
         if (!$product) {
             throw new \Exception\NotFoundException(sprintf('Товар @%s не найден.', $productUid));
