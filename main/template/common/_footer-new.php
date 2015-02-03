bd30dc2bb2243891e6e0c692130ed32dffca32d0
frontgirl <web@mow-01-ic237.enter.lan>
2015-02-03 11:41:51 +0300

    SITE-5100 fix

--- main/template/common/_footer-new.php
+++ main/template/common/_footer-new.php
@@ -2,9 +2,10 @@
     <div class="footer_t clearfix">
         <ul class="footer_cmpn clearfix">
             <li class="footer_cmpn_i"><a class="footer_cmpn_lk" href="/about_company">О компании</a></li>
+            <li class="footer_cmpn_i"><a class="footer_cmpn_lk" href="http://feedback.enter.ru/">Напишите нам</a></li>
             <li class="footer_cmpn_i"><a class="footer_cmpn_lk" href="/how_pay">Способы оплаты</a></li>
             <li class="footer_cmpn_i"><a class="footer_cmpn_lk" href="/credit">Покупка в кредит</a></li>
-            <li class="footer_cmpn_i"><a class="footer_cmpn_lk" href="http://my.enter.ru/community/job">Работа у нас</a></li>
+            <li class="footer_cmpn_i"><a class="footer_cmpn_lk" href="http://my.enter.ru/job">Работа у нас</a></li>
             <li class="footer_cmpn_i"><a class="footer_cmpn_lk" href="/b2b">Корпоративным клиентам</a></li>
             <li class="footer_cmpn_i footer_cmpn_i-last"><a class="footer_cmpn_lk" href="/research">ЦСИ</a></li>
         </ul>
