6c2937e2c1e5f46c21ff2fe0bf172e2f0302d132
g84 <green.840429@gmail.com>
2015-03-02 12:56:51 +0300

    SITE-5224 исправление дат

--- main/template/enterprize/_list.php
+++ main/template/enterprize/_list.php
@@ -62,8 +62,16 @@ $f = function(
                 'token'       => $coupon->getToken(),
                 'number'      => $coupon->getDiscount() ? $coupon->getDiscount()->getNumber() : null,
                 'discount'    => $helper->formatPrice($coupon->getPrice()) . ($coupon->getIsCurrency() ? ' <span class="rubl">p</span>' : '%'),
-                'start'       => $coupon->getStartDate() instanceof \DateTime ? $coupon->getStartDate()->format('d.m.Y') : null,
-                'end'         => $coupon->getEndDate() instanceof \DateTime ? $coupon->getEndDate()->format('d.m.Y') : null,
+                'start'       =>
+                    $coupon->getDiscount()
+                    ? ($coupon->getDiscount()->getStartDate() instanceof \DateTime ? $coupon->getDiscount()->getStartDate()->format('d.m.Y') : null)
+                    : ($coupon->getStartDate() instanceof \DateTime ? $coupon->getStartDate()->format('d.m.Y') : null)
+                ,
+                'end'         =>
+                    $coupon->getDiscount()
+                    ? ($coupon->getDiscount()->getEndDate() instanceof \DateTime ? $coupon->getDiscount()->getEndDate()->format('d.m.Y') : null)
+                    : ($coupon->getEndDate() instanceof \DateTime ? $coupon->getEndDate()->format('d.m.Y') : null)
+                ,
                 'description' => $coupon->getSegmentDescription(),
                 'minOrderSum' => $helper->formatPrice($coupon->getMinOrderSum()),
                 'isUserOwner' => (bool)$coupon->getDiscount(),
