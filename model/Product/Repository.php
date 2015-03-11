1544c2b043e91733218ce70252b46662b5ac94e3
Mihail Haritonov <mihail.haritonov@enter.ru>
2015-03-11 13:31:59 +0300

    SITE-5250 Убрать заглушку, ограничивающую offset на 5000

--- model/Product/Repository.php
+++ model/Product/Repository.php
@@ -336,8 +336,6 @@ class Repository {
     public function prepareIteratorByFilter(array $filter = [], array $sort = [], $offset = null, $limit = null, \Model\Region\Entity $region = null, $done, $fail = null) {
         \App::logger()->debug('Exec ' . __METHOD__ . ' ' . json_encode(func_get_args(), JSON_UNESCAPED_UNICODE));
 
-        $offset = $this->correctOffset($offset, $limit);
-
         $this->client->addQuery('listing/list',
             [
                 'region_id' => $region ? $region->getId() : \App::user()->getRegion()->getId(),
@@ -366,8 +364,6 @@ class Repository {
         \App::logger()->debug('Exec ' . __METHOD__ . ' ' . json_encode(func_get_args(), JSON_UNESCAPED_UNICODE));
 
         $client = clone $this->client;
-
-        $offset = $this->correctOffset($offset, $limit);
 
         $response = [];
         $client->addQuery('listing/list',
@@ -426,8 +422,6 @@ class Repository {
         \App::logger()->debug('Exec ' . __METHOD__ . ' ' . json_encode(func_get_args(), JSON_UNESCAPED_UNICODE));
 
         $client = clone $this->client;
-
-        $offset = $this->correctOffset($offset, $limit);
 
         $response = [];
         $client->addQuery('listing/list',
@@ -815,12 +809,5 @@ class Repository {
             }
         }
         return $productsGrouped;
-    }
-
-    private function correctOffset($offset, $limit) {
-        if ($offset > 5000 - $limit)
-            $offset = 5000 - $limit;
-
-        return $offset;
     }
 }
