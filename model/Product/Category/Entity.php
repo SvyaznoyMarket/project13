3e8e44f74b3bd9d4e1f7938308e06dfca8e57c0d
Mihail Haritonov <mihail.haritonov@enter.ru>
2015-03-17 18:05:08 +0300

    SITE-5277 Отключил новые графические фильтры в электроннике, т.к. редакция ещё не настроила фильтры

--- model/Product/Category/Entity.php
+++ model/Product/Category/Entity.php
@@ -432,7 +432,7 @@ class Entity extends BasicEntity {
             return in_array($this->getRootOrSelf()->getUi(), [
                 '616e6afd-fd4d-4ff4-9fe1-8f78236d9be6', // Бытовая техника
                 'f7a2f781-c776-4342-81e8-ab2ebe24c51a', // Мебель
-                'd91b814f-0470-4fd5-a2d0-a0449e63ab6f', // Электронника
+//                'd91b814f-0470-4fd5-a2d0-a0449e63ab6f', // Электронника
             ], true);
         }
 
