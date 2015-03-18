875912f50e58de43db7658e0f64f911b534f30e7
Zhukov Roman <roman.zhukov@enter.ru>
2015-03-18 13:34:12 +0300

    SITE-5229 Оплата плюсами "Связной Клуб" (убрал прыжок при клике)

--- main/template/order-v3-new/complete-blocks/_svyaznoy-club.php
+++ main/template/order-v3-new/complete-blocks/_svyaznoy-club.php
@@ -4,7 +4,7 @@
     <div class="orderPayment_block jsPaymentMethod" data-value="14">
         <span class="orderPayment_msg_info -short">Скидка за плюсы «Связной клуб»</span>
         <img class="orderPayment_msg_info_img" src="/styles/order/img/sclub_med.jpg">
-        <a href="#" class="btn-discount-orange">Получить скидку</a>
+        <a href="" class="btn-discount-orange">Получить скидку</a>
     </div>
 
 <? }; return $f;
