3a7248b14116d2b47355bb07e4c6fa0a9455177f
frontgirl <web@mow-01-ic237.enter.lan>
2015-04-03 12:09:18 +0300

    SITE-5337 refactoring form controls

--- main/template/product-page/content.php
+++ main/template/product-page/content.php
@@ -434,53 +434,55 @@
 <!-- попап добавления отзыва -->
 <div class="popup popup--add-review">
 	<i class="closer">×</i>
-		<div class="popup-close"></div>
-		<div class="popup__tl">Отзыв о товаре</div>
-		<div class="popup__product-line">
-			<div class="popup__product-line-img-wrap">
-				<img class="popup__product-line-img" src="http://fs10.enter.ru/1/1/120/01/338119.jpg">
-			</div>
-			<div class="popup__product-line-tl">
-				Купольная вытяжка Hotpoint-Ariston 7HHP 6 R (OW)/HA
-			</div>
+	<div class="popup__tl">Отзыв о товаре</div>
+	<div class="popup__product-line">
+		<div class="popup__product-line-img-wrap">
+			<img class="popup__product-line-img" src="http://fs10.enter.ru/1/1/120/01/338119.jpg">
 		</div>
-		<div class="popup-rating">
-			<span class="popup-rating__tl">Оценка:</span>
-			<span class="popup-rating__state">
-				<i class="popup-rating__i popup-rating__i--1"></i>
-				<i class="popup-rating__i popup-rating__i--2"></i>
-				<i class="popup-rating__i popup-rating__i--3"></i>
-				<i class="popup-rating__i popup-rating__i--4"></i>
-				<i class="popup-rating__i popup-rating__i--5"></i>
-			</span>
+		<div class="popup__product-line-tl">
+			Купольная вытяжка Hotpoint-Ariston 7HHP 6 R (OW)/HA
 		</div>
-		<form class="popup-form popup-form--review">
-			<fieldset class="form-ctrl__line">
-				<div class="form-ctrl__group form-ctrl__group--inline">
-					<input class="form-ctrl__input" type="text" name="name">
-					<label class="form-ctrl__input-lbl" for="name">Имя</label>
-				</div>
-				<div class="form-ctrl__group form-ctrl__group--inline">
-					<input class="form-ctrl__input" type="text" name="email">
-					<label class="form-ctrl__input-lbl form-ctrl__input-lbl--required" for="email">E-mail</label>
-				</div>
-			</fieldset>
-			
-			<div class="form-ctrl__group">
-				<label class="form-ctrl__textarea-lbl" for="email">Достоинства:</label>
-				<textarea class="form-ctrl__textarea">Тихо работает</textarea>
-			</div>
-			<div class="form-ctrl__group">
-				<label class="form-ctrl__textarea-lbl" for="email">Недостатки:</label>
-				<textarea class="form-ctrl__textarea">Громко шумит</textarea>
-			</div>
-			<div class="form-ctrl__group">
-				<label class="form-ctrl__textarea-lbl" for="email">Комментарий:</label>
-				<textarea class="form-ctrl__textarea">Что вообще происходит, непонятно</textarea>
-			</div>
-			<div class="form-ctrl__btn-container">
-				<button class="btn-type btn-type--buy btn-type--normal" type="submit">Отправить</button>
-			</div>
-		</form>
 	</div>
+	<div class="popup-rating">
+		<span class="popup-rating__tl">Оценка:</span>
+		<span class="popup-rating__state">
+			<i class="popup-rating__i popup-rating__i--1"></i>
+			<i class="popup-rating__i popup-rating__i--2"></i>
+			<i class="popup-rating__i popup-rating__i--3"></i>
+			<i class="popup-rating__i popup-rating__i--4"></i>
+			<i class="popup-rating__i popup-rating__i--5"></i>
+		</span>
+	</div>
+	<form class="popup-form popup-form--review">
+		<fieldset class="form-ctrl__line">
+			<div class="form-ctrl__group form-ctrl__group--inline">
+				<input class="form-ctrl__input form-ctrl__input--err" type="text" name="name">
+				<label class="form-ctrl__input-lbl" for="name">Имя</label>
+			</div>
+			<div class="form-ctrl__group form-ctrl__group--inline">
+				<input class="form-ctrl__input" type="text" name="email">
+				<label class="form-ctrl__input-lbl form-ctrl__input-lbl--required" for="email">E-mail</label>
+			</div>
+		</fieldset>
+		
+		<div class="form-ctrl__group">
+			<label class="form-ctrl__textarea-lbl" for="email">Достоинства:</label>
+			<textarea class="form-ctrl__textarea">Тихо работает</textarea>
+		</div>
+		<div class="form-ctrl__group">
+			<label class="form-ctrl__textarea-lbl" for="email">Недостатки:</label>
+			<textarea class="form-ctrl__textarea">Громко шумит</textarea>
+		</div>
+		<div class="form-ctrl__group">
+			<label class="form-ctrl__textarea-lbl" for="email">Комментарий:</label>
+			<textarea class="form-ctrl__textarea">Что вообще происходит, непонятно</textarea>
+		</div>
+		<div class="form-ctrl__btn-container">
+			<button class="btn-type btn-type--buy btn-type--normal" type="submit">Отправить</button>
+		</div>
+	</form>
+</div>
+<div class="popup popup--photo">
+	<i class="closer">×</i>
+
 </div>
