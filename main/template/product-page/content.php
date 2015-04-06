f86030ac7fe1372c7fe497596d0532219c69f345
tamili <ta.burlakova@gmail.com>
2015-04-06 10:45:05 +0300

    SITE-5337 fix bugs popup

--- main/template/product-page/content.php
+++ main/template/product-page/content.php
@@ -497,15 +497,19 @@
 <!-- попап добавления отзыва -->
 <div class="popup popup--add-review">
 	<i class="closer">×</i>
+
 	<div class="popup__tl">Отзыв о товаре</div>
+
 	<div class="popup__product-line">
 		<div class="popup__product-line-img-wrap">
 			<img class="popup__product-line-img" src="http://fs10.enter.ru/1/1/120/01/338119.jpg">
 		</div>
+
 		<div class="popup__product-line-tl">
 			Купольная вытяжка Hotpoint-Ariston 7HHP 6 R (OW)/HA
 		</div>
 	</div>
+
 	<div class="popup-rating">
 		<span class="popup-rating__tl">Оценка:</span>
 		<span class="popup-rating__state">
@@ -516,12 +520,14 @@
 			<i class="popup-rating__i popup-rating__i--5"></i>
 		</span>
 	</div>
+
 	<form class="popup-form popup-form--review form-ctrl">
 		<fieldset class="form-ctrl__line">
 			<div class="form-ctrl__group form-ctrl__group--inline">
 				<input class="form-ctrl__input" type="text" name="name">
 				<label class="form-ctrl__input-lbl" for="name">Имя</label>
 			</div>
+
 			<div class="form-ctrl__group form-ctrl__group--inline">
 				<input class="form-ctrl__input form-ctrl__input--err" type="text" name="email">
 				<label class="form-ctrl__input-lbl form-ctrl__input-lbl--required" for="email">E-mail</label>
@@ -532,16 +538,19 @@
 			<label class="form-ctrl__textarea-lbl" for="email">Достоинства:</label>
 			<textarea class="form-ctrl__textarea">Тихо работает</textarea>
 		</div>
+
 		<div class="form-ctrl__group">
 			<label class="form-ctrl__textarea-lbl" for="email">Недостатки:</label>
 			<textarea class="form-ctrl__textarea">Громко шумит</textarea>
 		</div>
+
 		<div class="form-ctrl__group">
 			<label class="form-ctrl__textarea-lbl" for="email">Комментарий:</label>
 			<textarea class="form-ctrl__textarea">Что вообще происходит, непонятно</textarea>
 		</div>
+
 		<div class="form-ctrl__btn-container">
-			<button class="btn-type btn-type--buy btn-type--normal" type="submit">Отправить</button>
+			<button class="btn-type btn-type--buy" type="submit">Отправить</button>
 		</div>
 	</form>
 </div>
@@ -549,15 +558,18 @@
 
 <div class="popup popup--photo">
 	<i class="closer">×</i>
+
 	<div class="product-card-photo">
 		<img src="http://fs10.enter.ru/1/1/500/61/113290.jpg" class="product-card-photo__img">
 		<div class="product-card-photo__ctrl product-card-photo__ctrl--prev"><span class="symb"></span></div>
 		<div class="product-card-photo__ctrl product-card-photo__ctrl--next"><span class="symb"></span></div>
+
 		<div class="product-card-photo-zoom">
 			<div class="product-card-photo-zoom__ctrl product-card-photo-zoom__ctrl--in">+</div>
 			<div class="product-card-photo-zoom__ctrl product-card-photo-zoom__ctrl--out">–</div>
 		</div>
 	</div>
+
 	<div class="product-card-photo-thumbs product-card-photo-thumbs--slides">
 		<ul class="product-card-photo-thumbs-list">
 			<li class="product-card-photo-thumbs__i"><img src="http://fs10.enter.ru/1/1/500/61/113290.jpg" class="product-card-photo-thumbs__img"></li>
@@ -572,9 +584,6 @@
 		<div class="product-card-photo-thumbs__btn product-card-photo-thumbs__btn--l product-card-photo-thumbs__btn--disabled"></div>
 		<div class="product-card-photo-thumbs__btn product-card-photo-thumbs__btn--r"></div>
 	</div>
-	<form class="popup-form popup-form--photo form-ctrl">
-		<div class="form-ctrl__btn-container">
-			<button class="btn-type btn-type--buy btn-type--normal" type="submit">В корзину</button>
-		</div>
-	</form>
+
+	<a href="" class="btn-type btn-type--buy">В корзину</a>
 </div>
