0cd4de173f12c365541aca8f17cd713dade74bf2
frontgirl <web@mow-01-ic237.enter.lan>
2015-04-07 16:36:08 +0300

    SITE-5396 layout fixes

--- main/template/product-giftery/__price.php
+++ main/template/product-giftery/__price.php
@@ -9,17 +9,16 @@ return function(
         return;
     }
 ?>
-    <? /* Старой цены у товаров marketplace пока не может быть
+
+        <div class="product-card-price">
+        <? /* Старой цены у товаров marketplace пока не может быть
     <? if ($product->getPriceOld()): ?>
         <div class="priceOld"><span><?= $helper->formatPrice($product->getPriceOld()) ?></span> <span class="rubl">p</span></div>
     <? endif ?>
     */ ?>
-
-    <span>
-        <div class="bPrice5321a13ebb1e5 bInputList">
-            от <strong itemprop="price"><?= \App::config()->partners['Giftery']['lowestPrice'] ?></strong>
-            <meta itemprop="priceCurrency" content="RUB" />
-            <span class="rubl">p</span>
+            <span class="product-card-price__inner">от <strong itemprop="price"><?= \App::config()->partners['Giftery']['lowestPrice'] ?></strong>
+                <meta itemprop="priceCurrency" content="RUB" />
+                <span class="rubl">p</span>
+            </span>
         </div>
-    </span>
 <? };
