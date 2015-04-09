9e61da1cb79bdade95f56dffb98b95ef038f0004
g84 <green.840429@gmail.com>
2015-04-09 10:48:19 +0300

    SITE-5406 wikimart: добавил номер корзины

--- main/template/userbar/_usercart-wikimart.php
+++ main/template/userbar/_usercart-wikimart.php
@@ -1,7 +1,7 @@
 <!-- При пустой корзине -->
 <li class="userbtn_i topbarfix_cart mEmpty">
     <a href="<?= $page->url('cart') ?>" class="topbarfix_cart_tl">
-        <span class="topbarfix_cart_tx">Корзина</span>
+        <span class="topbarfix_cart_tx">Корзина</span> <span id="wikimart-call-id"></span>
         <strong id="wikimart-cart-goodsnum" class="topbarfix_cart_qn topbarfix_cartQuan"></strong>
         <span id="wikimart-cart-price"></span> <span class="rubl">p</span>
     </a>
