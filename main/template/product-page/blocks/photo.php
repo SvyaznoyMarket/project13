a6dc1365c200bfba23175ffb7d4645367a66b1f7
Zhukov Roman <roman.zhukov@enter.ru>
2015-04-22 17:44:56 +0300

    SITE-5337 Карточка товара 2015 Фото товара и попап с фото

--- main/template/product-page/blocks/photo.php
+++ main/template/product-page/blocks/photo.php
@@ -10,13 +10,14 @@ $f = function(
 
     <!-- слайдер изображений товара -->
     <div class="product-card__l">
+
         <div class="product-card-photo">
             <? if ($product->getLabel()): ?>
                 <a class="product-card-photo-sticker" href=""><img src="<?= $product->getLabel()->getImageUrl(1) ?>" alt="<?= $helper->escape($product->getLabel()->getName()) ?>"></a>
             <? endif ?>
             <img src="<?= $product->getImageUrl(3) ?>"
-                 class="product-card-photo__img js-photo-zoomedImg jsOpenProductImgPopup"
-                 data-zoom-image="<?= $product->getImageUrl(5) ?>"
+                 class="product-card-photo__img js-photo-zoomedImg jsOpenProductImgPopup jsProductMiddlePhoto"
+                 data-zoom-image="<?= $product->getImageUrl(5) // elevatezoom ?>"
                  data-zoom-disable="true"
                  alt="<?= $helper->escape($product->getName()) ?>"
                  data-is-slot="<?= $product->getSlotPartnerOffer() ? 'true' : 'false' ?>"
@@ -25,17 +26,18 @@ $f = function(
         </div>
 
         <!-- если картинок больше 5 добавляем класс product-card-photo-thumbs--slides -->
-        <div class="product-card-photo-thumbs <?= count($product->getPhoto()) > 5 ? 'product-card-photo-thumbs--slides' : ''?>">
-            <ul class="product-card-photo-thumbs-list">
+        <div class="product-card-photo-thumbs jsProductThumbHolder <?= count($product->getPhoto()) > 5 ? 'product-card-photo-thumbs--slides' : ''?>">
+            <ul class="product-card-photo-thumbs-list jsProductThumbList">
                 <? foreach ($product->getPhoto() as $key => $photo) : ?>
                     <li class="product-card-photo-thumbs__i jsProductPhotoThumb <?= $key == 0 ? 'product-card-photo-thumbs__i--act' : '' ?>"
+                        data-middle-img="<?= $photo->getUrl(3) ?>"
                         data-big-img="<?= $photo->getUrl(5) ?>"
                         ><img src="<?= $photo->getUrl() ?>" class="product-card-photo-thumbs__img" /></li>
                 <? endforeach ?>
             </ul>
 
-            <div class="product-card-photo-thumbs__btn product-card-photo-thumbs__btn--l product-card-photo-thumbs__btn--disabled"></div>
-            <div class="product-card-photo-thumbs__btn product-card-photo-thumbs__btn--r"></div>
+            <div class="product-card-photo-thumbs__btn product-card-photo-thumbs__btn--l product-card-photo-thumbs__btn--disabled jsProductThumbBtn" data-dir="+="></div>
+            <div class="product-card-photo-thumbs__btn product-card-photo-thumbs__btn--r jsProductThumbBtn" data-dir="-="></div>
 
         </div>
 
@@ -77,11 +79,11 @@ $f = function(
             <i class="closer jsPopupCloser">×</i>
 
             <div class="product-card-photo">
-                <div class="product-card-photo__img jsProductPopupBigPhotoHolder" style="height: 620px; margin: 0 auto; overflow: hidden">
+                <div class="product-card-photo__img jsProductPopupBigPhotoHolder" style="height: 620px; margin: 0 auto; overflow: hidden; width: 785px">
                     <img src="" class="jsProductPopupBigPhoto" style="height: 620px; position: relative; top: 0; left: 0;" />
                 </div>
                 <div class="product-card-photo__ctrl product-card-photo__ctrl--prev jsProductPopupSlide" data-dir="-1"><span class="symb"></span></div>
-                <div class="product-card-photo__ctrl product-card-photo__ctrl--next jsProductPopupSlide" data-dir="+1"><span class="symb"></span></div>
+                <div class="product-card-photo__ctrl product-card-photo__ctrl--next jsProductPopupSlide" data-dir="1"><span class="symb"></span></div>
 
                 <div class="product-card-photo-zoom">
                     <div class="product-card-photo-zoom__ctrl product-card-photo-zoom__ctrl--in jsProductPopupZoom" data-dir="+1">+</div>
@@ -89,10 +91,10 @@ $f = function(
                 </div>
             </div>
 
-            <div class="product-card-photo-thumbs">
+            <div class="product-card-photo-thumbs" style="max-width: 670px;">
                 <ul class="product-card-photo-thumbs-list">
                     <? foreach ($product->getPhoto() as $key => $photo) : ?>
-                        <li class="product-card-photo-thumbs__i jsProductPhotoThumb"
+                        <li class="product-card-photo-thumbs__i jsPopupPhotoThumb"
                             data-big-img="<?= $photo->getUrl(5) ?>"
                             ><img src="<?= $photo->getUrl() ?>" class="product-card-photo-thumbs__img"></li>
                     <? endforeach ?>
