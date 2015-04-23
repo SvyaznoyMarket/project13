25ddaf19463abfbe8319b900ecc41f0e8633045d
tamili <ta.burlakova@gmail.com>
2015-04-23 11:34:16 +0300

    SITE-5366

--- main/template/product/__favoriteButton.php
+++ main/template/product/__favoriteButton.php
@@ -7,8 +7,8 @@ return function(
 ) {
 ?>
 
-<a href="<?= $helper->url('favorite.add', ['productUi' => $product->getUi()]) ?>" class="jsFavoriteLink product-card-tools__lk">
-    <i class="product-card-tools__icon i-tools-icon i-tools-icon--wish<? if ($favoriteProduct): ?> i-tools-icon--wishSelected<? endif ?>"></i>
+<a href="<?= $helper->url('favorite.add', ['productUi' => $product->getUi()]) ?>" class="jsFavoriteLink product-card-tools__lk <? if ($favoriteProduct): ?> product-card-tools__lk--active<? endif ?>">
+    <i class="product-card-tools__icon i-tools-icon i-tools-icon--wish"></i>
     <span class="product-card-tools__tx">В избранное</span>
 </a>
 
