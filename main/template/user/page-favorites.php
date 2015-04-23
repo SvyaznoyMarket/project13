f8625f16a8686eead57cd026b12d6307abdf604c
g84 <green.840429@gmail.com>
2015-04-23 15:55:50 +0300

    SITE-5457 Избранные товары в ЛК: ссылки на товары

--- main/template/user/page-favorites.php
+++ main/template/user/page-favorites.php
@@ -15,10 +15,14 @@
 	<div class="table-favorites table table--border-cell-hor">
 		<? foreach ($products as $product): ?>
 		<div class="table-row">
-			<div class="table-favorites__cell-left table-cell"><img src="<?= $product->getImageUrl(1) ?>" alt="<?= $helper->escape($product->getName()) ?>" class="table-favorites__img"></div>
+			<div class="table-favorites__cell-left table-cell">
+				<a href="<?= $product->getLink() ?>">
+					<img src="<?= $product->getImageUrl(1) ?>" alt="<?= $helper->escape($product->getName()) ?>" class="table-favorites__img">
+				</a>
+			</div>
 
 			<div class="table-favorites__cell-center table-cell">
-				<div class="table-favorites__name"><?= $product->getName() ?></div>
+				<a href="<?= $product->getLink() ?>" class="table-favorites__name"><?= $product->getName() ?></a>
 				<? if ($product->getPrice()): ?>
 					<div class="table-favorites__price"><?= $helper->formatPrice($product->getPrice()) ?> <span class="rubl">p</span></div>
 				<? endif ?>
