cdcdb170a70de12d658ff3ee6ce8dbccde39a822
g84 <green.840429@gmail.com>
2015-04-24 10:18:19 +0300

    SITE-5366 Изменил поведение ссылки

--- main/template/product/__favoriteButton.php
+++ main/template/product/__favoriteButton.php
@@ -5,12 +5,9 @@ return function(
     \Model\Product\Entity $product,
     \Model\Favorite\Product\Entity $favoriteProduct = null
 ) {
-    $removeUrl = null;
-
     if ($favoriteProduct) {
-        $text = 'В избранном';
-        $url = $helper->url('user.favorites');
-        $removeUrl = $helper->url('favorite.delete', ['productUi' => $product->getUi()]);
+        $text = 'Убрать из избранного';
+        $url = $helper->url('favorite.delete', ['productUi' => $product->getUi()]);
     } else {
         $text = 'В избранное';
         $url = $helper->url('favorite.add', ['productUi' => $product->getUi()]);
@@ -19,15 +16,12 @@ return function(
 
 <span class="id-favoriteButton-<?= $product->getUi() ?>">
     <a
-        <? if (!$favoriteProduct): ?> data-ajax="true"<? endif ?>
+        data-ajax="true"
         href="<?= $url ?>"
         class="jsFavoriteLink product-card-tools__lk <? if ($favoriteProduct): ?> product-card-tools__lk--active<? endif ?>"
     >
         <i class="product-card-tools__icon i-tools-icon i-tools-icon--wish"></i>
         <span class="product-card-tools__tx"><?= $text ?></span>
-        <? if ($removeUrl): ?>
-            <a class="jsFavoriteLink" data-ajax="true" href="<?= $removeUrl ?>" title="Удалить из избранного"> X</a>
-       <? endif ?>
     </a>
 </span>
 
