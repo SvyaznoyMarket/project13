a5c75b7f4691944ddcac4af3168f4dc8b27b3afd
frontgirl <web@mow-01-ic237.enter.lan>
2015-05-22 17:45:42 +0300

    SITE-5551 suppliers form

---
+++ main/template/supplier/page-new.php
@@ -0,0 +1,77 @@
+<div class="suppliers__head">
+    <div class="suppliers__tl"><i class="suppliers-icon"></i>Регистрация поставщика</div>
+    <div class="suppliers-login"><span class="suppliers-login__tl">Регистрировались?</span><a href="#" class="suppliers-login__lk supply-btn">Войти</a></div>
+</div>
+<div class="suppliers__head">
+    <div class="suppliers__tl"><i class="suppliers-icon"></i>
+            <div class="user">
+                <div class="user__name">ООО Пупкин и партнеры 42</div>
+                <ul class="user-info-list">
+                    <li class="user-info-list__i">Феофан Фролов</li>
+                    <li class="user-info-list__i">+7 (495) 775-00-06</li>
+                    <li class="user-info-list__i">feofan@pupkin42.ru</li>
+                </ul>
+            </div>
+    </div>
+    <div class="suppliers__right-top"><span class="suppliers__support">По всем вопросам звоните<br>+7 (495) 775-00-06</span></div>
+</div>
+<div class="suppliers__cnt">
+    <div class="suppliers__sect suppliers__files">
+        <div class="suppliers__sect-tl">Загрузить файл</div>
+        <ul class="suppliers-load-list">
+            <ol class="suppliers-load-list__i">Скачайте <a class="suppliers-load" href="#"><i class="suppliers-load__icon"></i><span class="suppliers-load__inn">Шаблон прайс-листа</span></a></ol>
+            <ol class="suppliers-load-list__i">Заполните данные</ol>
+            <ol class="suppliers-load-list__i">Загрузите прайс-лист на сайт: <div class="btn-wrap"><a class="load-btn" href="#">Выбрать файл</a></div></ol>
+        </ul>
+    </div>
+
+    <div class="suppliers__form suppliers__sect">
+        <form>
+            <div class="suppliers__sect-tl">Зарегистрировать поставщика</div>
+            <div class="control-group">
+                <label class="control-group__lbl">Форма собственности</label>
+                <div class="custom-select custom-select--suppliers">
+                    <select class="custom-select__inn">
+                        <option class="custom-select__i">Индивидуальный предприниматель</option>
+                        <option class="custom-select__i">Общество с ограниченной ответственностью</option>
+                        <option class="custom-select__i">Акционерное общество</option>
+                    </select>
+                </div>
+            </div>
+            <div class="control-group">
+                <label class="control-group__lbl">Юридический адрес</label>
+                <input class="control-group__input" placeholder="">
+            </div>
+            <div class="control-group">
+                <label class="control-group__lbl">Фактический адрес</label>
+                <input class="control-group__input" placeholder="">
+            </div>
+            <div class="control-group">
+                <label class="control-group__lbl">ИНН</label>
+                <input class="control-group__input" placeholder="">
+            </div>
+            <div class="control-group">
+                <label class="control-group__lbl">КПП</label>
+                <input class="control-group__input" placeholder="">
+            </div>
+            <div class="control-group">
+                <label class="control-group__lbl">Расчетный счет</label>
+                <input class="control-group__input" placeholder="">
+            </div>
+            <div class="control-group">
+                <label class="control-group__lbl">Корреспондентский счет</label>
+                <input class="control-group__input" placeholder="">
+            </div>
+            <div class="control-group">
+                <label class="control-group__lbl">БИК</label>
+                <input class="control-group__input" placeholder="">
+            </div>
+            <div class="control-group">
+                <label class="control-group__lbl">Код ОКПО</label>
+                <input class="control-group__input" placeholder="">
+            </div>
+            <input class="supply-btn" value="Сохранить" type="submit">
+        </form>
+    </div>
+    
+</div>
