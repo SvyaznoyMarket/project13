9568d68746e174beb5c793131f6fb13dc5b71667
frontgirl <web@mow-01-ic237.enter.lan>
2015-05-26 12:35:32 +0300

    SITE-5551 hint+fix

--- main/template/supplier/_file.php
+++ main/template/supplier/_file.php
@@ -3,7 +3,7 @@
  * @var $file \Model\Supplier\File
  */
 ?>
-<li class="prices-list__i">
+<li class="prices-list__i"  hint="<?= $file->name ?>">
     <i class="suppliers-load__icon"></i><span class="prices-list__file-name"><?= $file->name ?></span>
     <span class="prices-list__date"><?= $file->added->format('d.m.Y') ?></span>
 </li>
