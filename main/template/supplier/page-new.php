6af0a2f367875f588731e474c516931240507dd9
Zhukov Roman <roman.zhukov@enter.ru>
2015-05-27 19:04:30 +0300

    SITE-? Hide supplier

--- main/template/supplier/page-new.php
+++ main/template/supplier/page-new.php
@@ -8,39 +8,42 @@
 </div>
 <div class="suppliers__cnt">
     <div class="suppliers__slogan">Мы умеем продавать!</div>
-    <img class="suppliers__sl-icon" src="/styles/suppliers/img/sup-icons.png">
-    <ul class="suppliers-our-nums-list clearfix">
-        <li class="suppliers-our-nums-list__i">
-            <span class="suppliers-our-nums-list__num week-orders">115&thinsp;000</span>
-            <span class="suppliers-our-nums-list__txt">заказов в неделю</span>
-        </li>
-        <li class="suppliers-our-nums-list__i">
-            <span class="suppliers-our-nums-list__num day-watchers">500&thinsp;000</span>
-            <span class="suppliers-our-nums-list__txt">уникальных посетителей в день</span>
-        </li>
-        <li class="suppliers-our-nums-list__i">
-            <span class="suppliers-our-nums-list__num month-volume">1&thinsp;800&thinsp;000&thinsp;000</span>
-            <span class="suppliers-our-nums-list__txt">оборот в месяц, руб.</span>
-        </li>
-    </ul>
-    <h4 class="suppliers-collab__head">Cотрудничество с нами это:</h4>
-    <ul class="suppliers-collab-list">
-        <li class="suppliers-collab-list__i">новые возможности для вашего бизнеса;</li>
-        <li class="suppliers-collab-list__i">доступ к крупным каналам продаж;</li>
-        <li class="suppliers-collab-list__i">доставка по всей России;</li>
-        <li class="suppliers-collab-list__i">более 1000 точек самовывоза.</li>
-    </ul>
+    <div class="supplierToHide">
+        <img class="suppliers__sl-icon" src="/styles/suppliers/img/sup-icons.png">
+        <ul class="suppliers-our-nums-list clearfix">
+            <li class="suppliers-our-nums-list__i">
+                <span class="suppliers-our-nums-list__num week-orders">115&thinsp;000</span>
+                <span class="suppliers-our-nums-list__txt">заказов в неделю</span>
+            </li>
+            <li class="suppliers-our-nums-list__i">
+                <span class="suppliers-our-nums-list__num day-watchers">500&thinsp;000</span>
+                <span class="suppliers-our-nums-list__txt">уникальных посетителей в день</span>
+            </li>
+            <li class="suppliers-our-nums-list__i">
+                <span class="suppliers-our-nums-list__num month-volume">1&thinsp;800&thinsp;000&thinsp;000</span>
+                <span class="suppliers-our-nums-list__txt">оборот в месяц, руб.</span>
+            </li>
+        </ul>
+        <h4 class="suppliers-collab__head">Cотрудничество с нами это:</h4>
+        <ul class="suppliers-collab-list">
+            <li class="suppliers-collab-list__i">новые возможности для вашего бизнеса;</li>
+            <li class="suppliers-collab-list__i">доступ к крупным каналам продаж;</li>
+            <li class="suppliers-collab-list__i">доставка по всей России;</li>
+            <li class="suppliers-collab-list__i">более 1000 точек самовывоза.</li>
+        </ul>
+    </div>
+
     <div class="suppliers__short-form">
-        <div class="supply-btn__wrap">
-            <a href="#weCan" class="supply-btn supply-btn-big">Стать партнером</a>
+        <div class="supply-btn__wrap supplierToHide" >
+            <a href="#weCan" class="supply-btn supply-btn-big jsSupplierToHide">Стать партнером</a>
         </div>
-    <div class="suppliers-collab__info">По всем вопросам звоните +7 (495) 775-00-06</div>
+    <div class="suppliers-collab__info supplierToHide">По всем вопросам звоните +7 (495) 775-00-06</div>
 
         <form   id="b2bRegisterForm"
                 action="<?= \App::helper()->url('supplier.new') ?>"
                 method="post">
             
-            <div id="weCan" class="suppliers__slogan">Мы умеем продавать!</div>
+            <div id="weCan" class="suppliers__slogan supplierToHide">Мы умеем продавать!</div>
 
             <div class="control-group">
                 <label class="control-group__lbl">Наименование организации</label>
