d759bd07d1e6ef93e6da056e88b9e5695d355472
frontgirl <web@mow-01-ic237.enter.lan>
2015-06-23 11:42:30 +0300

    SITE-? баннер бесплатного самовывоза для всех случаев

--- main/template/layout-main-new.php
+++ main/template/layout-main-new.php
@@ -42,7 +42,7 @@
 
             <a class="header__bann" href="/self-points">
                 <div class="header__bann-tl">БЕСПЛАТНЫЙ САМОВЫВОЗ!</div>
-                 <div class="header__bann-c">* Минимальная сумма заказа — 1990 ₽</div>
+                 <div class="header__bann-c">* Минимальная сумма заказа — 1990 <span class="p">p</span></div>
                  <div class="header__bann-msg">25 точек выдачи в <?= \App::abTest()->isOrderMinSumRestriction() ? 'Воронеже' : 'Название города' ?></div>
                 <div class="close-btn jsMainOrderSumBannerCloser"></div>
             </a>
