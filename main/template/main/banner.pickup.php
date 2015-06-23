d810d7309c815f63efabf4924dc780a5d828fa84
Zhukov Roman <roman.zhukov@enter.ru>
2015-06-23 12:55:03 +0300

    SITE-5699 Добавляем Нижний Новгород и Рязань

--- main/template/main/banner.pickup.php
+++ main/template/main/banner.pickup.php
@@ -1,8 +1,24 @@
+<?
+$text = null;
+switch (\App::user()->getRegionId()) {
+    case 18074:
+        $text = '25 точек выдачи в Воронеже';
+        break;
+    case 99958:
+        $text = '19 точек выдачи в Нижнем Новгороде';
+        break;
+    case 10374:
+        $text = '11 точек выдачи в Рязани';
+        break;
+}
+
+?>
+
 <a class="header__bann" href="/self-points">
     <? if (\App::abTest()->isOrderMinSumRestriction()) : ?>
         <div class="header__bann-tl">БЕСПЛАТНЫЙ САМОВЫВОЗ!</div>
         <div class="header__bann-c">* Минимальная сумма заказа — 1990 <span class="p">p</span></div>
-        <div class="header__bann-msg">(25) точек выдачи в (Название города) </div>
+        <div class="header__bann-msg"><?= $text ?></div>
     <? else :?>
         <div class="header__bann-tl">САМОВЫВОЗ В ТВОЕМ ГОРОДЕ!</div>
         <div class="header__bann-btn">Найти</div>
