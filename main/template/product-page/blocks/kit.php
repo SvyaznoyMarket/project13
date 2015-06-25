f3f77932eac877f7c81a982ce6a367777d16bf09
frontgirl <web@mow-01-ic237.enter.lan>
2015-06-25 14:22:21 +0300

    SITE-5712 верстка для комплекта в новой карточке товара

--- main/template/product-page/blocks/kit.php
+++ main/template/product-page/blocks/kit.php
@@ -5,8 +5,61 @@ $f = function (
 ) { ?>
 
     <!-- набор-пакет -->
-    <div class="product-section clearfix" id="kit">
-        <h1>Базовая комплектация набора</h1>
+    <div class="product-section set-section clearfix" id="kit">
+        <div class="product-section__tl">Базовая комплектация набора</div>
+        <!--список комплектующих-->
+        <ul class="set-section-package">
+            <!-- элемент комплекта -->
+            <li class="set-section-package-i">
+                <a class="set-section-package-i__img" href="/product/furniture/komod-kseno-stl07807-2050403009443"><img src="http://4.imgenter.ru/uploads/media/08/e9/44/thumb_4b3b_product_120.jpeg"></a><!--/ изображение товара -->
+
+                <div class="set-section-package-i__desc rown">
+                    <div class="name"><a class="" href="/product/furniture/komod-kseno-stl07807-2050403009443">Комод «Ксено СТЛ.078.07»</a></div><!--/ название товара -->
+
+
+                    <!-- размеры товара -->
+                    <div class="column dimention">
+                        <span class="dimention__name">Высота</span>
+                        <span class="dimention__val">80.2</span>
+                    </div>
+
+                    <div class="column dimention">
+                        <span class="dimention__name">&nbsp;</span>
+                        <span class="dimention__val separation">x</span>
+                    </div>
+
+                    <div class="column dimention">
+                        <span class="dimention__name">Ширина</span>
+                        <span class="dimention__val">153</span>
+                    </div>
+
+                    <div class="column dimention">
+                        <span class="dimention__name">&nbsp;</span>
+                        <span class="dimention__val separation">x</span>
+                    </div>
+
+                    <div class="column dimention">
+                        <span class="dimention__name">Глубина</span>
+                        <span class="dimention__val">44.5</span>
+                    </div>
+
+                    <div class="column dimention">
+                        <span class="dimention__name">&nbsp;</span>
+                        <span class="dimention__val">см</span>
+                    </div>
+                    <!--/ размеры товара -->
+
+                </div>
+
+                <div class="set-section-package-i__price rown">
+                    7220&nbsp;<span class="rubl">p</span>
+                </div><!--/ цена -->
+
+                <div class="set-section-package-i__qnt rown">1 шт.</div><!--/ количество в наборе -->
+            </li>
+            <!-- элемент комплекта END -->
+        </ul>
+        <!--список комплектующих END-->
     </div>
 
 <? }; return $f;
