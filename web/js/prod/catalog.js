d0b494cb81d5644ad0ccc887b54539bf521f6b69
g84 <green.840429@gmail.com>
2015-07-06 13:24:57 +0300

    SITE-5770 клик по категории

--- web/js/prod/catalog.js
+++ web/js/prod/catalog.js
@@ -1,3 +1,24 @@
+$(function() {
+    var
+        $body = $('body'),
+
+        sliderData = $('#jsSlice').data('value')
+    ;
+
+    if (sliderData && (true === sliderData.isSale)) {
+        $body.on('click', '.js-productCategory-link', function() {
+            var
+                $el = $(this)
+            ;
+
+            $body.trigger('trackGoogleEvent', {
+                category: 'slices_sale',
+                action: 'category',
+                label: $el.find('[data-type="name"]').text()
+            });
+        });
+    }
+});
 /**
  * Catalog main config
  *
