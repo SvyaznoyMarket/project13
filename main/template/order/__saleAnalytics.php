ddcf51e9dea576f5eb0abe30bf4958ca66acc5a1
g84 <green.840429@gmail.com>
2015-07-08 17:03:30 +0300

    SITE-5770 Аналитика скидок на order.complete

--- main/template/order/__saleAnalytics.php
+++ main/template/order/__saleAnalytics.php
@@ -11,15 +11,32 @@ $f = function(
 
 $data = [];
 foreach ($orders as $order) {
-    $meta = $order->getMeta();
+    $isSale = false;
+    $isListing = false;
+    foreach ($order->getMeta() as $k => $v) {
+        if (false !== strpos($k, '.position') && is_array($v) && in_array('Listing', $v)) {
+            $isListing = true;
+        }
+        if (false !== strpos($k, '.from') && is_array($v)) {
+            foreach ($v as $value) {
+                if (false !== strpos($value, 'slices/all_labels') && is_array($v)) {
+                    $isSale = true;
+                    break;
+                }
+            }
+        }
+    }
 
-    $data[] = [
-        'category' => 'slices_sale',
-        'action'   => 'buy',
-        //'label'    => $order,
-    ];
+    if ($isSale) {
+        $data[] = [
+            'category' => 'slices_sale',
+            'action'   => 'buy',
+            'label'    => $isListing ? 'basket' : 'product',
+        ];
+    }
 }
 ?>
 
+<div class="jsOrderSaleAnalytics" data-value="<?= $helper->json($data) ?>"></div>
 
 <? }; return $f;
--- web/js/prod/catalog.js
+++ web/js/prod/catalog.js
@@ -1,3 +1,53 @@
+$(function() {
+    var
+        $body = $('body'),
+
+        sliderData = $('#jsSlice').data('value')
+    ;
+
+    if (sliderData && (true === sliderData.isSale)) {
+        // клик по ссылке на категорию
+        $body.on('click', '.js-productCategory-link', function() {
+            var
+                $el = $(this)
+            ;
+
+            $body.trigger('trackGoogleEvent', {
+                category: 'slices_sale',
+                action: 'category',
+                label: $el.find('[data-type="name"]').text()
+            });
+        });
+
+        // клик по ссылке на товар
+        $body.on('click', '.js-listing-item', function() {
+            var
+                $el = $(this)
+            ;
+
+            $body.trigger('trackGoogleEvent', {
+                category: 'slices_sale',
+                action: 'product',
+                label: ''
+            });
+        });
+
+        // клик по кнопке "Купить"
+        $body.on('click', '.jsBuyButton', function(e) {
+            var
+                $el = $(this)
+            ;
+
+            e.stopPropagation();
+
+            $body.trigger('trackGoogleEvent', {
+                category: 'slices_sale',
+                action: 'basket',
+                label: ''
+            });
+        });
+    }
+});
 ;(function() {
 	console.info('New catalog init: filter.js');
 
