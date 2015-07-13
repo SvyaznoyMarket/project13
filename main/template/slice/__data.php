c2eb511e008292196fcec9c70d646f4868b82066
g84 <green.840429@gmail.com>
2015-07-13 16:14:53 +0300

    SITE-5772 Метрики: фильтры

--- main/template/slice/__data.php
+++ main/template/slice/__data.php
@@ -2,12 +2,20 @@
 
 return function(
     \Helper\TemplateHelper $helper,
-    \Model\Slice\Entity $slice
+    \Model\Slice\Entity $slice,
+    \Model\Product\Category\Entity $category = null
 ) {
     $data = [
-        'token'  => $slice->getToken(),
-        'name'   => $slice->getName(),
-        'isSale' => 'all_labels' === $slice->getToken(),
+        'token'    => $slice->getToken(),
+        'name'     => $slice->getName(),
+        'isSale'   => 'all_labels' === $slice->getToken(),
+        'category' =>
+            $category
+            ? [
+                'name' => $category->name,
+            ]
+            : null
+        ,
     ];
 ?>
     <div id="jsSlice" data-value="<?= $helper->json($data) ?>"></div>
