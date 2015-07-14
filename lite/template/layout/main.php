791eb55713715a76268f39d510484758c68cbd17
tamili <ta.burlakova@gmail.com>
2015-07-14 14:10:08 +0300

    LITE-? add to compare

--- lite/template/layout/main.php
+++ lite/template/layout/main.php
@@ -71,13 +71,20 @@
                             <span class="user-controls__text">Сравнение</span>
                         </a>
 
+                        <div class="notice-dd notice-dd_compare" style="display: block">
+                            <div class="notice-compare">
+                                <div class="notice-compare__title">Товар добавлен к сравнению</div>
 
+                                <div class="notice-compare__img"><img src="http://a.imgenter.ru/uploads/media/ae/d3/e0/thumb_bcc6_product_160.jpeg" alt="" class="image"></div>
+                                <div class="notice-compare__desc">Чехол для Apple iPhone6 XtremeMac Microshield Acc Чехол для App XtremeMac</div>
+                            </div>
+                        </div>
                     </li>
 
                     <li class="user-controls__item user-controls__item_user active">
                         <a href="" class="user-controls__link js-popup-show" data-popup="login">
                             <span class="user-controls__icon"><i class="i-controls i-controls--user"></i></span>
-                            <span class="user-controls__text">Бурлакова Татьяна Владимировна    </span>
+                            <span class="user-controls__text">Бурлакова Татьяна Владимировна</span>
                         </a>
                     </li>
                 </ul>
