d907bbea8e72fb4edf19cd26d4e9b953c589a157
Mihail Haritonov <mihail.haritonov@enter.ru>
2015-07-17 16:24:12 +0300

    SITE-5532 Уменьшил кол-во отображаемых страниц, если отображается переключатель видов

--- main/template/product/__pagination.php
+++ main/template/product/__pagination.php
@@ -2,12 +2,14 @@
 
 return function(
     \Helper\TemplateHelper $helper,
-    \Iterator\EntityPager $pager
+    \Iterator\EntityPager $pager,
+    \Model\Product\Category\Entity $category = null
 ) { ?>
 
     <?= $helper->renderWithMustache('pagination', (new \View\PaginationAction())->execute(
         $helper,
-        $pager
+        $pager,
+        $category
     )) ?>
 
     <script class="tplPagination" type="text/html" data-partial="">
