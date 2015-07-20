ea7252c55c30444ca0c742755acd011b0a8692c4
Zhukov Roman <roman.zhukov@enter.ru>
2015-07-20 17:04:35 +0300

    LITE-? Разделение параплашки

--- lite/view/Product/IndexPage.php
+++ lite/view/Product/IndexPage.php
@@ -8,9 +8,21 @@ use View\LiteLayout;
 class IndexPage extends LiteLayout
 {
     protected $layout = 'layout/product';
+    /** @var \Model\Product\Entity */
+    protected $product;
+
+    public function prepare() {
+        parent::prepare();
+        $this->product = $this->getParam('product');
+    }
+
 
     public function blockContent() {
         return $this->render('product/content', $this->params);
+    }
+
+    public function blockFixedUserbar() {
+        return $this->render('product/_userbar.fixed', ['product' => $this->product]);
     }
 
 }
