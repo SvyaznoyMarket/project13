0090416d1df807960936d2e0698f544c9176e491
Zhukov Roman <roman.zhukov@enter.ru>
2015-07-22 15:24:42 +0300

    LITE-? Авторизация

--- lite/template/common/userbar/_user.php
+++ lite/template/common/userbar/_user.php
@@ -1,8 +1,11 @@
 <!-- Авторизованным добавлять класс active -->
-<li class="user-controls__item user-controls__item_user">
-    <a href="" class="user-controls__link js-popup-show js-module-require-onclick" data-popup="login" data-module="enter.auth">
+<li class="user-controls__item user-controls__item_user js-userbar-user">
+    <a href="<?= \App::router()->generate('user.login') ?>"
+       class="user-controls__link js-popup-show js-module-require-onclick js-userbar-user-link"
+       data-popup="login"
+       data-module="enter.auth">
         <span class="user-controls__icon"><i class="i-controls i-controls--user"></i></span>
-        <span class="user-controls__text" >Войти</span>
+        <span class="user-controls__text js-userbar-user-text" >Войти</span>
     </a>
 </li>
 
