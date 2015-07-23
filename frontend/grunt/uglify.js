f1641e36027d17fb0cf7490a02c6cc6eb51f767e
Aleksandr Zaytsev <13rentgen@gmail.com>
2015-07-23 18:48:19 +0300

    Disable drop console

--- frontend/grunt/uglify.js
+++ frontend/grunt/uglify.js
@@ -10,7 +10,7 @@ module.exports = function( grunt, options ) {
             sourceMap: true,
             banner: options.methods.createBanner(),
             compress: {
-                drop_console: true
+                // drop_console: true
             }
         }
 
