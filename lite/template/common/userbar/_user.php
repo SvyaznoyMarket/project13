18402a66fe49e6d1fa41aecfafc4f41e90a66d56
tamili <ta.burlakova@gmail.com>
2015-07-23 14:01:51 +0300

    LITE-? log out popup

--- lite/template/common/userbar/_user.php
+++ lite/template/common/userbar/_user.php
@@ -1,5 +1,5 @@
 <!-- Авторизованным добавлять класс active -->
-<li class="user-controls__item user-controls__item_user js-userbar-user">
+<li class="user-controls__item user-controls__item_user notice-show js-userbar-user">
     <a href="<?= \App::router()->generate('user.login') ?>"
        class="user-controls__link js-popup-show js-module-require-onclick js-userbar-user-link"
        data-popup="login"
@@ -7,6 +7,14 @@
         <span class="user-controls__icon"><i class="i-controls i-controls--user"></i></span>
         <span class="user-controls__text js-userbar-user-text" >Войти</span>
     </a>
+
+    <div class="notice-dd notice-dd_user jsCartNotice">
+		<ul class="notice-user">
+			<li class="notice-user__item"><a href="" class="notice-user__link notice-user__link_lk"><span class="underline">Личный кабинет</span></a></li>
+			<li class="notice-user__item"><a href="" class="notice-user__link notice-user__link_favorite"><span class="underline">Избранное</span></a></li>
+			<li class="notice-user__item"><a href="" class="notice-user__link"><span class="underline">Выйти</span></a></li>
+		</ul>
+    </div>
 </li>
 
 <?= $page->blockAuth() ?>
