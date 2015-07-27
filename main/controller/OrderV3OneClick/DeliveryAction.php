d6418946eb36ddd55df474a85ba10c397ddbd715
Zhukov Roman <roman.zhukov@enter.ru>
2015-07-27 11:37:29 +0300

    SITE-5903 Настройка акции "Все по..."

--- main/controller/OrderV3OneClick/DeliveryAction.php
+++ main/controller/OrderV3OneClick/DeliveryAction.php
@@ -166,6 +166,10 @@ class DeliveryAction {
         }
 
         $orderDelivery = new \Model\OrderDelivery\Entity($orderDeliveryData);
+
+        // TODO удалить
+        \Controller\OrderV3\DeliveryAction::modifyOrderForAction($orderDelivery);
+
         if (!(bool)$orderDelivery->orders) {
             foreach ($orderDelivery->errors as $error) {
                 if (708 == $error->code) {
