cb315c3f5ad637605ae1aac3b5223cd931d6b8eb
Mihail Haritonov <mihail.haritonov@enter.ru>
2015-07-30 14:50:45 +0300

    SITE-? Добавил в config-live закомментированный код для переключения на тестовые бэкэнды

--- config/config-live.php
+++ config/config-live.php
@@ -5,6 +5,26 @@ $c = require __DIR__ . '/config.php';
 
 $c->debug = false;
 
+//$c->coreV2['url']        = 'http://tester.core.ent3.ru/v2/';
+//$c->coreV2['url']        = 'http://cr1.core.ent3.ru/v2/';
+
+//$c->corePrivate['url']   = 'http://tester.core.ent3.ru/private/';
+
+//$c->reviewsStore['url']  = 'http://scms.ent3.ru/reviews/';
+//$c->scms['url']          = 'http://scms.ent3.ru/';
+//$c->scmsV2['url']        = 'http://scms.ent3.ru/v2/';
+//$c->scmsSeo['url']       = 'http://scms.ent3.ru/seo/';
+
+//$c->searchClient['url']  = 'http://search.ent3.ru/';
+
+//$c->coreV2['timeout']       = 30;
+//$c->corePrivate['timeout']  = 30;
+//$c->reviewsStore['timeout'] = 30;
+//$c->scms['timeout']         = 30;
+//$c->scmsV2['timeout']       = 30;
+//$c->scmsSeo['timeout']      = 30;
+//$c->searchClient['timeout'] = 30;
+
 $c->eventService['enabled'] = true;
 
 return $c;
