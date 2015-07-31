a6a2a412ee7dc2a2196bfdfc46480c207d3808d9
Mihail Haritonov <mihail.haritonov@enter.ru>
2015-08-01 00:54:19 +0300

    SITE-5853 Информация о количестве точек в разрезе города в шапке сайта

--- main/template/main/banner.pickup.php
+++ main/template/main/banner.pickup.php
@@ -1,20 +1,22 @@
-<? if (\App::request()->getPathInfo() !== '/delivery'): ?>
-    <?
+<?
     $helper = new \Helper\TemplateHelper();
-    
     $region = \App::user()->getRegion();
+?>
 
-    // TODO удалить даный блок if после реализации FCMS-779
-    if ($region->name) {
-        $scmsResponse = \App::scmsClient()->query('api/word-inflect', ['names' => [$region->name]], []);
+<? if (\App::request()->getPathInfo() !== '/delivery' && $region->pointCount): ?>
+    <?
+        // TODO удалить даный блок if после реализации FCMS-779
+        if ($region->name) {
+            $scmsResponse = \App::scmsClient()->query('api/word-inflect', ['names' => [$region->name]], []);
 
-        if (isset($scmsResponse[$region->name])) {
-            $region->names = new \Model\Inflections($scmsResponse[$region->name]);
+            if (isset($scmsResponse[$region->name])) {
+                $region->names = new \Model\Inflections($scmsResponse[$region->name]);
+            }
         }
-    }
     ?>
 
     <a class="header__bann" href="<?= \App::helper()->url('delivery') ?>">
-        Бесплатный самовывоз из <?= $region->pointCount ?> <?= $helper->numberChoice($region->pointCount, ['точка', 'точки', 'точек']) ?> в <?= $helper->escape($region->names->locativus) ?>. Для заказов от 1990 р.
+        Бесплатный самовывоз из <?= $region->pointCount ?> <?= $helper->numberChoice($region->pointCount, ['точка', 'точки', 'точек']) ?> <? if ($region->names->locativus): ?>в <?= $helper->escape($region->names->locativus) ?><? endif ?>.
+        Для заказов от 1990 р.
     </a>
 <? endif ?>
