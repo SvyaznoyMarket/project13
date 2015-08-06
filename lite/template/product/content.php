784f44deae32ff7f146b368b9c0d9604fd8bfca3
tamili <ta.burlakova@gmail.com>
2015-08-06 17:51:27 +0300

    LITE-? popup one click

--- lite/template/product/content.php
+++ lite/template/product/content.php
@@ -250,6 +250,26 @@ $buySender2 = $request->get('sender2');
 </section>
 <!--/ карточка товара -->
 
+<!-- попап одноклика -->
+<div class="popup popup_540" style="display: block;">
+    <div class="popup__close js-popup-close">×</div>
+
+    <div class="popup__title">Оформить заказ</div>
+
+    <form class="one-click">
+        <div class="one-click-product table">
+            <div class="one-click-product__img table-cell"><img src="http://5.imgenter.ru/uploads/media/d3/4a/6e/thumb_c1de_product_550.jpeg" alt="" class="image"></div>
+
+            <div class="one-click-product__desc table-cell">
+                <div class="one-click-product__name">Купольная вытяжка Hotpoint-Ariston 7HHP 6 R (OW)/HA</div>
+                <div class="one-click-product__price">649 р</div>
+            </div>
+        </div>
+        <button class="btn-primary btn-primary_bigger btn-primary_width btn-primary_centred">Оформить заказ</button>
+    </form>
+</div>
+<!--/ попап одноклика -->
+
 <div class="popup popup_set" style="display: block;">
     <div class="popup__close js-popup-close">×</div>
 
