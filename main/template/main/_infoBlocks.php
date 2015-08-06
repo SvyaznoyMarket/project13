2cc11c4c9d4a11f4259477617da4ba062253cee1
frontgirl <web@mow-01-ic237.enter.lan>
2015-08-06 11:52:30 +0300

    SITE-5942 текст в трастфакторе самовывоз

--- main/template/main/_infoBlocks.php
+++ main/template/main/_infoBlocks.php
@@ -49,7 +49,7 @@
         </li>
     </ul>
 
-    <a href="/dostavka" target="_blank" class="shopInfoTab_btn">Подробнее об условиях, стоимости, сроках и интервалах доставки</a>
+    <a href="/dostavka" target="_blank" class="shopInfoTab_btn">Подробнее об условиях самовывоза</a>
 </div>
 
 <div class="shopInfoTab shopInfoTab-v2 jsShopInfoBlock" data-id="2">
