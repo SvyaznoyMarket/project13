09c4a411375ac655e7a6861d21bf7574d9a187c5
tamili <ta.burlakova@gmail.com>
2015-08-12 15:16:35 +0300

    LITE-? fix

--- lite/template/common/_header.php
+++ lite/template/common/_header.php
@@ -35,7 +35,7 @@
             </div>
 
             <div class="header__line header__line_bottom">
-                <div class="nav-section show header__line-left">
+                <div class="nav-section header__line-left">
                     <a href="" class="nav-section__btn btn-primary">Каталог товаров</a>
                     <?= $page->blockNavigation() ?>
                 </div>
