2c44dd3a832b76b883ab6064b9a0963d3b47af05
tamili <ta.burlakova@gmail.com>
2015-08-13 17:08:21 +0300

    LITE-? popup 3D and video

--- lite/template/product/content.php
+++ lite/template/product/content.php
@@ -233,34 +233,32 @@ $reviewsData = null;
 </div>
 <!--/ карточка товара -->
 
+<!-- просмотр большого изображения товара -->
+<div class="popup popup-photo" style="display: none;">
+    <div class="popup__close">×</div>
 
-<div class="popup popup-photo jsProductImgPopup" style="display: none;">
-    <div class="popup__close jsPopupCloser">×</div>
-
-    <div class="product-card-photo jsProductPopupBigPhotoHolder">
-        <!-- <div class="product-card-photo__img" style="height: 620px; margin: 0 auto; overflow: hidden; width: 785px"> -->
-        <img src="http://c.imgenter.ru/uploads/media/7b/a4/d5/thumb_be1c_product_1500.jpeg" class="product-card-photo__img fixed jsProductPopupBigPhoto" style="max-height: 100%; max-width: 100%; top: 0px; left: 0px;">
-        <!-- </div> -->
+    <div class="product-card-photo">
+        <img src="http://c.imgenter.ru/uploads/media/7b/a4/d5/thumb_be1c_product_1500.jpeg" class="product-card-photo__img fixed" style="max-height: 100%; max-width: 100%; top: 0px; left: 0px;">
     </div>
 
-    <div class="product-card-photo__ctrl product-card-photo__ctrl--prev jsProductPopupSlide" data-dir="-1"><span class="symb"></span></div>
-    <div class="product-card-photo__ctrl product-card-photo__ctrl--next jsProductPopupSlide" data-dir="1"><span class="symb"></span></div>
+    <div class="product-card-photo__ctrl product-card-photo__ctrl--prev"><span class="symb"></span></div>
+    <div class="product-card-photo__ctrl product-card-photo__ctrl--next"><span class="symb"></span></div>
 
     <div class="product-card-photo-zoom">
-        <div class="product-card-photo-zoom__ctrl product-card-photo-zoom__ctrl--out jsProductPopupZoom jsProductPopupZoomOut disabled" data-dir="-1">–</div>
-        <div class="product-card-photo-zoom__ctrl product-card-photo-zoom__ctrl--in jsProductPopupZoom jsProductPopupZoomIn" data-dir="+1">+</div>
+        <div class="product-card-photo-zoom__ctrl product-card-photo-zoom__ctrl--out disabled">–</div>
+        <div class="product-card-photo-zoom__ctrl product-card-photo-zoom__ctrl--in">+</div>
     </div>
 
     <div class="product-card-photo-thumbs">
         <ul class="product-card-photo-thumbs-list">
-            <li class="product-card-photo-thumbs__i jsPopupPhotoThumb active" data-big-img="http://c.imgenter.ru/uploads/media/7b/a4/d5/thumb_be1c_product_1500.jpeg"><img src="http://3.imgenter.ru/uploads/media/7b/a4/d5/thumb_be1c_product_120.jpeg" class="product-card-photo-thumbs__img"></li>
-            <li class="product-card-photo-thumbs__i jsPopupPhotoThumb" data-big-img="http://e.imgenter.ru/uploads/media/f5/5a/b1/thumb_b437_product_1500.jpeg"><img src="http://7.imgenter.ru/uploads/media/f5/5a/b1/thumb_b437_product_120.jpeg" class="product-card-photo-thumbs__img"></li>
-            <li class="product-card-photo-thumbs__i jsPopupPhotoThumb" data-big-img="http://1.imgenter.ru/uploads/media/4a/1a/33/thumb_59d6_product_1500.jpeg"><img src="http://0.imgenter.ru/uploads/media/4a/1a/33/thumb_59d6_product_120.jpeg" class="product-card-photo-thumbs__img"></li>
-            <li class="product-card-photo-thumbs__i jsPopupPhotoThumb" data-big-img="http://8.imgenter.ru/uploads/media/8a/98/27/thumb_4f65_product_1500.jpeg"><img src="http://b.imgenter.ru/uploads/media/8a/98/27/thumb_4f65_product_120.jpeg" class="product-card-photo-thumbs__img"></li>
-            <li class="product-card-photo-thumbs__i jsPopupPhotoThumb" data-big-img="http://8.imgenter.ru/uploads/media/04/7e/39/thumb_c035_product_1500.jpeg"><img src="http://4.imgenter.ru/uploads/media/04/7e/39/thumb_c035_product_120.jpeg" class="product-card-photo-thumbs__img"></li>
-            <li class="product-card-photo-thumbs__i jsPopupPhotoThumb" data-big-img="http://c.imgenter.ru/uploads/media/a5/cb/16/thumb_77f4_product_1500.jpeg"><img src="http://4.imgenter.ru/uploads/media/a5/cb/16/thumb_77f4_product_120.jpeg" class="product-card-photo-thumbs__img"></li>
-            <li class="product-card-photo-thumbs__i jsPopupPhotoThumb" data-big-img="http://1.imgenter.ru/uploads/media/af/0e/83/thumb_1512_product_1500.jpeg"><img src="http://9.imgenter.ru/uploads/media/af/0e/83/thumb_1512_product_120.jpeg" class="product-card-photo-thumbs__img"></li>
-            <li class="product-card-photo-thumbs__i jsPopupPhotoThumb" data-big-img="http://3.imgenter.ru/uploads/media/c8/f7/e4/thumb_c0e4_product_1500.jpeg"><img src="http://b.imgenter.ru/uploads/media/c8/f7/e4/thumb_c0e4_product_120.jpeg" class="product-card-photo-thumbs__img"></li>
+            <li class="product-card-photo-thumbs__i active"><img src="http://3.imgenter.ru/uploads/media/7b/a4/d5/thumb_be1c_product_120.jpeg" class="product-card-photo-thumbs__img"></li>
+            <li class="product-card-photo-thumbs__i"><img src="http://7.imgenter.ru/uploads/media/f5/5a/b1/thumb_b437_product_120.jpeg" class="product-card-photo-thumbs__img"></li>
+            <li class="product-card-photo-thumbs__i"><img src="http://0.imgenter.ru/uploads/media/4a/1a/33/thumb_59d6_product_120.jpeg" class="product-card-photo-thumbs__img"></li>
+            <li class="product-card-photo-thumbs__i"><img src="http://b.imgenter.ru/uploads/media/8a/98/27/thumb_4f65_product_120.jpeg" class="product-card-photo-thumbs__img"></li>
+            <li class="product-card-photo-thumbs__i"><img src="http://4.imgenter.ru/uploads/media/04/7e/39/thumb_c035_product_120.jpeg" class="product-card-photo-thumbs__img"></li>
+            <li class="product-card-photo-thumbs__i"><img src="http://4.imgenter.ru/uploads/media/a5/cb/16/thumb_77f4_product_120.jpeg" class="product-card-photo-thumbs__img"></li>
+            <li class="product-card-photo-thumbs__i"><img src="http://9.imgenter.ru/uploads/media/af/0e/83/thumb_1512_product_120.jpeg" class="product-card-photo-thumbs__img"></li>
+            <li class="product-card-photo-thumbs__i"><img src="http://b.imgenter.ru/uploads/media/c8/f7/e4/thumb_c0e4_product_120.jpeg" class="product-card-photo-thumbs__img"></li>
         </ul>
 
         <div class="product-card-photo-thumbs__btn product-card-photo-thumbs__btn--l product-card-photo-thumbs__btn--disabled"></div>
@@ -268,5 +266,12 @@ $reviewsData = null;
     </div>
 
     <div class="popup-photo__price">121 649 <span class="rubl">P</span></div>
-    <a class="btn-primary btn-primary_normal" id="buyButton-211933-bc9a45c6c3fb7926527ed41a3cb0dc69" href="/cart/add-product/211933">Купить</a>
+    <a class="btn-primary btn-primary_normal" href="">Купить</a>
 </div>
+<!--/ просмотр большого изображения товара -->
+
+<!-- просмотр видео и 3D -->
+<div class="popup popup_710 popup_nopadding" style="display: none;">
+    <div class="popup__close">×</div>
+</div>
+<!--/ просмотр видео и 3D -->
