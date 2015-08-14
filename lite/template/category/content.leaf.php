1811328d651ac4b90a75a678e46fc2d87d1b64a1
tamili <ta.burlakova@gmail.com>
2015-08-14 15:56:41 +0300

    LITE-49

--- lite/template/category/content.leaf.php
+++ lite/template/category/content.leaf.php
@@ -42,7 +42,7 @@ $promoStyle = 'jewel' === $listingStyle && isset($catalogJson['promo_style']) ? 
             <? endif ?>
 
             <? if (isset($slice)) : ?>
-                <div class="section_title section_title_h1"><?= $slice->getName() ?></div>
+                <div class="section__title section__title_h1"><?= $slice->getName() ?></div>
             <? endif ?>
 
             <?= $page->blockCategories() ?>
