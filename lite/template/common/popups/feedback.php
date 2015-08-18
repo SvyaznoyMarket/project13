2855cbf76597587865a1f1fa76765ed28c2448fc
Zhukov Roman <roman.zhukov@enter.ru>
2015-08-18 16:15:02 +0300

    LITE-? Перенос шаблона обраной связи

---
+++ lite/template/common/popups/feedback.php
@@ -0,0 +1,29 @@
+<div class="popup-feedback popup popup_440 js-feedback-popup" style="display: none;">
+    <div class="popup__close js-popup-close">×</div>
+    <div class="popup__title">Обратная связь</div>
+
+    <form action="" class="form">
+        <div class="form__field">
+            <input id="" type="text" class="form__it it js-feedback-email" name="" value="">
+            <label for="" class="form__placeholder placeholder">Ваш email</label>
+        </div>
+
+        <div class="form__field">
+            <input id="" type="text" class="form__it it js-feedback-topic" name="" value="">
+            <label for="" class="form__placeholder placeholder">Тема письма</label>
+        </div>
+
+        <div class="form__field">
+            <textarea name="" id="" class="textarea js-feedback-text"></textarea>
+            <label for="" class="form__placeholder placeholder">Текст письма</label>
+        </div>
+
+        <div class="form__field">
+            <input type="file">
+        </div>
+
+        <div class="btn-container">
+            <button class="btn-primary btn-primary_bigger js-feedback-submit">Отправить</button>
+        </div>
+    </form>
+</div>
