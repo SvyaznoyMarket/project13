b7785dd7c77341eedfadfb87314ba6ca8e036539
tamili <ta.burlakova@gmail.com>
2015-08-19 16:42:08 +0300

    LITE-? add complete feedback status

--- lite/template/common/popups/feedback.php
+++ lite/template/common/popups/feedback.php
@@ -1,9 +1,8 @@
 <div class="popup-feedback popup popup_440 js-feedback-popup" style="display: none;">
     <div class="popup__close js-popup-close">×</div>
+    <div class="popup__title">Обратная связь</div>
 
     <form action="<?= $page->url('feedback.send') ?>" class="feedback-form form" method="post">
-        <div class="popup__title">Обратная связь</div>
-
         <div class="form__field">
             <input id="" type="text" class="form__it it js-feedback-email" data-required="true" name="email" value="">
             <label for="" class="form__placeholder placeholder placeholder_str">Ваш email</label>
@@ -29,8 +28,8 @@
     </form>
 
     <div class="feedback-complete">
-        <div class="popup__title btn-container">Спасибо за ваше обращение!</div>
+        <div class="feedback-complete__text"><span class="mark">Спасибо!</span></br>Мы обязательно рассмотрим ваше обращение.</div>
 
-        <div class="btn-container"><a href="" class="btn-primary btn-primary_bigger js-popup-close">OK</a></div>
+        <div class="btn-container"><a href="" class="btn-normal btn-normal_middle js-popup-close">Продолжить</a></div>
     </div>
 </div>
