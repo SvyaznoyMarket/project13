252954766c915a29d960833554c199eeaa2cb433
tamili <ta.burlakova@gmail.com>
2015-08-19 16:24:40 +0300

    LITE-? add complete feedback status

--- lite/template/common/popups/feedback.php
+++ lite/template/common/popups/feedback.php
@@ -1,21 +1,22 @@
 <div class="popup-feedback popup popup_440 js-feedback-popup" style="display: none;">
     <div class="popup__close js-popup-close">×</div>
-    <div class="popup__title">Обратная связь</div>
 
-    <form action="<?= $page->url('feedback.send') ?>" class="form" method="post">
+    <form action="<?= $page->url('feedback.send') ?>" class="feedback-form form" method="post">
+        <div class="popup__title">Обратная связь</div>
+
         <div class="form__field">
-            <input id="" type="text" class="form__it it js-feedback-email" name="email" value="">
-            <label for="" class="form__placeholder placeholder">Ваш email</label>
+            <input id="" type="text" class="form__it it js-feedback-email" data-required="true" name="email" value="">
+            <label for="" class="form__placeholder placeholder placeholder_str">Ваш email</label>
         </div>
 
         <div class="form__field">
-            <input id="" type="text" class="form__it it js-feedback-topic" name="subject" value="">
-            <label for="" class="form__placeholder placeholder">Тема письма</label>
+            <input id="" type="text" class="form__it it js-feedback-topic" data-required="true" name="subject" value="">
+            <label for="" class="form__placeholder placeholder placeholder_str">Тема письма</label>
         </div>
 
         <div class="form__field">
-            <textarea name="message" id="" class="textarea js-feedback-text"></textarea>
-            <label for="" class="form__placeholder placeholder">Текст письма</label>
+            <textarea name="message" id="" data-required="true" class="textarea js-feedback-text"></textarea>
+            <label for="" class="form__placeholder placeholder placeholder_str">Текст письма</label>
         </div>
 
         <div class="form__field">
@@ -26,4 +27,10 @@
             <button class="btn-primary btn-primary_bigger js-feedback-submit">Отправить</button>
         </div>
     </form>
+
+    <div class="feedback-complete">
+        <div class="popup__title btn-container">Спасибо за ваше обращение!</div>
+
+        <div class="btn-container"><a href="" class="btn-primary btn-primary_bigger js-popup-close">OK</a></div>
+    </div>
 </div>
