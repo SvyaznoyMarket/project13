9722a92293a2d06455e9b61e9a0aefbe572555de
tamili <ta.burlakova@gmail.com>
2015-08-20 12:23:54 +0300

    LITE-? order oader

--- frontend/js/modules/order/view/enter.order.step2.view.js
+++ frontend/js/modules/order/view/enter.order.step2.view.js
@@ -35,6 +35,7 @@
              */
             CSS_CLASSES = {
                 SUB_ORDER: 'js-order-block',
+                CHANGE_REGION_BTN: 'js-change-region',
                 COMMENT_BTN: 'js-order-comment',
                 COMMENT_AREA: 'js-order-comment-text',
                 SUBMIT_ORDER: 'js-order-submit',
@@ -74,6 +75,7 @@
                 });
 
                 // Setup events
+                this.events['click .' + CSS_CLASSES.CHANGE_REGION_BTN] = 'changeRegion';
                 this.events['click .' + CSS_CLASSES.COMMENT_BTN]       = 'toggleCommentArea';
                 this.events['click .' + CSS_CLASSES.SUBMIT_ORDER]      = 'submitOrder';
 
@@ -179,6 +181,20 @@
             },
 
             /**
+             * Обработчик смены региона
+             *
+             * @method      changeRegion
+             * @memberOf    module:enter.order.step2.view~OrderStep2View#
+             *
+             * @todo        написать обработчик
+             */
+            changeRegion: function() {
+                console.info('module:enter.order.step2.view~OrderStep2View#changeRegion');
+
+                return false;
+            },
+
+            /**
              * Отрисовка оформления заказа
              *
              * @method      render
@@ -208,7 +224,7 @@
                     }
                 }
 
-                this.$el.append(html);
+                this.$el.prepend(html);
 
                 this.initialize();
             }
