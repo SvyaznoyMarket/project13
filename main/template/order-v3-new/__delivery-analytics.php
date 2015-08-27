90258a4b76859b91d6871538b9e7a537ed5beecd
g84 <green840429@gmail.com>
2015-08-27 12:17:34 +0300

    SITE-6018 исправление для аналитики: критерий для no_options

--- main/template/order-v3-new/__delivery-analytics.php
+++ main/template/order-v3-new/__delivery-analytics.php
@@ -8,11 +8,14 @@ return function(
     try {
         // SITE-6016
         $testKey = \App::abTest()->getTest('order_delivery_type')->getChosenCase()->getKey();
+        /** @var \Model\OrderDelivery\Entity\Order|null $order */
+        $order = reset($orderDelivery->orders) ?: null;
         if (
             in_array(\App::user()->getRegion()->parentId, [76, 90])  // Воронеж, Ярославль
             && ('default' !== $testKey)
+            && $order
         ) {
-            if (1 === count($orderDelivery->orders)) {
+            if (1 === count($order->possible_delivery_groups)) {
                 $data[] = [
                     'category' => 'delivery_option',
                     'action'   => 'no_options',
