b947dbb0139f2c30d7f02f46d9d24275320d0ab5
Aleksandr Zaytsev <13rentgen@gmail.com>
2015-09-02 14:27:06 +0300

    LITE-? Order first step - bug fix

--- lite/template/order/page-new.php
+++ lite/template/order/page-new.php
@@ -19,7 +19,6 @@
                                data-mask="+7 (999) 999-99-99"
                                title="Введите номер телефона"
                                data-required="true"
-                               autofocus
                             >
                         <label class="form-order-login__label placeholder placeholder_str" for="order-phone-input">Телефон</label>
 <!--                        <span class="form-order-login__hint">Для смс о состоянии заказа</span>-->
