1f5535cfb360de0bb7e3b957ff436a7cef620fc2
Aleksandr Zaytsev <13rentgen@gmail.com>
2015-09-08 14:01:46 +0300

    LITE-224

--- lite/template/order/page-new.php
+++ lite/template/order/page-new.php
@@ -19,6 +19,7 @@
                                data-mask="+7 (999) 999-99-99"
                                title="Введите номер телефона"
                                data-required="true"
+                               placeholder="+7 (___) ___-__-__"
                             >
                         <label class="form-order-login__label placeholder placeholder_str" for="order-phone-input">Телефон</label>
 <!--                        <span class="form-order-login__hint">Для смс о состоянии заказа</span>-->
