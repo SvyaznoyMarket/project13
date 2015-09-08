8929d222d87a20e2abcba926aeb9f0393ed61885
g84 <green.840429@gmail.com>
2015-09-08 17:25:14 +0300

    SITE-5653 Корзина в оформлении заказа: cart

--- main/template/cart/page-cart.php
+++ main/template/cart/page-cart.php
@@ -1,9 +1,8 @@
 <?php
 /**
- * @var $page     Templating\HtmlLayout
- * @var $user     \Session\User
- * @var $orderUrl string
-*/
+ * @var $page Templating\HtmlLayout
+ * @var $user \Session\User
+ */
 
 $cart = $user->getCart();
 $helper = new \Helper\TemplateHelper();
@@ -15,95 +14,45 @@ $isNewProductPage = \App::abTest()->isNewProductPage();
     <?= $page->render('cart/_spinner') ?>
 </div>
 */ ?>
-<div class="order__wrap order-page">
-    <div class="pagehead">
-        <h1>Корзина</h1>
-        <div class="clear"></div>
-    </div>
-    <div class="jsKnockoutCart order-cart" data-bind="visible: cart().sum() == 0" style="display: none">
+
+    <div class="jsKnockoutCart" data-bind="visible: cart().sum() == 0" style="display: none">
         <?= $page->render('cart/_cart-empty') ?>
     </div>
 
-    <div class="jsKnockoutCart order-cart" data-bind="visible: cart().sum() > 0" style="display: none">
+    <div class="jsKnockoutCart" data-bind="visible: cart().sum() > 0" style="display: none">
 
-        <?//= $page->render('cart/partner/_adfox') ?>
+        <?= $page->render('cart/partner/_adfox') ?>
 
         <!-- ko foreach: cart().products() -->
-            <?= //$page->render('cart/_cart-item')
-                $page->render('cart/_cart-item-new')
-            ?>
+        <?= $page->render('cart/_cart-item') ?>
         <!-- /ko -->
 
-        <!--<div class="basketLine clearfix">-->
+        <div class="basketLine clearfix">
 
             <?= $page->render('cart/ab-self-delivery/_infoblock', ['cart' => $cart]) ?>
 
-            <?= $page->render('cart/_cart-total-new') ?>
+            <?= $page->render('cart/_cart-total') ?>
 
-        <!--</div>-->
+        </div>
 
     </div>
 
-    <!--<div class="backShop fl mNoPrint jsKnockoutCart" data-bind="visible: cart().sum() > 0" style="display: none">&lt; <a class="underline" href="<?/*= $backlink */?>">Вернуться к покупкам</a></div>-->
+    <div class="backShop fl mNoPrint jsKnockoutCart" data-bind="visible: cart().sum() > 0" style="display: none">&lt; <a class="underline" href="<?= $backlink ?>">Вернуться к покупкам</a></div>
 
+    <div class="basketBuy mNoPrint jsKnockoutCart" data-bind="visible: cart().sum() > 0" style="display: none">
+        <a href="<?= $page->url('order') ?>" class="bBigOrangeButton" data-bind="visible: !isMinOrderSumVisible()">Оформить заказ</a>
+    </div>
 
-        <div class="order-cart__btn mNoPrint jsKnockoutCart" data-bind="visible: cart().sum() > 0" style="display: none">
-            <a href="<?= (@$orderUrl ?: $page->url('order')) ?>" class="btn-type btn-type--buy btn-type--order" data-bind="visible: !isMinOrderSumVisible()">Оформить заказ</a>
-        </div>
-        <div class="order-cart__back mNoPrint jsKnockoutCart" data-bind="visible: cart().sum() > 0" style="display: none"><a href="<?= $backlink ?>"><span class="order-cart__back-txt">Вернуться к покупкам</span></a></div>
-        <div class="cart-alert jsKnockoutCart" data-bind="visible: isMinOrderSumVisible()" style="display: none;">
-            <span class="cart-alert__info">До оформления заказа осталось</span>
-            <span class="cart-alert__remain-sum"><span data-bind="text: minOrderSum - cart().sum()"><?= \App::config()->minOrderSum ?></span>&thinsp;<span class="rubl">p</span></span>
-        </div>
+    <div class="cart-alert jsKnockoutCart" data-bind="visible: isMinOrderSumVisible()" style="display: none;">
+        <span class="cart-alert__info">До оформления заказа осталось</span>
+        <span class="cart-alert__remain-sum"><span data-bind="text: minOrderSum - cart().sum()"><?= \App::config()->minOrderSum ?></span>&thinsp;<span class="rubl">p</span></span>
+    </div>
 
     <div class="clear"></div>
 
-    <? if (!$cart->count()): // жуткий костыль SITE-5289 ?>
-        <div id="js-cart-firstRecommendation" style="display: none;">
-            <? $page->startEscape()?>
-            <div class="basketLine">
-                <?= $helper->render($isNewProductPage ? 'product-page/blocks/slider' : 'product/__slider', [
-                    'type'      => 'alsoBought',
-                    'products'  => [],
-                    'url'       => $page->url('cart.recommended', [
-                        'sender' => [
-                            'position' => 'Basket',
-                        ],
-                    ]),
-                ]) ?>
-            </div>
-            <? $page->endEscape() ?>
-        </div>
-
-        <div class="basketLine">
-        <?= $helper->render($isNewProductPage ? 'product-page/blocks/slider' : 'product/__slider', [
-            'type'      => 'main',
-            'products'  => [],
-            'url'       => $page->url('cart.recommended', [
-                'sender' => [
-                    'position' => 'Basket',
-                ],
-            ]),
-        ]) ?>
-        </div>
-        <div class="basketLine">
-        <?= $helper->render($isNewProductPage ? 'product-page/blocks/slider' : 'product/__slider', [
-            'type'      => 'alsoBought',
-            'products'  => [],
-            'url'       => $page->url('cart.recommended', [
-                'sender' => [
-                    'position' => 'Basket',
-                ],
-            ]),
-        ]) ?>
-        </div>
-        <div class="cart--ep"><a href="/enterprize" title=""><img src="/css/bEmptyCart/img/ep.jpg" alt="" /></a></div>
-    <? endif ?>
-
-        <div class="clear"></div>
-
-
-    <? if (\App::config()->product['pullRecommendation'] && $cart->count()): ?>
+<? if (!$cart->count()): // жуткий костыль SITE-5289 ?>
+    <div id="js-cart-firstRecommendation" style="display: none;">
+        <? $page->startEscape()?>
         <div class="basketLine">
             <?= $helper->render($isNewProductPage ? 'product-page/blocks/slider' : 'product/__slider', [
                 'type'      => 'alsoBought',
@@ -115,9 +64,52 @@ $isNewProductPage = \App::abTest()->isNewProductPage();
                 ]),
             ]) ?>
         </div>
-    <? endif ?>
-</div>
-<div class="clear"></div>
+        <? $page->endEscape() ?>
+    </div>
+
+    <div class="basketLine">
+        <?= $helper->render($isNewProductPage ? 'product-page/blocks/slider' : 'product/__slider', [
+            'type'      => 'main',
+            'products'  => [],
+            'url'       => $page->url('cart.recommended', [
+                'sender' => [
+                    'position' => 'Basket',
+                ],
+            ]),
+        ]) ?>
+    </div>
+    <div class="basketLine">
+        <?= $helper->render($isNewProductPage ? 'product-page/blocks/slider' : 'product/__slider', [
+            'type'      => 'alsoBought',
+            'products'  => [],
+            'url'       => $page->url('cart.recommended', [
+                'sender' => [
+                    'position' => 'Basket',
+                ],
+            ]),
+        ]) ?>
+    </div>
+    <div class="cart--ep"><a href="/enterprize" title=""><img src="/css/bEmptyCart/img/ep.jpg" alt="" /></a></div>
+<? endif ?>
+
+    <div class="clear"></div>
+
+
+<? if (\App::config()->product['pullRecommendation'] && $cart->count()): ?>
+    <div class="basketLine">
+        <?= $helper->render($isNewProductPage ? 'product-page/blocks/slider' : 'product/__slider', [
+            'type'      => 'alsoBought',
+            'products'  => [],
+            'url'       => $page->url('cart.recommended', [
+                'sender' => [
+                    'position' => 'Basket',
+                ],
+            ]),
+        ]) ?>
+    </div>
+<? endif ?>
+
+    <div class="clear"></div>
 
 <? if (\App::config()->analytics['enabled']): ?>
     <?= $page->tryRender('cart/partner/_cityads') ?>
