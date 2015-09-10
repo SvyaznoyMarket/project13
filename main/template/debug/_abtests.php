ec82c4fb8d04e3bd17ed64980cfc76311d433409
Mihail Haritonov <mihail.haritonov@enter.ru>
2015-09-10 12:47:38 +0300

    SITE-? Поправил вёрстку на /switch

--- main/template/debug/_abtests.php
+++ main/template/debug/_abtests.php
@@ -3,29 +3,31 @@
  * @var $tests \Session\AbTest\Test[]|null
  */
 ?>
-<h2>Переключение АБ-тестов</h2>
-<ul style="margin-bottom: 60px">
-    <? foreach ($tests as $test) : ?>
+<div style="padding-left: 20px;">
+    <h2>Переключение АБ-тестов</h2>
+    <ul style="margin-bottom: 60px">
+        <? foreach ($tests as $test) : ?>
 
-    <li><span style="color: <?= $test->isActive() ? 'green' : 'red' ?>"><?= $test->name ?> (<?= $test->getKey() ?>)</span> <span style="color: gray;">(до <?= $test->getExpireDate() ?>)</span>
-        <? if ($test->gaSlotNumber) : ?>
-            <span style="color: #00ADE0">GA: slot <?= $test->gaSlotNumber ?>, scope <?= $test->gaSlotScope ?></span>
-        <? endif; ?>
-        <ul style="list-style-type: none; padding: 5px 0 10px 0;">
-        <? foreach ($test->getCases() as $case) : ?>
-            <li>
-                <label>
-                    <input type="radio" class="changeCase" name="<?= $test->getKey() ?>" value="<?= $case->getKey() ?>" <?= $test->getChosenCase() == $case ? 'checked' : '' ?> <?= !$test->isActive() ? 'disabled' : '' ?> />
-                    <?= $case->getName() ?> <span style="color: gray;"><?= $case->getTraffic() ?>%</span>
-                </label>
-                <span class="gaValue" style="color: #AAA; border: 1px solid #AAA; border-radius: 2px; font-size: 12px; padding: 0 2px; cursor: pointer" title="<?= $test->getKey().'_'.$case->getKey() ?>">GA</span>
-            </li>
+        <li><span style="color: <?= $test->isActive() ? 'green' : 'red' ?>"><?= $test->name ?> (<?= $test->getKey() ?>)</span> <span style="color: gray;">(до <?= $test->getExpireDate() ?>)</span>
+            <? if ($test->gaSlotNumber) : ?>
+                <span style="color: #00ADE0">GA: slot <?= $test->gaSlotNumber ?>, scope <?= $test->gaSlotScope ?></span>
+            <? endif; ?>
+            <ul style="list-style-type: none; padding: 5px 0 10px 0;">
+            <? foreach ($test->getCases() as $case) : ?>
+                <li>
+                    <label>
+                        <input type="radio" class="changeCase" name="<?= $test->getKey() ?>" value="<?= $case->getKey() ?>" <?= $test->getChosenCase() == $case ? 'checked' : '' ?> <?= !$test->isActive() ? 'disabled' : '' ?> />
+                        <?= $case->getName() ?> <span style="color: gray;"><?= $case->getTraffic() ?>%</span>
+                    </label>
+                    <span class="gaValue" style="color: #AAA; border: 1px solid #AAA; border-radius: 2px; font-size: 12px; padding: 0 2px; cursor: pointer" title="<?= $test->getKey().'_'.$case->getKey() ?>">GA</span>
+                </li>
+            <? endforeach ?>
+            </ul>
+        </li>
+
         <? endforeach ?>
-        </ul>
-    </li>
-
-    <? endforeach ?>
-</ul>
+    </ul>
+</div>
 
 <script type="text/javascript">
 
