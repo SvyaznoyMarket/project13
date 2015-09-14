92658f1dcc73307f6bfc85babd97ea548c511072
Zhukov Roman <roman.zhukov@enter.ru>
2015-09-14 15:37:39 +0300

    LITE-143 ОЗ Шаг 2 - пустая страница при смене региона в ОЗ

--- lite/template/order/_error.delivery.php
+++ lite/template/order/_error.delivery.php
@@ -11,6 +11,9 @@ return function(
     <section class="checkout">
         <h1 class="checkout__title">Ошибка</h1>
         <?= $error ?>
+        <h2><a href="/">Вернуться к покупкам</a></h2>
     </section>
+
+
 
 <? } ?>
