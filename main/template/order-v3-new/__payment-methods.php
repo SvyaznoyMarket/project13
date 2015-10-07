17c7c964de3cbc99cadcffb583e6b2d6eaf5acab
g84 <green.840429@gmail.com>
2015-10-07 12:28:05 +0300

    SITE-4744 Группировка методов оплат

--- main/template/order-v3-new/__payment-methods.php
+++ main/template/order-v3-new/__payment-methods.php
@@ -17,7 +17,11 @@ $f = function (
     $paymentMethodsByDiscount = [];
     foreach ($order->possible_payment_methods as $paymentMethod) {
         $index = in_array($paymentMethod->id, \App::config()->payment['discountIds']) ? 0 : 1;
-        $paymentMethodsByDiscount[$index][] = $paymentMethod;
+        if (in_array($paymentMethod->id, ['1', '2'])) {
+            $paymentMethodsByDiscount[$index]['grouped'][] = $paymentMethod;
+        } else {
+            $paymentMethodsByDiscount[$index][][] = $paymentMethod;
+        }
     }
     ksort($paymentMethodsByDiscount);
 ?>
@@ -31,13 +35,38 @@ $f = function (
         </div>
 
         <ul class="payment-methods__lst">
-        <? foreach ($paymentMethodsByDiscount as $paymentMethods): ?>
-            <? foreach ($paymentMethods as $paymentMethod): ?>
+        <? foreach ($paymentMethodsByDiscount as $paymentMethodChunk): ?>
+            <? foreach ($paymentMethodChunk as $groupIndex => $paymentMethods): ?>
             <?
-                $elementId = sprintf('paymentMethod-%s', $paymentMethod->id);
-                $checked = $order->payment_method_id == $paymentMethod->id;
+                $elementId = sprintf('paymentMethod-%s', $groupIndex);
+                $checked = in_array($order->payment_method_id, array_keys($paymentMethods));
             ?>
                 <li class="payment-methods__i">
+                <? if (count($paymentMethods) > 1): ?>
+                        <input
+                            id="<?= $elementId ?>"
+                            type="radio"
+                            name="payment-type[]"
+                            value=""
+                            class="customInput customInput-defradio2 js-customInput"
+                            <?= $checked ? 'checked' : '' ?>
+                        />
+                        <label for="<?= $elementId ?>" class="customLabel customLabel-defradio2 <?= $checked ? 'mChecked' : '' ?>"></label>
+                        <select class="customSel-inner">
+                        <? foreach ($paymentMethods as $paymentMethod): ?>
+                        <?
+                            $checked = $order->payment_method_id == $paymentMethod->id;
+                        ?>
+                            <option value="<?= $paymentMethod->id ?>" <?= $checked ? 'selected' : '' ?>><?= $paymentMethod->name ?></option>
+                        <? endforeach ?>
+                        </select>
+                <? else: ?>
+                <?
+                    $paymentMethod = reset($paymentMethods);
+
+                    $elementId = sprintf('paymentMethod-%s', $paymentMethod->id);
+                    $checked = $order->payment_method_id == $paymentMethod->id;
+                ?>
                     <input
                         id="<?= $elementId ?>"
                         type="radio"
@@ -57,6 +86,7 @@ $f = function (
                             <img class="payment-methods__img" src="<?= $image ?>">
                         <? endif ?>
                     </label>
+                <? endif ?>
                 </li>
             <? endforeach ?>
             <li class="payment-methods__i"><br /></li>
