4685612d98e58dfa60f20279b32c98825bc19e8b
tamili <ta.burlakova@gmail.com>
2015-10-07 14:32:48 +0300

    SITE-6204 sales grid

--- main/template/closed-sale/sale-index.php
+++ main/template/closed-sale/sale-index.php
@@ -1,1 +1,387 @@
-CLOSED SALE INDEX TEMPLATE
+<section class="s-sales">
+	<h1 class="s-sales__title">Secret Sale</h1>
+
+	<div class="s-sales-head">
+		<h2 class="s-sales-head__title">Новые акции</h2>
+		<div class="s-sales-head__counter">1 день 15:04:02</div>
+	</div>
+
+	<!-- Сетка скидочных категорий -->
+	<div class="s-sales-grid">
+		<!--
+			Строка в две колонки, широкая колонка слева.
+			Модификатор grid-float grid-float-left
+		 -->
+		<div class="s-sales-grid__row grid-float grid-float-left">
+			<div class="s-sales-grid__col">
+				<div class="s-sales-grid__cell">
+					<a class="s-sales-grid__link" href="">
+						<img src="http://img0.joyreactor.cc/pics/post/full/%D0%BA%D0%BE%D1%82%D0%B8%D0%BA%D0%B8-%D0%B6%D0%B8%D0%B2%D0%BD%D0%BE%D1%81%D1%82%D1%8C-wallpaper-%D0%B1%D0%B0%D1%8F%D0%BD-885065.jpeg" alt="" class="s-sales-grid__img">
+
+						<span class="s-sales-grid-desc">
+							<span class="s-sales-grid-desc__value">-70%</span>
+							<span class="s-sales-grid-desc__title">
+								<span class="s-sales-grid-desc__title-name">Очень длинный текст о милых и пушитых котаках</span>
+								<span class="s-sales-grid-desc__title-duration">Конец акции 22.09.2015 00:00</span>
+							</span>
+						</span>
+					</a>
+				</div>
+			</div>
+
+			<div class="s-sales-grid__col">
+				<div class="s-sales-grid__cell">
+					<a class="s-sales-grid__link" href="">
+						<img src="http://img0.joyreactor.cc/pics/post/full/%D0%BA%D0%BE%D1%82%D0%B8%D0%BA%D0%B8-%D0%B6%D0%B8%D0%B2%D0%BD%D0%BE%D1%81%D1%82%D1%8C-wallpaper-%D0%B1%D0%B0%D1%8F%D0%BD-885065.jpeg" alt="" class="s-sales-grid__img">
+
+						<span class="s-sales-grid-desc">
+							<span class="s-sales-grid-desc__value">-70%</span>
+							<span class="s-sales-grid-desc__title">
+								<span class="s-sales-grid-desc__title-name">Путешествия</span>
+								<span class="s-sales-grid-desc__title-duration">Конец акции 22.09.2015 00:00</span>
+							</span>
+						</span>
+					</a>
+				</div>
+
+				<div class="s-sales-grid__cell">
+					<a class="s-sales-grid__link" href="">
+						<img src="http://img0.joyreactor.cc/pics/post/full/%D0%BA%D0%BE%D1%82%D0%B8%D0%BA%D0%B8-%D0%B6%D0%B8%D0%B2%D0%BD%D0%BE%D1%81%D1%82%D1%8C-wallpaper-%D0%B1%D0%B0%D1%8F%D0%BD-885065.jpeg" alt="" class="s-sales-grid__img">
+
+						<span class="s-sales-grid-desc">
+							<span class="s-sales-grid-desc__value">-70%</span>
+							<span class="s-sales-grid-desc__title">
+								<span class="s-sales-grid-desc__title-name">Очень длинный текст о милых и пушитых котаках</span>
+								<span class="s-sales-grid-desc__title-duration">Конец акции 22.09.2015 00:00</span>
+							</span>
+						</span>
+					</a>
+				</div>
+			</div>
+		</div>
+		<!--END Конец строки -->
+
+		<!--
+			Строка с двумя ячейками, выста каждой ячейки 340 пиксел
+			Модификатор grid-2cell cell-h-340
+		 -->
+		<div class="s-sales-grid__row grid-2cell cell-h-340">
+			<div class="s-sales-grid__cell">
+				<a class="s-sales-grid__link" href="">
+					<img src="http://img0.joyreactor.cc/pics/post/full/%D0%BA%D0%BE%D1%82%D0%B8%D0%BA%D0%B8-%D0%B6%D0%B8%D0%B2%D0%BD%D0%BE%D1%81%D1%82%D1%8C-wallpaper-%D0%B1%D0%B0%D1%8F%D0%BD-885065.jpeg" alt="" class="s-sales-grid__img">
+
+					<span class="s-sales-grid-desc">
+						<span class="s-sales-grid-desc__value">-70%</span>
+						<span class="s-sales-grid-desc__title">
+							<span class="s-sales-grid-desc__title-name">Путешествия</span>
+							<span class="s-sales-grid-desc__title-duration">Конец акции 22.09.2015 00:00</span>
+						</span>
+					</span>
+				</a>
+			</div>
+
+			<div class="s-sales-grid__cell">
+				<a class="s-sales-grid__link" href="">
+					<img src="http://img0.joyreactor.cc/pics/post/full/%D0%BA%D0%BE%D1%82%D0%B8%D0%BA%D0%B8-%D0%B6%D0%B8%D0%B2%D0%BD%D0%BE%D1%81%D1%82%D1%8C-wallpaper-%D0%B1%D0%B0%D1%8F%D0%BD-885065.jpeg" alt="" class="s-sales-grid__img">
+
+					<span class="s-sales-grid-desc">
+						<span class="s-sales-grid-desc__value">-70%</span>
+						<span class="s-sales-grid-desc__title">
+							<span class="s-sales-grid-desc__title-name">Очень длинный текст о милых и пушитых котаках</span>
+							<span class="s-sales-grid-desc__title-duration">Конец акции 22.09.2015 00:00</span>
+						</span>
+					</span>
+				</a>
+			</div>
+		</div>
+		<!--END Конец строки -->
+
+		<!--
+			Строка с тремя ячейками, выста каждой ячейки 220 пиксел
+			Модификатор grid-3cell cell-h-220
+		 -->
+		<div class="s-sales-grid__row grid-3cell cell-h-220">
+			<div class="s-sales-grid__cell">
+				<a class="s-sales-grid__link" href="">
+					<img src="http://img0.joyreactor.cc/pics/post/full/%D0%BA%D0%BE%D1%82%D0%B8%D0%BA%D0%B8-%D0%B6%D0%B8%D0%B2%D0%BD%D0%BE%D1%81%D1%82%D1%8C-wallpaper-%D0%B1%D0%B0%D1%8F%D0%BD-885065.jpeg" alt="" class="s-sales-grid__img">
+
+					<span class="s-sales-grid-desc">
+						<span class="s-sales-grid-desc__value">-70%</span>
+						<span class="s-sales-grid-desc__title">
+							<span class="s-sales-grid-desc__title-name">Путешествия</span>
+							<span class="s-sales-grid-desc__title-duration">Конец акции 22.09.2015 00:00</span>
+						</span>
+					</span>
+				</a>
+			</div>
+
+			<div class="s-sales-grid__cell">
+				<a class="s-sales-grid__link" href="">
+					<img src="http://img0.joyreactor.cc/pics/post/full/%D0%BA%D0%BE%D1%82%D0%B8%D0%BA%D0%B8-%D0%B6%D0%B8%D0%B2%D0%BD%D0%BE%D1%81%D1%82%D1%8C-wallpaper-%D0%B1%D0%B0%D1%8F%D0%BD-885065.jpeg" alt="" class="s-sales-grid__img">
+
+					<span class="s-sales-grid-desc">
+						<span class="s-sales-grid-desc__value">-70%</span>
+						<span class="s-sales-grid-desc__title">
+							<span class="s-sales-grid-desc__title-name">Путешествия</span>
+							<span class="s-sales-grid-desc__title-duration">Конец акции 22.09.2015 00:00</span>
+						</span>
+					</span>
+				</a>
+			</div>
+
+			<div class="s-sales-grid__cell">
+				<a class="s-sales-grid__link" href="">
+					<img src="http://img0.joyreactor.cc/pics/post/full/%D0%BA%D0%BE%D1%82%D0%B8%D0%BA%D0%B8-%D0%B6%D0%B8%D0%B2%D0%BD%D0%BE%D1%81%D1%82%D1%8C-wallpaper-%D0%B1%D0%B0%D1%8F%D0%BD-885065.jpeg" alt="" class="s-sales-grid__img">
+
+					<span class="s-sales-grid-desc">
+						<span class="s-sales-grid-desc__value">-70%</span>
+						<span class="s-sales-grid-desc__title">
+							<span class="s-sales-grid-desc__title-name">Путешествия</span>
+							<span class="s-sales-grid-desc__title-duration">Конец акции 22.09.2015 00:00</span>
+						</span>
+					</span>
+				</a>
+			</div>
+		</div>
+		<!--END Конец строки -->
+
+		<!--
+			Строка в две колонки, широкая колонка справа.
+			Модификатор grid-float grid-float-right
+		 -->
+		<div class="s-sales-grid__row grid-float grid-float-right">
+			<div class="s-sales-grid__col">
+				<div class="s-sales-grid__cell">
+					<a class="s-sales-grid__link" href="">
+						<img src="http://img0.joyreactor.cc/pics/post/full/%D0%BA%D0%BE%D1%82%D0%B8%D0%BA%D0%B8-%D0%B6%D0%B8%D0%B2%D0%BD%D0%BE%D1%81%D1%82%D1%8C-wallpaper-%D0%B1%D0%B0%D1%8F%D0%BD-885065.jpeg" alt="" class="s-sales-grid__img">
+
+						<span class="s-sales-grid-desc">
+							<span class="s-sales-grid-desc__value">-70%</span>
+							<span class="s-sales-grid-desc__title">
+								<span class="s-sales-grid-desc__title-name">Путешествия</span>
+								<span class="s-sales-grid-desc__title-duration">Конец акции 22.09.2015 00:00</span>
+							</span>
+						</span>
+					</a>
+				</div>
+			</div>
+
+			<div class="s-sales-grid__col">
+				<div class="s-sales-grid__cell">
+					<a class="s-sales-grid__link" href="">
+						<img src="http://img0.joyreactor.cc/pics/post/full/%D0%BA%D0%BE%D1%82%D0%B8%D0%BA%D0%B8-%D0%B6%D0%B8%D0%B2%D0%BD%D0%BE%D1%81%D1%82%D1%8C-wallpaper-%D0%B1%D0%B0%D1%8F%D0%BD-885065.jpeg" alt="" class="s-sales-grid__img">
+
+						<span class="s-sales-grid-desc">
+							<span class="s-sales-grid-desc__value">-70%</span>
+							<span class="s-sales-grid-desc__title">
+								<span class="s-sales-grid-desc__title-name">Путешествия</span>
+								<span class="s-sales-grid-desc__title-duration">Конец акции 22.09.2015 00:00</span>
+							</span>
+						</span>
+					</a>
+				</div>
+
+				<div class="s-sales-grid__cell">
+					<a class="s-sales-grid__link" href="">
+						<img src="http://img0.joyreactor.cc/pics/post/full/%D0%BA%D0%BE%D1%82%D0%B8%D0%BA%D0%B8-%D0%B6%D0%B8%D0%B2%D0%BD%D0%BE%D1%81%D1%82%D1%8C-wallpaper-%D0%B1%D0%B0%D1%8F%D0%BD-885065.jpeg" alt="" class="s-sales-grid__img">
+
+						<span class="s-sales-grid-desc">
+							<span class="s-sales-grid-desc__value">-70%</span>
+							<span class="s-sales-grid-desc__title">
+								<span class="s-sales-grid-desc__title-name">Путешествия</span>
+								<span class="s-sales-grid-desc__title-duration">Конец акции 22.09.2015 00:00</span>
+							</span>
+						</span>
+					</a>
+				</div>
+			</div>
+		</div>
+		<!--END Конец строки -->
+
+		<!--
+			Строка во всю ширину сетки
+		 -->
+		<div class="s-sales-grid__row">
+			<div class="s-sales-grid__cell">
+				<a class="s-sales-grid__link" href="">
+					<img src="http://img0.joyreactor.cc/pics/post/full/%D0%BA%D0%BE%D1%82%D0%B8%D0%BA%D0%B8-%D0%B6%D0%B8%D0%B2%D0%BD%D0%BE%D1%81%D1%82%D1%8C-wallpaper-%D0%B1%D0%B0%D1%8F%D0%BD-885065.jpeg" alt="" class="s-sales-grid__img">
+
+					<span class="s-sales-grid-desc">
+						<span class="s-sales-grid-desc__value">-70%</span>
+						<span class="s-sales-grid-desc__title">
+							<span class="s-sales-grid-desc__title-name">Путешествия</span>
+							<span class="s-sales-grid-desc__title-duration">Конец акции 22.09.2015 00:00</span>
+						</span>
+					</span>
+				</a>
+			</div>
+		</div>
+		<!--END Конец строки -->
+	</div>
+	<!--END Сетка скидочных категорий -->
+
+	<div class="s-sales-head">
+		<h2 class="s-sales-head__title">Заканчиваются сегодня</h2>
+		<div class="s-sales-head__counter">15:04:02</div>
+	</div>
+
+	<!-- Сетка скидочных категорий -->
+	<div class="s-sales-grid">
+		<!--
+			Строка в две колонки, широкая колонка слева.
+			Модификатор grid-float grid-float-left
+		 -->
+		<div class="s-sales-grid__row grid-float grid-float-left">
+			<div class="s-sales-grid__col">
+				<div class="s-sales-grid__cell">
+					<a class="s-sales-grid__link" href="">
+						<img src="http://img0.joyreactor.cc/pics/post/full/%D0%BA%D0%BE%D1%82%D0%B8%D0%BA%D0%B8-%D0%B6%D0%B8%D0%B2%D0%BD%D0%BE%D1%81%D1%82%D1%8C-wallpaper-%D0%B1%D0%B0%D1%8F%D0%BD-885065.jpeg" alt="" class="s-sales-grid__img">
+
+						<span class="s-sales-grid-desc">
+							<span class="s-sales-grid-desc__value">-70%</span>
+							<span class="s-sales-grid-desc__title">
+								<span class="s-sales-grid-desc__title-name">Очень длинный текст о милых и пушитых котаках</span>
+								<span class="s-sales-grid-desc__title-duration">Конец акции 22.09.2015 00:00</span>
+							</span>
+						</span>
+					</a>
+				</div>
+			</div>
+
+			<div class="s-sales-grid__col">
+				<div class="s-sales-grid__cell">
+					<a class="s-sales-grid__link" href="">
+						<img src="http://img0.joyreactor.cc/pics/post/full/%D0%BA%D0%BE%D1%82%D0%B8%D0%BA%D0%B8-%D0%B6%D0%B8%D0%B2%D0%BD%D0%BE%D1%81%D1%82%D1%8C-wallpaper-%D0%B1%D0%B0%D1%8F%D0%BD-885065.jpeg" alt="" class="s-sales-grid__img">
+
+						<span class="s-sales-grid-desc">
+							<span class="s-sales-grid-desc__value">-70%</span>
+							<span class="s-sales-grid-desc__title">
+								<span class="s-sales-grid-desc__title-name">Путешествия</span>
+								<span class="s-sales-grid-desc__title-duration">Конец акции 22.09.2015 00:00</span>
+							</span>
+						</span>
+					</a>
+				</div>
+
+				<div class="s-sales-grid__cell">
+					<a class="s-sales-grid__link" href="">
+						<img src="http://img0.joyreactor.cc/pics/post/full/%D0%BA%D0%BE%D1%82%D0%B8%D0%BA%D0%B8-%D0%B6%D0%B8%D0%B2%D0%BD%D0%BE%D1%81%D1%82%D1%8C-wallpaper-%D0%B1%D0%B0%D1%8F%D0%BD-885065.jpeg" alt="" class="s-sales-grid__img">
+
+						<span class="s-sales-grid-desc">
+							<span class="s-sales-grid-desc__value">-70%</span>
+							<span class="s-sales-grid-desc__title">
+								<span class="s-sales-grid-desc__title-name">Очень длинный текст о милых и пушитых котаках</span>
+								<span class="s-sales-grid-desc__title-duration">Конец акции 22.09.2015 00:00</span>
+							</span>
+						</span>
+					</a>
+				</div>
+			</div>
+		</div>
+		<!--END Конец строки -->
+		<div class="s-sales-grid__row grid-float grid-float-right">
+			<div class="s-sales-grid__col">
+				<div class="s-sales-grid__cell">
+					<a class="s-sales-grid__link" href="">
+						<img src="http://img0.joyreactor.cc/pics/post/full/%D0%BA%D0%BE%D1%82%D0%B8%D0%BA%D0%B8-%D0%B6%D0%B8%D0%B2%D0%BD%D0%BE%D1%81%D1%82%D1%8C-wallpaper-%D0%B1%D0%B0%D1%8F%D0%BD-885065.jpeg" alt="" class="s-sales-grid__img">
+
+						<span class="s-sales-grid-desc">
+							<span class="s-sales-grid-desc__value">-70%</span>
+							<span class="s-sales-grid-desc__title">
+								<span class="s-sales-grid-desc__title-name">Очень длинный текст о милых и пушитых котаках</span>
+								<span class="s-sales-grid-desc__title-duration">Конец акции 22.09.2015 00:00</span>
+							</span>
+						</span>
+					</a>
+				</div>
+			</div>
+
+			<div class="s-sales-grid__col">
+				<div class="s-sales-grid__cell">
+					<a class="s-sales-grid__link" href="">
+						<img src="http://img0.joyreactor.cc/pics/post/full/%D0%BA%D0%BE%D1%82%D0%B8%D0%BA%D0%B8-%D0%B6%D0%B8%D0%B2%D0%BD%D0%BE%D1%81%D1%82%D1%8C-wallpaper-%D0%B1%D0%B0%D1%8F%D0%BD-885065.jpeg" alt="" class="s-sales-grid__img">
+
+						<span class="s-sales-grid-desc">
+							<span class="s-sales-grid-desc__value">-70%</span>
+							<span class="s-sales-grid-desc__title">
+								<span class="s-sales-grid-desc__title-name">Путешествия</span>
+								<span class="s-sales-grid-desc__title-duration">Конец акции 22.09.2015 00:00</span>
+							</span>
+						</span>
+					</a>
+				</div>
+
+				<div class="s-sales-grid__cell">
+					<a class="s-sales-grid__link" href="">
+						<img src="http://img0.joyreactor.cc/pics/post/full/%D0%BA%D0%BE%D1%82%D0%B8%D0%BA%D0%B8-%D0%B6%D0%B8%D0%B2%D0%BD%D0%BE%D1%81%D1%82%D1%8C-wallpaper-%D0%B1%D0%B0%D1%8F%D0%BD-885065.jpeg" alt="" class="s-sales-grid__img">
+
+						<span class="s-sales-grid-desc">
+							<span class="s-sales-grid-desc__value">-70%</span>
+							<span class="s-sales-grid-desc__title">
+								<span class="s-sales-grid-desc__title-name">Путешествия</span>
+								<span class="s-sales-grid-desc__title-duration">Конец акции 22.09.2015 00:00</span>
+							</span>
+						</span>
+					</a>
+				</div>
+			</div>
+		</div>
+		<!--END Конец строки -->
+
+		<!--
+			Строка в две колонки, широкая колонка справа.
+			Модификатор grid-float grid-float-right
+		 -->
+		<div class="s-sales-grid__row grid-float grid-float-right">
+			<div class="s-sales-grid__col">
+				<div class="s-sales-grid__cell">
+					<a class="s-sales-grid__link" href="">
+						<img src="http://img0.joyreactor.cc/pics/post/full/%D0%BA%D0%BE%D1%82%D0%B8%D0%BA%D0%B8-%D0%B6%D0%B8%D0%B2%D0%BD%D0%BE%D1%81%D1%82%D1%8C-wallpaper-%D0%B1%D0%B0%D1%8F%D0%BD-885065.jpeg" alt="" class="s-sales-grid__img">
+
+						<span class="s-sales-grid-desc">
+							<span class="s-sales-grid-desc__value">-70%</span>
+							<span class="s-sales-grid-desc__title">
+								<span class="s-sales-grid-desc__title-name">Путешествия</span>
+								<span class="s-sales-grid-desc__title-duration">Конец акции 22.09.2015 00:00</span>
+							</span>
+						</span>
+					</a>
+				</div>
+			</div>
+
+			<div class="s-sales-grid__col">
+				<div class="s-sales-grid__cell">
+					<a class="s-sales-grid__link" href="">
+						<img src="http://img0.joyreactor.cc/pics/post/full/%D0%BA%D0%BE%D1%82%D0%B8%D0%BA%D0%B8-%D0%B6%D0%B8%D0%B2%D0%BD%D0%BE%D1%81%D1%82%D1%8C-wallpaper-%D0%B1%D0%B0%D1%8F%D0%BD-885065.jpeg" alt="" class="s-sales-grid__img">
+
+						<span class="s-sales-grid-desc">
+							<span class="s-sales-grid-desc__value">-70%</span>
+							<span class="s-sales-grid-desc__title">
+								<span class="s-sales-grid-desc__title-name">Путешествия</span>
+								<span class="s-sales-grid-desc__title-duration">Конец акции 22.09.2015 00:00</span>
+							</span>
+						</span>
+					</a>
+				</div>
+
+				<div class="s-sales-grid__cell">
+					<a class="s-sales-grid__link" href="">
+						<img src="http://img0.joyreactor.cc/pics/post/full/%D0%BA%D0%BE%D1%82%D0%B8%D0%BA%D0%B8-%D0%B6%D0%B8%D0%B2%D0%BD%D0%BE%D1%81%D1%82%D1%8C-wallpaper-%D0%B1%D0%B0%D1%8F%D0%BD-885065.jpeg" alt="" class="s-sales-grid__img">
+
+						<span class="s-sales-grid-desc">
+							<span class="s-sales-grid-desc__value">-70%</span>
+							<span class="s-sales-grid-desc__title">
+								<span class="s-sales-grid-desc__title-name">Путешествия</span>
+								<span class="s-sales-grid-desc__title-duration">Конец акции 22.09.2015 00:00</span>
+							</span>
+						</span>
+					</a>
+				</div>
+			</div>
+		</div>
+		<!--END Конец строки -->
+	</div>
+	<!--END Сетка скидочных категорий -->
+</section>
+
+
