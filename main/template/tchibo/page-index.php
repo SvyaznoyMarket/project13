2f2770a86d1065cd8f6ffc46ea11b872919f59ff
tamili <ta.burlakova@gmail.com>
2015-10-27 16:04:01 +0300

    SITE-6286

--- main/template/tchibo/page-index.php
+++ main/template/tchibo/page-index.php
@@ -35,13 +35,49 @@ if ((bool)$siblingCategories) {
     } ?>
 </div>
 
+<?/* if ($isCategoriesOddCount && isset($promoContent) && !empty($promoContent)): ?>
+    <div class="tchiboCatalogInner">
+        <?= $promoContent ?>
+    </div>
+<? endif */?>
+
 <div class="s-sales-grid">
     <div class="s-sales-grid__row grid-3cell cell-h-220">
-        <? if ($isCategoriesOddCount && isset($promoContent) && !empty($promoContent)): ?>
-            <div class="tchiboCatalogInner">
-                <?= $promoContent ?>
-            </div>
-        <? endif ?>
+        <div class="s-sales-grid__cell">
+            <a class="s-sales-grid__link" href="">
+                <img src="http://scms.enter.ru/uploads/media/56/5d/1e/4ba9c57c55cb91b4b574daf4371c68c4261866b7.png" alt="" class="s-sales-grid__img">
+                <span class="s-sales-grid-desc">
+                    <span class="s-sales-grid-desc__value">-10%</span>
+                    <span class="s-sales-grid-desc__title">
+                        <span class="s-sales-grid-desc__title-name">Фишка со скидкой</span>
+                    </span>
+                </span>
+            </a>
+        </div>
+
+        <div class="s-sales-grid__cell">
+            <a class="s-sales-grid__link" href="">
+                <img src="http://scms.enter.ru/uploads/media/f6/a5/57/afa3ddf741fda9f4557542ab981a712feccaf1ed.png" alt="" class="s-sales-grid__img">
+                <span class="s-sales-grid-desc">
+                    <span class="s-sales-grid-desc__value">-70%</span>
+                    <span class="s-sales-grid-desc__title">
+                        <span class="s-sales-grid-desc__title-name">Tchibo скидки</span>
+                    </span>
+                </span>
+            </a>
+        </div>
+
+        <div class="s-sales-grid__cell">
+            <a class="s-sales-grid__link" href="">
+                <img src="http://scms.enter.ru/uploads/media/56/5d/1e/4ba9c57c55cb91b4b574daf4371c68c4261866b7.png" alt="" class="s-sales-grid__img">
+                <span class="s-sales-grid-desc">
+                    <span class="s-sales-grid-desc__value">-17%</span>
+                    <span class="s-sales-grid-desc__title">
+                        <span class="s-sales-grid-desc__title-name">Фишка со скидкой</span>
+                    </span>
+                </span>
+            </a>
+        </div>
     </div>
 </div>
 
@@ -90,6 +126,35 @@ if ((bool)$siblingCategories) {
                 </a>
             </div>
         <? endforeach; ?>
+    </div>
+
+    <div class="s-sales-grid__row grid-3cell cell-h-220">
+        <div class="s-sales-grid__cell">
+            <a class="s-sales-grid__link" href="">
+                <img src="http://scms.enter.ru/uploads/media/56/5d/1e/4ba9c57c55cb91b4b574daf4371c68c4261866b7.png" alt="" class="s-sales-grid__img">
+                <span class="s-sales-grid-desc">
+                    <span class="s-sales-grid-desc__title">Фишка со скидкой</span>
+                </span>
+            </a>
+        </div>
+
+        <div class="s-sales-grid__cell">
+            <a class="s-sales-grid__link" href="">
+                <img src="http://scms.enter.ru/uploads/media/f6/a5/57/afa3ddf741fda9f4557542ab981a712feccaf1ed.png" alt="" class="s-sales-grid__img">
+                <span class="s-sales-grid-desc">
+                    <span class="s-sales-grid-desc__title">Tchibo скидки</span>
+                </span>
+            </a>
+        </div>
+
+        <div class="s-sales-grid__cell">
+            <a class="s-sales-grid__link" href="">
+                <img src="http://scms.enter.ru/uploads/media/56/5d/1e/4ba9c57c55cb91b4b574daf4371c68c4261866b7.png" alt="" class="s-sales-grid__img">
+                <span class="s-sales-grid-desc">
+                    <span class="s-sales-grid-desc__title">Фишка со скидкой</span>
+                </span>
+            </a>
+        </div>
     </div>
 </div>
 
