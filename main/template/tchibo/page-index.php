3b64ec4c2d9851bfc2f802389c958e2012153124
tamili <ta.burlakova@gmail.com>
2015-10-29 18:11:57 +0300

    SITE-6286 правки

--- main/template/tchibo/page-index.php
+++ main/template/tchibo/page-index.php
@@ -91,9 +91,9 @@ if ((bool)$siblingCategories) {
         <input type="text" placeholder="Введите Ваш e-mail адрес" class="b-subscribe-to-sale__input subscribe-form__email" name="email" />
         <input type="hidden" value="13" name="channel" />
 
-        <button 
-            class="b-subscribe-to-sale__button subscribe-form__btn" 
-            data-url="<?= $page->url('subscribe.create') ?>" 
+        <button
+            class="b-subscribe-to-sale__button subscribe-form__btn"
+            data-url="<?= $page->url('subscribe.create') ?>"
             data-error-msg="<?= $page->escape('Вы уже подписаны на рассылку! О всех проблемах сообщайте на my.enter.ru/feedback/') ?>">
             Подписаться
         </button>
@@ -144,7 +144,9 @@ if ((bool)$siblingCategories) {
             <a class="s-sales-grid__link" href="">
                 <img src="http://scms.enter.ru/uploads/media/56/5d/1e/4ba9c57c55cb91b4b574daf4371c68c4261866b7.png" alt="" class="s-sales-grid__img">
                 <span class="s-sales-grid-desc">
-                    <span class="s-sales-grid-desc__title">Фишка со скидкой</span>
+                    <span class="s-sales-grid-desc__title">
+                        <span class="s-sales-grid-desc__title-name">Фишка со скидкой</span>
+                    </span>
                 </span>
             </a>
         </div>
@@ -153,7 +155,9 @@ if ((bool)$siblingCategories) {
             <a class="s-sales-grid__link" href="">
                 <img src="http://scms.enter.ru/uploads/media/f6/a5/57/afa3ddf741fda9f4557542ab981a712feccaf1ed.png" alt="" class="s-sales-grid__img">
                 <span class="s-sales-grid-desc">
-                    <span class="s-sales-grid-desc__title">Tchibo скидки</span>
+                    <span class="s-sales-grid-desc__title">
+                        <span class="s-sales-grid-desc__title-name">Tchibo скидки</span>
+                    </span>
                 </span>
             </a>
         </div>
@@ -162,7 +166,9 @@ if ((bool)$siblingCategories) {
             <a class="s-sales-grid__link" href="">
                 <img src="http://scms.enter.ru/uploads/media/56/5d/1e/4ba9c57c55cb91b4b574daf4371c68c4261866b7.png" alt="" class="s-sales-grid__img">
                 <span class="s-sales-grid-desc">
-                    <span class="s-sales-grid-desc__title">Фишка со скидкой</span>
+                    <span class="s-sales-grid-desc__title">
+                        <span class="s-sales-grid-desc__title-name">Фишка со скидкой</span>
+                    </span>
                 </span>
             </a>
         </div>
