7cd929ddec65a11bf6cfba6bb213d7b4965460e7
aleksandr.anpilogov <earlvrotten@ya.ru>
2015-11-02 15:21:11 +0300

    test

--- main/template/cart/_cart-total-1509.php
+++ main/template/cart/_cart-total-1509.php
@@ -1,7 +1,9 @@
 <div id="total" class="order-cart__common-total">
-
+    <div class="order-cart__min-sum" data-bind="visible: isMinOrderSumVisible()">
+        <span class="order-cart__min-sum-desc">Минимальная стоимость заказа 990 <span class="rubl">p</span></span>
+    </div>
     <div class="order-cart__total" id="commonSum">
-        <span class="order-cart__sum-txt">Сумма заказа:</span>
+        <span class="order-cart__sum-txt">Сумма заказа:123</span>
 
         <div class="order-cart__total-sum">
             <span class="price" data-bind="html: window.printPrice(cart().sum())"></span>
