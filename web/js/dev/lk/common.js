7000a84322c0b2f611f3c272fa99abca57b07116
g84 <green.840429@gmail.com>
2015-11-19 16:54:24 +0300

    SITE-6252 Убрал инициализаци слайдеров

--- web/js/dev/lk/common.js
+++ web/js/dev/lk/common.js
@@ -1,19 +1,4 @@
 +function($){
-    $('.js-slider').goodsSlider({
-        leftArrowSelector: '.goods-slider__btn--prev',
-        rightArrowSelector: '.goods-slider__btn--next',
-        sliderWrapperSelector: '.goods-slider__inn',
-        sliderSelector: '.goods-slider-list',
-        itemSelector: '.goods-slider-list__i'
-    });
-    $('.js-slider-2').goodsSlider({
-        leftArrowSelector: '.goods-slider__btn--prev',
-        rightArrowSelector: '.goods-slider__btn--next',
-        sliderWrapperSelector: '.goods-slider__inn',
-        sliderSelector: '.goods-slider-list',
-        itemSelector: '.goods-slider-list__i'
-    });
-
     $('body').on('click', '.js-user-subscribe-input', function(event) {
         var
             $el = $(this),
