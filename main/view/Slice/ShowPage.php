c6a3eef51716469b338dcb8816f78a70372a6234
Mihail Haritonov <mihail.haritonov@enter.ru>
2015-11-30 20:12:58 +0300

    SITE-6437 Автоматическое формирование страниц (Срез по товару + Бренд)

--- main/view/Slice/ShowPage.php
+++ main/view/Slice/ShowPage.php
@@ -5,18 +5,6 @@ namespace View\Slice;
 class ShowPage extends \View\DefaultLayout {
     protected $layout  = 'layout-oneColumn';
 
-    public function prepare() {
-        /** @var $slice \Model\Slice\Entity */
-        $slice = $this->getParam('slice') instanceof \Model\Slice\Entity ? $this->getParam('slice') : null;
-        if (!$slice) {
-            return;
-        }
-
-        $this->setTitle($slice->getTitle());
-        $this->addMeta('description', $slice->getMetaDescription());
-        $this->addMeta('keywords', $slice->getMetaKeywords());
-    }
-
     public function slotContent() {
         return $this->render('slice/content', $this->params);
     }
