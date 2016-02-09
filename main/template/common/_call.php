82089b8f0f4b3e38ae526ff6ea39bb799bbe058a
aleksandr.anpilogov <earlvrotten@ya.ru>
2016-02-09 15:20:09 +0300

    SITE-6622 Полная верстка обратного звонка

--- main/template/common/_call.php
+++ main/template/common/_call.php
@@ -1,15 +1,43 @@
-<div class="call-back-btn js-callback-button" >
-    <!--<div class="call-back-btn__alert">!</div>-->
-
+<div class="call-back-btn js-callback-button">
+    <? if(true): ?><div class="call-back-btn__alert">!</div><? endif; ?>
 
     <? if(true): ?>
         <img src="/styles/callback/img/phone.png" alt="#">
         <? else: ?>
         <img src="/styles/callback/img/bell.png" alt="#" style="top: 56%;">
     <? endif; ?>
-    
+    <div class="call-back-btn__inner-txt" style="display:none;">Нужна помощь</div>
     <div class="call-back-btn__txt js-callback-button-txt">Помогу найти</div>
 </div>
 
 
 
+<div class="js-callback-popup callback-popup">
+    <div class="callback-popup__content js-callback-popup-content">
+        <a class="js-callback-popup-close callback-popup__close" href="#">&times;</a>
+        <div class="callback-popup__cell">
+            <? if(true): ?>
+                <img src="/styles/callback/img/phone-b.png" alt="#">
+            <? else: ?>
+                <img src="/styles/callback/img/bell-b.png" alt="#">
+            <? endif; ?>
+        </div>
+
+        <div class="callback-popup__cell callback-popup__cell_right">
+            <div class="callback-popup__title">
+            <span>Оставьте номер телефона<br>
+            и мы вам перезвоним
+            </span>
+            </div>
+            <form action="#">
+                <label class="callback-popup__phone">
+                    <input type="text" >
+                </label>
+
+                <fieldset class="callback-popup__btn-block">
+                    <input class="callback-popup__btn" type="submit" value="Перезвонить мне">
+                </fieldset>
+            </form>
+        </div>
+    </div>
+</div>
--- web/js/dev/common/call.js
+++ web/js/dev/common/call.js
@@ -6,9 +6,12 @@
     var callback = $('.js-callback-button'),
         callBackTxt = $('.js-callback-button-txt'),
         counter = 1,
-        duration = 20000,
-        durationDel = 5000,
-        time = 5000,
+        duration = 26000,
+        durationDel = 4000,
+        isActiveTime = 6000,
+        popup = $('.js-callback-popup'),
+        popupClose = $('.js-callback-popup-close'),
+        popupContent = $('.js-callback-popup-content'),
 
         isActive = function(time){
             callback.addClass('is-active');
@@ -24,15 +27,44 @@
 
             callBackTxt.addClass('is-active');
 
-            setTimeout(isDelTxt, durationDel)
+            setTimeout(isDelTxt, durationDel);
         },
 
         isDelTxt = function(){
             callBackTxt.removeClass('is-active').addClass('is-hide');
+        },
+
+        isActivePopup = function(e){
+
+            e.preventDefault();
+
+            popup.addClass('is-active');
+        },
+        isDelPopup = function(e){
+
+            e.preventDefault();
+
+            popup.removeClass('is-active');
         };
 
     setInterval(isActiveTxt, duration);
 
-    setTimeout(isActive, time);
-})();
+    setTimeout(isActive, isActiveTime);
 
+    callback.on('click', isActivePopup);
+
+    popup.on('click', isDelPopup);
+
+    $(window).on('keydown', function(e){
+        if( e.keyCode === 27 ) {
+            isDelPopup(e);
+        }
+    });
+
+    popupContent.on('click', function(e){
+        e.stopPropagation();
+    });
+
+    popupClose.on('click', isDelPopup);
+
+})();
