42c44a4b36cbf9d4fb72f60514664834e498514d
aleksandr.anpilogov <earlvrotten@ya.ru>
2016-02-12 16:58:27 +0300

    SITE-6645 Доделал скрипт

--- web/js/dev/common/header-slider.js
+++ web/js/dev/common/header-slider.js
@@ -2,28 +2,6 @@
  * Created by alexandr.anpilogov on 12.02.16.
  */
 
-/*;(function($){
-    var $body = $(body);
-
-    console.log('pkfhdjivdfkjn1pkfhdjivdfkjn1pkfhdjivdfkjn1pkfhdjivdfkjn1pkfhdjivdfkjn1pkfhdjivdfkjn1pkfhdjivdfkjn1pkfhdjivdfkjn1');
-    alert('pkfhdjivdfkjn1pkfhdjivdfkjn1pkfhdjivdfkjn1pkfhdjivdfkjn1pkfhdjivdfkjn1pkfhdjivdfkjn1pkfhdjivdfkjn1pkfhdjivdfkjn1');
-
-    $body.on('click', function(e){
-        e.preventDefault();
-
-        var $this = $(this),
-            container = $this.closest($('.js-header-slider')),
-            item = container.find($('.js-header-slider-item')),
-            itemW = item.outerWidth(),
-            itemN = item.length;
-
-        console.log(itemW, itemN);
-
-/!*        if($this.hasClass('js-header-slider-btn-prev')){
-
-        }*!/
-    });
-})(jQuery);*/
 
 ;(function($){
     var $body = $('body'),
@@ -59,6 +37,10 @@
                 duration: times
             });
         };
+
+    if($('.js-header-slider-item').length == 1){
+        $('.js-header-slider-btn').css('display', 'none');
+    }
 
     $body.on('click', '.js-header-slider-btn', slider);
 })(jQuery);
