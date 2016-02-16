307a11acedd9a3108a3bbccb1d823342b6b79813
aleksandr.anpilogov <earlvrotten@ya.ru>
2016-02-16 17:25:51 +0300

    SITE-6644 fix script

--- web/js/dev/common/checkStatus.js
+++ web/js/dev/common/checkStatus.js
@@ -2,27 +2,33 @@
  * Created by alexandr.anpilogov on 16.02.16.
  */
 ;(function(){
-   var $body = $('body');
+   var $body = $('body'),
+       overlay = '<div class="order-status-overlay"><div>',
+       creatingPopup = function(e){
+           e.preventDefault();
+
+           var $template = $('#tpl-order-statusForm'),
+               $form = Mustache.render($template.html(), {action: "/"});
+
+
+           removePpopup();
+
+           $body.append($form).append(overlay);
+           $('.js-order-name').mask('kkkk-nnnnnn').focus();
+       },
+       removePpopup = function(){
+           if($body.find('.js-popup-status')){
+               $body.find('.js-order-status').remove();
+               $body.find('.order-status-overlay').remove();
+           }
+       };
+
     $.mask.definitions['n'] = '[0-9]';
     $.mask.definitions['k'] = '[а-яА-ЯёЁa-zA-Z]';
 
-    $body.on('click', '.js-checkStatus', function(e){
-        e.preventDefault();
+    $body.on('click', '.js-checkStatus', creatingPopup);
 
-        if($body.find('.js-popup-status')){
-            $body.find('.js-popup-status').remove();
-        }
+    $body.on('click', '.js-order-status-del', removePpopup);
 
-        var $template = $('#tpl-order-statusForm'),
-            $form = Mustache.render($template.html(), {action: "/"});
-
-        $body.append($form);
-        $('.js-order-name').mask('kkkk-nnnnnn').focus();
-    });
-
-    $body.on('click', '.js-order-status-del', function(){
-        $body.find('.js-order-status').remove();
-    });
-
-
+    $body.on('click', '.order-status-overlay', removePpopup)
 })();
