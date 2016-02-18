988268878f4a97bcb087ffd6f600b7129ccd2890
g84 <green.840429@gmail.com>
2016-02-18 19:10:53 +0300

    SITE-6644 добавил контроллер и запрос на проверку статуса заказа

---
+++ lib/EnterQuery/Order/GetStatusByNumberErp.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace EnterQuery\Order
+{
+    use EnterQuery\Order\GetStatusByNumberErp\Response;
+
+    class GetStatusByNumberErp
+    {
+        use \EnterQuery\CurlQueryTrait;
+        use \EnterQuery\CoreQueryTrait;
+
+        /** @var string */
+        public $numberErp;
+        /** @var Response */
+        public $response;
+
+        public function __construct($numberErp = null)
+        {
+            $this->response = new Response();
+
+            $this->numberErp = $numberErp;
+        }
+
+        /**
+         * @return $this
+         */
+        public function prepare()
+        {
+            $this->prepareCurlQuery(
+                $this->buildUrl(
+                    'v2/order/get-status',
+                    [
+                        'number_erp' => $this->numberErp,
+                    ]
+                ),
+                [], // data
+                function($response, $statusCode) {
+                    $result = $this->decodeResponse($response, $statusCode)['result'];
+
+                    $this->response->status = isset($result['status']['id']) ? $result['status'] : null;
+
+                    return $result; // for cache
+                },
+                2,
+                [0, 0.3]
+            );
+
+            return $this;
+        }
+    }
+}
+
+namespace EnterQuery\Order\GetStatusByNumberErp
+{
+    class Response
+    {
+        /** @var array|null */
+        public $status = [];
+    }
+}
