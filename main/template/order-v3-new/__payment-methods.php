a772f69fa06c689420e05987c25d1c03e994d0df
Mihail Haritonov <mihail.haritonov@enter.ru>
2016-02-20 17:31:53 +0300

    SITE-6625 Новое оформление заказа, отложить заказ и товар в избранное, смена региона на сайте и ОЗ

--- main/template/order-v3-new/__payment-methods.php
+++ main/template/order-v3-new/__payment-methods.php
@@ -25,47 +25,43 @@ return function (
         <div class="payments-types-table__cell">
             <div class="payments-types-table__head">Выбрать способ оплаты</div>
             <div class="order-ctrl__custom-select js-order-payment-methods-dropbox-container">
-            <span class="order-ctrl__custom-select-item_title js-order-payment-methods-dropbox-opener">
-                <? if (!empty($paymentMethods['selectedPaymentMethod']['name'])): ?>
-                    <?= $helper->escape($paymentMethods['selectedPaymentMethod']['name']) ?>
-                <? else: ?>
-                    ...
-                <? endif ?>
-            </span>
+                <span class="order-ctrl__custom-select-item_title js-order-payment-methods-dropbox-opener">
+                    <? if (!empty($paymentMethods['selectedPaymentMethod']['name'])): ?>
+                        <?= $helper->escape($paymentMethods['selectedPaymentMethod']['name']) ?>
+                    <? else: ?>
+                        ...
+                    <? endif ?>
+                </span>
 
-                <div class="paymentMethods order-ctrl__custom-select-list js-order-payment-methods-dropbox-content">
-                    <? foreach ($paymentMethods['paymentMethodGroups'] as $paymentMethodGroup): ?>
-                        <ul class="payment-methods__lst order-ctrl__custom-select-item <? if ($paymentMethodGroup['discount']): ?>payment-methods__lst_discount<? endif ?> js-order-payment-methods-dropbox-item">
-                            <? foreach ($paymentMethodGroup['paymentMethods'] as $paymentMethod): ?>
-                                <li class="payment-methods__i">
-                                    <?
-                                    $elementId = sprintf('order_%s-paymentMethod_%s', md5($order->block_name), $paymentMethod['id']);
-                                    ?>
+                <ul class="paymentMethods order-ctrl__custom-select-list payment-methods__lst order-ctrl__custom-select-item js-order-payment-methods-dropbox-content">
+                    <? foreach ($paymentMethods['paymentMethods'] as $paymentMethod): ?>
+                        <li class="payment-methods__i <? if ($paymentMethod['discount']): ?><? endif ?> js-order-payment-methods-dropbox-item">
+                            <?
+                            $elementId = sprintf('order_%s-paymentMethod_%s', md5($order->block_name), $paymentMethod['id']);
+                            ?>
 
-                                    <input
-                                        id="<?= $elementId ?>"
-                                        type="radio"
-                                        name="payment-type-<?= md5($order->block_name) ?>[]"
-                                        value="<?= $paymentMethod['id'] ?>"
-                                        <? if ($paymentMethod['isOnline']): ?>
-                                            data-online="true"
-                                        <? endif ?>
-                                        data-value="<?= $helper->json([
-                                            'block_name'        => $order->block_name,
-                                            'payment_method_id' => $paymentMethod['id'],
-                                        ]) ?>"
-                                        class="customInput customInput-defradio2 js-order-paymentMethod js-customInput"
-                                        <?= $paymentMethod['selected'] ? 'checked' : '' ?>
-                                    />
-                                    <label for="<?= $elementId ?>" class="customLabel customLabel-defradio2 <?= $paymentMethod['selected'] ? 'mChecked' : '' ?>">
-                                        <span class="payment-methods__i-txt"><?= $helper->escape($paymentMethod['name']) ?></span>
-                                        <?= $helper->renderWithMustache('order-v3-new/paymentMethod/discount', ['discount' => $paymentMethodGroup['discount'], 'single' => true]) ?>
-                                    </label>
-                                </li>
-                            <? endforeach ?>
-                        </ul>
+                            <input
+                                id="<?= $elementId ?>"
+                                type="radio"
+                                name="payment-type-<?= md5($order->block_name) ?>[]"
+                                value="<?= $paymentMethod['id'] ?>"
+                                <? if ($paymentMethod['isOnline']): ?>
+                                    data-online="true"
+                                <? endif ?>
+                                data-value="<?= $helper->json([
+                                    'block_name'        => $order->block_name,
+                                    'payment_method_id' => $paymentMethod['id'],
+                                ]) ?>"
+                                class="customInput customInput-defradio2 js-order-paymentMethod js-customInput"
+                                <?= $paymentMethod['selected'] ? 'checked' : '' ?>
+                            />
+                            <label for="<?= $elementId ?>" class="customLabel customLabel-defradio2 <?= $paymentMethod['selected'] ? 'mChecked' : '' ?>">
+                                <span class="payment-methods__i-txt"><?= $helper->escape($paymentMethod['name']) ?></span>
+                                <?= $helper->renderWithMustache('order-v3-new/paymentMethod/discount', ['discount' => $paymentMethod['discount'], 'single' => true]) ?>
+                            </label>
+                        </li>
                     <? endforeach ?>
-                </div>
+                </ul>
             </div>
         </div>
 
