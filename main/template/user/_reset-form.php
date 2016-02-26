1c993651c0dd9d700ced7490c2bffd362425d75e
g84 <green.840429@gmail.com>
2016-02-26 15:55:55 +0300

    SITE-6644 установка куки user.infoCookieName

--- main/template/user/_reset-form.php
+++ main/template/user/_reset-form.php
@@ -8,10 +8,9 @@
     <span class="authForm_reset-close js-authForm-close">Закрыть</span>
     <div class="authForm_reset-txt">
         <span>Новый пароль отправлен на </span>
-        <span class="js-reset-email-message">mail@mail.ru</span>
+        <span class="js-reset-email-message"></span>
     </div>
 </div>
-
 
 <form id="loginReset" class="authForm authForm_reset js-loginReset" action="<?= $page->url('user.forgot') ?>" method="post" style="display: ">
     <input type="text" class="authForm_it textfield js-inputReset" name="forgot[login]" value="" placeholder="Email или телефон">
