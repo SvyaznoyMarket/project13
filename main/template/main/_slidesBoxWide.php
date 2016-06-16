87f0f2b7daea966e741ebb1f72cef5b9f09920b9
Mihail Haritonov <mihail.haritonov@enter.ru>
2016-06-16 18:06:48 +0300

    SITE-6728 Баннер коллекций "Детские товары"

--- main/template/main/_slidesBoxWide.php
+++ main/template/main/_slidesBoxWide.php
@@ -1,22 +1,22 @@
 <div class="slidesBox slidesBox-bg3 slidesBox-full jsSlidesWide">
 
     <div class="slidesBox_h">
-        <? if (false): ?><div class="slidesBox_btn slidesBox_btn-l jsSlidesWideButton jsSlidesWideLeft"></div><? endif ?>
+        <? /* <div class="slidesBox_btn slidesBox_btn-l jsSlidesWideButton jsSlidesWideLeft"></div> */ ?>
 
         <div class="slidesBox_h_c">
-            <div class="slidesBox_t jsSlidesWideName">Твой образ</div>
+            <div class="slidesBox_t jsSlidesWideName">Детские товары</div>
 
             <ul class="slidesBox_dott">
-                <? if (false): ?><li class="slidesBox_dott_i slidesBox_dott_i-act" data-name="Эстет"></li><? endif ?>
-                <li class="slidesBox_dott_i-act" data-name="Твой образ"></li>
+                <? /* <li class="slidesBox_dott_i slidesBox_dott_i-act" data-name="Эстет"></li> */ ?>
+                <li class="slidesBox_dott_i-act" data-name="Детские товары"></li>
             </ul>
         </div>
-        <? if (false): ?><div class="slidesBox_btn slidesBox_btn-r jsSlidesWideButton jsSlidesWideRight"></div><? endif ?>
+        <? /* <div class="slidesBox_btn slidesBox_btn-r jsSlidesWideButton jsSlidesWideRight"></div> */ ?>
     </div>
 
     <div class="slidesBox_inn">
         <ul class="slidesBox_lst clearfix jsSlidesWideHolder">
-            <? if (false): ?>
+            <? /*
             <li class="slidesBox_i jsSlidesWideItem">
                 <a href="/slices/estet">
                     <img src="styles/mainpage/img/estet/main-collection-estet-background.jpg" alt="bg" class="slidesBox_img">
@@ -67,57 +67,57 @@
 
                 <a href="/slices/estet" class="slidesBox_btnview" style="bottom:30px;">Посмотреть<br/>коллекцию</a>
             </li>
-            <? endif ?>
+            */ ?>
 
             <li class="slidesBox_i jsSlidesWideItem">
-                <a href="/catalog/odezhda">
+                <a href="/catalog/children">
                     <img src="styles/mainpage/img/yourImage/bg.jpg" alt="bg" class="slidesBox_img">
                 </a>
 
                 <ul class="slidesBox_items" style="bottom:30px;">
                     <li class="slidesBox_items_i">
                         <div class="slideItem_flt">
-                            <div class="slideItem_flt_i">Шарф вязаный Tchibo</div>
+                            <div class="slideItem_flt_i">Кукла-модель Hasbro MLPEG "2 куклы в упаковке"</div>
                         </div>
 
-                        <a href="/product/tchibo/sharf-vyazaniy-tchibo-tsvet-zeleniy-2201908001146" class="slidesBox_items_lk"><img src="styles/mainpage/img/yourImage/img-1.png" alt="" class="slidesBox_items_img"></a>
+                        <a href="/product/children/kukla-model-hasbro-mlpeg-quot2-kukli-v-upakovkequot-v-assortimente-2010102020989" class="slidesBox_items_lk"><img src="styles/mainpage/img/yourImage/img-1.png?v2" alt="" class="slidesBox_items_img"></a>
                     </li>
 
                     <li class="slidesBox_items_i">
                         <div class="slideItem_flt">
-                            <div class="slideItem_flt_i">Кожаный ремень Tchibo</div>
+                            <div class="slideItem_flt_i">Мягкая игрушка Toivy "Медведь"</div>
                         </div>
 
-                        <a href="/product/tchibo/koganiy-remen-tchibo-2201908002167" class="slidesBox_items_lk"><img src="styles/mainpage/img/yourImage/img-2.png" alt="" class="slidesBox_items_img"></a>
+                        <a href="/product/children/myagkaya-igrushka-toivy-quotmedvedquot-12-sm-2010103009211" class="slidesBox_items_lk"><img src="styles/mainpage/img/yourImage/img-2.png?v2" alt="" class="slidesBox_items_img"></a>
                     </li>
 
                     <li class="slidesBox_items_i">
                         <div class="slideItem_flt">
-                            <div class="slideItem_flt_i">Рубашка красная</div>
+                            <div class="slideItem_flt_i">Фигурка Hasbro Transformers 4 "Констракт-Боты: Войны"</div>
                         </div>
 
-                        <a href="/product/odezhda/rubashka-krasnaia-2460102023785" class="slidesBox_items_lk"><img src="styles/mainpage/img/yourImage/img-3.png" alt="" class="slidesBox_items_img"></a>
+                        <a href="/product/children/figurka-hasbro-transformers-4-quotkonstrakt-boti-voyniquot-v-assortimente-2010104017925" class="slidesBox_items_lk"><img src="styles/mainpage/img/yourImage/img-3.png?v2" alt="" class="slidesBox_items_img"></a>
                     </li>
 
                     <li class="slidesBox_items_i">
                         <div class="slideItem_flt">
-                            <div class="slideItem_flt_i">Шарф Tchibo</div>
+                            <div class="slideItem_flt_i">Мягкая игрушка Toivy "Бизон-кубик"</div>
                         </div>
 
-                        <a href="/product/tchibo/sharf-tchibo-2201908002235" class="slidesBox_items_lk"><img src="styles/mainpage/img/yourImage/img-4.png" alt="" class="slidesBox_items_img"></a>
+                        <a href="/product/children/myagkaya-igrushka-toivy-quotbizon-kubikquot-10-sm-2010103009006" class="slidesBox_items_lk"><img src="styles/mainpage/img/yourImage/img-4.png?v2" alt="" class="slidesBox_items_img"></a>
                     </li>
 
                     <li class="slidesBox_items_i">
                         <div class="slideItem_flt">
-                            <div class="slideItem_flt_i">Толстовка черная</div>
+                            <div class="slideItem_flt_i">Кукла Hasbro MLPEG "Рейнбоу Дэш"</div>
                         </div>
 
-                        <a href="/product/odezhda/tolstovka-chiernaia-2460102034972" class="slidesBox_items_lk"><img src="styles/mainpage/img/yourImage/img-5.png" alt="" class="slidesBox_items_img"></a>
+                        <a href="/product/children/kukla-hasbro-mlpeg-quotreynbou-deshquot-s-aksessuarami-2010102018344" class="slidesBox_items_lk"><img src="styles/mainpage/img/yourImage/img-5.png?v2" alt="" class="slidesBox_items_img"></a>
                     </li>
 
                 </ul>
 
-                <a href="/catalog/odezhda" class="slidesBox_btnview" style="bottom:30px;">Посмотреть<br/>коллекцию</a>
+                <a href="/catalog/children" class="slidesBox_btnview" style="bottom:30px;">Посмотреть<br/>все товары</a>
             </li>
         </ul>
     </div>
