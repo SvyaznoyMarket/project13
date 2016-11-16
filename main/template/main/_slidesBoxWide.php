be43c6658b78c40528381bfca51f243e8e1e5095
Mihail Haritonov <mihail.haritonov@enter.ru>
2016-11-16 17:19:10 +0300

    SITE-6773 Баннер для главной страницы для НГ (в ротацию после авто)

--- main/template/main/_slidesBoxWide.php
+++ main/template/main/_slidesBoxWide.php
@@ -1,15 +1,15 @@
-<? $url = '/catalog/do_it_yourself/aksessuari-dlya-avtomobiley-225'; ?>
+<? $url = '/catalog/household/tovari-dlya-novogo-goda-i-rogdestva-1268'; ?>
 <div class="slidesBox slidesBox-bg3 slidesBox-full slidesBox-v2 jsSlidesWide">
 
     <div class="slidesBox_h">
         <? /* <div class="slidesBox_btn slidesBox_btn-l jsSlidesWideButton jsSlidesWideLeft"></div> */ ?>
 
         <div class="slidesBox_h_c">
-            <div class="slidesBox_t jsSlidesWideName">Товары для авто</div>
+            <div class="slidesBox_t jsSlidesWideName">Товары для Нового Года</div>
 
             <ul class="slidesBox_dott">
                 <? /* <li class="slidesBox_dott_i slidesBox_dott_i-act" data-name="Эстет"></li> */ ?>
-                <li class="slidesBox_dott_i-act" data-name="Товары для авто"></li>
+                <li class="slidesBox_dott_i-act" data-name="Товары для Нового Года"></li>
             </ul>
         </div>
         <? /* <div class="slidesBox_btn slidesBox_btn-r jsSlidesWideButton jsSlidesWideRight"></div> */ ?>
@@ -19,48 +19,48 @@
         <ul class="slidesBox_lst clearfix jsSlidesWideHolder">
             <li class="slidesBox_i jsSlidesWideItem">
                 <a href="<?=$url?>">
-                    <img src="styles/mainpage/img/yourImage/bg.jpg?v3" alt="bg" class="slidesBox_img">
+                    <img src="styles/mainpage/img/yourImage/bg.jpg?v4" alt="bg" class="slidesBox_img">
                 </a>
 
                 <ul class="slidesBox_items">
                     <li class="slidesBox_items_i">
                         <div class="slideItem_flt">
-                            <div class="slideItem_flt_i">Шины и принадлежности</div>
+                            <div class="slideItem_flt_i">Ёлки</div>
                         </div>
 
-                        <a href="/catalog/doityourself/aksessuari-dlya-avtomobiley-shini-i-kolesnie-diski-2373" class="slidesBox_items_lk"><img src="styles/mainpage/img/yourImage/img-1.png?v3" alt="" class="slidesBox_items_img"></a>
+                        <a href="/catalog/household/tovari-dlya-novogo-goda-i-rogdestva-novogodnie-elki-1271" class="slidesBox_items_lk"><img src="styles/mainpage/img/yourImage/img-1.png?v4" alt="" class="slidesBox_items_img"></a>
                     </li>
 
                     <li class="slidesBox_items_i">
                         <div class="slideItem_flt">
-                            <div class="slideItem_flt_i">Автоаксессуары</div>
+                            <div class="slideItem_flt_i">Украшения и гирлянды</div>
                         </div>
 
-                        <a href="/catalog/doityourself/avtoaksessuari-4020" class="slidesBox_items_lk"><img src="styles/mainpage/img/yourImage/img-2.png?v3" alt="" class="slidesBox_items_img"></a>
+                        <a href="/catalog/household/tovari-dlya-novogo-goda-i-rogdestva-ukrasheniya-i-girlyandi-2498" class="slidesBox_items_lk"><img src="styles/mainpage/img/yourImage/img-2.png?v4" alt="" class="slidesBox_items_img"></a>
                     </li>
 
                     <li class="slidesBox_items_i">
                         <div class="slideItem_flt">
-                            <div class="slideItem_flt_i">Автомобильный инструмент</div>
+                            <div class="slideItem_flt_i">Ёлочные игрушки</div>
                         </div>
 
-                        <a href="/catalog/doityourself/aksessuari-dlya-avtomobiley-avtomobilniy-instrument-2369" class="slidesBox_items_lk"><img src="styles/mainpage/img/yourImage/img-3.png?v3" alt="" class="slidesBox_items_img"></a>
+                        <a href="/catalog/household/tovari-dlya-novogo-goda-i-rogdestva-elochnie-ukrasheniya-i-girlyandi-1270" class="slidesBox_items_lk"><img src="styles/mainpage/img/yourImage/img-3.png?v4" alt="" class="slidesBox_items_img"></a>
                     </li>
 
                     <li class="slidesBox_items_i">
                         <div class="slideItem_flt">
-                            <div class="slideItem_flt_i">Компрессоры, насосы и манометры</div>
+                            <div class="slideItem_flt_i">Сувениры</div>
                         </div>
 
-                        <a href="/catalog/do_it_yourself/aksessuari-dlya-avtomobiley-nasosi-i-manometri-1107" class="slidesBox_items_lk"><img src="styles/mainpage/img/yourImage/img-4.png?v3" alt="" class="slidesBox_items_img"></a>
+                        <a href="/catalog/household/tovari-dlya-novogo-goda-i-rogdestva-suveniri-svechi-i-novogodniy-dekor-1272" class="slidesBox_items_lk"><img src="styles/mainpage/img/yourImage/img-4.png?v4" alt="" class="slidesBox_items_img"></a>
                     </li>
 
                     <li class="slidesBox_items_i">
                         <div class="slideItem_flt">
-                            <div class="slideItem_flt_i">Скребки, щетки и лопаты</div>
+                            <div class="slideItem_flt_i">Новогодние костюмы</div>
                         </div>
 
-                        <a href="/catalog/doityourself/avtoaksessuari-skrebki-shchetki-i-lopati-4059" class="slidesBox_items_lk"><img src="styles/mainpage/img/yourImage/img-5.png?v3" alt="" class="slidesBox_items_img"></a>
+                        <a href="/catalog/household/tovari-dlya-novogo-goda-i-rogdestva-novogodnie-kostyumi-2505" class="slidesBox_items_lk"><img src="styles/mainpage/img/yourImage/img-5.png?v4" alt="" class="slidesBox_items_img"></a>
                     </li>
 
                 </ul>
