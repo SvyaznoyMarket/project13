a53d204788eb4aa7459dc1b76da011e07cb60aaa
Mihail Haritonov <mihail.haritonov@enter.ru>
2016-11-18 21:12:36 +0300

    SITE-6774 Установить пиксель Вконтакте

--- main/template/_headJavascript.php
+++ main/template/_headJavascript.php
@@ -4,4 +4,4 @@
         document.documentElement.className = document.documentElement.className.replace("no-js","js");
     </script>
 
-
+    <script type="text/javascript">(window.Image ? (new Image()) : document.createElement('img')).src = location.protocol + '//vk.com/rtrg?r=F*38t3zojEvVHIr*szrll43EEnPOGwAq209A4bgaebwTDjUM2Op*u27ZwZ3nObewqEkJl6Y3DPHBHd1No3rGcXfFqgQ9ocZ*b*RlGq8DfWz0R77GRqAYJ8AYyNBQOhL5mws/cbGZiYcWhW1CtDPyXypllu6Q*EMXOrEMK6kUVrw-&pixel_id=1000043444';</script>
