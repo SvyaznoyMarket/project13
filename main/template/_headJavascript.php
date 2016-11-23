3f0c1d9a45aac3dab8f6682e00ec908833b64442
Mihail Haritonov <mihail.haritonov@enter.ru>
2016-11-23 13:18:04 +0300

    SITE-6776 Пиксель для ФБ (установка пикселя без настройки событий)

--- main/template/_headJavascript.php
+++ main/template/_headJavascript.php
@@ -5,3 +5,22 @@
     </script>
 
     <script type="text/javascript">(window.Image ? (new Image()) : document.createElement('img')).src = location.protocol + '//vk.com/rtrg?r=F*38t3zojEvVHIr*szrll43EEnPOGwAq209A4bgaebwTDjUM2Op*u27ZwZ3nObewqEkJl6Y3DPHBHd1No3rGcXfFqgQ9ocZ*b*RlGq8DfWz0R77GRqAYJ8AYyNBQOhL5mws/cbGZiYcWhW1CtDPyXypllu6Q*EMXOrEMK6kUVrw-&pixel_id=1000043444';</script>
+
+    <!-- Facebook Pixel Code -->
+    <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function()
+        {n.callMethod? n.callMethod.apply(n,arguments):n.queue.push(arguments)}
+        ;
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window,document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '1705507656433873');
+        fbq('track', 'PageView');
+    </script>
+    <noscript>
+        <img height="1" width="1" src="https://www.facebook.com/tr?id=1705507656433873&ev=PageView&noscript=1"/>
+    </noscript>
+    <!-- End Facebook Pixel Code -->
