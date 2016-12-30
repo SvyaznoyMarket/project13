9f3b15347f37fd981e4eeaa9b5918be0a282809b
Mihail Haritonov <mihail.haritonov@enter.ru>
2016-12-30 17:26:09 +0300

    SITE-6783 Нижний блок на гл странице (товары для дома)

--- main/template/main/_slidesBoxWide.php
+++ main/template/main/_slidesBoxWide.php
@@ -1,15 +1,15 @@
-<? $url = '/catalog/household/tovari-dlya-novogo-goda-i-rogdestva-1268'; ?>
+<? $url = '/catalog/household'; ?>
 <div class="slidesBox slidesBox-bg3 slidesBox-full slidesBox-v2 jsSlidesWide">
 
     <div class="slidesBox_h">
         <? /* <div class="slidesBox_btn slidesBox_btn-l jsSlidesWideButton jsSlidesWideLeft"></div> */ ?>
 
         <div class="slidesBox_h_c">
-            <div class="slidesBox_t jsSlidesWideName">Товары для Нового Года</div>
+            <div class="slidesBox_t jsSlidesWideName">Товары для дома</div>
 
             <ul class="slidesBox_dott">
                 <? /* <li class="slidesBox_dott_i slidesBox_dott_i-act" data-name="Эстет"></li> */ ?>
-                <li class="slidesBox_dott_i-act" data-name="Товары для Нового Года"></li>
+                <li class="slidesBox_dott_i-act" data-name="Товары для дома"></li>
             </ul>
         </div>
         <? /* <div class="slidesBox_btn slidesBox_btn-r jsSlidesWideButton jsSlidesWideRight"></div> */ ?>
@@ -19,48 +19,48 @@
         <ul class="slidesBox_lst clearfix jsSlidesWideHolder">
             <li class="slidesBox_i jsSlidesWideItem">
                 <a href="<?=$url?>">
-                    <img src="styles/mainpage/img/yourImage/bg.jpg?v4" alt="bg" class="slidesBox_img">
+                    <img src="styles/mainpage/img/yourImage/bg.jpg?v5" alt="bg" class="slidesBox_img">
                 </a>
 
                 <ul class="slidesBox_items">
                     <li class="slidesBox_items_i">
                         <div class="slideItem_flt">
-                            <div class="slideItem_flt_i">Ёлки</div>
+                            <div class="slideItem_flt_i">Посуда и кухонные принадлежности</div>
                         </div>
 
-                        <a href="/catalog/household/tovari-dlya-novogo-goda-i-rogdestva-novogodnie-elki-1271" class="slidesBox_items_lk"><img src="styles/mainpage/img/yourImage/img-1.png?v4" alt="" class="slidesBox_items_img"></a>
+                        <a href="/catalog/household/posuda-i-kuhonnie-prinadlegnosti-3549" class="slidesBox_items_lk"><img src="styles/mainpage/img/yourImage/img-1.png?v5" alt="" class="slidesBox_items_img"></a>
                     </li>
 
                     <li class="slidesBox_items_i">
                         <div class="slideItem_flt">
-                            <div class="slideItem_flt_i">Украшения и гирлянды</div>
+                            <div class="slideItem_flt_i">Текстиль</div>
                         </div>
 
-                        <a href="/catalog/household/tovari-dlya-novogo-goda-i-rogdestva-ukrasheniya-i-girlyandi-2498" class="slidesBox_items_lk"><img src="styles/mainpage/img/yourImage/img-2.png?v4" alt="" class="slidesBox_items_img"></a>
+                        <a href="/catalog/household/tekstil-3670" class="slidesBox_items_lk"><img src="styles/mainpage/img/yourImage/img-2.png?v5" alt="" class="slidesBox_items_img"></a>
                     </li>
 
                     <li class="slidesBox_items_i">
                         <div class="slideItem_flt">
-                            <div class="slideItem_flt_i">Ёлочные игрушки</div>
+                            <div class="slideItem_flt_i">Декор интерьера</div>
                         </div>
 
-                        <a href="/catalog/household/tovari-dlya-novogo-goda-i-rogdestva-elochnie-ukrasheniya-i-girlyandi-1270" class="slidesBox_items_lk"><img src="styles/mainpage/img/yourImage/img-3.png?v4" alt="" class="slidesBox_items_img"></a>
+                        <a href="/catalog/household/dekor-interera-3669" class="slidesBox_items_lk"><img src="styles/mainpage/img/yourImage/img-3.png?v5" alt="" class="slidesBox_items_img"></a>
                     </li>
 
                     <li class="slidesBox_items_i">
                         <div class="slideItem_flt">
-                            <div class="slideItem_flt_i">Сувениры</div>
+                            <div class="slideItem_flt_i">Товары для ванной, бани, сауны</div>
                         </div>
 
-                        <a href="/catalog/household/tovari-dlya-novogo-goda-i-rogdestva-suveniri-svechi-i-novogodniy-dekor-1272" class="slidesBox_items_lk"><img src="styles/mainpage/img/yourImage/img-4.png?v4" alt="" class="slidesBox_items_img"></a>
+                        <a href="/catalog/household/tovari-dlya-vannoy-bani-sauni-3989" class="slidesBox_items_lk"><img src="styles/mainpage/img/yourImage/img-4.png?v5" alt="" class="slidesBox_items_img"></a>
                     </li>
 
                     <li class="slidesBox_items_i">
                         <div class="slideItem_flt">
-                            <div class="slideItem_flt_i">Новогодние костюмы</div>
+                            <div class="slideItem_flt_i">Хозяйственные товары</div>
                         </div>
 
-                        <a href="/catalog/household/tovari-dlya-novogo-goda-i-rogdestva-novogodnie-kostyumi-2505" class="slidesBox_items_lk"><img src="styles/mainpage/img/yourImage/img-5.png?v4" alt="" class="slidesBox_items_img"></a>
+                        <a href="/catalog/household/hozyaystvennie-tovari-3815" class="slidesBox_items_lk"><img src="styles/mainpage/img/yourImage/img-5.png?v5" alt="" class="slidesBox_items_img"></a>
                     </li>
 
                 </ul>
