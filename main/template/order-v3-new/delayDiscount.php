7d6f05dd87aa4ace5cd6113b5603364efaf9b443
enter-m-haritonov <mihail.haritonov@enter.ru>
2017-05-17 17:43:56 +0300

    FRONT-149 Исправление работы чекбоксов "Получить скидку за увеличение срока доставки" при наличии нескольких подзаказов

--- main/template/order-v3-new/delayDiscount.php
+++ main/template/order-v3-new/delayDiscount.php
@@ -5,8 +5,8 @@
 ?>
     <? if ($order->delay_discount): ?>
         <div class="order-bill__delay-discount">
-            <input id="delayDiscount" type="checkbox" class="customInput customInput-checkbox js-customInput js-order-changeHasDelayDiscount" name="" value="1" <? if ($order->has_delay_discount): ?>checked="checked"<? endif ?> />
-            <label for="delayDiscount" class="customLabel customLabel-checkbox">Получить скидку за увеличение срока доставки (+3 дня)</label>
+            <input id="delayDiscount-<?= $helper->escape($order->block_name) ?>" type="checkbox" class="customInput customInput-checkbox js-customInput js-order-changeHasDelayDiscount" name="" value="1" <? if ($order->has_delay_discount): ?>checked="checked"<? endif ?> />
+            <label for="delayDiscount-<?= $helper->escape($order->block_name) ?>" class="customLabel customLabel-checkbox">Получить скидку за увеличение срока доставки (+3 дня)</label>
         </div>
     <? endif ?>
 <? } ?>
